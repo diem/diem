004cf3dea40430ea3feac9f3e7044663525909a5
Luke Schoen <ltfschoen@users.noreply.github.com>
2019-06-21 13:09:04 -0700

    Fixing comment on AC status
    Updating AC status comment to include all options

--- admission_control/admission_control_proto/src/lib.rs
+++ admission_control/admission_control_proto/src/lib.rs
@@ -49,7 +49,7 @@ impl FromProto for AdmissionControlStatus {
 /// Rust structure for SubmitTransactionResponse protobuf definition.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct SubmitTransactionResponse {
-    /// AC status returned to client if any, it includes can be either error or accepted status.
+    /// AC status returned to client if any - it can be one of: accepted, blacklisted, or rejected.
     pub ac_status: Option<AdmissionControlStatus>,
     /// Mempool error status if any.
     pub mempool_error: Option<MempoolAddTransactionStatus>,
