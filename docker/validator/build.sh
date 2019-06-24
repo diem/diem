78988282f76cb028a7b246c9f54c7342628cd13e
Philipp Gillé <philipp.gille@gmail.com>
2019-06-24 11:14:53 -0700

    [script] make docker image build script directory-independent

--- docker/validator/build.sh
+++ docker/validator/build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
 PROXY=""
 if [ "$https_proxy" ]; then
     PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
 fi
 
-docker build -f docker/validator/validator.Dockerfile . --tag libra_e2e --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY
+docker build -f $DIR/validator.Dockerfile $DIR/../.. --tag libra_e2e --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY
