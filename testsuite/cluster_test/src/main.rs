e1e3e3cc02090b62d709b6bbdf6b47a2ab8e8ae1
Andrey Chursin <achursin@calibra.com>
2019-07-26 11:09:24 -0700

    [cluster_test] Organize main code into ClusterTestRunner struct
    This is mostly refactoring/moving code.
    This change organizes main.rs actions into ClusterTestRunner struct.
    It does not change actions code.
    
    New action 'run_experiment_in_loop' is added to run experiments periodically.
    Main motivation for this change is to have 'run_experiment' as a separate function so that run_experiment_in_loop can reuse it.

--- testsuite/cluster_test/src/main.rs
+++ testsuite/cluster_test/src/main.rs
@@ -7,6 +7,7 @@ use cluster_test::{
 };
 use std::{
     collections::HashSet,
+    env,
     sync::mpsc::{self, TryRecvError},
     thread,
     time::{Duration, Instant},
@@ -17,24 +18,64 @@ const HEALTH_POLL_INTERVAL: Duration = Duration::from_secs(5);
 
 pub fn main() {
     let matches = arg_matches();
-
-    let workplace = matches
-        .value_of(ARG_WORKPLACE)
-        .expect("workplace should be set");
-    let aws = Aws::new(workplace.into());
-    let logs = AwsLogTail::spawn_new(aws.clone()).expect("Failed to start aws log tail");
-    println!("Aws log thread started");
-    let cluster = Cluster::discover(&aws).expect("Failed to discover cluster");
-    println!("Discovered {} peers", cluster.instances().len());
+    let mut runner = ClusterTestRunner::setup(&matches);
 
     if matches.is_present(ARG_RUN) {
-        let mut health_check_runner = HealthCheckRunner::new_all(cluster.clone());
-        let events = logs.recv_all();
-        if !health_check_runner.run(&events).is_empty() {
+        runner.run_experiments_in_loop();
+    } else if matches.is_present(ARG_RUN_ONCE) {
+        runner.run_single_experiment();
+    } else if matches.is_present(ARG_TAIL_LOGS) {
+        runner.tail_logs();
+    } else if matches.is_present(ARG_HEALTH_CHECK) {
+        runner.run_health_check();
+    }
+}
+
+struct ClusterTestRunner {
+    logs: AwsLogTail,
+    cluster: Cluster,
+    health_check_runner: HealthCheckRunner,
+}
+
+impl ClusterTestRunner {
+    /// Discovers cluster, setup log, etc
+    pub fn setup(matches: &ArgMatches) -> Self {
+        let workplace = matches
+            .value_of(ARG_WORKPLACE)
+            .expect("workplace should be set");
+        let aws = Aws::new(workplace.into());
+        let logs = AwsLogTail::spawn_new(aws.clone()).expect("Failed to start aws log tail");
+        println!("Aws log thread started");
+        let cluster = Cluster::discover(&aws).expect("Failed to discover cluster");
+        println!("Discovered {} peers", cluster.instances().len());
+        let health_check_runner = HealthCheckRunner::new_all(cluster.clone());
+        Self {
+            logs,
+            cluster,
+            health_check_runner,
+        }
+    }
+
+    /// Run experiments every EXPERIMENT_INTERVAL seconds until fails
+    pub fn run_experiments_in_loop(&mut self) {
+        let experiment_interval_sec = match env::var("EXPERIMENT_INTERVAL") {
+            Ok(s) => s.parse().expect("EXPERIMENT_INTERVAL env is not a number"),
+            Err(..) => 15,
+        };
+        let experiment_interval = Duration::from_secs(experiment_interval_sec);
+        loop {
+            self.run_single_experiment();
+            thread::sleep(experiment_interval);
+        }
+    }
+
+    pub fn run_single_experiment(&mut self) {
+        let events = self.logs.recv_all();
+        if !self.health_check_runner.run(&events).is_empty() {
             panic!("Some validators are unhealthy before experiment started");
         }
 
-        let experiment = RebootRandomValidator::new(&cluster);
+        let experiment = RebootRandomValidator::new(&self.cluster);
         println!(
             "{}Starting experiment {}{}{}{}",
             style::Bold,
@@ -63,8 +104,8 @@ pub fn main() {
             // Receive all events that arrived to aws log tail within next 1 second
             // This assumes so far that event propagation time is << 1s, this need to be refined
             // in future to account for actual event propagation delay
-            let events = logs.recv_all_until_deadline(deadline);
-            let failed_validators = health_check_runner.run(&events);
+            let events = self.logs.recv_all_until_deadline(deadline);
+            let failed_validators = self.health_check_runner.run(&events);
             for failed in failed_validators {
                 if !affected_validators.contains(&failed) {
                     panic!(
@@ -101,8 +142,8 @@ pub fn main() {
             // Receive all events that arrived to aws log tail within next 1 second
             // This assumes so far that event propagation time is << 1s, this need to be refined
             // in future to account for actual event propagation delay
-            let events = logs.recv_all_until_deadline(deadline);
-            let failed_validators = health_check_runner.run(&events);
+            let events = self.logs.recv_all_until_deadline(deadline);
+            let failed_validators = self.health_check_runner.run(&events);
             let mut still_affected_validator = HashSet::new();
             for failed in failed_validators {
                 if !affected_validators.contains(&failed) {
@@ -120,48 +161,50 @@ pub fn main() {
 
         println!("Experiment completed");
     }
-    if matches.is_present(ARG_TAIL_LOGS) {
-        for log in logs.event_receiver {
-            println!("{:?}", log);
-        }
-    } else if matches.is_present(HEALTH_CHECK) {
-        let mut health_check_runner = HealthCheckRunner::new_all(cluster.clone());
+
+    fn run_health_check(&mut self) {
         loop {
             let deadline = Instant::now() + Duration::from_secs(1);
             // Receive all events that arrived to aws log tail within next 1 second
             // This assumes so far that event propagation time is << 1s, this need to be refined
             // in future to account for actual event propagation delay
-            let events = logs.recv_all_until_deadline(deadline);
-            health_check_runner.run(&events);
+            let events = self.logs.recv_all_until_deadline(deadline);
+            self.health_check_runner.run(&events);
+        }
+    }
+
+    fn tail_logs(self) {
+        for log in self.logs.event_receiver {
+            println!("{:?}", log);
         }
     }
 }
 
-const ARG_TAIL_LOGS: &str = "tail-logs";
-const HEALTH_CHECK: &str = "health-check";
-const ARG_RUN: &str = "run";
 const ARG_WORKPLACE: &str = "workplace";
+// Actions:
+const ARG_TAIL_LOGS: &str = "tail-logs";
+const ARG_HEALTH_CHECK: &str = "health-check";
+const ARG_RUN: &str = "run";
+const ARG_RUN_ONCE: &str = "run-once";
 
 fn arg_matches() -> ArgMatches<'static> {
     let run = Arg::with_name(ARG_RUN).long("--run");
+    let run_once = Arg::with_name(ARG_RUN_ONCE).long("--run-once");
     let workplace = Arg::with_name(ARG_WORKPLACE)
         .long("--workplace")
         .short("-w")
         .takes_value(true)
         .required(true);
     let tail_logs = Arg::with_name(ARG_TAIL_LOGS).long("--tail-logs");
-    let health_check = Arg::with_name(HEALTH_CHECK)
-        .long("--health-check")
-        .conflicts_with(ARG_TAIL_LOGS);
-    // This grouping means at least one action (tail logs, or run test, or both) is required
+    let health_check = Arg::with_name(ARG_HEALTH_CHECK).long("--health-check");
+    // This grouping requires one and only one action (tail logs, run test, etc)
     let action_group = ArgGroup::with_name("action")
-        .multiple(true)
-        .args(&[ARG_TAIL_LOGS, ARG_RUN, HEALTH_CHECK])
+        .args(&[ARG_TAIL_LOGS, ARG_RUN, ARG_RUN_ONCE, ARG_HEALTH_CHECK])
         .required(true);
 
     App::new("cluster_test")
         .author("Libra Association <opensource@libra.org>")
         .group(action_group)
-        .args(&[workplace, run, tail_logs, health_check])
+        .args(&[workplace, run, run_once, tail_logs, health_check])
         .get_matches()
 }
