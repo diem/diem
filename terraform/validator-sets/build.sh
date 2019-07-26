c628c8b45ceb3fb9ba33b9157a1f559fa51eedcb
Michael Gorven <mgorven@calibra.com>
2019-07-26 15:45:41 -0700

    [config] Rebuild dev config for key format change

--- terraform/validator-sets/build.sh
+++ terraform/validator-sets/build.sh
@@ -8,11 +8,11 @@ mkdir -p "$OUTDIR"
 
 cd ../..
 
-if [ ! -e "../setup_scripts/terraform/testnet/validator-sets/$OUTDIR/mint.key" ]; then
-    cargo run --bin generate_keypair -- -o "../setup_scripts/terraform/testnet/validator-sets/$OUTDIR/mint.key"
+if [ ! -e "terraform/validator-sets/$OUTDIR/mint.key" ]; then
+	cargo run --bin generate_keypair -- -o "terraform/validator-sets/$OUTDIR/mint.key"
 fi
 
-cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "../setup_scripts/terraform/testnet/validator-sets/$OUTDIR/mint.key" -o "../setup_scripts/terraform/testnet/validator-sets/$OUTDIR" -d "$@" # -r config/data/configs/overrides/testnet.node.config.override.toml
+cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR" -d "$@" # -r config/data/configs/overrides/testnet.node.config.override.toml
 
 cd -
 cd $OUTDIR
