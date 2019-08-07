80c6bf4a56ec780d9597a0787ab41904bf41e79c
David Wolinsky <davidiw@fb.com>
2019-08-07 11:37:23 -0700

    [types]  Add canonical serialization support for RawTransaction
    Per the title, this is the last diff in the series that enables
    canonical serialization. In the ensuing diffs, we will replace the
    protobuf serialization with the new form as it is used throughout the
    code base and across the wire.
    
    refs #454

--- types/src/unit_tests/transaction_test.rs
+++ types/src/unit_tests/transaction_test.rs
@@ -3,7 +3,10 @@
 
 use crate::{
     account_address::AccountAddress,
-    transaction::{Program, RawTransaction, SignedTransaction},
+    transaction::{Program, RawTransaction, SignedTransaction, TransactionPayload},
+};
+use canonical_serialization::{
+    CanonicalDeserializer, CanonicalSerializer, SimpleDeserializer, SimpleSerializer,
 };
 use nextgen_crypto::ed25519::*;
 use proptest::prelude::*;
@@ -39,5 +42,27 @@ proptest! {
         let txn = raw_txn.sign(&sk1, pk1).unwrap();
         let signed_txn = txn.into_inner();
         assert!(signed_txn.check_signature().is_ok());
+    }
+
+    #[test]
+    fn transaction_payload_round_trip_canonical_serialization(txn_payload in any::<TransactionPayload>()) {
+        let mut serializer = SimpleSerializer::<Vec<u8>>::new();
+        serializer.encode_struct(&txn_payload).unwrap();
+        let serialized_bytes = serializer.get_output();
+
+        let mut deserializer = SimpleDeserializer::new(&serialized_bytes);
+        let output: TransactionPayload = deserializer.decode_struct().unwrap();
+        assert_eq!(txn_payload, output);
+    }
+
+    #[test]
+    fn transaction_round_trip_canonical_serialization(raw_txn in any::<RawTransaction>()) {
+        let mut serializer = SimpleSerializer::<Vec<u8>>::new();
+        serializer.encode_struct(&raw_txn).unwrap();
+        let serialized_bytes = serializer.get_output();
+
+        let mut deserializer = SimpleDeserializer::new(&serialized_bytes);
+        let output: RawTransaction = deserializer.decode_struct().unwrap();
+        assert_eq!(raw_txn, output);
     }
 }
