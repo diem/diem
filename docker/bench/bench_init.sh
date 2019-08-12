61d26b5758602a80a1f14aa9dba2927d239182e6
fbandersnatch <frumious@calibra.com>
2019-08-12 16:01:18 -0700

    [docker][tiny] Remove debug interface option from benchmark docker entry script

--- docker/bench/bench_init.sh
+++ docker/bench/bench_init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "$MINT_KEY" | base64 -d > /opt/libra/etc/mint.key
-/opt/libra/bin/ruben -a $AC_HOST -f /opt/libra/etc/mint.key -d $AC_DEBUG --metrics_server_address "0.0.0.0:14297" $@
+/opt/libra/bin/ruben -a $AC_HOST -f /opt/libra/etc/mint.key --metrics_server_address "0.0.0.0:14297" $@
