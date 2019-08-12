39f7d4cda40ac9edb828afdc85507f6dbe3a0896
Apisak Srihamat <36180751+srihamat@users.noreply.github.com>
2019-08-12 22:20:38 +0700

    Fixed the merge conflict

--- docker/bench/bench_init.sh
+++ docker/bench/bench_init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "$MINT_KEY" | base64 -d > /opt/libra/etc/mint.key
-/opt/libra/bin/ruben -a $AC_HOST -f /opt/libra/etc/mint.key --metrics_server_address "0.0.0.0:14297" $@
+
+/opt/libra/bin/ruben -a $AC_HOST -f /opt/libra/etc/mint.key --metrics_server_address "0.0.0.0:9101" $@
