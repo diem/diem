b18b973a8281767a44d34e2ed463ca89dec75215
Michael Gorven <mgorven@calibra.com>
2019-08-13 10:35:53 -0700

    [config] Rebuild peer keys for key changes
    Also change build.sh to not overwrite `node.config.toml`, now that it
    only specifies overridden values.

--- terraform/validator-sets/build.sh
+++ terraform/validator-sets/build.sh
@@ -12,9 +12,7 @@ if [ ! -e "terraform/validator-sets/$OUTDIR/mint.key" ]; then
 	cargo run --bin generate_keypair -- -o "terraform/validator-sets/$OUTDIR/mint.key"
 fi
 
-cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR" -d "$@" # -r config/data/configs/overrides/testnet.node.config.override.toml
+cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR" -d "$@"
 
 cd -
-cd $OUTDIR
-ls *.node.config.toml | head -n1 | xargs -I{} mv {} node.config.toml
-rm *.node.config.toml
+rm $OUTDIR/*.node.config.toml
