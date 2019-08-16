1f1e639f33ede5a08e7e450a8bdb9806b987b72e
Rashmi Mudduluru <rashmimudduluru@calibra.com>
2019-08-16 09:49:17 -0700

    Mofifies comments

--- consensus/src/chained_bft/liveness/pacemaker.rs
+++ consensus/src/chained_bft/liveness/pacemaker.rs
@@ -181,14 +181,11 @@ impl Pacemaker {
         highest_timeout_certificates: HighestTimeoutCertificates,
     ) -> Self {
         assert!(pacemaker_timeout_quorum_size > 0);
-        // It is safe to assume that "highest_qc_round" will not reach
-        // a value close to "std::u64::MAX" as the round numbers are
-        // periodically reset to 0 in all validators and highest_qc_round
-        // is equal to the highest of these round numbers.
-        // The highest round number shouldn't exceed "std::u64::MAX - 2"
-        // in order to prevent addition overflow caused by the 3 chain
-        // safety rule check (consensus/src/chained_bft/block_storage/block_store.rs: "pub fn
-        // need_sync_for_quorum_cert").
+        // Round numbers:
+        // - are reset to 0 periodically.
+        // - do not exceed std::u64::MAX - 2 per the 3 chain safety rule
+        // (consensus/src/chained_bft/block_storage/block_store.rs: pub fn
+        // need_sync_for_quorum_cert).
         assume!(highest_qc_round <= std::u64::MAX - 2);
         // The starting round is maximum(highest quorum certificate,
         // highest timeout certificate round) + 1.  Note that it is possible this
