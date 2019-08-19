ec8e9ffd6ba673b05e36fddf3d7a064d6b8cbba8
Sam Blackshear <shb@calibra.com>
2019-08-19 16:43:30 -0400

    [language][bytecode verifier] trait for cfg
    The abstract interpreter relies on a specific implementation of the CFG. This commit makes it rely on a CFG trait instead. Making the CFG more abstract will make it easier to support useful features like backward analysis without changing the interpreter.

--- language/bytecode_verifier/src/code_unit_verifier.rs
+++ language/bytecode_verifier/src/code_unit_verifier.rs
@@ -4,7 +4,7 @@
 //! This module implements the checker for verifying correctness of function bodies.
 //! The overall verification is split between stack_usage_verifier.rs and
 //! abstract_interpreter.rs. CodeUnitVerifier simply orchestrates calls into these two files.
-use crate::control_flow_graph::{ControlFlowGraph, VMControlFlowGraph};
+use crate::control_flow_graph::VMControlFlowGraph;
 use vm::{
     access::ModuleAccess,
     errors::{VMStaticViolation, VerificationError},
