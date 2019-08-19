293b17793ea433ce59e4ee468ccd6e084330ef30
Sam Blackshear <shb@calibra.com>
2019-08-19 16:47:43 -0400

    [language][bytecode verifier] removing unused functions from CFG

--- language/bytecode_verifier/src/control_flow_graph.rs
+++ language/bytecode_verifier/src/control_flow_graph.rs
@@ -13,14 +13,6 @@ pub type BlockId = CodeOffset;
 
 /// A trait that specifies the basic requirements for a CFG
 pub trait ControlFlowGraph {
-    /// Given a block ID, return the reachable blocks from that block
-    /// including the block itself.
-    fn reachable_from(&self, block_id: BlockId) -> Vec<&BasicBlock>;
-
-    /// Given an offset into the bytecode return the basic block ID that contains
-    /// that offset
-    fn block_id_of_offset(&self, code_offset: CodeOffset) -> Option<BlockId>;
-
     /// Given a block ID, return the corresponding basic block. Return None if
     /// the block ID is invalid.
     fn block_of_id(&self, block_id: BlockId) -> Option<&BasicBlock>;
@@ -117,59 +109,11 @@ impl VMControlFlowGraph {
             block_ids.insert(pc + 1);
         }
     }
-
-    /// A utility function that implements BFS-reachability from block_id with
-    /// respect to get_targets function
-    fn traverse_by(
-        &self,
-        get_targets: fn(&BasicBlock) -> &Vec<BlockId>,
-        block_id: BlockId,
-    ) -> Vec<&BasicBlock> {
-        let mut ret = Vec::new();
-        // We use this index to keep track of our frontier.
-        let mut index = 0;
-        // Guard against cycles
-        let mut seen = Set::new();
-
-        let block = &self.blocks[&block_id];
-        ret.push(block);
-        seen.insert(&block_id);
-
-        while index < ret.len() {
-            let block = ret[index];
-            index += 1;
-            let successors = get_targets(&block);
-            for block_id in successors.iter() {
-                if !seen.contains(&block_id) {
-                    ret.push(&self.blocks[&block_id]);
-                    seen.insert(block_id);
-                }
-            }
-        }
-
-        ret
-    }
 }
 
 const ENTRY_BLOCK_ID: BlockId = 0;
 
 impl ControlFlowGraph for VMControlFlowGraph {
-    fn reachable_from(&self, block_id: BlockId) -> Vec<&BasicBlock> {
-        self.traverse_by(|block: &BasicBlock| &block.successors, block_id)
-    }
-
-    fn block_id_of_offset(&self, code_offset: CodeOffset) -> Option<BlockId> {
-        let mut index = None;
-
-        for (block_id, block) in &self.blocks {
-            if block.entry >= code_offset && block.exit <= code_offset {
-                index = Some(*block_id);
-            }
-        }
-
-        index
-    }
-
     fn block_of_id(&self, block_id: BlockId) -> Option<&BasicBlock> {
         if self.blocks.contains_key(&block_id) {
             Some(&self.blocks[&block_id])
