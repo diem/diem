527d6c90f9371fe56fbbf4a6e29a1580ce5696e5
Bob Wilson <bobwilson@calibra.com>
2019-08-20 14:49:54 -0700

    [config] Serialize TrustedPeersConfig in sorted order
    Copy the peer values into a BTreeMap so that they get written out in
    sorted order. This should avoid unnecessary churn in the .toml file.
    
    Test Plan: Ran "cargo test". Also ran "build.sh dev -n 4" in
    the terraform/validator-sets/ directory and checked that the
    order in the dev/trusted_peers.config.toml file was preserved.

--- config/src/trusted_peers.rs
+++ config/src/trusted_peers.rs
@@ -10,9 +10,10 @@ use nextgen_crypto::{
 use rand::{rngs::StdRng, SeedableRng};
 use serde::{de::DeserializeOwned, Deserialize, Deserializer, Serialize, Serializer};
 use std::{
-    collections::HashMap,
+    collections::{BTreeMap, HashMap},
     convert::TryFrom,
     fs::File,
+    hash::BuildHasher,
     io::{Read, Write},
     path::Path,
 };
@@ -134,7 +135,20 @@ where
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TrustedPeersConfig {
     #[serde(flatten)]
+    #[serde(serialize_with = "serialize_ordered_map")]
     pub peers: HashMap<String, TrustedPeer>,
+}
+
+pub fn serialize_ordered_map<S, H>(
+    value: &HashMap<String, TrustedPeer, H>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+    H: BuildHasher,
+{
+    let ordered: BTreeMap<_, _> = value.iter().collect();
+    ordered.serialize(serializer)
 }
 
 impl TrustedPeersConfig {
