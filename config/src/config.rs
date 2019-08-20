b90037a2705ed854f6934f4364642c58ee753aa3
SunMi Lee <sunmilee@calibra.com>
2019-08-20 14:26:10 -0700

    [state-sync] Validate request chunk parameters

--- config/src/config.rs
+++ config/src/config.rs
@@ -620,6 +620,10 @@ pub struct StateSyncConfig {
     pub tick_interval_ms: u64,
     // default timeout used for long polling to remote peer
     pub long_poll_timeout_ms: u64,
+    // valid maximum chunk limit for sanity check
+    pub max_chunk_limit: u64,
+    // valid maximum timeout limit for sanity check
+    pub max_timeout_ms: u64,
 }
 
 impl Default for StateSyncConfig {
@@ -628,6 +632,8 @@ impl Default for StateSyncConfig {
             chunk_limit: 1000,
             tick_interval_ms: 10,
             long_poll_timeout_ms: 30000,
+            max_chunk_limit: 1000,
+            max_timeout_ms: 120_000,
         }
     }
 }
