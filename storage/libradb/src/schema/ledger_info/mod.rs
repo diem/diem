34535aa8614fb8f0d433e5e6396fc5e0db241136
Aaron Gao <gzh@calibra.com>
2019-08-22 15:43:42 -0700

    [storage] change ledger_info key from Verison to epoch_num

--- storage/libradb/src/schema/ledger_info/mod.rs
+++ storage/libradb/src/schema/ledger_info/mod.rs
@@ -3,13 +3,13 @@
 
 //! This module defines physical storage schema for LedgerInfoWithSignatures structure.
 //!
-//! Serialized LedgerInfoWithSignatures identified by version.
+//! Serialized LedgerInfoWithSignatures identified by `epoch_num`.
 //! ```text
-//! |<--key-->|<---------------value------------->|
-//! | version | ledger_info_with_signatures bytes |
+//! |<---key--->|<---------------value------------->|
+//! | epoch_num | ledger_info_with_signatures bytes |
 //! ```
 //!
-//! `Version` is serialized in big endian so that records in RocksDB will be in order of it's
+//! `epoch_num` is serialized in big endian so that records in RocksDB will be in order of it's
 //! numeric value.
 
 use crate::schema::ensure_slice_len_eq;
@@ -23,22 +23,22 @@ use schemadb::{
     DEFAULT_CF_NAME,
 };
 use std::mem::size_of;
-use types::{ledger_info::LedgerInfoWithSignatures, transaction::Version};
+use types::ledger_info::LedgerInfoWithSignatures;
 
 define_schema!(
     LedgerInfoSchema,
-    Version,
+    u64, /* epoch num */
     LedgerInfoWithSignatures<Ed25519Signature>,
     DEFAULT_CF_NAME
 );
 
-impl KeyCodec<LedgerInfoSchema> for Version {
+impl KeyCodec<LedgerInfoSchema> for u64 {
     fn encode_key(&self) -> Result<Vec<u8>> {
         Ok(self.to_be_bytes().to_vec())
     }
 
     fn decode_key(data: &[u8]) -> Result<Self> {
-        ensure_slice_len_eq(data, size_of::<Version>())?;
+        ensure_slice_len_eq(data, size_of::<Self>())?;
         Ok((&data[..]).read_u64::<BigEndian>()?)
     }
 }
