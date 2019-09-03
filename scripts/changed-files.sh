6f1122c832c865da300518c70a8201f561367218
Brandon Williams <bmwill@fb.com>
2019-09-03 15:10:50 -0700

    [ci] show diff of changed files

--- scripts/changed-files.sh
+++ scripts/changed-files.sh
@@ -3,7 +3,13 @@
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
 
+set -e
+
 # Check for modified or untracked files after CI has run
-changes="$(git status --porcelain)"
-echo "${changes}"
-[[ -z "${changes}" ]]
+diff="$(git diff)"
+echo "${diff}"
+[[ -z "${diff}" ]]
+
+changed_files="$(git status --porcelain)"
+echo "${changed_files}"
+[[ -z "${changed_files}" ]]
