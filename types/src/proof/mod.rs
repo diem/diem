f0aee306ee2ffc20d2f1c18f8d06a7a4514dc075
Qinfan Wu <wqfish@calibra.com>
2019-09-03 14:45:53 -0700

    [Proof] Move in-memory accumulator implementation to types/proof
    Since we will use this implementation for both accumulator consistency
    proof and scratchpad purposes, move the code so it can be shared.

--- types/src/proof/mod.rs
+++ types/src/proof/mod.rs
@@ -1,6 +1,7 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod accumulator;
 pub mod position;
 #[cfg(any(test, feature = "testing"))]
 pub mod proptest_proof;
