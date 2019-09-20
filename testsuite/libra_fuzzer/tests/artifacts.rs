ae29cf3e76909bf37904385f38fa5facb18479bb
Brandon Williams <bmwill@calibra.com>
2019-09-20 12:08:47 -0700

    [libra-fuzzer] convert to using datatest-stable
    Closes: #1016
    Approved by: sunshowers

--- testsuite/libra_fuzzer/tests/artifacts.rs
+++ testsuite/libra_fuzzer/tests/artifacts.rs
@@ -3,9 +3,6 @@
 
 //! Test artifacts: examples known to have crashed in the past.
 
-#![feature(custom_test_frameworks)]
-#![test_runner(datatest::runner)]
-
 use libra_fuzzer::FuzzTarget;
 use rusty_fork::{fork, rusty_fork_id};
 use stats_alloc::{Region, StatsAlloc, INSTRUMENTED_SYSTEM};
@@ -17,11 +14,9 @@ static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
 /// The memory limit for each deserializer, in bytes.
 const MEMORY_LIMIT: usize = 256 * 1024 * 1024;
 
-#[datatest::files("artifacts", {
-    artifact_path in r"^.*/.*"
-})]
-#[test]
-fn test_artifact(artifact_path: &Path) {
+datatest_stable::harness!(test_artifact, "artifacts", r"^.*/.*");
+
+fn test_artifact(artifact_path: &Path) -> datatest_stable::Result<()> {
     let test_name = test_name(artifact_path);
 
     if no_fork() {
@@ -43,6 +38,8 @@ fn test_artifact(artifact_path: &Path) {
         )
         .expect("forking test failed");
     }
+
+    Ok(())
 }
 
 fn no_fork() -> bool {
