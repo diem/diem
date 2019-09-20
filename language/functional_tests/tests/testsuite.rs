94ded3952ef0e3a40f6ae3dbcebe14eb988d6f36
Brandon Williams <bmwill@calibra.com>
2019-09-20 11:39:41 -0700

    [language] convert functional_tests to using datatest-stable
    Closes: #1016
    Approved by: sunshowers

--- language/functional_tests/tests/testsuite.rs
+++ language/functional_tests/tests/testsuite.rs
@@ -1,15 +1,16 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-#![feature(custom_test_frameworks)]
-#![test_runner(datatest::runner)]
-
-use functional_tests::{checker::check, errors::*, evaluator::eval, utils::parse_input};
+use functional_tests::{checker::check, evaluator::eval, utils::parse_input};
+use std::{fs::File, io::Read, path::Path};
 
 // Runs all tests under the test/testsuite directory.
-#[datatest::files("tests/testsuite", { input in r".*\.mvir" })]
-fn functional_tests(input: &str) -> Result<()> {
-    let (config, directives, transactions) = parse_input(input)?;
+fn functional_tests(path: &Path) -> datatest_stable::Result<()> {
+    let mut file = File::open(path)?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+
+    let (config, directives, transactions) = parse_input(&input)?;
     let res = eval(&config, &transactions)?;
     if let Err(e) = check(&res, &directives) {
         if res.use_debug_output {
@@ -17,7 +18,9 @@ fn functional_tests(input: &str) -> Result<()> {
         } else {
             println!("{}", res);
         }
-        return Err(e);
+        return Err(e.into());
     }
     Ok(())
 }
+
+datatest_stable::harness!(functional_tests, "tests/testsuite", r".*\.mvir");
