189ca919b9a8f0cf65b0a6b70c9e73c90b81307e
Clark Barrett <cbarrett@fb.com>
2019-09-20 17:39:57 -0700

    Added unit tests, updates to translator module.
    Modify main.rs to not require a script for now.  Update unit tests to
    remove too-sensitive comparison with golden output.
    
    Fix whitespace problem.
    
    Fix linter errors.
    
    One more formatting fix.
    
    Closes: #1019
    Approved by: sblackshear

--- language/stackless_bytecode/bytecode_to_boogie/src/main.rs
+++ language/stackless_bytecode/bytecode_to_boogie/src/main.rs
@@ -1,9 +1,6 @@
 use bytecode_to_boogie::translator::BoogieTranslator;
 use bytecode_verifier::VerifiedModule;
-use ir_to_bytecode::{
-    compiler::{compile_module, compile_program},
-    parser::{parse_module, parse_program},
-};
+use ir_to_bytecode::{compiler::compile_module, parser::parse_module};
 use std::{
     env,
     fs::{self, File},
@@ -16,10 +13,10 @@ use types::account_address::AccountAddress;
 fn compile_files(file_names: Vec<String>) -> Vec<VerifiedModule> {
     let mut verified_modules = stdlib_modules().to_vec();
     let files_len = file_names.len();
-    let dep_files = &file_names[0..files_len - 1];
+    //    let dep_files = &file_names[0..files_len - 1];
+    let dep_files = &file_names[0..files_len];
 
-    // assuming the last file is a program that might contain a script
-    let main_file = &file_names[files_len - 1];
+    //    let main_file = &file_names[files_len - 1];
     let address = AccountAddress::default();
     for file_name in dep_files {
         let code = fs::read_to_string(file_name).unwrap();
@@ -37,25 +34,25 @@ fn compile_files(file_names: Vec<String>) -> Vec<VerifiedModule> {
             }
         }
     }
-    let main_code = fs::read_to_string(main_file).unwrap();
-    let program = parse_program(&main_code).unwrap();
-    let address = AccountAddress::default();
-    let compiled_program =
-        compile_program(address, program, &verified_modules).expect("program failed to compile");
-    let mut main_modules = compiled_program.modules;
-    main_modules.push(compiled_program.script.into_module());
-    for module in main_modules {
-        let verified_module_res = VerifiedModule::new(module);
+    // let main_code = fs::read_to_string(main_file).unwrap();
+    // let program = parse_program(&main_code).unwrap();
+    // let address = AccountAddress::default();
+    // let compiled_program =
+    //     compile_program(address, program, &verified_modules).expect("program failed to compile");
+    // let mut main_modules = compiled_program.modules;
+    // main_modules.push(compiled_program.script.into_module());
+    // for module in main_modules {
+    //     let verified_module_res = VerifiedModule::new(module);
 
-        match verified_module_res {
-            Err(e) => {
-                panic!("{:?}", e);
-            }
-            Ok(verified_module) => {
-                verified_modules.push(verified_module);
-            }
-        }
-    }
+    //     match verified_module_res {
+    //         Err(e) => {
+    //             panic!("{:?}", e);
+    //         }
+    //         Ok(verified_module) => {
+    //             verified_modules.push(verified_module);
+    //         }
+    //     }
+    // }
 
     verified_modules
 }
@@ -65,7 +62,6 @@ fn main() {
     let file_names = &args[1..];
     // read files and compile into compiled modules
     let modules = compile_files(file_names.to_vec());
-
     let mut ts = BoogieTranslator::new(&modules);
     let mut res = String::new();
 
