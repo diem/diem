57fefc920611a3dce331a850c0cd6e6d1caf0768
tiangong3624749 <tiangong3624749@163.com>
2019-09-25 00:56:44 +0800

    impl ExecutionService.

--- execution/execution_service/src/lib.rs
+++ execution/execution_service/src/lib.rs
@@ -18,6 +18,7 @@ use proto_conv::{FromProto, IntoProto};
 use std::sync::Arc;
 use storage_client::{StorageRead, StorageWrite};
 use vm_runtime::MoveVM;
+use futures03::executor::block_on;
 
 #[derive(Clone)]
 pub struct ExecutionService {
@@ -40,6 +41,34 @@ impl ExecutionService {
         ));
         ExecutionService { executor }
     }
+
+    pub fn execute_block_inner(&self, request: execution_proto::proto::execution::ExecuteBlockRequest)
+                               -> execution_proto::proto::execution::ExecuteBlockResponse {
+        let req = ExecuteBlockRequest::from_proto(request).unwrap();
+
+        let resp = self.executor.execute_block(
+            req.transactions,
+            req.parent_block_id,
+            req.block_id,
+        );
+        let response = block_on(resp).expect("Response sender was unexpectedly dropped.").expect("Failed to execute genesis block.");
+        response.into_proto()
+    }
+
+    pub fn commit_block_inner(
+        &self,
+        request: execution_proto::proto::execution::CommitBlockRequest,
+    ) -> execution_proto::proto::execution::CommitBlockResponse {
+        let req = CommitBlockRequest::from_proto(request).expect("err.");
+        let resp = self.executor.commit_block(req.ledger_info_with_sigs);
+        let response = block_on(resp).expect("Response sender was unexpectedly dropped.").expect("Failed to execute genesis block.");
+        response.into_proto()
+    }
+
+    pub fn execute_chunk_inner(&self, request: execution_proto::proto::execution::ExecuteChunkRequest) {
+        let req = ExecuteChunkRequest::from_proto(request).expect("from proto err.");
+        self.executor.execute_chunk(req.txn_list_with_proof, req.ledger_info_with_sigs);
+    }
 }
 
 impl execution_proto::proto::execution_grpc::Execution for ExecutionService {
@@ -59,9 +88,9 @@ impl execution_proto::proto::execution_grpc::Execution for ExecutionService {
                     ),
                     sink,
                 )
-                .boxed()
-                .unit_error()
-                .compat();
+                    .boxed()
+                    .unit_error()
+                    .compat();
                 ctx.spawn(fut);
             }
             Err(err) => {
@@ -106,9 +135,9 @@ impl execution_proto::proto::execution_grpc::Execution for ExecutionService {
                         .execute_chunk(req.txn_list_with_proof, req.ledger_info_with_sigs),
                     sink,
                 )
-                .boxed()
-                .unit_error()
-                .compat();
+                    .boxed()
+                    .unit_error()
+                    .compat();
                 ctx.spawn(fut);
             }
             Err(err) => {
@@ -149,13 +178,13 @@ async fn process_response<T>(
 fn process_conversion_error<T>(
     err: failure::Error,
     sink: grpcio::UnarySink<T>,
-) -> impl Future<Item = (), Error = ()> {
+) -> impl Future<Item=(), Error=()> {
     set_failure_message(
         RpcStatusCode::InvalidArgument,
         format!("Failed to convert request from Protobuf: {}", err),
         sink,
     )
-    .map_err(default_reply_error_logger)
+        .map_err(default_reply_error_logger)
 }
 
 fn set_failure_message<T>(
