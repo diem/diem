262c0aaf0306c88b858c55cdf984fae8f0e26f34
Brandon Williams <bmwill@calibra.com>
2019-09-26 17:25:08 -0700

    [logger] remove http logger
    The http logger hasn't been used for a long time and is effectively dead
    code so lets get rid of it.

--- common/logger/src/lib.rs
+++ common/logger/src/lib.rs
@@ -24,18 +24,11 @@ extern crate slog;
 
 mod collector_serializer;
 mod glog_format;
-mod http_local_slog_drain;
-mod http_log_client;
 mod kv_categorizer;
 mod security;
 mod simple_logger;
 
-use crate::{
-    http_local_slog_drain::HttpLocalSlogDrain, http_log_client::HttpLogClient,
-    kv_categorizer::ErrorCategorizer,
-};
-use arc_swap::ArcSwap;
-use failure::prelude::*;
+use crate::kv_categorizer::ErrorCategorizer;
 use glog_format::GlogFormat;
 use lazy_static::lazy_static;
 use slog::{o, Discard, Drain, FilterLevel, Logger, Never};
@@ -45,15 +38,11 @@ use slog_envlogger::{EnvLogger, LogBuilder};
 pub use slog_scope::{crit, debug, error, info, trace, warn};
 use slog_scope::{set_global_logger, GlobalLoggerGuard};
 use slog_term::{PlainDecorator, TermDecorator};
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 
 /// Logger prelude which includes all logging macros.
 pub mod prelude {
-    pub use crate::{
-        log_collector_crit, log_collector_debug, log_collector_error, log_collector_info,
-        log_collector_trace, log_collector_warn,
-        security::{security_log, SecurityEvent},
-    };
+    pub use crate::security::{security_log, SecurityEvent};
     pub use slog::{slog_crit, slog_debug, slog_error, slog_info, slog_trace, slog_warn};
     pub use slog_scope::{crit, debug, error, info, trace, warn};
 }
@@ -127,26 +116,6 @@ lazy_static! {
     };
 }
 
-lazy_static! {
-    static ref GLOBAL_LOG_COLLECTOR: ArcSwap<Logger> =
-        ArcSwap::from(Arc::new(Logger::root(Discard, o!())));
-}
-
-#[derive(Clone, Debug)]
-pub enum LoggerType {
-    // Logger sending data to http destination. Data includes endpoint.
-    Http(String),
-    // Logger sending data to stdio and stderr, which will be used along with
-    // kv_categorizer
-    StdOutput,
-}
-
-pub fn set_global_log_collector(collector: LoggerType, is_async: bool, chan_size: Option<usize>) {
-    // Log collector should be available at this time.
-    let log_collector = get_log_collector(collector, is_async, chan_size).unwrap();
-    GLOBAL_LOG_COLLECTOR.store(Arc::new(log_collector));
-}
-
 /// Create and setup default global logger following the env-logger conventions,
 ///  i.e. configured by environment variable RUST_LOG.
 /// This is useful to make logging optional in unit tests.
@@ -157,22 +126,6 @@ pub fn try_init_for_testing() {
 /// Create and setup default global logger for use in end to end testing.
 pub fn init_for_e2e_testing() {
     ::lazy_static::initialize(&END_TO_END_TESTING_ENVLOGGER_GUARD);
-}
-
-// Get external logger according to config.
-fn get_log_collector(
-    collector: LoggerType,
-    is_async: bool,
-    chan_size: Option<usize>,
-) -> Result<Logger> {
-    match collector {
-        LoggerType::Http(http_endpoint) => {
-            let client = HttpLogClient::new(http_endpoint);
-            let drain = HttpLocalSlogDrain::new(client?);
-            Ok(get_logger(is_async, chan_size, drain))
-        }
-        LoggerType::StdOutput => Ok(create_default_root_logger(is_async, chan_size)),
-    }
 }
 
 fn get_logger<D>(is_async: bool, chan_size: Option<usize>, drain: D) -> Logger
@@ -189,45 +142,3 @@ where
         Logger::root(Mutex::new(drain).fuse(), o!())
     }
 }
-
-/// Access the `Global Logger Collector` for the current logging scope
-///
-/// This function doesn't have to clone the Logger
-/// so it might be a bit faster.
-pub fn with_logger<F, R>(f: F) -> R
-where
-    F: FnOnce(&Logger) -> R,
-{
-    f(&(*GLOBAL_LOG_COLLECTOR.load()))
-}
-
-/// Log a critical level message using current log collector
-#[macro_export]
-macro_rules! log_collector_crit( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_crit![logger, $($args)+])
-};);
-/// Log a error level message using current log collector
-#[macro_export]
-macro_rules! log_collector_error( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_error![logger, $($args)+])
-};);
-/// Log a warning level message using current log collector
-#[macro_export]
-macro_rules! log_collector_warn( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_warn![logger, $($args)+])
-};);
-/// Log a info level message using current log collector
-#[macro_export]
-macro_rules! log_collector_info( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_info![logger, $($args)+])
-};);
-/// Log a debug level message using current log collector
-#[macro_export]
-macro_rules! log_collector_debug( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_debug![logger, $($args)+])
-};);
-/// Log a trace level message using current log collector
-#[macro_export]
-macro_rules! log_collector_trace( ($($args:tt)+) => {
-    $crate::with_logger(|logger| slog_trace![logger, $($args)+])
-};);
