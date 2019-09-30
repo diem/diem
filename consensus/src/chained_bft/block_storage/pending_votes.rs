0aea8b85a29aa1c8ad3f1fa39a06c814c99e8a83
Dmitri Perelman <dmitrip@calibra.com>
2019-09-30 12:24:14 -0700

    [Consensus] Simplify PendingVotes processing
    Summary:
    We used to keep the pending votes in a nested hashmap {block_id: {ledger_info_digest: votes}}.
    It happened organically because we were first using the executed_id instead of ledger_info_digest.
    However, today ledger_info_digest covers both the execution result and the voted block id, hence there is no reason for
    keeping any mapping based on block id at all.
    This change removes one level in the hashmap and converts it to simply {ledger_info_digest: votes}.
    
    Testing: existing unit test coverage

--- consensus/src/chained_bft/block_storage/pending_votes.rs
+++ consensus/src/chained_bft/block_storage/pending_votes.rs
@@ -13,34 +13,26 @@ use types::{
     validator_verifier::VerifyError,
 };
 
-/// This structure maintains tuple of block_id and LedgerInfo for last voted block by an Author
-/// We only remember latest vote from Author. Digest is used to identify and prune pending vote from
-/// same Author.
-struct BlockPendingVote {
-    block_id: HashValue,
-    digest: HashValue,
-}
-
 /// Last pending votes of the authors. Should be cleared upon reconfiguration.
 pub struct PendingVotes {
-    /// `id_to_votes` might keep multiple LedgerInfos per proposed block in order
+    /// `li_digest_to_votes` might keep multiple LedgerInfos per proposed block in order
     /// to tolerate non-determinism in execution: given a proposal, a QuorumCertificate is going
     /// to be collected only for all the votes that carry identical LedgerInfo.
     /// LedgerInfo digest covers the potential commit ids, as well as the vote information
     /// (including the 3-chain of a voted proposal).
-    /// Thus, the structure of `id_to_votes` is as follows:
-    /// HashMap<proposed_block_id, HashMap<ledger_info_digest, LedgerInfoWithSignatures>>
-    id_to_votes: HashMap<HashValue, HashMap<HashValue, LedgerInfoWithSignatures>>,
-    /// Map of Author to last voted block id & digest. Any pending vote from Author is cleaned up
+    /// Thus, the structure of `li_digest_to_votes` is as follows:
+    /// HashMap<ledger_info_digest, LedgerInfoWithSignatures>
+    li_digest_to_votes: HashMap<HashValue, LedgerInfoWithSignatures>,
+    /// Map of Author to last voted ledger info digest. Any pending vote from Author is cleaned up
     /// whenever new vote is added by same Author
-    author_to_last_voted_block_id: HashMap<Author, BlockPendingVote>,
+    author_to_last_voted_li_digest: HashMap<Author, HashValue>,
 }
 
 impl PendingVotes {
     pub fn new() -> Self {
         PendingVotes {
-            id_to_votes: HashMap::new(),
-            author_to_last_voted_block_id: HashMap::new(),
+            li_digest_to_votes: HashMap::new(),
+            author_to_last_voted_li_digest: HashMap::new(),
         }
     }
 
@@ -50,25 +42,17 @@ impl PendingVotes {
         validator_verifier: Arc<ValidatorVerifier>,
     ) -> VoteReceptionResult {
         let author = vote_msg.author();
-        let block_id = vote_msg.vote_data().block_id();
+        // Note that the digest covers the ledger info information, which is also indirectly
+        // covering vote data hash (in its `consensus_data_hash` field).
+        let li_digest = vote_msg.ledger_info().hash();
 
         if let Err(e) = self.check_vote_valid(vote_msg) {
             return e;
         }
 
-        // All the votes collected for all the execution results of a given proposal.
-        let block_votes = self
-            .id_to_votes
-            .entry(block_id)
-            .or_insert_with(HashMap::new);
-
-        // Note that the digest covers the ledger info information, which is also indirectly
-        // covering vote data hash (in its `consensus_data_hash` field).
-        let digest = vote_msg.ledger_info().hash();
-        let li_with_sig = block_votes.entry(digest).or_insert_with(|| {
+        let li_with_sig = self.li_digest_to_votes.entry(li_digest).or_insert_with(|| {
             LedgerInfoWithSignatures::new(vote_msg.ledger_info().clone(), HashMap::new())
         });
-
         vote_msg.signature().clone().add_to_li(author, li_with_sig);
 
         match validator_verifier.check_voting_power(li_with_sig.signatures().keys()) {
@@ -85,40 +69,33 @@ impl PendingVotes {
 
     /// Check if vote is valid. If this is the first vote from Author, add it to map. If Author has
     /// already voted on same block then return DuplicateVote error. If Author has already voted
-    /// on some other block, prune last vote and insert new one in map.
+    /// on some other result, prune the last vote and insert new one in map.
     fn check_vote_valid(&mut self, vote_msg: &VoteMsg) -> Result<(), VoteReceptionResult> {
         let author = vote_msg.author();
-        let block_id = vote_msg.vote_data().block_id();
-        let digest = vote_msg.ledger_info().hash();
-
-        let last_voted_block = match self
-            .author_to_last_voted_block_id
-            .insert(author, BlockPendingVote { block_id, digest })
+        let li_digest = vote_msg.ledger_info().hash();
+        let last_voted_li_digest = match self
+            .author_to_last_voted_li_digest
+            .insert(author, li_digest)
         {
             None => {
                 // First vote from Author, do nothing.
                 return Ok(());
             }
-            Some(last_voted_block) => last_voted_block,
+            Some(last_voted_li_digest) => last_voted_li_digest,
         };
 
         // Prune last pending vote from Author
-        if block_id == last_voted_block.block_id {
-            // Author has already voted for this block
+        if li_digest == last_voted_li_digest {
+            // Author has already voted for this LedgerInfo
             return Err(VoteReceptionResult::DuplicateVote);
         }
 
-        if let Some(block_pending_votes) = self.id_to_votes.get_mut(&last_voted_block.block_id) {
-            if let Some(li_digest_to_sig) = block_pending_votes.get_mut(&last_voted_block.digest) {
-                // Removing signature from last voted block
-                li_digest_to_sig.remove_signature(author);
-                if li_digest_to_sig.signatures().is_empty() {
-                    // Last vote/signature for block, remove digest entry
-                    block_pending_votes.remove(&last_voted_block.digest);
-                    if block_pending_votes.is_empty() {
-                        self.id_to_votes.remove(&last_voted_block.block_id);
-                    }
-                }
+        if let Some(li_pending_votes) = self.li_digest_to_votes.get_mut(&last_voted_li_digest) {
+            // Removing signature from last voted block
+            li_pending_votes.remove_signature(author);
+            if li_pending_votes.signatures().is_empty() {
+                // Last vote for that LI digest, remove the digest entry
+                self.li_digest_to_votes.remove(&last_voted_li_digest);
             }
         }
         Ok(())
