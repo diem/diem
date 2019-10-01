12b8cd086b48a7cc3b87369d359afe8652b9c45b
Dmitri Perelman <dmitrip@calibra.com>
2019-09-30 22:38:00 -0700

    [Consensus] Insert vote can generate TimeoutCertificate
    Summary:
    We extend the VoteReceptionResult enum to include TimeoutCertificate, which is returned by the block tree if there are 2f+1 votes for the same round.
    Note that QC is higher priority than the TC, hence in case there are 2f+1 votes for the same ledger info digest, QC is returned.
    The returned TimeoutCertificate value is not used by the event processor yet, to be done in the followup commits.
    
    Testing:
    updated the unit tests checking vote aggregation logic
    
    Closes: #1132
    Approved by: dmitri-perelman

--- consensus/src/chained_bft/block_storage/pending_votes.rs
+++ consensus/src/chained_bft/block_storage/pending_votes.rs
@@ -1,17 +1,30 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::chained_bft::common::Round;
+use crate::chained_bft::consensus_types::timeout_certificate::TimeoutCertificate;
 use crate::chained_bft::{
     block_storage::VoteReceptionResult,
     common::Author,
     consensus_types::{quorum_cert::QuorumCert, vote_msg::VoteMsg},
 };
 use crypto::{hash::CryptoHash, HashValue};
+use logger::prelude::*;
 use std::{collections::HashMap, sync::Arc};
 use types::{
     crypto_proxies::{LedgerInfoWithSignatures, ValidatorVerifier},
     validator_verifier::VerifyError,
 };
+
+#[cfg(test)]
+#[path = "pending_votes_test.rs"]
+mod pending_votes_test;
+
+struct LastVoteInfo {
+    li_digest: HashValue,
+    round: Round,
+    is_timeout: bool, // true if a vote includes a round signature that can be aggregated to TC
+}
 
 /// Last pending votes of the authors. Should be cleared upon reconfiguration.
 pub struct PendingVotes {
@@ -23,37 +36,66 @@ pub struct PendingVotes {
     /// Thus, the structure of `li_digest_to_votes` is as follows:
     /// HashMap<ledger_info_digest, LedgerInfoWithSignatures>
     li_digest_to_votes: HashMap<HashValue, LedgerInfoWithSignatures>,
-    /// Map of Author to last voted ledger info digest. Any pending vote from Author is cleaned up
-    /// whenever new vote is added by same Author
-    author_to_last_voted_li_digest: HashMap<Author, HashValue>,
+    /// Tracks all the signatures of the votes for the given round. In case we succeed to
+    /// aggregate 2f+1 signatures for the same round a TimeoutCertificate is formed.
+    /// Note that QuorumCert has higher priority than TimeoutCertificate (in case 2f+1 votes are
+    /// gathered for the same ledger info we are going to generate QuorumCert and not the
+    /// TimeoutCertificate).
+    round_to_tc: HashMap<Round, TimeoutCertificate>,
+    /// Map of Author to last vote info. Any pending vote from Author is cleaned up
+    /// whenever a new vote is added by same Author
+    author_to_last_voted_info: HashMap<Author, LastVoteInfo>,
 }
 
 impl PendingVotes {
     pub fn new() -> Self {
         PendingVotes {
             li_digest_to_votes: HashMap::new(),
-            author_to_last_voted_li_digest: HashMap::new(),
+            round_to_tc: HashMap::new(),
+            author_to_last_voted_info: HashMap::new(),
         }
     }
 
+    /// Insert a vote and if the vote is valid, return a QuorumCertificate preferentially over a
+    /// TimeoutCertificate if either can can be formed
     pub fn insert_vote(
         &mut self,
         vote_msg: &VoteMsg,
         validator_verifier: Arc<ValidatorVerifier>,
     ) -> VoteReceptionResult {
-        let author = vote_msg.author();
+        if let Err(e) = self.replace_prev_vote(vote_msg) {
+            return e;
+        }
+        let vote_aggr_res = self.aggregate_qc(vote_msg, &validator_verifier);
+        if let VoteReceptionResult::NewQuorumCertificate(_) = vote_aggr_res {
+            return vote_aggr_res;
+        }
+        match self.aggregate_tc(vote_msg, &validator_verifier) {
+            Some(VoteReceptionResult::NewTimeoutCertificate(tc)) => {
+                VoteReceptionResult::NewTimeoutCertificate(tc)
+            }
+            _ => vote_aggr_res,
+        }
+    }
+
+    /// Check whether the newly inserted vote completes a QC
+    fn aggregate_qc(
+        &mut self,
+        vote_msg: &VoteMsg,
+        validator_verifier: &Arc<ValidatorVerifier>,
+    ) -> VoteReceptionResult {
         // Note that the digest covers the ledger info information, which is also indirectly
         // covering vote data hash (in its `consensus_data_hash` field).
         let li_digest = vote_msg.ledger_info().hash();
-
-        if let Err(e) = self.check_vote_valid(vote_msg) {
-            return e;
-        }
-
         let li_with_sig = self.li_digest_to_votes.entry(li_digest).or_insert_with(|| {
             LedgerInfoWithSignatures::new(vote_msg.ledger_info().clone(), HashMap::new())
         });
-        vote_msg.signature().clone().add_to_li(author, li_with_sig);
+        // TODO: we'd prefer to use LedgerInfoWithSignatures::add_signature instead, but the
+        // CryptoProxy types should be properly updated first.
+        vote_msg
+            .signature()
+            .clone()
+            .add_to_li(vote_msg.author(), li_with_sig);
 
         match validator_verifier.check_voting_power(li_with_sig.signatures().keys()) {
             Ok(_) => VoteReceptionResult::NewQuorumCertificate(Arc::new(QuorumCert::new(
@@ -67,37 +109,98 @@ impl PendingVotes {
         }
     }
 
-    /// Check if vote is valid. If this is the first vote from Author, add it to map. If Author has
+    /// In case a timeout certificate is formed (there are 2f+1 votes in the same round) return the
+    /// new TimeoutCertificate, otherwise, return None.
+    fn aggregate_tc(
+        &mut self,
+        vote_msg: &VoteMsg,
+        validator_verifier: &Arc<ValidatorVerifier>,
+    ) -> Option<VoteReceptionResult> {
+        let round_signature = match vote_msg.round_signature() {
+            Some(round_signature) => round_signature.clone(),
+            None => {
+                return None;
+            }
+        };
+        let round = vote_msg.vote_data().block_round();
+        let tc = self
+            .round_to_tc
+            .entry(round)
+            .or_insert_with(|| TimeoutCertificate::new(round, HashMap::new()));
+        tc.add_signature(vote_msg.author(), round_signature);
+        match validator_verifier.check_voting_power(tc.signatures().keys()) {
+            Ok(_) => Some(VoteReceptionResult::NewTimeoutCertificate(Arc::new(
+                tc.clone(),
+            ))),
+            Err(VerifyError::TooLittleVotingPower { .. }) => None,
+            _ => panic!("Unexpected verification error, vote_msg = {}", vote_msg),
+        }
+    }
+
+    /// If this is the first vote from Author, add it to map. If Author has
     /// already voted on same block then return DuplicateVote error. If Author has already voted
     /// on some other result, prune the last vote and insert new one in map.
-    fn check_vote_valid(&mut self, vote_msg: &VoteMsg) -> Result<(), VoteReceptionResult> {
+    fn replace_prev_vote(&mut self, vote_msg: &VoteMsg) -> Result<(), VoteReceptionResult> {
         let author = vote_msg.author();
+        let round = vote_msg.vote_data().block_round();
         let li_digest = vote_msg.ledger_info().hash();
-        let last_voted_li_digest = match self
-            .author_to_last_voted_li_digest
-            .insert(author, li_digest)
-        {
+        let is_timeout = vote_msg.round_signature().is_some();
+        let vote_info = LastVoteInfo {
+            li_digest,
+            round,
+            is_timeout,
+        };
+        let last_voted_info = match self.author_to_last_voted_info.insert(author, vote_info) {
             None => {
                 // First vote from Author, do nothing.
                 return Ok(());
             }
-            Some(last_voted_li_digest) => last_voted_li_digest,
+            Some(last_voted_info) => last_voted_info,
         };
 
-        // Prune last pending vote from Author
-        if li_digest == last_voted_li_digest {
-            // Author has already voted for this LedgerInfo
-            return Err(VoteReceptionResult::DuplicateVote);
+        if li_digest == last_voted_info.li_digest {
+            if is_timeout == last_voted_info.is_timeout {
+                // Author has already voted for the very same LedgerInfo
+                return Err(VoteReceptionResult::DuplicateVote);
+            } else {
+                // Author has already voted for this LedgerInfo, but this time the VoteMsg's
+                // round signature is different.
+                // Do not replace the prev vote, try to may be gather a TC.
+                return Ok(());
+            }
         }
 
-        if let Some(li_pending_votes) = self.li_digest_to_votes.get_mut(&last_voted_li_digest) {
+        // Prune last pending vote from the pending votes.
+        if let Some(li_pending_votes) = self.li_digest_to_votes.get_mut(&last_voted_info.li_digest)
+        {
             // Removing signature from last voted block
             li_pending_votes.remove_signature(author);
             if li_pending_votes.signatures().is_empty() {
                 // Last vote for that LI digest, remove the digest entry
-                self.li_digest_to_votes.remove(&last_voted_li_digest);
+                self.li_digest_to_votes.remove(&last_voted_info.li_digest);
             }
         }
+
+        // Prune last pending vote from the pending timeout certificates.
+        if round == last_voted_info.round {
+            // The same author has already sent a vote for this round but a different LedgerInfo
+            // digest: this is not a valid behavior.
+            error!(
+                "Validator {} sent two different votes for the same round {}!",
+                author.short_str(),
+                round
+            );
+            return Err(VoteReceptionResult::EquivocateVote);
+        }
+        if let Some(pending_tc) = self.round_to_tc.get_mut(&last_voted_info.round) {
+            // Removing signature from last tc
+            pending_tc.remove_signature(author);
+            if pending_tc.signatures().is_empty() {
+                // Last vote for that round, remove the TC
+                self.round_to_tc.remove(&last_voted_info.round);
+            }
+        }
+
         Ok(())
     }
 }
---
+++ consensus/src/chained_bft/block_storage/pending_votes_test.rs
@@ -0,0 +1,310 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::chained_bft::block_storage::pending_votes::PendingVotes;
+use crate::chained_bft::common::Round;
+use crate::chained_bft::{
+    block_storage::VoteReceptionResult,
+    consensus_types::{vote_data::VoteData, vote_msg::VoteMsg},
+};
+use crypto::HashValue;
+use std::sync::Arc;
+use types::crypto_proxies::random_validator_verifier;
+use types::ledger_info::LedgerInfo;
+
+fn random_ledger_info() -> LedgerInfo {
+    LedgerInfo::new(
+        0,
+        HashValue::random(),
+        HashValue::random(),
+        HashValue::random(),
+        0,
+        0,
+        None,
+    )
+}
+
+fn random_vote_data(round: Round) -> VoteData {
+    assert!(round >= 1);
+    VoteData::new(
+        HashValue::random(),
+        HashValue::random(),
+        round,
+        HashValue::random(),
+        round - 1,
+        HashValue::random(),
+        if round >= 2 { round - 2 } else { round - 1 },
+    )
+}
+
+#[test]
+/// Verify that votes are properly aggregated to QC based on their LedgerInfo digest
+fn test_qc_aggregation() {
+    ::logger::try_init_for_testing();
+    let (signers, validator) = random_validator_verifier(4, Some(2), false);
+    let validator_verifier = Arc::new(validator);
+    let mut pending_votes = PendingVotes::new();
+
+    let li1 = random_ledger_info();
+    let vote_data_1 = random_vote_data(1);
+    let vote_data_1_author_0 = VoteMsg::new(
+        vote_data_1.clone(),
+        signers[0].author(),
+        li1.clone(),
+        &signers[0],
+    );
+
+    // first time a new vote is added the result is VoteAdded
+    assert_eq!(
+        pending_votes.insert_vote(&vote_data_1_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+    // same author voting for the same thing: result is DuplicateVote
+    assert_eq!(
+        pending_votes.insert_vote(&vote_data_1_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::DuplicateVote
+    );
+    // same author voting for a different result in the same round:
+    // override the prev value and return equivocation
+    let li2 = random_ledger_info();
+    let vote_data_2 = random_vote_data(1);
+    let vote_data_2_author_0 = VoteMsg::new(
+        vote_data_2.clone(),
+        signers[0].author(),
+        li2.clone(),
+        &signers[0],
+    );
+    assert_eq!(
+        pending_votes.insert_vote(&vote_data_2_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::EquivocateVote
+    );
+    // A different author voting for a different result in the same round but without a round
+    // signature: VoteAdded
+    let vote_data_2_author_1 = VoteMsg::new(
+        vote_data_2.clone(),
+        signers[1].author(),
+        li2.clone(),
+        &signers[1],
+    );
+    assert_eq!(
+        pending_votes.insert_vote(&vote_data_2_author_1, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+    // Two votes for the ledger info form a QC
+    let vote_data_2_author_2 = VoteMsg::new(
+        vote_data_2.clone(),
+        signers[2].author(),
+        li2.clone(),
+        &signers[2],
+    );
+    match pending_votes.insert_vote(&vote_data_2_author_2, Arc::clone(&validator_verifier)) {
+        VoteReceptionResult::NewQuorumCertificate(qc) => {
+            assert!(validator_verifier
+                .check_voting_power(qc.ledger_info().signatures().keys())
+                .is_ok());
+        }
+        _ => {
+            panic!("No QC formed.");
+        }
+    };
+}
+
+#[test]
+/// Verify that only the last votes are kept in the system for qc aggregation
+fn test_qc_aggregation_keep_last_only() {
+    ::logger::try_init_for_testing();
+
+    let (signers, validator) = random_validator_verifier(4, Some(2), false);
+    let validator_verifier = Arc::new(validator);
+    let mut pending_votes = PendingVotes::new();
+
+    let li1 = random_ledger_info();
+    let vote_round_1 = random_vote_data(1);
+    let vote_round_1_author_0 = VoteMsg::new(
+        vote_round_1.clone(),
+        signers[0].author(),
+        li1.clone(),
+        &signers[0],
+    );
+
+    // first time a new vote is added the result is VoteAdded
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_1_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // same author voting for the next round: the previous vote is replaced
+    let li2 = random_ledger_info();
+    let vote_round_2 = random_vote_data(2);
+    let vote_round_2_author_0 = VoteMsg::new(
+        vote_round_2.clone(),
+        signers[0].author(),
+        li2.clone(),
+        &signers[0],
+    );
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_2_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // another author voting for round 1 cannot form a QC because the old vote is gone
+    let vote_round_1_author_1 = VoteMsg::new(
+        vote_round_1.clone(),
+        signers[1].author(),
+        li1.clone(),
+        &signers[1],
+    );
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_1_author_1, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // another author voting for the vote data in round 2 can finally form a QC
+    let vote_round_2_author_1 = VoteMsg::new(
+        vote_round_2.clone(),
+        signers[1].author(),
+        li2.clone(),
+        &signers[1],
+    );
+    match pending_votes.insert_vote(&vote_round_2_author_1, Arc::clone(&validator_verifier)) {
+        VoteReceptionResult::NewQuorumCertificate(qc) => {
+            assert!(validator_verifier
+                .check_voting_power(qc.ledger_info().signatures().keys())
+                .is_ok());
+        }
+        _ => {
+            panic!("No QC formed.");
+        }
+    };
+}
+
+#[test]
+/// Verify that votes are properly aggregated to TC based on their rounds
+fn test_tc_aggregation() {
+    ::logger::try_init_for_testing();
+
+    let (signers, validator) = random_validator_verifier(4, Some(2), false);
+    let validator_verifier = Arc::new(validator);
+    let mut pending_votes = PendingVotes::new();
+
+    let li1 = random_ledger_info();
+    let vote_round_1 = random_vote_data(1);
+    let mut vote_round_1_author_0 = VoteMsg::new(
+        vote_round_1.clone(),
+        signers[0].author(),
+        li1.clone(),
+        &signers[0],
+    );
+    vote_round_1_author_0.add_round_signature(&signers[0]);
+
+    // first time a new vote is added the result is VoteAdded
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_1_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // another vote for the same round with a different value cannot form a TC if it doesn't have a
+    // round signature
+    let li2 = random_ledger_info();
+    let vote2_round_1 = random_vote_data(1);
+    let mut vote2_round_1_author_1 = VoteMsg::new(
+        vote2_round_1.clone(),
+        signers[1].author(),
+        li2.clone(),
+        &signers[1],
+    );
+    assert_eq!(
+        pending_votes.insert_vote(&vote2_round_1_author_1, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // if that vote is now enhanced with a round signature, it can form a TC
+    vote2_round_1_author_1.add_round_signature(&signers[1]);
+    match pending_votes.insert_vote(&vote2_round_1_author_1, Arc::clone(&validator_verifier)) {
+        VoteReceptionResult::NewTimeoutCertificate(tc) => {
+            assert!(validator_verifier
+                .check_voting_power(tc.signatures().keys())
+                .is_ok());
+        }
+        _ => {
+            panic!("No TC formed.");
+        }
+    };
+}
+
+#[test]
+/// Verify that only the last votes are kept in the system for TC aggregation
+fn test_tc_aggregation_keep_last_only() {
+    ::logger::try_init_for_testing();
+
+    let (signers, validator) = random_validator_verifier(4, Some(2), false);
+    let validator_verifier = Arc::new(validator);
+    let mut pending_votes = PendingVotes::new();
+
+    let li1 = random_ledger_info();
+    let vote_round_1 = random_vote_data(1);
+    let mut vote_round_1_author_0 = VoteMsg::new(
+        vote_round_1.clone(),
+        signers[0].author(),
+        li1.clone(),
+        &signers[0],
+    );
+    vote_round_1_author_0.add_round_signature(&signers[0]);
+
+    // first time a new vote is added the result is VoteAdded
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_1_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // A vote for round 2 overrides the previous vote
+    let li2 = random_ledger_info();
+    let vote_round_2 = random_vote_data(2);
+    let mut vote_round_2_author_0 = VoteMsg::new(
+        vote_round_2.clone(),
+        signers[0].author(),
+        li2.clone(),
+        &signers[0],
+    );
+    vote_round_2_author_0.add_round_signature(&signers[0]);
+    assert_eq!(
+        pending_votes.insert_vote(&vote_round_2_author_0, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // a new vote for round 1 cannot form a TC
+    let li3 = random_ledger_info();
+    let vote3_round_1 = random_vote_data(1);
+    let mut vote3_round_1_author_1 = VoteMsg::new(
+        vote3_round_1.clone(),
+        signers[1].author(),
+        li3.clone(),
+        &signers[1],
+    );
+    vote3_round_1_author_1.add_round_signature(&signers[1]);
+    assert_eq!(
+        pending_votes.insert_vote(&vote3_round_1_author_1, Arc::clone(&validator_verifier)),
+        VoteReceptionResult::VoteAdded(1)
+    );
+
+    // a new vote for round 2 should form a TC
+    let li4 = random_ledger_info();
+    let vote4_round_2 = random_vote_data(2);
+    let mut vote4_round_2_author_1 = VoteMsg::new(
+        vote4_round_2.clone(),
+        signers[1].author(),
+        li4.clone(),
+        &signers[1],
+    );
+    vote4_round_2_author_1.add_round_signature(&signers[1]);
+    match pending_votes.insert_vote(&vote4_round_2_author_1, Arc::clone(&validator_verifier)) {
+        VoteReceptionResult::NewTimeoutCertificate(tc) => {
+            assert!(validator_verifier
+                .check_voting_power(tc.signatures().keys())
+                .is_ok());
+        }
+        _ => {
+            panic!("No TC formed.");
+        }
+    };
+}
