4555a3090eed8507e6ce466e810b1806fb2319e2
Yuan xy <yuan_xin_yu@hotmail.com>
2019-10-02 10:17:07 +0800

    [types] Format code to hex str

--- types/src/unit_tests/mod.rs
+++ types/src/unit_tests/mod.rs
@@ -4,6 +4,7 @@
 mod access_path_test;
 mod address_test;
 mod canonical_serialization_examples;
+mod code_debug_fmt_test;
 mod contract_event_proto_conversion_test;
 mod get_with_proof_proto_conversion_test;
 mod identifier_test;
