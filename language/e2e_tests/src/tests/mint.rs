e1b9ad8ff1669fd5df8f22545d37c6dee5c048d2
Runtian Zhou <runtianz@calibra.com>
2019-10-02 16:16:43 -0700

    fixup! [e2e_test] Get rid of the CI diff compare check for gensis blob change.

--- language/e2e_tests/src/tests/mint.rs
+++ language/e2e_tests/src/tests/mint.rs
@@ -1,10 +1,10 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::executor::FakeExecutor;
 use crate::{
     account::{Account, AccountData},
     common_transactions::mint_txn,
-    executor::test_all_genesis,
     gas_costs::TXN_RESERVED,
     transaction_status_eq,
 };
@@ -16,91 +16,94 @@ use types::{
 #[test]
 fn mint_to_existing() {
     // create a FakeExecutor with a genesis from file
-    test_all_genesis(|mut executor| {
-        let genesis_account = Account::new_association();
+    // We can't run mint test on terraform genesis as we don't have the private key to sign the
+    // mint transaction.
+    let mut executor = FakeExecutor::from_genesis_file();
+    let genesis_account = Account::new_association();
 
-        // create and publish a sender with 1_000_000 coins
-        let receiver = AccountData::new(1_000_000, 10);
-        executor.add_account_data(&receiver);
+    // create and publish a sender with 1_000_000 coins
+    let receiver = AccountData::new(1_000_000, 10);
+    executor.add_account_data(&receiver);
 
-        let mint_amount = 1_000;
-        let txn = mint_txn(&genesis_account, receiver.account(), 1, mint_amount);
+    let mint_amount = 1_000;
+    let txn = mint_txn(&genesis_account, receiver.account(), 1, mint_amount);
 
-        // execute transaction
-        let txns: Vec<SignedTransaction> = vec![txn];
-        let output = executor.execute_block(txns);
-        let txn_output = output.get(0).expect("must have a transaction output");
-        assert_eq!(
-            output[0].status(),
-            &TransactionStatus::Keep(VMStatus::new(StatusCode::EXECUTED))
-        );
-        println!("write set {:?}", txn_output.write_set());
-        executor.apply_write_set(txn_output.write_set());
+    // execute transaction
+    let txns: Vec<SignedTransaction> = vec![txn];
+    let output = executor.execute_block(txns);
+    let txn_output = output.get(0).expect("must have a transaction output");
+    assert_eq!(
+        output[0].status(),
+        &TransactionStatus::Keep(VMStatus::new(StatusCode::EXECUTED))
+    );
+    println!("write set {:?}", txn_output.write_set());
+    executor.apply_write_set(txn_output.write_set());
 
-        // check that numbers in stored DB are correct
-        let gas = txn_output.gas_used();
-        let sender_balance = 1_000_000_000 - gas;
-        let receiver_balance = 1_000_000 + mint_amount;
+    // check that numbers in stored DB are correct
+    let gas = txn_output.gas_used();
+    let sender_balance = 1_000_000_000 - gas;
+    let receiver_balance = 1_000_000 + mint_amount;
 
-        let updated_sender = executor
-            .read_account_resource(&genesis_account)
-            .expect("sender must exist");
-        let updated_receiver = executor
-            .read_account_resource(receiver.account())
-            .expect("receiver must exist");
-        assert_eq!(sender_balance, updated_sender.balance());
-        assert_eq!(receiver_balance, updated_receiver.balance());
-        assert_eq!(2, updated_sender.sequence_number());
-        assert_eq!(10, updated_receiver.sequence_number());
-    });
+    let updated_sender = executor
+        .read_account_resource(&genesis_account)
+        .expect("sender must exist");
+    let updated_receiver = executor
+        .read_account_resource(receiver.account())
+        .expect("receiver must exist");
+    assert_eq!(sender_balance, updated_sender.balance());
+    assert_eq!(receiver_balance, updated_receiver.balance());
+    assert_eq!(2, updated_sender.sequence_number());
+    assert_eq!(10, updated_receiver.sequence_number());
 }
 
 #[test]
 fn mint_to_new_account() {
     // create a FakeExecutor with a genesis from file
-    test_all_genesis(|mut executor| {
-        let genesis_account = Account::new_association();
+    // We can't run mint test on terraform genesis as we don't have the private key to sign the
+    // mint transaction.
 
-        // create and publish a sender with TXN_RESERVED coins
-        let new_account = Account::new();
+    let mut executor = FakeExecutor::from_genesis_file();
+    let genesis_account = Account::new_association();
 
-        let mint_amount = TXN_RESERVED;
-        let txn = mint_txn(&genesis_account, &new_account, 1, mint_amount);
+    // create and publish a sender with TXN_RESERVED coins
+    let new_account = Account::new();
 
-        // execute transaction
-        let txns: Vec<SignedTransaction> = vec![txn];
-        let output = executor.execute_block(txns);
-        let txn_output = output.get(0).expect("must have a transaction output");
-        assert!(transaction_status_eq(
-            &output[0].status(),
-            &TransactionStatus::Keep(VMStatus::new(StatusCode::EXECUTED))
-        ));
-        executor.apply_write_set(txn_output.write_set());
+    let mint_amount = TXN_RESERVED;
+    let txn = mint_txn(&genesis_account, &new_account, 1, mint_amount);
 
-        // check that numbers in stored DB are correct
-        let gas = txn_output.gas_used();
-        let sender_balance = 1_000_000_000 - gas;
-        let receiver_balance = mint_amount;
+    // execute transaction
+    let txns: Vec<SignedTransaction> = vec![txn];
+    let output = executor.execute_block(txns);
+    let txn_output = output.get(0).expect("must have a transaction output");
+    assert!(transaction_status_eq(
+        &output[0].status(),
+        &TransactionStatus::Keep(VMStatus::new(StatusCode::EXECUTED))
+    ));
+    executor.apply_write_set(txn_output.write_set());
 
-        let updated_sender = executor
-            .read_account_resource(&genesis_account)
-            .expect("sender must exist");
-        let updated_receiver = executor
-            .read_account_resource(&new_account)
-            .expect("receiver must exist");
-        assert_eq!(sender_balance, updated_sender.balance());
-        assert_eq!(receiver_balance, updated_receiver.balance());
-        assert_eq!(2, updated_sender.sequence_number());
-        assert_eq!(0, updated_receiver.sequence_number());
+    // check that numbers in stored DB are correct
+    let gas = txn_output.gas_used();
+    let sender_balance = 1_000_000_000 - gas;
+    let receiver_balance = mint_amount;
 
-        // Mint can only be called from genesis address;
-        let txn = mint_txn(&new_account, &new_account, 0, mint_amount);
-        let txns: Vec<SignedTransaction> = vec![txn];
-        let output = executor.execute_block(txns);
+    let updated_sender = executor
+        .read_account_resource(&genesis_account)
+        .expect("sender must exist");
+    let updated_receiver = executor
+        .read_account_resource(&new_account)
+        .expect("receiver must exist");
+    assert_eq!(sender_balance, updated_sender.balance());
+    assert_eq!(receiver_balance, updated_receiver.balance());
+    assert_eq!(2, updated_sender.sequence_number());
+    assert_eq!(0, updated_receiver.sequence_number());
 
-        assert!(transaction_status_eq(
-            &output[0].status(),
-            &TransactionStatus::Keep(VMStatus::new(StatusCode::MISSING_DATA))
-        ));
-    });
+    // Mint can only be called from genesis address;
+    let txn = mint_txn(&new_account, &new_account, 0, mint_amount);
+    let txns: Vec<SignedTransaction> = vec![txn];
+    let output = executor.execute_block(txns);
+
+    assert!(transaction_status_eq(
+        &output[0].status(),
+        &TransactionStatus::Keep(VMStatus::new(StatusCode::MISSING_DATA))
+    ));
 }
