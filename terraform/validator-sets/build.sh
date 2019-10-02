99146a8084d640a4b970e24465ae19460388bc23
Michael Gorven <mgorven@calibra.com>
2019-10-02 14:25:58 -0700

    [terraform] Generate keys for full node networks
    Create `val` and `fn` subdirectories for validator and full node
    networks respectively. Generate keys for full node network (not actually
    used yet). Support additional network keypairs in docker image. Remove
    60 validator set which isn't used AFAIK.
    
    Test Plan: Built validator docker image and ran locally. Applied changes
    to mgorven workspace.
    
    Closes: #1174
    Approved by: phoenix-antigravity

--- terraform/validator-sets/build.sh
+++ terraform/validator-sets/build.sh
@@ -12,24 +12,21 @@ if [ ! -e "terraform/validator-sets/$OUTDIR/mint.key" ]; then
 	cargo run --bin generate-keypair -- -o "terraform/validator-sets/$OUTDIR/mint.key"
 fi
 
-cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR" -d "$@"
+cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR/val" -d -r validator "$@"
+cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR/fn" -u "terraform/validator-sets/$OUTDIR/val/0" -r full_node -n 10
 
 cd -
-cd $OUTDIR
 
-# Remove all generated node.config.toml files.
-find . -mindepth 2 -iname node.config.toml | xargs rm
-# Move all keys files to single top-level directory.
-find . -mindepth 2 -iname '*.keys.toml' | xargs -I '{}' mv '{}' ./
-# Move all seed peers files to top-level directory.
-find . -mindepth 2 -iname '*.seed_peers.config.toml' | xargs rm
-# Move all network peers files to top-level directory.
-find . -mindepth 2 -iname '*.network_peers.config.toml' -exec mv -f {} ./network_peers.config.toml \;
-# Move all consensus peers files to top-level directory.
-find . -mindepth 2 -iname 'consensus_peers.config.toml' -exec mv -f {} ./consensus_peers.config.toml \;
-# Move all genesis.blob files to top-level directory.
-find . -mindepth 2 -iname 'genesis.blob' -exec mv -f {} ./genesis.blob \;
-# Delete all directories.
-find . -mindepth 1 -type d | xargs rmdir
+cd $OUTDIR/val
+mv */*.keys.toml .
+mv 1/*.network_peers.config.toml network_peers.config.toml
+mv 1/consensus_peers.config.toml ../consensus_peers.config.toml
+mv 1/genesis.blob ../
+rm */*.toml */*.blob
+find . -mindepth 1 -type d -print0 | xargs -0 rmdir
 
-cd -
+cd ../fn
+mv */*.keys.toml .
+mv 0/*.network_peers.config.toml network_peers.config.toml
+rm */*.toml */*.blob
+find . -mindepth 1 -type d -print0 | xargs -0 rmdir
