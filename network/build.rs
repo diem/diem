fd2afce492994e7bfdd01006690f69b9d45e06ed
Brandon Williams <bmwill@calibra.com>
2019-10-04 09:32:21 -0700

    [network] stop generating network protos with rust-protobuf

--- network/build.rs
+++ network/build.rs
@@ -5,7 +5,6 @@
 fn main() {
     let proto_files = [
         "src/proto/consensus.proto",
-        "src/proto/network.proto",
         "src/proto/state_synchronizer.proto",
         "src/proto/admission_control.proto",
     ];
