b7b5c5d4c9c8b75dd51377157b154a47ccc98ea2
Brandon Williams <bmwill@calibra.com>
2019-10-05 09:05:14 -0700

    [storage] build protos with prost
    Closes: #1212
    Approved by: mimoo

--- storage/storage_proto/src/proto/mod.rs
+++ storage/storage_proto/src/proto/mod.rs
@@ -3,7 +3,11 @@
 
 #![allow(bare_trait_objects)]
 
-use types::proto::{account_state_blob, get_with_proof, ledger_info, proof, transaction};
+use ::types::proto::{account_state_blob, get_with_proof, ledger_info, proof, transaction, types};
+
+pub mod storage_prost {
+    include!(concat!(env!("OUT_DIR"), "/storage.rs"));
+}
 
 pub mod storage;
 pub mod storage_grpc;
