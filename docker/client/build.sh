556f3447a0efc2a3389a3dcd641fde122a0dfa1d
Young Yang Liauw <youngyl@fb.com>
2019-10-07 17:52:13 -0700

    [CI] create docker base builder
    Docker layer caching is not consistent in nightly builds. Create a base
    builder to improve caching.

--- docker/client/build.sh
+++ docker/client/build.sh
@@ -4,8 +4,20 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 PROXY=""
-if [ "$https_proxy" ]; then
-    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
+if [ -z ${https_proxy+x} ]; then
+  PROXY="--build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
 fi
 
-docker build -f $DIR/client.Dockerfile $DIR/../.. --tag libra_client --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY
+echo "Building base..."
+BASE_BUILDER_DIR=$DIR/../base
+cd $BASE_BUILDER_DIR && pwd && ./build.sh
+
+echo "Building client.."
+docker build \
+  -f $DIR/client.Dockerfile \
+  $DIR/../.. \
+  --tag libra_client \
+  --build-arg GIT_REV="$(git rev-parse HEAD)" \
+  --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" \
+  --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+  $PROXY
