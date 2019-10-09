a6a1b86aeace19974d40cf8137d9e2fc18ff29eb
Young Yang Liauw <youngyl@fb.com>
2019-10-09 12:01:30 -0700

    Revert "[CI] create docker base builder"
    This reverts commit bc2aafdb50a81d5b50c96e084597c57374536d7c.
    
    Closes: #1264
    Approved by: bmwill

--- docker/client/client.Dockerfile
+++ docker/client/client.Dockerfile
@@ -1,5 +1,19 @@
-### Base Builder Image ###
-FROM libra_base as builder
+### Build Image ###
+FROM debian:stretch as builder
+
+RUN echo "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list \
+    && apt-get update && apt-get install -y protobuf-compiler/stretch-backports cmake curl clang \
+    && apt-get clean && rm -r /var/lib/apt/lists/*
+
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain none
+ENV PATH "$PATH:/root/.cargo/bin"
+
+WORKDIR /libra
+COPY rust-toolchain /libra/rust-toolchain
+RUN rustup install $(cat rust-toolchain)
+
+COPY . /libra
+RUN cargo build --release -p libra-node -p client -p benchmark
 RUN strip target/release/client
 
 ### Production Image ###
--- docker/validator/build.sh
+++ docker/validator/build.sh
@@ -4,20 +4,8 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 PROXY=""
-if [ -z ${https_proxy+x} ]; then
-  PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
+if [ "$https_proxy" ]; then
+    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
 fi
 
-echo "Building base..."
-BASE_BUILDER_DIR=$DIR/../base
-cd $BASE_BUILDER_DIR && pwd && ./build.sh
-
-echo "Building client..."
-docker build \
-  -f $DIR/validator.Dockerfile \
-  $DIR/../.. \
-  --tag libra_e2e \
-  --build-arg GIT_REV="$(git rev-parse HEAD)" \
-  --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" \
-  --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
-  $PROXY
+docker build -f $DIR/validator.Dockerfile $DIR/../.. --tag libra_e2e --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY
