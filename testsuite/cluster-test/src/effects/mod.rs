86e7a50ed9db84ad3d38d4f95dc5079ffd8615a2
Andrey Chursin <achursin@calibra.com>
2019-10-09 14:36:34 -0700

    [cluster_test] Change effect trait bound from Sync to Send
    Basically it means that Effect is safe to be send between thread, as long as one thread owns Effect at a time.
    
    This means `Effect` can have mutable state, and does not have to wrap this state with Mutex, this will simplify writing effects with state.
    
    In order to make this work with thread pool and execute_jobs, activate/deactivate job takes `&mut` ref, instead of `&`. `&mut T` can be send safely between threads(e.g. &mut T implements Send if T implements Send), while &T only implements Send if T implements Sync, which is much stricter marker.

--- testsuite/cluster-test/src/effects/mod.rs
+++ testsuite/cluster-test/src/effects/mod.rs
@@ -6,12 +6,12 @@ pub use reboot::Reboot;
 use std::fmt::Display;
 pub use stop_container::StopContainer;
 
-pub trait Action: Display + Sync {
+pub trait Action: Display + Send {
     fn apply(&self) -> failure::Result<()>;
     fn is_complete(&self) -> bool;
 }
 
-pub trait Effect: Display + Sync {
+pub trait Effect: Display + Send {
     fn activate(&self) -> failure::Result<()>;
     fn deactivate(&self) -> failure::Result<()>;
 }
