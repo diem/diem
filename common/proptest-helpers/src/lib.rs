58e71b41152b8bd063f80eecf358905a2f17f354
François Garillot <francois@garillot.net>
2019-10-14 16:11:51 -0700

    fixup! standardize proptest_helpers -> proptest-helpers

--- common/proptest-helpers/src/lib.rs
+++ common/proptest-helpers/src/lib.rs
@@ -30,7 +30,7 @@ use std::{
 ///
 /// ```
 /// use proptest::prelude::*;
-/// use proptest-helpers::with_stack_size;
+/// use proptest_helpers::with_stack_size;
 ///
 /// with_stack_size(4 * 1024 * 1024, || proptest!(|(x in 0usize..128)| {
 ///     // assertions go here
