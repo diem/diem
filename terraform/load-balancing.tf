9a20cddfa65ecba60d74b55747588245afeee2e0
Michael Gorven <mgorven@calibra.com>
2019-10-17 14:47:57 -0700

    [terraform] Add NLB listener for fullnodes
    Connections to port 8001 on the load balancer will go to fullnodes.

--- terraform/load-balancing.tf
+++ terraform/load-balancing.tf
@@ -43,6 +43,30 @@ resource "aws_lb_listener" "validator-ac" {
   }
 }
 
+resource "aws_lb_target_group" "fullnode-ac" {
+  name     = "${terraform.workspace}-fullnode"
+  protocol = "TCP"
+  port     = 8000
+  vpc_id   = aws_vpc.testnet.id
+}
+
+resource "aws_lb_target_group_attachment" "fullnode-ac" {
+  count            = var.num_fullnodes
+  target_group_arn = aws_lb_target_group.fullnode-ac.arn
+  target_id        = element(aws_instance.fullnode.*.id, count.index)
+}
+
+resource "aws_lb_listener" "fullnode-ac" {
+  load_balancer_arn = aws_lb.validator-ac.arn
+  port              = 8001
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fullnode-ac.arn
+  }
+}
+
 resource "aws_route53_record" "validator-ac" {
   count   = local.zone_id == "" ? 0 : 1
   zone_id = local.zone_id
