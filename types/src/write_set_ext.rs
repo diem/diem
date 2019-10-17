9e03eb885ffe7ffb23e8e8bf3ed9cf0e2ec60b5f
caojiafeng <funfriendcjf@gmail.com>
2019-10-17 19:43:44 +0800

    chore(writeset): transfrom writeset from/to btreemap of bytes

--- types/src/write_set_ext.rs
+++ types/src/write_set_ext.rs
@@ -3,32 +3,49 @@
 
 use crate::access_path::AccessPath;
 use crate::account_address::AccountAddress;
-use crate::write_set::WriteSet;
-use canonical_serialization::SimpleSerializer;
+use crate::write_set::{WriteOp, WriteSet, WriteSetMut};
+use canonical_serialization::{SimpleDeserializer, SimpleSerializer};
 use failure::{Error, Result};
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 use std::convert::TryFrom;
 
-impl TryFrom<&WriteSet> for HashMap<AccountAddress, BTreeMap<Vec<u8>, Vec<u8>>> {
+impl TryFrom<&WriteSet> for BTreeMap<AccountAddress, BTreeMap<Vec<u8>, Vec<u8>>> {
     type Error = Error;
 
     fn try_from(ws: &WriteSet) -> Result<Self> {
-        let mut account_state = HashMap::new();
+        let mut account_state = BTreeMap::new();
 
         for (ap, wp) in ws.iter() {
             let AccessPath { address, path } = ap;
             let write_op_blob = SimpleSerializer::<Vec<u8>>::serialize(wp)?;
             match account_state.entry(*address) {
-                ::std::collections::hash_map::Entry::Vacant(e) => {
+                ::std::collections::btree_map::Entry::Vacant(e) => {
                     let mut state = BTreeMap::new();
                     state.insert(path.clone(), write_op_blob);
                     e.insert(state);
                 }
-                std::collections::hash_map::Entry::Occupied(mut e) => {
+                std::collections::btree_map::Entry::Occupied(mut e) => {
                     e.get_mut().insert(path.clone(), write_op_blob);
                 }
             }
         }
         Ok(account_state)
+    }
+}
+
+impl TryFrom<&BTreeMap<AccountAddress, BTreeMap<Vec<u8>, Vec<u8>>>> for WriteSet {
+    type Error = Error;
+
+    fn try_from(value: &BTreeMap<AccountAddress, BTreeMap<Vec<u8>, Vec<u8>>>) -> Result<Self> {
+        let mut ws = WriteSetMut::default();
+        for (account, state) in value.into_iter() {
+            for (data_path, data) in state.into_iter() {
+                ws.push((
+                    AccessPath::new(*account, data_path.clone()),
+                    SimpleDeserializer::deserialize::<WriteOp>(data)?,
+                ));
+            }
+        }
+        ws.freeze()
     }
 }
