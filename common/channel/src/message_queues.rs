c723bb169fcfc400c5a0f7009d7bdad58cf765f0
Ankush Agarwal <ankushagarwal11@gmail.com>
2019-10-24 12:04:40 -0700

    fixup! [enhancement] Create PerValidatorQueue which implements MessageQueue trait

--- common/channel/src/message_queues.rs
+++ common/channel/src/message_queues.rs
@@ -1,7 +1,7 @@
 use crate::libra_channel::MessageQueue;
 use libra_logger::{self, prelude::*};
 use libra_types::account_address::AccountAddress;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 /// ValidatorMessage trait needs to be implemented by every message which gets pushed into the Libra
 /// channel so that we can ensure fairness among validators
@@ -16,6 +16,7 @@ pub trait ValidatorMessage {
 /// QueueStyle also determines the policy for dropping messages.
 /// With LIFO, oldest messages are dropped.
 /// With FIFO, newest messages are dropped.
+#[derive(Clone, Copy)]
 pub enum QueueStyle {
     LIFO,
     FIFO,
@@ -30,13 +31,14 @@ pub enum QueueStyle {
 /// fashion among validators.
 /// If there are no messages, in any of the queues, `None` is returned.
 pub struct PerValidatorQueue<T> {
+    /// QueueStyle for the messages stored per validator
     queue_style: QueueStyle,
     /// per_validator_queue maintains a map from a Validator to a queue
     /// of all the messages from that Validator. A Validator is
     /// represented by AccountAddress
     per_validator_queue: HashMap<AccountAddress, VecDeque<T>>,
-    /// This is a list of Validators which have pending messages
-    /// This list will be used for performing round robin among
+    /// This is a (round-robin)queue of Validators which have pending messages
+    /// This queue will be used for performing round robin among
     /// Validators for choosing the next message
     round_robin_queue: VecDeque<AccountAddress>,
     /// Maximum number of messages to store per validator
@@ -55,37 +57,66 @@ impl<T> PerValidatorQueue<T> {
             queue_style,
             max_queue_size: max_queue_size_per_validator,
             per_validator_queue: HashMap::new(),
-            round_robin_queue: Vec::new(),
+            round_robin_queue: VecDeque::new(),
         }
     }
+
+    /// Given a validator, pops the message from its queue and returns the message
+    /// It also returns a boolean indicating whether the validators queue is empty
+    /// after popping the message
+    fn pop_from_validator_queue(&mut self, validator: AccountAddress) -> (Option<T>, bool) {
+        if let Some(q) = self.per_validator_queue.get_mut(&validator) {
+            if q.is_empty() {
+                return (None, true);
+            }
+            // Extract message from the validator's queue
+            let retval = match self.queue_style {
+                QueueStyle::FIFO => q.pop_front(),
+                QueueStyle::LIFO => q.pop_back(),
+            };
+            (retval, q.is_empty())
+        } else {
+            (None, true)
+        }
+    }
+
 }
+
+
 
 impl<T: ValidatorMessage> MessageQueue for PerValidatorQueue<T> {
     type Message = T;
 
+    /// push a message to the appropriate queue in per_validator_queue
+    /// add the validator to round_robin_queue if it didnt already exist
     fn push(&mut self, message: Self::Message) {
         let validator = message.get_validator();
+        let max_queue_size = self.max_queue_size;
         let validator_message_queue = self.per_validator_queue
             .entry(validator)
-            .or_insert_with(|| VecDeque::with_capacity(self.max_queue_size));
+            .or_insert_with(|| VecDeque::with_capacity(max_queue_size));
+        // Add the validator to our round-robin queue is its not already there
         if validator_message_queue.is_empty() {
             self.round_robin_queue.push_back(validator);
         }
-        if validator_message_queue.len() == self.max_queue_size {
+        // Push the message to the actual validator message queue
+        if validator_message_queue.len() == max_queue_size {
             match self.queue_style {
                 // Drop the newest message for FIFO
                 QueueStyle::FIFO => (),
                 // Drop the oldest message for LIFO
                 QueueStyle::LIFO => {
-                    q.pop_front();
-                    q.push_back(message);
+                    validator_message_queue.pop_front();
+                    validator_message_queue.push_back(message);
                 }
             };
         } else {
-            q.push_back(message);
+            validator_message_queue.push_back(message);
         }
     }
 
+    /// pop a message from the appropriate queue in per_validator_queue
+    /// remove the validator from the round_robin_queue if it has no more messages
     fn pop(&mut self) -> Option<Self::Message> {
         let validator;
         if let Some(v) = self.round_robin_queue.pop_front() {
@@ -93,26 +124,10 @@ impl<T: ValidatorMessage> MessageQueue for PerValidatorQueue<T> {
         } else {
             return None;
         }
-        let retval;
-        if let Some(q) = self.per_validator_queue.get_mut(&validator) {
-            if q.is_empty() {
-                crit!("validator: {:?} is present in round_robin_queue, its queue has zero messages", validator);
-                return None;
-            }
-            // Extract message from the validator's queue
-            retval = match self.queue_style {
-                QueueStyle::FIFO => q.pop_front(),
-                QueueStyle::LIFO => q.pop_back(),
-            };
-            // If the validator has no more messages, remove it from the round robin list:
-            // self.round_robin_queue
-            if !q.is_empty() {
-                self.round_robin_queue.push_back(validator);
-            }
-        } else {
-            crit!("validator: {:?} is present in round_robin_queue, its missing from self.per_validator_queue", validator);
-            return None;
+        let (message, is_q_empty) = self.pop_from_validator_queue(validator);
+        if !is_q_empty {
+            self.round_robin_queue.push_back(validator);
         }
-        retval
+        message
     }
 }
