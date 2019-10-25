b71220e742e05d664534a9b5ce2f9a06e19cecd7
youngyl <youngyl@fb.com>
2019-10-25 10:12:55 -0700

    [TF] fix validator sets gen
    We recently made changes in build to work around Cargo's feature unification
    so that we can separate testing and fuzzing code (1d1cb400). We need to update
    the build script that generates the validator config to keep it in sync.
    
    Closes: #1515
    Approved by: ankushagarwal

--- terraform/validator-sets/build.sh
+++ terraform/validator-sets/build.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 set -e
 
-OUTDIR="${1?[Specify relative output directory]}"
+OUT_DIR="${1?[Specify relative output directory]}"
 shift
 
-mkdir -p "$OUTDIR"
+LIBRA_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd)"
+TF_WORK_DIR="$LIBRA_DIR/terraform/validator-sets"
+OUTPUT_DIR="$TF_WORK_DIR/$OUT_DIR"
+mkdir -p "$OUTPUT_DIR"
 
-cd ../..
 
-if [ ! -e "terraform/validator-sets/$OUTDIR/mint.key" ]; then
-	cargo run --bin generate-keypair -- -o "terraform/validator-sets/$OUTDIR/mint.key"
+if [ ! -e "$OUTPUT_DIR/mint.key" ]; then
+	cargo run -p generate-keypair --bin generate-keypair -- -o "$OUTPUT_DIR/mint.key"
 fi
 
-cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR/val" -d -r validator "$@"
-cargo run --bin libra-config -- -b config/data/configs/node.config.toml -m "terraform/validator-sets/$OUTDIR/mint.key" -o "terraform/validator-sets/$OUTDIR/fn" -u "terraform/validator-sets/$OUTDIR/val/0" -r full_node -n 10
+cd $LIBRA_DIR && cargo run -p config-builder --bin libra-config -- -b $LIBRA_DIR/config/data/configs/node.config.toml -m "$OUTPUT_DIR/mint.key" -o "$OUTPUT_DIR/val" -d -r validator "$@"
+cd $LIBRA_DIR && cargo run -p config-builder --bin libra-config -- -b $LIBRA_DIR/config/data/configs/node.config.toml -m "$OUTPUT_DIR/mint.key" -o "$OUTPUT_DIR/fn" -u "$OUTPUT_DIR/val/0" -r full_node -n 10
 
-cd -
 
-cd $OUTDIR/val
+cd "$OUTPUT_DIR/val"
 mv */*.keys.toml .
 mv 1/*.network_peers.config.toml network_peers.config.toml
 mv 1/consensus_peers.config.toml ../consensus_peers.config.toml
@@ -25,7 +26,7 @@ mv 1/genesis.blob ../
 rm */*.toml */*.blob
 find . -mindepth 1 -type d -print0 | xargs -0 rmdir
 
-cd ../fn
+cd "$OUTPUT_DIR/fn"
 mv */*.keys.toml .
 mv 0/*.network_peers.config.toml network_peers.config.toml
 rm */*.toml */*.blob
