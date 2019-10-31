08cab7a13eed69af91aa11cb498c9a7d0d24a2cb
Brandon Williams <bmwill@calibra.com>
2019-10-31 10:21:20 -0700

    [vm] fix oom discovered by fuzzer
    An oom was discovered by fuzzing the interface used to deserialize a VM
    Value given a VM StructDef. The oom is caused by the deserialization
    logic reading a u32 from the input stream, which is intended to be the
    length of a variable length vector, and attempting to pre-allocate a
    vector of that length.
    
    The fix to this oom is to:
        1) Stop attempting to pre-allocate the entire vector and instead
           allocate entries as needed.
        2) push the checks earlier which determine if this is even a valid
           `NativeStructType`.
    
    This works in this particular case because the inputs provided by the
    fuzzer are not guaranteed to be a valid (StructDef, bytes) pairs and by
    the time we try to deserialize each element (of the potentially very
    long vector) another deserialization error is encountered.

--- language/vm/vm-runtime/vm-runtime-types/src/native_structs/serializer.rs
+++ language/vm/vm-runtime/vm-runtime-types/src/native_structs/serializer.rs
@@ -12,15 +12,15 @@ pub(crate) fn deserialize_vector(
     deserializer: &mut lcs::Deserializer<'_>,
     ty: &NativeStructType,
 ) -> VMResult<NativeVector> {
-    //TODO investigate if we can use `deserialize_seq` instead of reading the length explicitly
-    let len = u32::deserialize(&mut *deserializer)
-        .map_err(|_| vm_error(Location::new(), StatusCode::DATA_FORMAT_ERROR))?
-        as usize;
-    let mut val = Vec::with_capacity(len);
     if ty.type_actuals().len() != 1 {
         return Err(vm_error(Location::new(), StatusCode::DATA_FORMAT_ERROR));
     };
     let elem_type = &ty.type_actuals()[0];
+    //TODO investigate if we can use `deserialize_seq` instead of reading the length explicitly
+    let len = u32::deserialize(&mut *deserializer)
+        .map_err(|_| vm_error(Location::new(), StatusCode::DATA_FORMAT_ERROR))?
+        as usize;
+    let mut val = Vec::new();
     for _i in 0..len {
         val.push(MutVal::new(deserialize_value(deserializer, elem_type)?));
     }
