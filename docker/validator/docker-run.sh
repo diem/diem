1f918aa09ab634cbbb1daa64b52c63f290e624b7
Andrey Chursin <achursin@calibra.com>
2019-10-31 11:51:28 -0700

    [docker] Move docker CMD into docker-run.sh
    Instead of having complicated command in docker CMD, this diff moves setting up environment into `docker-run.sh` file and sets CMD to this shell file.
    
    Closes: #1580
    Approved by: opsguy

---
+++ docker/validator/docker-run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -ex
+
+cd /opt/libra/etc
+echo "$NODE_CONFIG" > node.config.toml
+echo "$SEED_PEERS" > seed_peers.config.toml
+echo "$NETWORK_KEYPAIRS" > network_keypairs.config.toml
+echo "$CONSENSUS_KEYPAIR" > consensus_keypair.config.toml
+echo "$FULLNODE_KEYPAIRS" > fullnode_keypairs.config.toml
+exec /opt/libra/bin/libra-node -f node.config.toml
