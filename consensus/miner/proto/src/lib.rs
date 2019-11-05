9cfa28dfd26601a9e05d2a89fa198a2e19f4c6a9
fikgol <hit.cs.lijun@gmail.com>
2019-11-05 20:03:31 +0800

    Add miner rpc

---
+++ consensus/miner/proto/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod proto;
+pub use proto::miner;
---
+++ consensus/miner/src/client.rs
@@ -0,0 +1,23 @@
+use grpcio;
+
+use std::env;
+use std::sync::Arc;
+
+use grpcio::{ChannelBuilder, EnvBuilder};
+use proto::{
+    miner::{
+        MineCtxRequest,
+        MinerProxyClient,
+    }
+};
+
+fn main() {
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("127.0.0.1:4251");
+    let client = MinerProxyClient::new(ch);
+
+    let req = MineCtxRequest {};
+    let mine_ctx = client.get_mine_ctx(&req);
+    //client.Mined(MinedBlockRequest)
+    println!("proof {:?}", mine_ctx);
+}
