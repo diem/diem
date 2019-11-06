9ec4f538bf2a5594008ed78545b2ef1f49eed140
aldenhu <aldenhu@calibra.com>
2019-11-06 11:36:22 -0800

    [storage] `LedgerStore::get_epoch()`

--- storage/libradb/src/ledger_store/ledger_info_test.rs
+++ storage/libradb/src/ledger_store/ledger_info_test.rs
@@ -6,6 +6,7 @@ use crate::{change_set::ChangeSet, LibraDB};
 use libra_tools::tempdir::TempPath;
 use libra_types::block_info::BlockInfo;
 use libra_types::ledger_info::LedgerInfo;
+use libra_types::validator_set::ValidatorSet;
 use proptest::{collection::vec, prelude::*};
 
 prop_compose! {
@@ -21,23 +22,32 @@ prop_compose! {
         partial_ledger_infos_with_sigs in vec(
             any_with::<LedgerInfoWithSignatures>((1..3).into()).no_shrink(), 1..100
         ),
-        start_epoch in 0..10000u64,
+        start_version in 0..10000u64,
     ) -> Vec<LedgerInfoWithSignatures> {
         partial_ledger_infos_with_sigs
             .iter()
             .enumerate()
             .map(|(i, p)| {
                 let ledger_info = p.ledger_info();
+                let version = start_version + i as u64;
+                let epoch: u64 = version / 3;
+                // epoch changes at versions that are multiplications of 3
+                let next_validator_set = if version % 3 == 2 {
+                    Some(ValidatorSet::new(vec![]))
+                } else {
+                    None
+                };
+
                 LedgerInfoWithSignatures::new(
                     LedgerInfo::new(
                         BlockInfo::new(
-                            start_epoch + i as u64,
-                            start_epoch + i as u64,
+                            version,
+                            epoch,
                             ledger_info.consensus_block_id(),
                             ledger_info.transaction_accumulator_hash(),
                             start_epoch + i as Version,
                             ledger_info.timestamp_usecs(),
-                            None,
+                            next_validator_set,
                         ),
                         ledger_info.consensus_data_hash(),
                     ),
@@ -55,11 +65,12 @@ proptest! {
     fn test_ledger_info_put_get_verify(
         ledger_infos_with_sigs in arb_ledger_infos_with_sigs()
     ) {
+        // set up
         let tmp_dir = TempPath::new();
         let db = LibraDB::new(&tmp_dir);
         let store = &db.ledger_store;
-        let start_epoch = ledger_infos_with_sigs.first().unwrap().ledger_info().epoch();
 
+        // write to DB
         let mut cs = ChangeSet::new();
         ledger_infos_with_sigs
             .iter()
@@ -67,6 +78,35 @@ proptest! {
             .collect::<Result<Vec<_>>>()
             .unwrap();
         store.db.write_schemas(cs.batch).unwrap();
-        prop_assert_eq!(db.ledger_store.get_latest_ledger_infos_per_epoch(start_epoch).unwrap(), ledger_infos_with_sigs);
+
+        // verify get_latest_ledger_infos_per_epoch()
+        let mut epoch_ledgers: Vec<_> = ledger_infos_with_sigs
+            .iter()
+            .filter(|x| x.ledger_info().version() % 3 == 2)
+            .map(|x| x.clone())
+            .collect();
+        let last = ledger_infos_with_sigs.last().unwrap();
+        if last.ledger_info().version() % 3 != 2 {
+            epoch_ledgers.push(last.clone());
+        }
+        let start_epoch = ledger_infos_with_sigs.first().unwrap().ledger_info().epoch();
+        prop_assert_eq!(
+            db.ledger_store.get_latest_ledger_infos_per_epoch(start_epoch).unwrap(),
+            epoch_ledgers
+        );
+
+        // verify get_epoch()
+        let mut seen_first_epoch_change = false;
+        for x in ledger_infos_with_sigs {
+            if seen_first_epoch_change {
+                prop_assert_eq!(
+                    db.ledger_store.get_epoch(x.ledger_info().version()).unwrap(),
+                    x.ledger_info().epoch()
+                );
+            } else if x.ledger_info().version() % 3 == 2 {
+                seen_first_epoch_change = true;
+            }
+        }
+
     }
 }
--- storage/libradb/src/ledger_store/mod.rs
+++ storage/libradb/src/ledger_store/mod.rs
@@ -4,6 +4,7 @@
 //! This file defines ledger store APIs that are related to the main ledger accumulator, from the
 //! root(LedgerInfo) to leaf(TransactionInfo).
 
+use crate::schema::epoch_by_version::EpochByVersionSchema;
 use crate::{
     change_set::ChangeSet,
     errors::LibraDbError,
@@ -58,6 +59,26 @@ impl LedgerStore {
             db,
             latest_ledger_info: ArcSwap::from(Arc::new(ledger_info)),
         }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_epoch(&self, version: Version) -> Result<u64> {
+        let mut iter = self
+            .db
+            .iter::<EpochByVersionSchema>(ReadOptions::default())?;
+        iter.seek_for_prev(&version)?;
+        let (epoch_start_version, epoch) = iter
+            .next()
+            .transpose()?
+            .ok_or_else(|| LibraDbError::NotFound(format!("version {}", version)))?;
+        ensure!(
+            epoch_start_version <= version,
+            "DB corruption: looking for epoch at version {}, got epoch {} started at version {}",
+            version,
+            epoch,
+            epoch_start_version
+        );
+        Ok(epoch)
     }
 
     /// Return the ledger infos with their least 2f+1 signatures starting from `start_epoch` to
@@ -192,6 +213,14 @@ impl LedgerStore {
         ledger_info_with_sigs: &LedgerInfoWithSignatures,
         cs: &mut ChangeSet,
     ) -> Result<()> {
+        let ledger_info = ledger_info_with_sigs.ledger_info();
+
+        if ledger_info.next_validator_set().is_some() {
+            let epoch = ledger_info_with_sigs.ledger_info().epoch();
+            let version = ledger_info_with_sigs.ledger_info().version();
+            cs.batch
+                .put::<EpochByVersionSchema>(&(version + 1), &(epoch + 1))?;
+        }
         cs.batch.put::<LedgerInfoSchema>(
             &ledger_info_with_sigs.ledger_info().epoch(),
             ledger_info_with_sigs,
