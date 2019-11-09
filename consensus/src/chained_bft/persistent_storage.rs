bc7c87a3efa10d8298116346d538cfa406c66835
David Wolinsky <davidiw@fb.com>
2019-11-09 07:54:48 -0800

    fixup! [consensus] eliminate consensus state from consensus db

--- consensus/src/chained_bft/persistent_storage.rs
+++ consensus/src/chained_bft/persistent_storage.rs
@@ -286,24 +286,6 @@ impl<T: Payload> PersistentStorage<T> for StorageWriteProxy {
         });
         let mut blocks = initial_data.2;
         let mut quorum_certs: Vec<_> = initial_data.3;
-        // bootstrap the empty store with genesis block and qc.
-        if blocks.is_empty() && quorum_certs.is_empty() {
-            let genesis_ledger_info = read_client
-                .get_latest_ledger_infos_per_epoch(0)
-                .expect("Storage should commit genesis before start consensus")
-                .pop()
-                .expect("Storage should commit genesis before start consensus");
-            let genesis =
-                Block::make_genesis_block_from_ledger_info(genesis_ledger_info.ledger_info());
-            quorum_certs.push(QuorumCert::certificate_for_genesis_from_ledger_info(
-                genesis_ledger_info.ledger_info(),
-                genesis.id(),
-            ));
-            blocks.push(genesis);
-            proxy
-                .save_tree(vec![blocks[0].clone()], vec![quorum_certs[0].clone()])
-                .expect("unable to bootstrap the storage with genesis block");
-        }
         let blocks_repr: Vec<String> = blocks.iter().map(|b| format!("\n\t{}", b)).collect();
         info!(
             "The following blocks were restored from ConsensusDB : {}",
