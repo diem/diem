76f2165b3df0830b1e903bc52e355cd97538711d
David Wolinsky <davidiw@fb.com>
2019-11-12 11:58:02 -0800

    [config] move execution_config to own file
    In addition, I also made the genesis_file_location a PathBuf instead of
    a string
    
    Closes: #1722
    Approved by: mimoo

---
+++ config/src/config/execution_config.rs
@@ -0,0 +1,23 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(default)]
+pub struct ExecutionConfig {
+    pub address: String,
+    pub port: u16,
+    pub genesis_file_location: PathBuf,
+}
+
+impl Default for ExecutionConfig {
+    fn default() -> ExecutionConfig {
+        ExecutionConfig {
+            address: "localhost".to_string(),
+            port: 6183,
+            genesis_file_location: PathBuf::from("genesis.blob"),
+        }
+    }
+}
