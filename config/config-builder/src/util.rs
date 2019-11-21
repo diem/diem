d17179b726e660f40686fd4fec7fdc32874679a0
David Wolinsky <davidiw@fb.com>
2019-11-21 00:16:51 -0800

    [config] simplify validator network generation
    Like the previous commit, this leverages the various struct methods to
    generate a validator network. As a result, we able to begin deleting a
    lot of helper code. However, that helper code has a few more
    dependencies that need to be cleaned up before we can completely remove
    it.

--- config/config-builder/src/util.rs
+++ config/config-builder/src/util.rs
@@ -1,10 +1,7 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use libra_config::{
-    config::NodeConfig,
-    trusted_peers::{ConsensusPeersConfig, NetworkPeersConfig},
-};
+use libra_config::config::NodeConfig;
 use libra_crypto::{
     ed25519::{Ed25519PrivateKey, Ed25519PublicKey},
     test_utils::KeyPair,
@@ -14,19 +11,6 @@ use libra_types::transaction::Transaction;
 use rand::{rngs::StdRng, SeedableRng};
 use std::process;
 use vm_genesis;
-
-pub fn genesis_transaction(
-    faucet_account_keypair: &KeyPair<Ed25519PrivateKey, Ed25519PublicKey>,
-    consensus_peers_config: &ConsensusPeersConfig,
-    network_peers_config: &NetworkPeersConfig,
-) -> Transaction {
-    let genesis_transaction = vm_genesis::encode_genesis_transaction_with_validator(
-        &faucet_account_keypair.private_key,
-        faucet_account_keypair.public_key.clone(),
-        consensus_peers_config.get_validator_set(network_peers_config),
-    );
-    Transaction::UserTransaction(genesis_transaction.into_inner())
-}
 
 /// Returns the config as well as the genesis keypair
 pub fn get_test_config() -> (NodeConfig, KeyPair<Ed25519PrivateKey, Ed25519PublicKey>) {
@@ -40,11 +24,20 @@ pub fn get_test_config() -> (NodeConfig, KeyPair<Ed25519PrivateKey, Ed25519Publi
     let mut rng = StdRng::from_seed(seed);
 
     let private_key = Ed25519PrivateKey::generate_for_testing(&mut rng);
-    let keypair = KeyPair::from(private_key);
-    config.execution.genesis = Some(genesis_transaction(
-        &keypair,
-        &config.consensus.consensus_peers,
-        &config.validator_network.as_ref().unwrap().network_peers,
+    let keypair: KeyPair<Ed25519PrivateKey, Ed25519PublicKey> = KeyPair::from(private_key);
+    let network_peers = &config.validator_network.as_ref().unwrap().network_peers;
+
+    config.execution.genesis = Some(Transaction::UserTransaction(
+        vm_genesis::encode_genesis_transaction_with_validator(
+            &keypair.private_key,
+            keypair.public_key.clone(),
+            config
+                .consensus
+                .consensus_peers
+                .get_validator_set(network_peers),
+        )
+        .into_inner(),
     ));
+
     (config, keypair)
 }
