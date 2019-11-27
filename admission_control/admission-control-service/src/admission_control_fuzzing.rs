diem/diem

commit f102b9fd44310adbbb1d283bdac378af78e1d83b

[rust] move to futures 0.3 and tokio 0.2
Closes: #1657
Approved by: bothra90

admission_control/admission-control-service/Cargo.toml
@@ -12,12 +12,12 @@ edition = "2018"
 [dependencies]
 bytes = "0.4.12"
 prost = "0.5.0"
-futures = { version = "=0.3.0-alpha.19", package = "futures-preview", features = ["compat"] }
+futures = { version = "0.3.0", features = ["compat"] }
 grpcio = { version = "=0.5.0-alpha.4", default-features = false, features = ["protobuf-codec"] }
 num_cpus = "1.10.1"
 lazy_static = "1.3.0"
 rand = "0.6.5"
-tokio = "=0.2.0-alpha.6"
+tokio = { version = "0.2", features = ["full"] }
 prometheus = { version = "0.7.0", default-features = false }
 
 admission-control-proto = { path = "../admission-control-proto", version = "0.1.0" }

consensus/Cargo.toml
@@ -12,7 +12,7 @@ edition = "2018"
 [dependencies]
 byteorder = { version = "1.3.2", default-features = false }
 bytes = "0.4.12"
-futures = { version = "=0.3.0-alpha.19", package = "futures-preview" }
+futures = "0.3.0"
 grpcio = { version = "=0.5.0-alpha.4", default-features = false }
 lazy_static = { version = "1.3.0", default-features = false }
 mirai-annotations = { version = "1.4.0", default-features = false }
@@ -27,7 +27,7 @@ serde = { version = "1.0.99", default-features = false }
 serde_json = "1.0"
 siphasher = { version = "0.3.0", default-features = false }
 termion = { version = "1.5.3", default-features = false }
-tokio = "=0.2.0-alpha.6"
+tokio = { version = "0.2", features = ["full"] }
 prometheus = { version = "0.7.0", default-features = false }
 proptest = { version = "0.9.4", optional = true }
 

network/netcore/Cargo.toml
@@ -11,11 +11,11 @@ edition = "2018"
 
 [dependencies]
 bytes = { version = "0.4.12", default-features = false }
-futures = { version = "=0.3.0-alpha.19", package = "futures-preview", features = ["io-compat", "compat"] }
+futures = { version = "0.3.0", features = ["io-compat", "compat"] }
 futures_01 = { version = "0.1.28", package = "futures" }
 parity-multiaddr = { version = "0.5.0", default-features = false }
 pin-project = "0.4.2"
-tokio = "=0.2.0-alpha.6"
+tokio = { version = "0.2", features = ["full"] }
 yamux = { version = "0.2.1", default-features = false }
 
 memsocket = { path = "../memsocket", version = "0.1.0" }

network/socket-bench-server/Cargo.toml
@@ -11,10 +11,11 @@ edition = "2018"
 
 [dependencies]
 bytes = { version = "0.4.12", default-features = false }
-futures = { version = "=0.3.0-alpha.19", package = "futures-preview", features = ["async-await", "io-compat", "compat"] }
+futures = { version = "0.3.0", features = ["io-compat", "compat"] }
 futures_01 = { version = "0.1.28", package = "futures" }
 parity-multiaddr = "0.5.0"
-tokio = "=0.2.0-alpha.6"
+tokio = { version = "0.2", features = ["full"] }
+tokio-util = { version = "0.2", features = ["codec"] }
 
 libra-logger = { path = "../../common/logger", version = "0.1.0" }
 memsocket = { path = "../memsocket", version = "0.1.0" }

network/src/protocols/direct_send/mod.rs
@@ -63,10 +63,8 @@ use std::{
     collections::{hash_map::Entry, HashMap},
     fmt::Debug,
 };
-use tokio::{
-    codec::{Framed, LengthDelimitedCodec},
-    runtime::TaskExecutor,
-};
+use tokio::runtime::Handle;
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
 #[cfg(test)]
 mod test;
@@ -109,7 +107,7 @@ impl Debug for Message {
 /// The DirectSend actor.
 pub struct DirectSend<TSubstream> {
     /// A handle to a tokio executor.
-    executor: TaskExecutor,
+    executor: Handle,
     /// Channel to receive requests from other upstream actors.
     ds_requests_rx: channel::Receiver<DirectSendRequest>,
     /// Channels to send notifictions to upstream actors.
@@ -127,7 +125,7 @@ where
     TSubstream: AsyncRead + AsyncWrite + Send + Unpin + Debug + 'static,
 {
     pub fn new(
-        executor: TaskExecutor,
+        executor: Handle,
         ds_requests_rx: channel::Receiver<DirectSendRequest>,
         ds_notifs_tx: channel::Sender<DirectSendNotification>,
         peer_mgr_notifs_rx: channel::Receiver<PeerManagerNotification<TSubstream>>,
@@ -191,7 +189,7 @@ where
                         peer_id,
                         Message {
                             protocol: protocol.clone(),
-                            mdata: data.freeze(),
+                            mdata: data.freeze().as_ref().into(),
                         },
                     );
                     ds_notifs_tx
@@ -218,7 +216,7 @@ where
     // Create a new message queue and spawn a task to forward the messages from the queue to the
     // corresponding substream.
     async fn start_message_queue_handler(
-        executor: TaskExecutor,
+        executor: Handle,
         mut peer_mgr_reqs_tx: PeerManagerRequestSender<TSubstream>,
         peer_id: PeerId,
         protocol: ProtocolId,
@@ -238,7 +236,13 @@ where
 
         // Spawn a task to forward the messages from the queue to the substream.
         let f_substream = async move {
-            if let Err(e) = msg_rx.map(Ok).forward(substream).await {
+            // TODO(bmwill) migrate to bytes 0.5 everywhere
+            if let Err(e) = msg_rx
+                .map(|b| bytes05::Bytes::copy_from_slice(b.as_ref()))
+                .map(Ok)
+                .forward(substream)
+                .await
+            {
                 warn!(
                     "Forward messages to peer {} error {:?}",
                     peer_id.short_str(),

network/src/protocols/direct_send/test.rs
@@ -15,10 +15,8 @@ use futures::{sink::SinkExt, stream::StreamExt};
 use libra_types::PeerId;
 use memsocket::MemorySocket;
 use netcore::compat::IoCompat;
-use tokio::{
-    codec::{Framed, LengthDelimitedCodec},
-    runtime::{Runtime, TaskExecutor},
-};
+use tokio::runtime::{Handle, Runtime};
+use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
 const PROTOCOL_1: &[u8] = b"/direct_send/1.0.0";
 const PROTOCOL_2: &[u8] = b"/direct_send/2.0.0";
@@ -27,7 +25,7 @@ const MESSAGE_2: &[u8] = b"Direct Send 2";
 const MESSAGE_3: &[u8] = b"Direct Send 3";
 
 fn start_direct_send_actor(
-    executor: TaskExecutor,
+    executor: Handle,
 ) -> (
     channel::Sender<DirectSendRequest>,
     channel::Receiver<DirectSendNotification>,
@@ -90,10 +88,10 @@ async fn expect_open_substream_request<TSubstream>(
 
 #[test]
 fn test_inbound_substream() {
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (_ds_requests_tx, mut ds_notifs_rx, mut peer_mgr_notifs_tx, _peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream, listener_substream) = MemorySocket::new_pair();
@@ -103,11 +101,11 @@ fn test_inbound_substream() {
         let mut dialer_substream =
             Framed::new(IoCompat::new(dialer_substream), LengthDelimitedCodec::new());
         dialer_substream
-            .send(Bytes::from_static(MESSAGE_1))
+            .send(bytes05::Bytes::from_static(MESSAGE_1))
             .await
             .unwrap();
         dialer_substream
-            .send(Bytes::from_static(MESSAGE_2))
+            .send(bytes05::Bytes::from_static(MESSAGE_2))
             .await
             .unwrap();
     };
@@ -138,10 +136,10 @@ fn test_inbound_substream() {
 
 #[test]
 fn test_inbound_substream_closed() {
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (_ds_requests_tx, mut ds_notifs_rx, mut peer_mgr_notifs_tx, _peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream, listener_substream) = MemorySocket::new_pair();
@@ -151,7 +149,7 @@ fn test_inbound_substream_closed() {
         let mut dialer_substream =
             Framed::new(IoCompat::new(dialer_substream), LengthDelimitedCodec::new());
         dialer_substream
-            .send(Bytes::from_static(MESSAGE_1))
+            .send(bytes05::Bytes::from_static(MESSAGE_1))
             .await
             .unwrap();
         // close the substream on the dialer side
@@ -181,10 +179,10 @@ fn test_inbound_substream_closed() {
 
 #[test]
 fn test_outbound_single_protocol() {
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (mut ds_requests_tx, _ds_notifs_rx, _peer_mgr_notifs_tx, mut peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream, listener_substream) = MemorySocket::new_pair();
@@ -241,10 +239,10 @@ fn test_outbound_single_protocol() {
 
 #[test]
 fn test_outbound_multiple_protocols() {
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (mut ds_requests_tx, _ds_notifs_rx, _peer_mgr_notifs_tx, mut peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream_1, listener_substream_1) = MemorySocket::new_pair();
@@ -314,10 +312,10 @@ fn test_outbound_multiple_protocols() {
 #[test]
 fn test_outbound_not_connected() {
     ::libra_logger::try_init_for_testing();
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (mut ds_requests_tx, _ds_notifs_rx, _peer_mgr_notifs_tx, mut peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream, listener_substream) = MemorySocket::new_pair();
@@ -386,10 +384,10 @@ fn test_outbound_not_connected() {
 #[test]
 fn test_outbound_connection_closed() {
     ::libra_logger::try_init_for_testing();
-    let rt = Runtime::new().unwrap();
+    let mut rt = Runtime::new().unwrap();
 
     let (mut ds_requests_tx, _ds_notifs_rx, _peer_mgr_notifs_tx, mut peer_mgr_reqs_rx) =
-        start_direct_send_actor(rt.executor());
+        start_direct_send_actor(rt.handle().clone());
 
     let peer_id = PeerId::random();
     let (dialer_substream_1, listener_substream_1) = MemorySocket::new_pair();

commit 61312822e2e63143c731d7dde1d4f4bb9b7d983c

rust: update most crates to bytes 0.5
Most crates are update to bytes 0.5, eliminating some potential
performance regressions introduced in f102b9fd ([rust] move to futures
0.3 and tokio 0.2, 2019-11-06).

Some crates still need to have 0.4 because prost still requires the 0.4
version imported globally as `::bytes`. Once prost does a release to
update to bytes 0.5 we can finish moving the rest of the crates to 0.5.

admission_control/admission-control-service/Cargo.toml
@@ -10,7 +10,7 @@ publish = false
 edition = "2018"
 
 [dependencies]
-bytes = "0.4.12"
+bytes = "0.5"
 prost = "0.5.0"
 futures = { version = "0.3.0", features = ["compat"] }
 grpcio = { version = "=0.5.0-alpha.4", default-features = false, features = ["protobuf-codec"] }

admission_control/admission-control-service/src/admission_control_fuzzing.rs
@@ -7,6 +7,7 @@ use channel;
 use futures::executor::block_on;
 use libra_config::config::{AdmissionControlConfig, RoleType};
 use libra_proptest_helpers::ValueGenerator;
+use libra_prost_ext::MessageExt;
 use libra_types::transaction::SignedTransaction;
 use network::validator_network::AdmissionControlNetworkSender;
 use proptest;
@@ -30,9 +31,7 @@ pub fn generate_corpus(gen: &mut ValueGenerator) -> Vec<u8> {
     let mut req = SubmitTransactionRequest::default();
     req.transaction = Some(signed_txn.into());
 
-    let mut bytes = bytes::BytesMut::with_capacity(req.encoded_len());
-    req.encode(&mut bytes).unwrap();
-    bytes.to_vec()
+    req.to_vec().unwrap()
 }
 
 /// fuzzer takes a serialized SubmitTransactionRequest an process it with an admission control

common/prost-ext/Cargo.toml
@@ -12,5 +12,5 @@ edition = "2018"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-bytes = "0.4.12"
+bytes = "0.5"
 prost = "0.5.0"

common/prost-ext/src/lib.rs
@@ -3,7 +3,7 @@
 
 #![forbid(unsafe_code)]
 
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
 use prost::{EncodeError, Message};
 
 impl<T: ?Sized> MessageExt for T where T: Message {}
@@ -13,9 +13,9 @@ pub trait MessageExt: Message {
     where
         Self: Sized,
     {
-        let mut bytes = BytesMut::with_capacity(self.encoded_len());
+        let mut bytes = Vec::with_capacity(self.encoded_len());
         self.encode(&mut bytes)?;
-        Ok(bytes.freeze())
+        Ok(bytes.into())
     }
 
     fn to_vec(&self) -> Result<Vec<u8>, EncodeError>

consensus/Cargo.toml
@@ -11,7 +11,7 @@ edition = "2018"
 
 [dependencies]
 byteorder = { version = "1.3.2", default-features = false }
-bytes = "0.4.12"
+bytes = "0.5"
 futures = "0.3.0"
 grpcio = { version = "=0.5.0-alpha.4", default-features = false }
 lazy_static = { version = "1.3.0", default-features = false }

crypto/crypto/Cargo.toml
@@ -11,7 +11,7 @@ edition = "2018"
 
 [dependencies]
 byteorder = "1.3.2"
-bytes = "0.4.12"
+bytes = "0.5"
 curve25519-dalek = { git = "https://github.com/calibra/curve25519-dalek.git", branch = "fiat", default-features = false }
 digest = "0.8.1"
 ed25519-dalek = { git = "https://github.com/calibra/ed25519-dalek.git", branch = "fiat", features = ["serde"], default-features = false }

crypto/crypto/src/hash.rs
@@ -347,7 +347,7 @@ impl fmt::Display for HashValue {
 
 impl From<HashValue> for Bytes {
     fn from(value: HashValue) -> Bytes {
-        value.hash.as_ref().into()
+        Bytes::copy_from_slice(value.hash.as_ref())
     }
 }
 

network/netcore/Cargo.toml
@@ -10,7 +10,7 @@ publish = false
 edition = "2018"
 
 [dependencies]
-bytes = { version = "0.4.12", default-features = false }
+bytes = "0.5"
 futures = { version = "0.3.0", features = ["io-compat", "compat"] }
 futures_01 = { version = "0.1.28", package = "futures" }
 parity-multiaddr = { version = "0.5.0", default-features = false }

network/socket-bench-server/Cargo.toml
@@ -10,7 +10,7 @@ publish = false
 edition = "2018"
 
 [dependencies]
-bytes = { version = "0.4.12", default-features = false }
+bytes = "0.5"
 futures = { version = "0.3.0", features = ["io-compat", "compat"] }
 futures_01 = { version = "0.1.28", package = "futures" }
 parity-multiaddr = "0.5.0"

network/src/lib.rs
@@ -28,4 +28,4 @@ mod transport;
 mod utils;
 
 /// Type for unique identifier associated with each network protocol
-pub type ProtocolId = bytes::Bytes;
+pub type ProtocolId = bytes05::Bytes;

network/src/protocols/direct_send/mod.rs
@@ -49,7 +49,7 @@ use crate::{
     peer_manager::{PeerManagerNotification, PeerManagerRequestSender},
     ProtocolId,
 };
-use bytes::Bytes;
+use bytes05::Bytes;
 use channel;
 use futures::{
     io::{AsyncRead, AsyncWrite},
@@ -94,7 +94,7 @@ impl Debug for Message {
         let mdata_str = if self.mdata.len() <= 10 {
             format!("{:?}", self.mdata)
         } else {
-            format!("{:?}...", self.mdata.slice_to(10))
+            format!("{:?}...", self.mdata.slice(..10))
         };
         write!(
             f,
@@ -189,7 +189,7 @@ where
                         peer_id,
                         Message {
                             protocol: protocol.clone(),
-                            mdata: data.freeze().as_ref().into(),
+                            mdata: data.freeze(),
                         },
                     );
                     ds_notifs_tx
@@ -236,13 +236,7 @@ where
 
         // Spawn a task to forward the messages from the queue to the substream.
         let f_substream = async move {
-            // TODO(bmwill) migrate to bytes 0.5 everywhere
-            if let Err(e) = msg_rx
-                .map(|b| bytes05::Bytes::copy_from_slice(b.as_ref()))
-                .map(Ok)
-                .forward(substream)
-                .await
-            {
+            if let Err(e) = msg_rx.map(Ok).forward(substream).await {
                 warn!(
                     "Forward messages to peer {} error {:?}",
                     peer_id.short_str(),

network/src/protocols/direct_send/test.rs
@@ -9,7 +9,7 @@ use crate::{
     protocols::direct_send::{DirectSend, DirectSendNotification, DirectSendRequest, Message},
     ProtocolId,
 };
-use bytes::Bytes;
+use bytes05::Bytes;
 use channel;
 use futures::{sink::SinkExt, stream::StreamExt};
 use libra_types::PeerId;
@@ -101,11 +101,11 @@ fn test_inbound_substream() {
         let mut dialer_substream =
             Framed::new(IoCompat::new(dialer_substream), LengthDelimitedCodec::new());
         dialer_substream
-            .send(bytes05::Bytes::from_static(MESSAGE_1))
+            .send(Bytes::from_static(MESSAGE_1))
             .await
             .unwrap();
         dialer_substream
-            .send(bytes05::Bytes::from_static(MESSAGE_2))
+            .send(Bytes::from_static(MESSAGE_2))
             .await
             .unwrap();
     };
@@ -149,7 +149,7 @@ fn test_inbound_substream_closed() {
         let mut dialer_substream =
             Framed::new(IoCompat::new(dialer_substream), LengthDelimitedCodec::new());
         dialer_substream
-            .send(bytes05::Bytes::from_static(MESSAGE_1))
+            .send(Bytes::from_static(MESSAGE_1))
             .await
             .unwrap();
         // close the substream on the dialer side

network/src/protocols/discovery/test.rs
@@ -34,7 +34,7 @@ fn get_raw_message(msg: DiscoveryMsg) -> Message {
 
 fn parse_raw_message(msg: Message) -> Result<DiscoveryMsg, NetworkError> {
     assert_eq!(msg.protocol, DISCOVERY_DIRECT_SEND_PROTOCOL);
-    let msg = DiscoveryMsg::decode(msg.mdata)?;
+    let msg = DiscoveryMsg::decode(msg.mdata.as_ref())?;
     Ok(msg)
 }
 

network/src/protocols/health_checker/mod.rs
@@ -23,7 +23,7 @@ use crate::{
     utils::MessageExt,
     validator_network::{Event, HealthCheckerNetworkEvents, HealthCheckerNetworkSender},
 };
-use bytes::Bytes;
+use bytes05::Bytes;
 use futures::{
     channel::oneshot,
     stream::{FusedStream, FuturesUnordered, Stream, StreamExt},

network/src/protocols/health_checker/test.rs
@@ -69,7 +69,7 @@ async fn expect_ping(
         ProtocolId::from_static(HEALTH_CHECKER_RPC_PROTOCOL)
     );
 
-    let req_msg = HealthCheckerMsg::decode(req_data).unwrap();
+    let req_msg = HealthCheckerMsg::decode(req_data.as_ref()).unwrap();
     match req_msg.message {
         Some(HealthCheckerMsg_oneof::Ping(ping_msg)) => (ping_msg, res_tx),
         _ => panic!("Unexpected HealthCheckerMsg: {:?}", req_msg),
@@ -125,7 +125,7 @@ async fn send_inbound_ping(
 
 async fn expect_pong(res_rx: oneshot::Receiver<Result<Bytes, RpcError>>) {
     let res_data = res_rx.await.unwrap().unwrap();
-    let res_msg = HealthCheckerMsg::decode(res_data).unwrap();
+    let res_msg = HealthCheckerMsg::decode(res_data.as_ref()).unwrap();
     match res_msg.message {
         Some(HealthCheckerMsg_oneof::Pong(_)) => {}
         _ => panic!("Unexpected HealthCheckerMsg: {:?}", res_msg),

network/src/protocols/rpc/mod.rs
@@ -62,7 +62,7 @@ use crate::{
     ProtocolId,
 };
 use bounded_executor::BoundedExecutor;
-use bytes::Bytes;
+use bytes05::Bytes;
 use channel;
 use error::RpcError;
 use futures::{
@@ -362,9 +362,7 @@ where
     let mut substream = Framed::new(IoCompat::new(substream), LengthDelimitedCodec::new());
     // Send the rpc request data.
     let req_len = req_data.len();
-    substream
-        .buffered_send(bytes05::Bytes::copy_from_slice(req_data.as_ref()))
-        .await?;
+    substream.buffered_send(req_data).await?;
     // We won't send anything else on this substream, so we can half-close our
     // output side.
     substream.close().await?;
@@ -377,7 +375,7 @@ where
 
     // Wait for listener's response.
     let res_data = match substream.next().await {
-        Some(res_data) => res_data?.freeze().as_ref().into(),
+        Some(res_data) => res_data?.freeze(),
         None => return Err(io::Error::from(io::ErrorKind::UnexpectedEof).into()),
     };
 
@@ -449,7 +447,7 @@ where
     let mut substream = Framed::new(IoCompat::new(substream), LengthDelimitedCodec::new());
     // Read the rpc request data.
     let req_data = match substream.next().await {
-        Some(req_data) => req_data?.freeze().as_ref().into(),
+        Some(req_data) => req_data?.freeze(),
         None => return Err(io::Error::from(io::ErrorKind::UnexpectedEof).into()),
     };
     counters::LIBRA_NETWORK_RPC_MESSAGES
@@ -482,9 +480,7 @@ where
     let res_len = res_data.len();
 
     // Send the response to remote
-    substream
-        .buffered_send(bytes05::Bytes::copy_from_slice(res_data.as_ref()))
-        .await?;
+    substream.buffered_send(res_data).await?;
 
     // We won't send anything else on this substream, so we can half-close
     // our output. The initiator will have also half-closed their side before

network/src/validator_network/mod.rs
@@ -12,7 +12,7 @@ use crate::{
     utils::MessageExt,
     ProtocolId,
 };
-use bytes::Bytes;
+use bytes05::Bytes;
 use futures::{
     channel::oneshot,
     ready,
@@ -135,11 +135,11 @@ impl<TMessage: Message + Default> Stream for NetworkEvents<TMessage> {
             NetworkNotification::NewPeer(peer_id) => Ok(Event::NewPeer(peer_id)),
             NetworkNotification::LostPeer(peer_id) => Ok(Event::LostPeer(peer_id)),
             NetworkNotification::RecvRpc(peer_id, rpc_req) => {
-                let req_msg = TMessage::decode(rpc_req.data)?;
+                let req_msg = TMessage::decode(rpc_req.data.as_ref())?;
                 Ok(Event::RpcRequest((peer_id, req_msg, rpc_req.res_tx)))
             }
             NetworkNotification::RecvMessage(peer_id, msg) => {
-                let msg = TMessage::decode(msg.mdata)?;
+                let msg = TMessage::decode(msg.mdata.as_ref())?;
                 Ok(Event::Message((peer_id, msg)))
             }
         }))
@@ -278,7 +278,7 @@ impl<TMessage: Message + Default> NetworkSender<TMessage> {
 
         // wait for response and deserialize
         let res_data = res_rx.await??;
-        let res_msg = TMessage::decode(res_data)?;
+        let res_msg = TMessage::decode(res_data.as_ref())?;
         Ok(res_msg)
     }
 

state-synchronizer/Cargo.toml
@@ -29,7 +29,7 @@ libra-types = { path = "../types", version = "0.1.0" }
 vm-runtime = { path = "../language/vm/vm-runtime", version = "0.1.0" }
 
 [dev-dependencies]
-bytes = "0.4.12"
+bytes = "0.5"
 
 config-builder = { path = "../config/config-builder", version = "0.1.0" }
 libra-crypto = { path = "../crypto/crypto", version = "0.1.0" }

types/Cargo.toml
@@ -12,7 +12,8 @@ edition = "2018"
 [dependencies]
 bech32 = "0.6.0"
 byteorder = { version = "1.3.2", default-features = false }
-bytes = { version = "0.4.12", default-features = false }
+bytes = "0.4.12"
+bytes05 = { version = "0.5", package = "bytes" }
 chrono = { version = "0.4.7", default-features = false, features = ["clock"] }
 hex = { version = "0.3.2", default-features = false }
 itertools = { version = "0.8.0", default-features = false }

types/src/account_address.rs
@@ -4,7 +4,7 @@
 #![forbid(unsafe_code)]
 
 use bech32::{Bech32, FromBase32, ToBase32};
-use bytes::Bytes;
+use bytes05::Bytes;
 use failure::prelude::*;
 use hex;
 use libra_crypto::{
@@ -170,7 +170,7 @@ impl TryFrom<Bytes> for AccountAddress {
 
 impl From<AccountAddress> for Bytes {
     fn from(addr: AccountAddress) -> Bytes {
-        addr.0.as_ref().into()
+        Bytes::copy_from_slice(addr.0.as_ref())
     }
 }
 

consensus/src/chained_bft/event_processor_test.rs
@@ -714,7 +714,7 @@ fn process_block_retrieval() {
             .await;
         match rx1.await {
             Ok(Ok(bytes)) => {
-                let msg = ConsensusMsg::decode(bytes).unwrap();
+                let msg = ConsensusMsg::decode(bytes.as_ref()).unwrap();
                 let response = match msg.message {
                     Some(ConsensusMsg_oneof::RespondBlock(proto)) => {
                         BlockRetrievalResponse::<TestPayload>::try_from(proto)
@@ -740,7 +740,7 @@ fn process_block_retrieval() {
             .await;
         match rx2.await {
             Ok(Ok(bytes)) => {
-                let msg = ConsensusMsg::decode(bytes).unwrap();
+                let msg = ConsensusMsg::decode(bytes.as_ref()).unwrap();
                 let response = match msg.message {
                     Some(ConsensusMsg_oneof::RespondBlock(proto)) => {
                         BlockRetrievalResponse::<TestPayload>::try_from(proto)
@@ -765,7 +765,7 @@ fn process_block_retrieval() {
             .await;
         match rx3.await {
             Ok(Ok(bytes)) => {
-                let msg = ConsensusMsg::decode(bytes).unwrap();
+                let msg = ConsensusMsg::decode(bytes.as_ref()).unwrap();
                 let response = match msg.message {
                     Some(ConsensusMsg_oneof::RespondBlock(proto)) => {
                         BlockRetrievalResponse::<TestPayload>::try_from(proto)