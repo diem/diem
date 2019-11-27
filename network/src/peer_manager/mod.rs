3b8edf5ae1e381109b20795a031100c31f8e5207
Abhay Bothra <bothra90@users.noreply.github.com>
2019-11-26 16:56:20 -0800

    [network] Simplify check for peer actor event loop shutdown
    Closes: #1854
    Approved by: bmwill

--- network/src/peer_manager/mod.rs
+++ network/src/peer_manager/mod.rs
@@ -868,8 +868,7 @@ where
         let mut substream_rx = self.connection.listen_for_inbound().fuse();
         let mut pending_outbound_substreams = FuturesUnordered::new();
         let mut pending_inbound_substreams = FuturesUnordered::new();
-
-        loop {
+        while !self.shutdown {
             futures::select! {
                 maybe_req = self.requests_rx.next() => {
                     if let Some(request) = maybe_req {
@@ -922,10 +921,6 @@ where
                     // Do nothing since these futures have an output of "()"
                 },
                 complete => unreachable!(),
-            }
-
-            if self.shutdown {
-                break;
             }
         }
         debug!(
