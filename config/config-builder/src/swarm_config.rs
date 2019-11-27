9457ea3560eda54499e90ae5680ad893b108fc9e
David Wolinsky <davidiw@fb.com>
2019-11-27 13:12:56 -0800

    [config] Streamline config generation
    Historically there were two methods to generate configs but the nuances
    between the two paths is minimal. This commit unifies those paths
    showing that the two paths are extremely similar now. There was also no
    benefit in exposing the APIs for ConfigBuilder

--- config/config-builder/src/swarm_config.rs
+++ config/config-builder/src/swarm_config.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Convenience structs and functions for generating configuration for a swarm of libra nodes
-use failure::{bail, ensure, Result};
+use failure::{bail, ensure, format_err, Result};
 use libra_config::{
     config::{NodeConfig, PersistableConfig, RoleType},
     generator,
@@ -14,101 +14,6 @@ use libra_crypto::{
 use libra_types::transaction::Transaction;
 use std::path::{Path, PathBuf};
 use vm_genesis;
-
-pub fn full_node_swarm(
-    template: NodeConfig,
-    num_nodes: usize,
-    prune_seed_peers: bool,
-    is_ipv4: bool,
-    key_seed: Option<[u8; 32]>,
-    output_dir: &Path,
-    is_permissioned: bool,
-    upstream_peer_dir: PathBuf,
-) -> Result<Vec<PathBuf>> {
-    ensure!(num_nodes > 0, "Cannot build 0 NodeConfigs");
-    let mut upstream_peer = NodeConfig::load(&upstream_peer_dir.join("node.config.toml"))?;
-    let mut configs = generator::full_node_swarm(
-        &mut upstream_peer,
-        template,
-        num_nodes,
-        prune_seed_peers,
-        is_ipv4,
-        key_seed,
-        is_permissioned,
-    )?;
-    ensure!(
-        configs.len() == num_nodes,
-        format!(
-            "Expected {}, found {} NodeConfigs",
-            configs.len(),
-            num_nodes
-        )
-    );
-    let mut config_files = Vec::new();
-
-    for (index, config) in configs.iter_mut().enumerate() {
-        let node_dir = output_dir.join(format!("{}", index));
-        std::fs::create_dir_all(&node_dir).expect("unable to create config dir");
-
-        config.set_data_dir(node_dir.clone())?;
-        config.save(&PathBuf::from("node.config.toml"));
-        config_files.push(node_dir.join("node.config.toml"));
-    }
-
-    upstream_peer.save(&PathBuf::from("node.config.toml"));
-
-    Ok(config_files)
-}
-
-pub fn validator_swarm(
-    template: NodeConfig,
-    num_nodes: usize,
-    faucet_key: KeyPair<Ed25519PrivateKey, Ed25519PublicKey>,
-    prune_seed_peers: bool,
-    is_ipv4: bool,
-    key_seed: Option<[u8; 32]>,
-    output_dir: &Path,
-) -> Result<Vec<PathBuf>> {
-    ensure!(num_nodes > 0, "Cannot build 0 NodeConfigs");
-    let mut configs =
-        generator::validator_swarm(template, num_nodes, prune_seed_peers, is_ipv4, key_seed)?;
-    ensure!(
-        configs.len() == num_nodes,
-        format!(
-            "Expected {}, found {} NodeConfigs",
-            configs.len(),
-            num_nodes
-        )
-    );
-
-    let mut config_files = Vec::new();
-    let consensus_peers = &configs[0].consensus.consensus_peers;
-    let network = match configs[0].validator_network.as_ref() {
-        Some(network) => network,
-        None => bail!("Unable to read validator_network"),
-    };
-
-    let genesis = Transaction::UserTransaction(
-        vm_genesis::encode_genesis_transaction_with_validator(
-            &faucet_key.private_key,
-            faucet_key.public_key.clone(),
-            consensus_peers.get_validator_set(&network.network_peers),
-        )
-        .into_inner(),
-    );
-
-    for (index, config) in configs.iter_mut().enumerate() {
-        let node_dir = output_dir.join(format!("{}", index));
-        std::fs::create_dir_all(&node_dir).expect("unable to create config dir");
-
-        config.set_data_dir(node_dir.clone())?;
-        config_files.push(node_dir.join("node.config.toml"));
-        config.execution.genesis = Some(genesis.clone());
-        config.save(&PathBuf::from("node.config.toml"));
-    }
-
-    Ok(config_files)
-}
 
 pub struct SwarmConfigBuilder {
     num_nodes: usize,
@@ -210,59 +115,104 @@ impl SwarmConfigBuilder {
         self
     }
 
-    pub fn build(mut self) -> Result<Vec<PathBuf>> {
-        // verify required fields
-        let faucet_key_path = self.faucet_account_keypair_filepath.clone();
-        let faucet_key = self.faucet_account_keypair.take().unwrap_or_else(|| {
-            generate_keypair::load_key_from_file(
-                faucet_key_path.expect("Must provide faucet key file"),
-            )
-            .expect("Faucet account key is required to generate config")
-        });
+    pub fn build(&mut self) -> Result<Vec<PathBuf>> {
+        ensure!(self.num_nodes > 0, "Cannot build 0 NodeConfigs");
 
-        // generate all things needed for generation
+        let faucet_key = if let Some(keypair) = self.faucet_account_keypair.take() {
+            keypair
+        } else {
+            let faucet_key_path = self
+                .faucet_account_keypair_filepath
+                .as_ref()
+                .ok_or_else(|| format_err!("Must provide faucet keys or faucet key file"))?;
+            generate_keypair::load_key_from_file(faucet_key_path)?
+        };
+
         if !self.output_dir.is_dir() {
             if !self.output_dir.exists() {
-                // generate if doesn't exist
-                std::fs::create_dir(&self.output_dir).expect("Failed to create output dir");
+                std::fs::create_dir(&self.output_dir)?;
             }
-            assert!(
+            ensure!(
                 !self.output_dir.is_file(),
                 "Output-dir is a file, expecting a directory"
             );
         }
 
-        // read template
-        let template = if let Some(template_path) = self.template_path {
+        let template = if let Some(template_path) = &&self.template_path {
             NodeConfig::load_config(template_path)
         } else {
             NodeConfig::default()
         };
 
-        if self.role == RoleType::Validator {
-            validator_swarm(
+        let mut configs;
+        let genesis;
+
+        if self.role.is_validator() {
+            configs = generator::validator_swarm(
                 template,
                 self.num_nodes,
-                faucet_key,
                 self.force_discovery,
                 self.is_ipv4,
                 self.key_seed,
-                &self.output_dir,
-            )
+            )?;
+            let config = configs
+                .get(0)
+                .ok_or_else(|| format_err!("Missing NodeConfig"))?;
+            let consensus_peers = &config.consensus.consensus_peers;
+            let network = match config.validator_network.as_ref() {
+                Some(network) => network,
+                None => bail!("Unable to read validator_network"),
+            };
+
+            genesis = Some(Transaction::UserTransaction(
+                vm_genesis::encode_genesis_transaction_with_validator(
+                    &faucet_key.private_key,
+                    faucet_key.public_key.clone(),
+                    consensus_peers.get_validator_set(&network.network_peers),
+                )
+                .into_inner(),
+            ));
         } else {
-            full_node_swarm(
+            let upstream_peer_dir = self
+                .upstream_peer_dir
+                .as_ref()
+                .ok_or_else(|| format_err!("Missing upstream_peer_dir"))?;
+            let upstream_peer_dir = PathBuf::from(upstream_peer_dir);
+            let mut upstream_peer = NodeConfig::load(upstream_peer_dir.join("node.config.toml"))?;
+            genesis = upstream_peer.execution.genesis.clone();
+            configs = generator::full_node_swarm(
+                &mut upstream_peer,
                 template,
                 self.num_nodes,
                 self.force_discovery,
                 self.is_ipv4,
                 self.key_seed,
-                &self.output_dir,
                 self.is_permissioned,
-                PathBuf::from(
-                    self.upstream_peer_dir
-                        .expect("Upstream config path not set"),
-                ),
-            )
+            )?;
+            upstream_peer.save(&PathBuf::from("node.config.toml"));
         }
+
+        ensure!(
+            configs.len() == self.num_nodes,
+            format!(
+                "Expected {}, found {} NodeConfigs",
+                configs.len(),
+                self.num_nodes
+            )
+        );
+
+        let mut config_files = vec![];
+
+        for (index, config) in configs.iter_mut().enumerate() {
+            let node_dir = self.output_dir.join(format!("{}", index));
+            std::fs::create_dir_all(&node_dir).expect("unable to create config dir");
+
+            config.set_data_dir(node_dir.clone())?;
+            config_files.push(node_dir.join("node.config.toml"));
+            config.execution.genesis = genesis.clone();
+            config.save(&PathBuf::from("node.config.toml"));
+        }
+
+        Ok(config_files)
     }
 }
