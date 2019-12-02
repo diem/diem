2b09b8daade33815ada5342760053bb7856d0df9
Brandon Williams <bmwill@calibra.com>
2019-12-02 15:58:03 -0800

    grpc-helpers: use anyhow directly
    Closes: #1881
    Approved by: davidiw

--- common/grpc-helpers/src/lib.rs
+++ common/grpc-helpers/src/lib.rs
@@ -3,7 +3,7 @@
 
 #![forbid(unsafe_code)]
 
-use failure::{prelude::*, Result};
+use anyhow::{format_err, Error, Result};
 use futures::{compat::Future01CompatExt, future::Future, prelude::*};
 use futures_01::future::Future as Future01;
 use grpcio::{ChannelBuilder, EnvBuilder, ServerBuilder};
@@ -22,7 +22,7 @@ pub fn default_reply_error_logger<T: std::fmt::Debug>(e: T) {
     error!("Failed to reply error due to {:?}", e)
 }
 
-pub fn create_grpc_invalid_arg_status(method: &str, err: ::failure::Error) -> ::grpcio::RpcStatus {
+pub fn create_grpc_invalid_arg_status(method: &str, err: ::anyhow::Error) -> ::grpcio::RpcStatus {
     let msg = format!("Request failed {}", err);
     error!("{} failed with {}", method, &msg);
     ::grpcio::RpcStatus::new(::grpcio::RpcStatusCode::INVALID_ARGUMENT, Some(msg))
