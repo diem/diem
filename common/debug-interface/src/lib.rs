78b4ec55cc8aa0492be319b4ae432733d8baee6f
Brandon Williams <bmwill@calibra.com>
2019-12-02 15:35:52 -0800

    debug-interface: use anyhow directly

--- common/debug-interface/src/lib.rs
+++ common/debug-interface/src/lib.rs
@@ -4,7 +4,7 @@
 #![forbid(unsafe_code)]
 
 use crate::proto::{GetNodeDetailsRequest, NodeDebugInterfaceClient};
-use failure::prelude::*;
+use anyhow::{Context, Result};
 use grpcio::{ChannelBuilder, EnvBuilder};
 use std::{collections::HashMap, sync::Arc};
 
@@ -51,7 +51,7 @@ impl NodeDebugClient {
             .into_iter()
             .map(|(k, v)| match v.parse::<i64>() {
                 Ok(v) => Ok((k, v)),
-                Err(_) => Err(format_err!(
+                Err(_) => Err(anyhow::format_err!(
                     "Failed to parse stat value to i64 {}: {}",
                     &k,
                     &v
