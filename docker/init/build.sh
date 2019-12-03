88e82f9055fc7517553d1a618e21e6448930c10e
Michael Gorven <mgorven@calibra.com>
2019-12-03 10:17:30 -0800

    [docker] Add libra_init image build
    This will be used as an initContainer in k8s to setup configuration for
    validators/fullnodes. It contains `libra-config` to generate fullnode
    keys, and `envsubst` to interpolate configuration.
    
    Test Plan: Built it

---
+++ docker/init/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+set -e
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+PROXY=""
+if [ "$https_proxy" ]; then
+    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
+fi
+
+docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_init --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
