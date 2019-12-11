62736a05199be0d93b0df794e4b915413de14369
Qinfan Wu <wqfish@calibra.com>
2019-12-11 11:58:51 -0800

    [Storage] Better test for ledger store
    * Test more cases.
    * Separate tests for get_epoch_change_ledger_infos and get_epoch.

--- storage/libradb/src/ledger_store/ledger_info_test.rs
+++ storage/libradb/src/ledger_store/ledger_info_test.rs
@@ -4,8 +4,12 @@
 use super::*;
 use crate::{change_set::ChangeSet, LibraDB};
 use libra_tools::tempdir::TempPath;
-use libra_types::proptest_types::{AccountInfoUniverse, LedgerInfoWithSignaturesGen};
+use libra_types::{
+    proptest_types::{AccountInfoUniverse, LedgerInfoWithSignaturesGen},
+    transaction::Version,
+};
 use proptest::{collection::vec, prelude::*};
+use std::path::Path;
 
 fn arb_ledger_infos_with_sigs() -> impl Strategy<Value = Vec<LedgerInfoWithSignatures>> {
     (
@@ -13,74 +17,116 @@ fn arb_ledger_infos_with_sigs() -> impl Strategy<Value = Vec<LedgerInfoWithSigna
         vec((any::<LedgerInfoWithSignaturesGen>(), 1..10usize), 1..100),
     )
         .prop_map(|(mut universe, gens)| {
-            gens.into_iter()
+            let ledger_infos_with_sigs: Vec<_> = gens
+                .into_iter()
                 .map(|(ledger_info_gen, block_size)| {
                     ledger_info_gen.materialize(&mut universe, block_size)
                 })
-                .collect()
+                .collect();
+            assert_eq!(get_first_epoch(&ledger_infos_with_sigs), 0);
+            ledger_infos_with_sigs
         })
 }
 
+fn get_first_epoch(ledger_infos_with_sigs: &[LedgerInfoWithSignatures]) -> u64 {
+    ledger_infos_with_sigs
+        .first()
+        .unwrap()
+        .ledger_info()
+        .epoch()
+}
+
+fn get_last_epoch(ledger_infos_with_sigs: &[LedgerInfoWithSignatures]) -> u64 {
+    ledger_infos_with_sigs.last().unwrap().ledger_info().epoch()
+}
+
+fn get_last_version(ledger_infos_with_sigs: &[LedgerInfoWithSignatures]) -> Version {
+    ledger_infos_with_sigs
+        .last()
+        .unwrap()
+        .ledger_info()
+        .version()
+}
+
 proptest! {
-    #![proptest_config(ProptestConfig::with_cases(10))]
+    #![proptest_config(ProptestConfig::with_cases(20))]
 
     #[test]
-    fn test_ledger_info_put_get_verify(
-        ledger_infos_with_sigs in arb_ledger_infos_with_sigs()
+    fn test_get_epoch_change_ledger_infos(
+        (ledger_infos_with_sigs, start_epoch, end_epoch) in arb_ledger_infos_with_sigs()
+            .prop_flat_map(|ledger_infos_with_sigs| {
+                let first_epoch = get_first_epoch(&ledger_infos_with_sigs);
+                let last_epoch = get_last_epoch(&ledger_infos_with_sigs);
+                (
+                    Just(ledger_infos_with_sigs),
+                    first_epoch..=last_epoch,
+                )
+            })
+            .prop_flat_map(|(ledger_infos_with_sigs, start_epoch)| {
+                let last_epoch = get_last_epoch(&ledger_infos_with_sigs);
+                (
+                    Just(ledger_infos_with_sigs),
+                    Just(start_epoch),
+                    (start_epoch..=last_epoch),
+                )
+            })
     ) {
-        // set up
         let tmp_dir = TempPath::new();
-        let db = LibraDB::new(&tmp_dir);
-        let store = &db.ledger_store;
+        let db = set_up(&tmp_dir, &ledger_infos_with_sigs);
 
-        // write to DB
-        let mut cs = ChangeSet::new();
-        ledger_infos_with_sigs
-            .iter()
-            .map(|info| store.put_ledger_info(info, &mut cs))
-            .collect::<Result<Vec<_>>>()
+        let actual = db
+            .ledger_store
+            .get_epoch_change_ledger_infos(start_epoch, end_epoch)
             .unwrap();
-        store.db.write_schemas(cs.batch).unwrap();
-
-        // verify get_epoch_change_ledger_infos()
-        let epoch_ledgers: Vec<_> = ledger_infos_with_sigs
-            .iter()
-            .filter(|info| info.ledger_info().next_validator_set().is_some())
-            .cloned()
-            .collect();
-        let epoch_change_versions: Vec<_> = epoch_ledgers.iter().map(|li| {
-            let ledger_info = li.ledger_info();
-            (ledger_info.epoch(), ledger_info.version())
-        }).collect();
-        let start_epoch = ledger_infos_with_sigs.first().unwrap().ledger_info().epoch();
-        let latest_ledger_info = ledger_infos_with_sigs.last().unwrap().ledger_info();
-        let latest_epoch = latest_ledger_info.epoch();
-        let proof = store.get_epoch_change_ledger_infos(start_epoch, latest_epoch).unwrap();
-        let expected_proof = epoch_ledgers
-                .into_iter()
-                .filter(|li| li.ledger_info().epoch() < latest_epoch)
-                .collect::<Vec<_>>();
-        prop_assert_eq!(&proof, &expected_proof);
-
-        // verify get_epoch()
-        for pair in (&epoch_change_versions).windows(2) {
-            let prev = pair[0];
-            let prev_epoch = prev.0;
-            let prev_epoch_last_ver = prev.1;
-            let this = pair[1];
-            let this_epoch = this.0;
-            let this_epoch_last_ver = this.1;
-            prop_assert_eq!(prev_epoch + 1, this_epoch);
-
-            let mid_ver = (prev_epoch_last_ver + this_epoch_last_ver) / 2;
-            let expected_for_mid_ver = if mid_ver > prev_epoch_last_ver { this_epoch } else { prev_epoch };
-            prop_assert_eq!
-            (
-                store.get_epoch(mid_ver).unwrap(), expected_for_mid_ver,
-                "prev_epoch_last_ver: {}, this_epoch_last_ver: {}, mid_ver: {}",
-                    prev_epoch_last_ver, this_epoch_last_ver, mid_ver
-            );
-            prop_assert_eq!(store.get_epoch(this_epoch_last_ver).unwrap(), this_epoch, "{}", this_epoch_last_ver);
-        }
+        let expected = ledger_infos_with_sigs
+            .into_iter()
+            .filter(|ledger_info_with_sigs| {
+                let li = ledger_info_with_sigs.ledger_info();
+                start_epoch <= li.epoch()
+                    && li.epoch() < end_epoch
+                    && li.next_validator_set().is_some()
+            })
+            .collect::<Vec<_>>();
+        prop_assert_eq!(actual, expected);
     }
+
+    #[test]
+    fn test_get_epoch(
+        (ledger_infos_with_sigs, version) in arb_ledger_infos_with_sigs()
+            .prop_flat_map(|ledger_infos_with_sigs| {
+                let last_version = get_last_version(&ledger_infos_with_sigs);
+                (
+                    Just(ledger_infos_with_sigs),
+                    0..=last_version,
+                )
+            })
+    ) {
+        let tmp_dir = TempPath::new();
+        let db = set_up(&tmp_dir, &ledger_infos_with_sigs);
+
+        let actual = db.ledger_store.get_epoch(version).unwrap();
+        // Find the first LI that is at or after version.
+        let index = ledger_infos_with_sigs
+            .iter()
+            .position(|x| x.ledger_info().version() >= version)
+            .unwrap();
+        let expected = ledger_infos_with_sigs[index].ledger_info().epoch();
+        prop_assert_eq!(actual, expected);
+    }
+}
+
+fn set_up(path: &impl AsRef<Path>, ledger_infos_with_sigs: &[LedgerInfoWithSignatures]) -> LibraDB {
+    let db = LibraDB::new(path);
+    let store = &db.ledger_store;
+
+    // Write LIs to DB.
+    let mut cs = ChangeSet::new();
+    ledger_infos_with_sigs
+        .iter()
+        .map(|info| store.put_ledger_info(info, &mut cs))
+        .collect::<Result<Vec<_>>>()
+        .unwrap();
+    store.db.write_schemas(cs.batch).unwrap();
+
+    db
 }
