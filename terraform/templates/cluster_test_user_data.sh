4c0fce92d0056d41e5dcba3db867f19cb8edf067
Andrey Chursin <achursin@calibra.com>
2019-12-13 12:23:31 -0800

    [cluster-test] Additional terraform setup for cluster test instance
    Previously to properly setup cluster test instance some manual steps were needed after `terraform apply`, for example copying /libra_rsa key, copying `ct` binary and setting up some permissions
    
    With this change cluster test runner instance is ready to go after terraform apply is done
    
    Also it changes instance type family c5d->c5 and increases size of root block device for cluster test runner
    
    Closes: #2034
    Approved by: ankushagarwal

---
+++ terraform/templates/cluster_test_user_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+set -e
+
+cat > /libra_rsa <<'EOF'
+${ssh_key}
+EOF
+
+cat > /usr/local/bin/ct <<'EOF'
+${ct}
+EOF
+
+chmod +x /usr/local/bin/ct
+
+yum install git nano awscli -y
+usermod -a -G docker ec2-user
+
+cat > /etc/profile.d/libra_prompt.sh <<EOF
+export PS1="[\u@cluster-test-runner \w]$ "
+EOF
