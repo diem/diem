ec2885cf7e60ca5f41aaac8d8fc15b99afef4d69
Runtian Zhou <runtianz@calibra.com>
2019-12-16 11:01:17 +0800

    fixup! [vm] Create entry point for MoveVM

--- language/vm/vm-runtime/src/chain_state.rs
+++ language/vm/vm-runtime/src/chain_state.rs
@@ -1,3 +1,6 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::data_cache::{RemoteCache, TransactionDataCache};
 use libra_types::{
     access_path::AccessPath,
