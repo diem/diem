fc7a7cf376a2fed3ef7f769949ef3a6f0fb7ae65
Ankush Agarwal <ankushagarwal11@gmail.com>
2019-12-20 11:58:40 -0800

    [codebuild] Add a buildspec to build and push validator images
    Closes: #2086
    Approved by: opsguy

---
+++ docker/tag-and-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+# tag-and-push.sh is used tag an image with multiple tags and push them to the target repo
+# Example:
+# SOURCE=libra_validator:latest TARGET_REPO=1234567890.dkr.ecr.us-west-2.amazonaws.com/libra_cluster_test TARGET_TAGS=master,master_39cnja0 tag-and-push.sh
+
+set -e
+
+TARGET_TAGS_ARR=(${TARGET_TAGS//,/ })
+for TAG in "${TARGET_TAGS_ARR[@]}"
+do
+  TARGET=${TARGET_REPO}:${TAG}
+  echo "Tagging ${SOURCE} to ${TARGET}"
+  docker tag ${SOURCE} ${TARGET}
+  echo "Pushing ${SOURCE} to ${TARGET}"
+  docker push ${TARGET}
+done
