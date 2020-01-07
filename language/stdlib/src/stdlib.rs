9891be871c1deb9861c5f9a6d660ad757ba878d3
Brandon Williams <bmwill@calibra.com>
2020-01-07 12:09:27 -0800

    move-stdlib: convert to once_cell

--- language/stdlib/src/stdlib.rs
+++ language/stdlib/src/stdlib.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use ir_to_bytecode::parser::{ast::ModuleDefinition, parse_module};
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
 macro_rules! make_module_definition {
     ($source_path: literal) => {{
@@ -11,46 +11,45 @@ macro_rules! make_module_definition {
     }};
 }
 
-lazy_static! {
-    static ref ACCOUNT_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/libra_account.mvir");
-    static ref COIN_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/libra_coin.mvir");
-    static ref NATIVE_HASH_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/hash.mvir");
-    static ref SIGNATURE_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/signature.mvir");
-    static ref VALIDATOR_CONFIG_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/validator_config.mvir");
-    static ref LIBRA_SYSTEM_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/libra_system.mvir");
-    static ref ADDRESS_UTIL_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/address_util.mvir");
-    static ref U64_UTIL_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/u64_util.mvir");
-    static ref VECTOR_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/vector.mvir");
-    static ref BYTEARRAY_UTIL_MODULE: ModuleDefinition =
-        make_module_definition!("../modules/bytearray_util.mvir");
-    static ref GAS_SCHEDULE: ModuleDefinition = make_module_definition!("../modules/gas_schedule.mvir");
-    static ref MODULE_DEFS: Vec<&'static ModuleDefinition> = {
-        // Note: a module can depend on earlier modules in the list, but not vice versa. Don't try
-        // to rearrange without considering this!
-        vec![
-            &*ADDRESS_UTIL_MODULE,
-            &*BYTEARRAY_UTIL_MODULE,
-            &*COIN_MODULE,
-            &*NATIVE_HASH_MODULE,
-            &*SIGNATURE_MODULE,
-            &*U64_UTIL_MODULE,
-            &*VECTOR_MODULE,
-            &*VALIDATOR_CONFIG_MODULE,
-            &*GAS_SCHEDULE, // depends on Vector
-            &*ACCOUNT_MODULE, // depends on LibraCoin, Event, AddressUtil, BytearrayUtil, U64Util
-            &*LIBRA_SYSTEM_MODULE, // depends on LibraAccount, ValidatorConfig
-        ]
-    };
-}
+static ACCOUNT_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/libra_account.mvir"));
+static COIN_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/libra_coin.mvir"));
+static NATIVE_HASH_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/hash.mvir"));
+static SIGNATURE_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/signature.mvir"));
+static VALIDATOR_CONFIG_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/validator_config.mvir"));
+static LIBRA_SYSTEM_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/libra_system.mvir"));
+static ADDRESS_UTIL_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/address_util.mvir"));
+static U64_UTIL_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/u64_util.mvir"));
+static VECTOR_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/vector.mvir"));
+static BYTEARRAY_UTIL_MODULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/bytearray_util.mvir"));
+static GAS_SCHEDULE: Lazy<ModuleDefinition> =
+    Lazy::new(|| make_module_definition!("../modules/gas_schedule.mvir"));
+static MODULE_DEFS: Lazy<Vec<&'static ModuleDefinition>> = Lazy::new(|| {
+    // Note: a module can depend on earlier modules in the list, but not vice versa. Don't try
+    // to rearrange without considering this!
+    vec![
+        &*ADDRESS_UTIL_MODULE,
+        &*BYTEARRAY_UTIL_MODULE,
+        &*COIN_MODULE,
+        &*NATIVE_HASH_MODULE,
+        &*SIGNATURE_MODULE,
+        &*U64_UTIL_MODULE,
+        &*VECTOR_MODULE,
+        &*VALIDATOR_CONFIG_MODULE,
+        &*GAS_SCHEDULE,        // depends on Vector
+        &*ACCOUNT_MODULE,      // depends on LibraCoin, Event, AddressUtil, BytearrayUtil, U64Util
+        &*LIBRA_SYSTEM_MODULE, // depends on LibraAccount, ValidatorConfig
+    ]
+});
 
 pub fn account_module() -> ModuleDefinition {
     ACCOUNT_MODULE.clone()
