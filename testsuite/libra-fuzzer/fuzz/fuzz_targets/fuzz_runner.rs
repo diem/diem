2d3a0b28704b84178f43410087da171da1c3b808
Brandon Williams <bmwill@calibra.com>
2020-01-07 11:14:52 -0800

    libra-fuzzer: convert to once_cell
    Closes: #2170
    Approved by: sunshowers

--- testsuite/libra-fuzzer/fuzz/fuzz_targets/fuzz_runner.rs
+++ testsuite/libra-fuzzer/fuzz/fuzz_targets/fuzz_runner.rs
@@ -3,23 +3,24 @@
 
 #![no_main]
 
-use lazy_static::lazy_static;
 use libfuzzer_sys::fuzz_target;
 use libra_fuzzer::FuzzTarget;
+use once_cell::sync::Lazy;
 use std::process;
 
-lazy_static! {
-    static ref FUZZ_TARGET: FuzzTarget = {
-        match FuzzTarget::from_env() {
-            Ok(target) => target,
-            Err(err) => {
-                // lazy_static behaves poorly with panics, so abort here.
-                eprintln!("*** [fuzz_runner] Error while determining fuzz target: {}", err);
-                process::abort();
-            }
+static FUZZ_TARGET: Lazy<FuzzTarget> = Lazy::new(|| {
+    match FuzzTarget::from_env() {
+        Ok(target) => target,
+        Err(err) => {
+            // Lazy behaves poorly with panics, so abort here.
+            eprintln!(
+                "*** [fuzz_runner] Error while determining fuzz target: {}",
+                err
+            );
+            process::abort();
         }
-    };
-}
+    }
+});
 
 fuzz_target!(|data: &[u8]| {
     FUZZ_TARGET.fuzz(data);
