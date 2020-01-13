5770faec1cc8f91c24d496423fd1582b0fb72ce8
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-01-13 13:23:17 -0800

    [build] Create a wrapper script for aws codebuild

--- docker/tag-and-push.sh
+++ docker/tag-and-push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
-# tag-and-push.sh is used tag an image with multiple tags and push them to the target repo
+# tag-and-push.sh is used tag an image with multiple tags and push them to the target repo. Use ":" as the separator
+# between multiple tags
 # Example:
-# SOURCE=libra_validator:latest TARGET_REPO=1234567890.dkr.ecr.us-west-2.amazonaws.com/libra_cluster_test TARGET_TAGS=master,master_39cnja0 tag-and-push.sh
+# SOURCE=libra_validator:latest TARGET_REPO=1234567890.dkr.ecr.us-west-2.amazonaws.com/libra_cluster_test TARGET_TAGS=master:master_39cnja0 tag-and-push.sh
 
 set -e
 
-TARGET_TAGS_ARR=(${TARGET_TAGS//,/ })
+TARGET_TAGS_ARR=(${TARGET_TAGS//:/ })
 for TAG in "${TARGET_TAGS_ARR[@]}"
 do
   TARGET=${TARGET_REPO}:${TAG}
