2402503fbdc3b2c7b16807a84f43de0eda995d29
David Wolinsky <davidiw@fb.com>
2020-01-13 11:50:26 -0800

    [consensus] refactor SafetyRulesManager
    Some of this helper code can be reused without needing to generate an
    object, so separating it accordingly.
    
    Closes: #2208
    Approved by: davidiw

--- consensus/safety-rules/src/safety_rules_manager.rs
+++ consensus/safety-rules/src/safety_rules_manager.rs
@@ -14,6 +14,43 @@ use libra_config::config::{NodeConfig, SafetyRulesBackend, SafetyRulesService};
 use libra_types::crypto_proxies::ValidatorSigner;
 use std::sync::{Arc, RwLock};
 
+pub fn extract_service_inputs(
+    config: &mut NodeConfig,
+) -> (ValidatorSigner, Box<dyn PersistentStorage>) {
+    let private_key = config
+        .test
+        .as_mut()
+        .expect("Missing test config")
+        .consensus_keypair
+        .as_mut()
+        .expect("Missing consensus keypair in test config")
+        .take_private()
+        .expect("Failed to take Consensus private key, key absent or already read");
+
+    let author = config
+        .validator_network
+        .as_ref()
+        .expect("Missing validator network")
+        .peer_id;
+
+    let validator_signer = ValidatorSigner::new(author, private_key);
+
+    let storage = match &config.consensus.safety_rules.backend {
+        SafetyRulesBackend::InMemoryStorage => InMemoryStorage::default_storage(),
+        SafetyRulesBackend::OnDiskStorage(config) => {
+            if config.default {
+                OnDiskStorage::default_storage(config.path())
+                    .expect("Unable to allocate SafetyRules storage")
+            } else {
+                OnDiskStorage::new_storage(config.path())
+                    .expect("Unable to instantiate SafetyRules storage")
+            }
+        }
+    };
+
+    (validator_signer, storage)
+}
+
 enum SafetyRulesWrapper<T> {
     Local(Arc<RwLock<SafetyRules<T>>>),
     Serializer(Arc<RwLock<SerializerService<T>>>),
@@ -26,38 +63,8 @@ pub struct SafetyRulesManager<T> {
 
 impl<T: Payload> SafetyRulesManager<T> {
     pub fn new(config: &mut NodeConfig) -> Self {
-        let private_key = config
-            .test
-            .as_mut()
-            .expect("Missing test config")
-            .consensus_keypair
-            .as_mut()
-            .expect("Missing consensus keypair in test config")
-            .take_private()
-            .expect("Failed to take Consensus private key, key absent or already read");
-
-        let author = config
-            .validator_network
-            .as_ref()
-            .expect("Missing validator network")
-            .peer_id;
-
-        let validator_signer = ValidatorSigner::new(author, private_key);
-
+        let (validator_signer, storage) = extract_service_inputs(config);
         let sr_config = &config.consensus.safety_rules;
-        let storage = match &sr_config.backend {
-            SafetyRulesBackend::InMemoryStorage => InMemoryStorage::default_storage(),
-            SafetyRulesBackend::OnDiskStorage(config) => {
-                if config.default {
-                    OnDiskStorage::default_storage(config.path())
-                        .expect("Unable to allocate SafetyRules storage")
-                } else {
-                    OnDiskStorage::new_storage(config.path())
-                        .expect("Unable to instantiate SafetyRules storage")
-                }
-            }
-        };
-
         match sr_config.service {
             SafetyRulesService::Local => Self::new_local(storage, validator_signer),
             SafetyRulesService::Serializer => Self::new_serializer(storage, validator_signer),
@@ -111,6 +118,7 @@ impl<T: Payload> SafetyRulesManager<T> {
                 Box::new(SerializerClient::new(serializer_service.clone()))
             }
             SafetyRulesWrapper::Thread(thread) => Box::new(thread.client()),
+            _ => panic!("Unable to retrieve client for this service"),
         }
     }
 }
