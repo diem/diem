bd150d263fd1ae693d302c8c98b58825e286999a
Qinfan Wu <wqfish@calibra.com>
2020-01-14 11:59:10 -0800

    [Storage] Use LCS to replace protobuf encoding
    There is some inconsistency in storage encoding. This change replaces
    all protobuf usage with LCS.
    
    Fixes https://github.com/libra/libra/issues/2166

--- storage/libradb/src/schema/transaction_info/mod.rs
+++ storage/libradb/src/schema/transaction_info/mod.rs
@@ -15,14 +15,11 @@
 use crate::schema::TRANSACTION_INFO_CF_NAME;
 use anyhow::{ensure, Result};
 use byteorder::{BigEndian, ReadBytesExt};
-use libra_prost_ext::MessageExt;
 use libra_types::transaction::{TransactionInfo, Version};
-use prost::Message;
 use schemadb::{
     define_schema,
     schema::{KeyCodec, ValueCodec},
 };
-use std::convert::TryInto;
 use std::mem::size_of;
 
 define_schema!(
@@ -49,12 +46,11 @@ impl KeyCodec<TransactionInfoSchema> for Version {
 
 impl ValueCodec<TransactionInfoSchema> for TransactionInfo {
     fn encode_value(&self) -> Result<Vec<u8>> {
-        let event: libra_types::proto::types::TransactionInfo = self.clone().into();
-        Ok(event.to_vec()?)
+        lcs::to_bytes(self).map_err(Into::into)
     }
 
     fn decode_value(data: &[u8]) -> Result<Self> {
-        libra_types::proto::types::TransactionInfo::decode(data)?.try_into()
+        lcs::from_bytes(data).map_err(Into::into)
     }
 }
 
--- storage/libradb/src/schema/transaction_info/test.rs
+++ storage/libradb/src/schema/transaction_info/test.rs
@@ -2,18 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::*;
-use libra_crypto::HashValue;
-use libra_types::{transaction::TransactionInfo, vm_error::StatusCode};
+use libra_types::transaction::{TransactionInfo, Version};
+use proptest::prelude::*;
 use schemadb::schema::assert_encode_decode;
 
-#[test]
-fn test_encode_decode() {
-    let txn_info = TransactionInfo::new(
-        HashValue::random(),
-        HashValue::random(),
-        HashValue::random(),
-        7,
-        StatusCode::EXECUTED,
-    );
-    assert_encode_decode::<TransactionInfoSchema>(&0u64, &txn_info);
+proptest! {
+    #[test]
+    fn test_encode_decode(version in any::<Version>(), txn_info in any::<TransactionInfo>()) {
+        assert_encode_decode::<TransactionInfoSchema>(&version, &txn_info);
+    }
 }
