916a8c4098d3c8be0b9dc6649ccfe0a85902ee9a
Christina Lee <ccelee@fb.com>
2020-01-29 08:42:44 -0800

    [dashboard] fix admission control panel in Prometheus overview dashboard

---
+++ admission_control/admission-control-service/src/counters.rs
@@ -0,0 +1,15 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use once_cell::sync::Lazy;
+use prometheus::IntGaugeVec;
+
+/// Cumulative number of requests that admission control service receives
+pub static REQUESTS: Lazy<IntGaugeVec> = Lazy::new(|| {
+    register_int_gauge_vec!(
+        "libra_admission_control_service_requests_count",
+        "Cumulative number of requests that admission control receives",
+        &["type"] // type of request: "submit_transaction", "update_to_latest_ledger"
+    )
+    .unwrap()
+});
