6faa819f34d5c6f7507daed42d9685b61e0d3468
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-01-30 10:40:08 -0800

    [terraform][cluster-test] Add Flamegraph repo to cluster-test host

--- terraform/templates/ec2_user_data.sh
+++ terraform/templates/ec2_user_data.sh
@@ -34,4 +34,8 @@ cat > /etc/profile.d/libra_prompt.sh <<EOF
 export PS1="[\u:validator@\h \w]$ "
 EOF
 
-yum -y install ngrep tcpdump perf gdb nmap-ncat strace htop sysstat tc
+yum -y install ngrep tcpdump perf gdb nmap-ncat strace htop sysstat tc git
+
+if [ ! -d /usr/local/etc/FlameGraph ] ; then
+    git clone --depth 1 https://github.com/brendangregg/FlameGraph /usr/local/etc/FlameGraph
+fi
