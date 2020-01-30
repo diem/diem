1020024bfe7223a64a60809c259c3ac18156e2d0
Qinfan Wu <wqfish@calibra.com>
2020-01-29 17:43:57 -0800

    fixup! [Executor/Storage] Add a simple benchmark

--- executor/src/utils.rs
+++ executor/src/utils.rs
@@ -1,3 +1,6 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::{ExecutedTrees, Executor};
 use libra_config::config::NodeConfig;
 use std::sync::Arc;
