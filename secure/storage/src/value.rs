c3c0d8145dba054cefe0ce9890e8d5e88728541f
David Wolinsky <davidiw@fb.com>
2020-01-31 16:44:21 -0800

    [secure] add base64 encoding for storage values
    This enables us to store these object into restful services that won't
    like raw bytes. Note, this shows that this depends on the lcs issue fix
    in the previous commit.
    
    Closes: #2401
    Approved by: zekun000

--- secure/storage/src/value.rs
+++ secure/storage/src/value.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::error::Error;
+use base64;
 use libra_crypto::ed25519::Ed25519PrivateKey;
 use serde::{Deserialize, Serialize};
 
@@ -27,5 +28,41 @@ impl Value {
         } else {
             Err(Error::UnexpectedValueType)
         }
+    }
+
+    pub fn from_base64(input: &str) -> Result<Value, Error> {
+        let bytes = base64::decode(input)?;
+        let value = lcs::from_bytes(&bytes)?;
+        Ok(value)
+    }
+
+    pub fn to_base64(&self) -> Result<String, Error> {
+        let bytes = lcs::to_bytes(self)?;
+        Ok(base64::encode(&bytes))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use libra_crypto::Uniform;
+    use rand::{rngs::StdRng, SeedableRng};
+
+    #[test]
+    fn u64() {
+        let value = Value::U64(12341);
+        let base64 = value.to_base64().unwrap();
+        let out_value = Value::from_base64(&base64).unwrap();
+        assert_eq!(value, out_value);
+    }
+
+    #[test]
+    fn ed25519_private_key() {
+        let mut rng = StdRng::from_seed([13u8; 32]);
+        let value = Ed25519PrivateKey::generate_for_testing(&mut rng);
+        let value = Value::Ed25519PrivateKey(value);
+        let base64 = value.to_base64().unwrap();
+        let out_value = Value::from_base64(&base64).unwrap();
+        assert_eq!(value, out_value);
     }
 }
