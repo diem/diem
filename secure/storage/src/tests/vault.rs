766c3e58bfc70daf41d933648f13e5c13cd9a7f4
David Wolinsky <davidiw@fb.com>
2020-02-03 10:39:42 -0800

    [vault] implement policy framework
    See https://github.com/libra/libra/issues/2331 for more information on
    the goals.
    This leverages the WebAPI from Vault as best as possible to map our
    desired behavior into practical behavior.
    Note, Vault does not support incremental updates to policy, so we have
    to simulate that by first reading the policy, amending it internally,
    and then recreating it.
    Vault also has a default policy applied to everyone called default, so
    when anyone is selected, we add that to the default policy.
    
    Closes: #2406
    Approved by: zekun000

--- secure/storage/src/tests/vault.rs
+++ secure/storage/src/tests/vault.rs
@@ -2,13 +2,101 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{tests::suite, vault::VaultStorage};
+use crate::{Capability, Error, Identity, Permission, Policy, Storage, Value};
 
 #[ignore]
 #[test]
 fn vault() {
     let host = "http://localhost:8200".to_string();
     let token = "root_token".to_string();
-    let storage = VaultStorage::new(host, token);
+    let mut storage = VaultStorage::new(host.clone(), token);
+    storage.reset().unwrap();
+
+    // @TODO davidiw, the following needs to be made generic but right now the creation of a token
+    // / service is very backend specific.
+    let reader: String = "reader".to_string();
+    let writer: String = "writer".to_string();
+
+    let anyone = Policy::public();
+    let root = Policy::new(vec![]);
+    let partial = Policy::new(vec![
+        Permission::new(Identity::User(reader.clone()), vec![Capability::Read]),
+        Permission::new(
+            Identity::User(writer.clone()),
+            vec![Capability::Read, Capability::Write],
+        ),
+    ]);
+    let full = Policy::new(vec![
+        Permission::new(
+            Identity::User(reader.clone()),
+            vec![Capability::Read, Capability::Write],
+        ),
+        Permission::new(
+            Identity::User(writer.clone()),
+            vec![Capability::Read, Capability::Write],
+        ),
+    ]);
+
+    // Initialize data and policies
+
+    storage.create("anyone", Value::U64(1), &anyone).unwrap();
+    storage.create("root", Value::U64(2), &root).unwrap();
+    storage.create("partial", Value::U64(3), &partial).unwrap();
+    storage.create("full", Value::U64(4), &full).unwrap();
+
+    // Verify initial reading works correctly
+
+    assert_eq!(storage.get("anyone"), Ok(Value::U64(1)));
+    assert_eq!(storage.get("root"), Ok(Value::U64(2)));
+    assert_eq!(storage.get("partial"), Ok(Value::U64(3)));
+    assert_eq!(storage.get("full"), Ok(Value::U64(4)));
+
+    let writer_token = storage.client.create_token(vec![&writer]).unwrap();
+    let mut writer = VaultStorage::new(host.clone(), writer_token);
+    assert_eq!(writer.get("anyone"), Ok(Value::U64(1)));
+    assert_eq!(writer.get("root"), Err(Error::PermissionDenied));
+    assert_eq!(writer.get("partial"), Ok(Value::U64(3)));
+    assert_eq!(writer.get("full"), Ok(Value::U64(4)));
+
+    let reader_token = storage.client.create_token(vec![&reader]).unwrap();
+    let mut reader = VaultStorage::new(host, reader_token);
+    assert_eq!(reader.get("anyone"), Ok(Value::U64(1)));
+    assert_eq!(reader.get("root"), Err(Error::PermissionDenied));
+    assert_eq!(reader.get("partial"), Ok(Value::U64(3)));
+    assert_eq!(reader.get("full"), Ok(Value::U64(4)));
+
+    // Attempt writes followed by reads for correctness
+
+    writer.set("anyone", Value::U64(5)).unwrap();
+    assert_eq!(
+        writer.set("root", Value::U64(6)),
+        Err(Error::PermissionDenied)
+    );
+    writer.set("partial", Value::U64(7)).unwrap();
+    writer.set("full", Value::U64(8)).unwrap();
+
+    assert_eq!(storage.get("anyone"), Ok(Value::U64(5)));
+    assert_eq!(storage.get("root"), Ok(Value::U64(2)));
+    assert_eq!(storage.get("partial"), Ok(Value::U64(7)));
+    assert_eq!(storage.get("full"), Ok(Value::U64(8)));
+
+    reader.set("anyone", Value::U64(9)).unwrap();
+    assert_eq!(
+        reader.set("root", Value::U64(10)),
+        Err(Error::PermissionDenied)
+    );
+    assert_eq!(
+        reader.set("partial", Value::U64(11)),
+        Err(Error::PermissionDenied)
+    );
+    reader.set("full", Value::U64(12)).unwrap();
+
+    assert_eq!(storage.get("anyone"), Ok(Value::U64(9)));
+    assert_eq!(storage.get("root"), Ok(Value::U64(2)));
+    assert_eq!(storage.get("partial"), Ok(Value::U64(7)));
+    assert_eq!(storage.get("full"), Ok(Value::U64(12)));
+
+    // Now begin the generic test
     storage.reset().unwrap();
     suite::run_test_suite(Box::new(storage), "VaultStorage");
 }
