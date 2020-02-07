13d436db5bc4930760ae3c13213761756cecb3f9
Qinfan Wu <wqfish@calibra.com>
2020-02-06 16:43:49 -0800

    [Storage] Format some code using rustfmt
    Just comment out the macros, put the test body in a dummy function,
    format using rustfmt, and put the macros back...

--- storage/storage-service/src/storage_service_test.rs
+++ storage/storage-service/src/storage_service_test.rs
@@ -38,28 +38,25 @@ proptest! {
 
     #[test]
     fn test_storage_service_basic(blocks in arb_blocks_to_commit().no_shrink()) {
-        let(mut rt, _tmp_dir, read_client, write_client) =
+        let (mut rt, _tmp_dir, read_client, write_client) =
             start_test_storage_with_read_write_client();
 
         let mut version = 0;
         let mut all_accounts = BTreeMap::new();
 
         for (txns_to_commit, ledger_info_with_sigs) in &blocks {
-            rt.block_on(write_client
-                .save_transactions(txns_to_commit.clone(),
-                                   version, /* first_version */
-                                   Some(ledger_info_with_sigs.clone()),
-                )).unwrap();
+            rt.block_on(write_client.save_transactions(
+                txns_to_commit.clone(),
+                version, /* first_version */
+                Some(ledger_info_with_sigs.clone()),
+            ))
+            .unwrap();
             version += txns_to_commit.len() as u64;
             let mut account_states = HashMap::new();
             // Get the ground truth of account states.
-            txns_to_commit
-                .iter()
-                .for_each(|txn_to_commit|
-                          account_states.extend(txn_to_commit
-                                                .account_states()
-                                                .clone())
-                );
+            txns_to_commit.iter().for_each(|txn_to_commit| {
+                account_states.extend(txn_to_commit.account_states().clone())
+            });
 
             // Record all account states.
             for (address, blob) in account_states.iter() {
@@ -68,30 +65,32 @@ proptest! {
 
             let account_state_request_items = account_states
                 .keys()
-                .map(|address| RequestItem::GetAccountState{
-                    address: *address,
-                }).collect::<Vec<_>>();
+                .map(|address| RequestItem::GetAccountState { address: *address })
+                .collect::<Vec<_>>();
             let (
                 response_items,
                 response_ledger_info_with_sigs,
                 _validator_change_proof,
                 _ledger_consistency_proof,
-            ) = rt.block_on(read_client
-                .update_to_latest_ledger(0, account_state_request_items)).unwrap();
+            ) = rt
+                .block_on(read_client.update_to_latest_ledger(0, account_state_request_items))
+                .unwrap();
             for ((address, blob), response_item) in zip_eq(account_states, response_items) {
-                    match response_item {
-                        ResponseItem::GetAccountState {
-                            account_state_with_proof,
-                        } => {
-                            prop_assert_eq!(&Some(blob), &account_state_with_proof.blob);
-                            prop_assert!(account_state_with_proof.verify(
+                match response_item {
+                    ResponseItem::GetAccountState {
+                        account_state_with_proof,
+                    } => {
+                        prop_assert_eq!(&Some(blob), &account_state_with_proof.blob);
+                        prop_assert!(account_state_with_proof
+                            .verify(
                                 response_ledger_info_with_sigs.ledger_info(),
                                 version - 1,
                                 address,
-                            ).is_ok())
-                        }
-                        _ => unreachable!()
+                            )
+                            .is_ok())
                     }
+                    _ => unreachable!(),
+                }
             }
 
             // Assert ledger info.
@@ -99,7 +98,9 @@ proptest! {
         }
 
         // Check state backup for all account states.
-        let stream = rt.block_on(read_client.backup_account_state(version - 1)).unwrap();
+        let stream = rt
+            .block_on(read_client.backup_account_state(version - 1))
+            .unwrap();
         let backup_responses = rt.block_on(stream.collect::<Vec<_>>());
         for ((hash, blob), response) in zip_eq(all_accounts, backup_responses) {
             let resp = response.unwrap();
