fbcfd0f3b6864d6b73574aedeffe21f1a227be6d
David Wolinsky <davidiw@fb.com>
2020-02-06 17:39:47 -0800

    [docker] Improve safety rules docker
    - Make a run file that starts safety rules for local purposes
    - Add appropriate mkdir for safety rules so that it can actually start
    
    Usage:
    
    in validator-dynamic:
    - set nodes="1"
    - add "-e CFG_SAFETY_RULES_ADDR="172.18.0.2:8888" \" to the docker run
    at the end of the file
    
    Then call run on both and the magic happens.
    Safety rules was able to restart without any issues.

---
+++ docker/safety-rules/docker-run-dynamic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+set -ex
+
+declare -a params
+if [ -n "${CFG_BASE_CONFIG}" ]; then # Path to base config
+	    echo "${CFG_BASE_CONFIG}" > /opt/libra/etc/base.config.toml
+	    params+="-t /opt/libra/etc/base.config.toml "
+fi
+if [ -n "${CFG_NODE_INDEX}" ]; then
+	    params+="-i ${CFG_NODE_INDEX} "
+fi
+if [ -n "${CFG_NUM_VALIDATORS}" ]; then # Total number of nodes in this network
+	    params+="-n ${CFG_NUM_VALIDATORS} "
+fi
+if [ -n "${CFG_SEED}" ]; then # Random seed to use
+	    params+="-s ${CFG_SEED} "
+fi
+if [ -n "${CFG_SAFETY_RULES_LISTEN_ADDR}" ]; then
+    params+="--safety-rules-addr ${CFG_SAFETY_RULES_LISTEN_ADDR}"
+fi
+
+/opt/libra/bin/config-builder safety-rules \
+    --data-dir /opt/libra/data/common \
+    --output-dir /opt/libra/etc/ \
+    ${params[@]}
+
+exec /opt/libra/bin/safety-rules /opt/libra/etc/node.config.toml
