72886386d5614687ba17d05af133fc07f4b43eda
Abhay Bothra <bothra90@users.noreply.github.com>
2020-02-10 16:08:39 -0800

    [network] Move framing read/write utility library to netcore/transport

--- network/netcore/src/negotiate/outbound.rs
+++ network/netcore/src/negotiate/outbound.rs
@@ -1,9 +1,9 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::negotiate::{
+use crate::{
     framing::{read_u16frame, write_u16frame},
-    PROTOCOL_INTERACTIVE, PROTOCOL_NOT_SUPPORTED, PROTOCOL_SELECT,
+    negotiate::{PROTOCOL_INTERACTIVE, PROTOCOL_NOT_SUPPORTED, PROTOCOL_SELECT},
 };
 use bytes::BytesMut;
 use futures::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
@@ -93,10 +93,12 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::negotiate::{
+    use crate::{
         framing::{read_u16frame, write_u16frame},
-        outbound::{negotiate_outbound_interactive, negotiate_outbound_select},
-        PROTOCOL_INTERACTIVE, PROTOCOL_NOT_SUPPORTED, PROTOCOL_SELECT,
+        negotiate::{
+            outbound::{negotiate_outbound_interactive, negotiate_outbound_select},
+            PROTOCOL_INTERACTIVE, PROTOCOL_NOT_SUPPORTED, PROTOCOL_SELECT,
+        },
     };
     use bytes::BytesMut;
     use futures::{executor::block_on, future::join, io::AsyncWriteExt};
