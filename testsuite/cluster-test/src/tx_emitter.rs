d7095a369399f933e5286133a5b661e18a6eff8d
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-02-25 13:32:57 -0800

    [cluster-test] Increase TXN_MAX_WAIT by 20 seconds
    Closes: #2699
    Approved by: andll

--- testsuite/cluster-test/src/tx_emitter.rs
+++ testsuite/cluster-test/src/tx_emitter.rs
@@ -475,7 +475,7 @@ async fn query_sequence_numbers(
 const MAX_GAS_AMOUNT: u64 = 1_000_000;
 const GAS_UNIT_PRICE: u64 = 0;
 const TXN_EXPIRATION_SECONDS: i64 = 50;
-const TXN_MAX_WAIT: Duration = Duration::from_secs(TXN_EXPIRATION_SECONDS as u64 + 10);
+const TXN_MAX_WAIT: Duration = Duration::from_secs(TXN_EXPIRATION_SECONDS as u64 + 30);
 const LIBRA_PER_NEW_ACCOUNT: u64 = 1_000_000;
 
 fn gen_submit_transaction_request(
