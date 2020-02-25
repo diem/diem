9225033968eedd88c89d72520e273b640515376a
David Wolinsky <davidiw@fb.com>
2020-02-24 21:43:41 -0800

    [network] Move health checker network into health checker
    To make networking as effective as possible, it makes sense to host the
    interface into networking in the code itself. This allows for binding
    the types sent across the network into local types. While this isn't
    done in this PR, this is part of the future goals. This PR is just a
    bridge to that future.
    
    Closes: #2708
    Approved by: zekun000

--- network/src/validator_network/mod.rs
+++ network/src/validator_network/mod.rs
@@ -22,7 +22,6 @@ use pin_project::pin_project;
 use prost::Message;
 use std::{default::Default, marker::PhantomData, pin::Pin, time::Duration};
 
-pub mod health_checker;
 pub mod network_builder;
 
 #[cfg(test)]
@@ -30,10 +29,6 @@ mod test;
 #[cfg(any(feature = "testing", test))]
 pub mod test_network;
 
-// Public re-exports
-pub use health_checker::{
-    HealthCheckerNetworkEvents, HealthCheckerNetworkSender, HEALTH_CHECKER_RPC_PROTOCOL,
-};
 use libra_types::PeerId;
 
 /// Events received by network clients in a validator
