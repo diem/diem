7317320a51c802326c1669f4bba5a644a1eb926d
Philip Hayes <philiphayes@calibra.com>
2020-03-01 12:07:34 -0800

    [types] use pub fields in `DiscoveryInfo` so unpacking is easier
    Closes: #2760
    Approved by: davidiw

--- types/src/discovery_info.rs
+++ types/src/discovery_info.rs
@@ -19,54 +19,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct DiscoveryInfo {
     // The validator's account address.
-    account_address: AccountAddress,
+    pub account_address: AccountAddress,
     // This static pubkey is used in the connection handshake to authenticate
     // this particular validator.
-    validator_network_identity_pubkey: X25519StaticPublicKey,
+    pub validator_network_identity_pubkey: X25519StaticPublicKey,
     // Other validators can dial this validator at this multiaddress.
-    validator_network_address: Multiaddr,
+    pub validator_network_address: Multiaddr,
     // This static pubkey is used in the connection handshake to authenticate
     // this validator's full nodes.
-    fullnodes_network_identity_pubkey: X25519StaticPublicKey,
+    pub fullnodes_network_identity_pubkey: X25519StaticPublicKey,
     // Other full nodes and clients can dial this validator's full nodes at this
     // multiaddress.
-    fullnodes_network_address: Multiaddr,
-}
-
-impl DiscoveryInfo {
-    pub fn new(
-        account_address: AccountAddress,
-        validator_network_identity_pubkey: X25519StaticPublicKey,
-        validator_network_address: Multiaddr,
-        fullnodes_network_identity_pubkey: X25519StaticPublicKey,
-        fullnodes_network_address: Multiaddr,
-    ) -> Self {
-        DiscoveryInfo {
-            account_address,
-            validator_network_identity_pubkey,
-            validator_network_address,
-            fullnodes_network_identity_pubkey,
-            fullnodes_network_address,
-        }
-    }
-
-    pub fn account_address(&self) -> &AccountAddress {
-        &self.account_address
-    }
-
-    pub fn validator_network_identity_pubkey(&self) -> &X25519StaticPublicKey {
-        &self.validator_network_identity_pubkey
-    }
-
-    pub fn validator_network_address(&self) -> &Multiaddr {
-        &self.validator_network_address
-    }
-
-    pub fn fullnodes_network_identity_pubkey(&self) -> &X25519StaticPublicKey {
-        &self.fullnodes_network_identity_pubkey
-    }
-
-    pub fn fullnodes_network_address(&self) -> &Multiaddr {
-        &self.fullnodes_network_address
-    }
+    pub fullnodes_network_address: Multiaddr,
 }
