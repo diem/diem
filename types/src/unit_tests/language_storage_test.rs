85c5d2d1d8d596061f38a548d8fa9cc57462b360
Rain <rain1@calibra.com>
2020-03-05 17:32:41 -0800

    [types] remove unused language_storage.proto
    Closes: #2825
    Approved by: hermanventer

--- types/src/unit_tests/language_storage_test.rs
+++ types/src/unit_tests/language_storage_test.rs
@@ -2,15 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{language_storage::ModuleId, test_helpers::assert_canonical_encode_decode};
-use libra_prost_ext::test_helpers::assert_protobuf_encode_decode;
 use proptest::prelude::*;
 
 proptest! {
-    #[test]
-    fn test_module_id_protobuf_roundtrip(module_id in any::<ModuleId>()) {
-        assert_protobuf_encode_decode::<crate::proto::types::ModuleId, ModuleId>(&module_id);
-    }
-
     #[test]
     fn test_module_id_canonical_roundtrip(module_id in any::<ModuleId>()) {
         assert_canonical_encode_decode(module_id);
