35a787fba073d6f4a97ad884429b6a8bbf20ccd8
David Wolinsky <davidiw@fb.com>
2020-03-15 21:55:58 -0700

    [key-manager] build rotation transaction builder
    This builds the rotation transaction framework and a means to test it
    end to end. This also validates recent work on retrieving
    ValidatorConfig and ValidatorSet from storage.
    
    The one thing I wonder about writing this is that there are a lot of
    places in the code that want executor and/or storage but none of them
    have them as conveniently as this. For example, executor tests heavily
    depend on the grpc interface, yuck. JSON-RPC built a compatible storage
    engine rather than use LibraDB which is really cool but won't work with
    executor as it currently stands -- at least not in an easy way. So here
    we have yet another approach. I guess once we start cleaning up
    executor, we could drag the JSON-RPC storage into that code base and
    really simplify testing of these things.
    
    Closes: #2966
    Approved by: zekun000

--- secure/key-manager/src/lib.rs
+++ secure/key-manager/src/lib.rs
@@ -3,5 +3,50 @@
 
 #![forbid(unsafe_code)]
 
+use libra_crypto::{
+    ed25519::{Ed25519PrivateKey, Ed25519PublicKey},
+    PrivateKey,
+};
+use libra_transaction_scripts;
+use libra_types::{
+    account_address::AccountAddress,
+    transaction::{RawTransaction, Script, Transaction, TransactionArgument},
+};
+use std::time::{Duration, SystemTime};
+
 #[cfg(test)]
 mod tests;
+
+const GAS_UNIT_PRICE: u64 = 0;
+const MAX_GAS_AMOUNT: u64 = 400_000;
+const TXN_EXPIRATION: u64 = 100;
+
+fn expiration_time(until: u64) -> Duration {
+    let now = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    std::time::Duration::from_secs(now + until)
+}
+
+pub fn build_transaction(
+    sender: AccountAddress,
+    seq_id: u64,
+    signing_key: &Ed25519PrivateKey,
+    new_key: &Ed25519PublicKey,
+) -> Transaction {
+    let script = Script::new(
+        libra_transaction_scripts::ROTATE_CONSENSUS_PUBKEY_TXN.clone(),
+        vec![TransactionArgument::U8Vector(new_key.to_bytes().to_vec())],
+    );
+    let raw_txn = RawTransaction::new_script(
+        sender,
+        seq_id,
+        script,
+        MAX_GAS_AMOUNT,
+        GAS_UNIT_PRICE,
+        expiration_time(TXN_EXPIRATION),
+    );
+    let signed_txn = raw_txn.sign(signing_key, signing_key.public_key()).unwrap();
+    Transaction::UserTransaction(signed_txn.into_inner())
+}
