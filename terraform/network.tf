f86d1c47aeaccbd647253c9a0fa32252e811db7e
Sherry Xiao <sherryxiao@fb.com>
2020-03-17 09:58:55 -0700

    [tf] Allow faucet to talk to JSON-RPC port
    Closes: #2994
    Approved by: phoenix-antigravity

--- terraform/network.tf
+++ terraform/network.tf
@@ -125,12 +125,12 @@ resource "aws_security_group_rule" "validator-ac" {
 }
 
 resource "aws_security_group_rule" "validator-jsonrpc" {
-  security_group_id        = aws_security_group.validator.id
-  type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.jsonrpc-lb.id
+  security_group_id = aws_security_group.validator.id
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.testnet.cidr_block]
 }
 
 resource "aws_security_group" "jsonrpc-lb" {
