abfad10df178e496ea93fed3ce157c4f3aa44d52
Mathieu Baudet <mathieubaudet@calibra.com>
2020-03-17 17:34:19 -0700

    [generate-format] add CLI option for new serde tracer
    Closes: #3060
    Approved by: bmwill

--- testsuite/generate-format/src/main.rs
+++ testsuite/generate-format/src/main.rs
@@ -1,24 +1,24 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use lcs;
+use clap::{App, Arg};
 use libra_types::{contract_event, transaction};
-use serde_reflection::Tracer;
-
 use proptest::{
     prelude::*,
     test_runner::{Config, FileFailurePersistence, TestRunner},
 };
+use serde_reflection::Tracer;
 use serde_yaml;
 use std::sync::{Arc, Mutex};
 
-fn main() {
+fn add_proptest_serialization_tracing(tracer: Tracer) -> Tracer {
     let mut runner = TestRunner::new(Config {
         failure_persistence: Some(Box::new(FileFailurePersistence::Off)),
         ..Config::default()
     });
 
-    let tracer = Arc::new(Mutex::new(Tracer::new(lcs::is_human_readable())));
+    // Wrap the tracer for (hypothetical) concurrent accesses.
+    let tracer = Arc::new(Mutex::new(tracer));
 
     runner
         .run(&any::<transaction::Transaction>(), |v| {
@@ -34,13 +34,36 @@ fn main() {
         })
         .unwrap();
 
-    // Pull a finalized registry out of the Arc-mutex-ed tracer.
-    let registry = Arc::try_unwrap(tracer)
-        .unwrap()
-        .into_inner()
-        .unwrap()
-        .registry()
+    // Recover the Arc-mutex-ed tracer.
+    Arc::try_unwrap(tracer).unwrap().into_inner().unwrap()
+}
+
+fn add_deserialization_tracing(mut tracer: Tracer) -> Tracer {
+    tracer.trace_type::<transaction::Transaction>().unwrap();
+    tracer
+        .trace_type::<contract_event::ContractEvent>()
         .unwrap();
+    tracer
+}
+
+fn main() {
+    let matches = App::new("Libra format generator")
+        .about("Trace serde (de)serialization to generate format descriptions for Libra types")
+        .arg(
+            Arg::with_name("with_deserialize")
+                .long("with_deserialize")
+                .help("Trace deserialization instead of serialization"),
+        )
+        .get_matches();
+
+    let mut tracer = Tracer::new(lcs::is_human_readable());
+
+    tracer = add_proptest_serialization_tracing(tracer);
+    if matches.is_present("with_deserialize") {
+        tracer = add_deserialization_tracing(tracer);
+    }
+
+    let registry = tracer.registry().unwrap();
     let output = serde_yaml::to_string(&registry).unwrap();
     println!("{}", output);
 }
