116903c3e84befbabef0da465a724207b8fd9ff7
Christina Lee <ccelee@fb.com>
2020-03-24 09:06:50 -0700

    [on-chain configs] add Rust interface for representing/reading on-chain configs
    Closes: #3108
    Approved by: zekun000

--- language/libra-vm/src/on_chain_configs.rs
+++ language/libra-vm/src/on_chain_configs.rs
@@ -1,10 +1,8 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use libra_types::on_chain_config::{access_path_for_config, VMPublishingOption};
-use move_core_types::identifier::Identifier;
+use libra_types::on_chain_config::{OnChainConfig, VMPublishingOption};
 use move_vm_state::data_cache::RemoteCache;
-use once_cell::sync::Lazy;
 
 #[derive(Debug, Clone)]
 pub(crate) struct VMConfig {
@@ -13,20 +11,10 @@ pub(crate) struct VMConfig {
 
 impl VMConfig {
     pub fn load_on_chain_config(state_view: &dyn RemoteCache) -> Option<Self> {
-        Some(VMConfig {
-            publishing_options: load_script_whitelist(state_view)?,
-        })
+        if let Ok(publishing_options) = VMPublishingOption::fetch_config(&Box::new(state_view)) {
+            Some(VMConfig { publishing_options })
+        } else {
+            None
+        }
     }
-}
-
-static SCRIPT_WHITELIST: Lazy<Identifier> =
-    Lazy::new(|| Identifier::new("ScriptWhitelist").unwrap());
-
-fn load_script_whitelist(state_view: &dyn RemoteCache) -> Option<VMPublishingOption> {
-    let access_path = access_path_for_config(SCRIPT_WHITELIST.clone());
-    state_view
-        .get(&access_path)
-        .ok()?
-        .and_then(|bytes| lcs::from_bytes::<Vec<u8>>(&bytes).ok())
-        .and_then(|bytes| lcs::from_bytes::<VMPublishingOption>(&bytes).ok())
 }
