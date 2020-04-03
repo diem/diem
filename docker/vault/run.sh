28fdeb7f5ea28d777aa7775b8744b45d14dcf84d
Michael Gorven <mgorven@calibra.com>
2020-04-03 10:24:23 -0700

    [docker] Automatically enable Vault transit engine

--- docker/vault/run.sh
+++ docker/vault/run.sh
@@ -3,14 +3,25 @@
 # SPDX-License-Identifier: Apache-2.0
 set -ex
 
+IP="172.18.0.3"
+PORT="8200"
+TOKEN="root_token"
+
 docker network create --subnet 172.18.0.0/24 testnet || true
 
 docker run \
     --cap-add=IPC_LOCK \
-    -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' \
-    -e 'VAULT_DEV_ROOT_TOKEN_ID=root_token' \
-    --ip 172.18.0.3 \
+    -e "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:$PORT" \
+    -e "VAULT_DEV_ROOT_TOKEN_ID=$TOKEN" \
+    --ip "$IP" \
     --network testnet \
-    --publish 8200:8200 \
+    --publish "$PORT:$PORT" \
     --detach \
     vault
+
+docker run \
+    -e "VAULT_ADDR=http://$IP:$PORT" \
+    -e "VAULT_TOKEN=$TOKEN" \
+    --network testnet \
+    --entrypoint vault \
+    vault secrets enable transit
