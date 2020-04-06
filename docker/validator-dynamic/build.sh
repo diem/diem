e362b41d014e3b1c4d8762bb60dfe70e80c0e9d6
Young Yang Liauw <youngyl@fb.com>
2020-04-06 15:11:54 -0700

    [docker] update dep in validator_dynamic build
    Build the dependent base image first in validator_dynamic docker build.
    
    Closes: #3278
    Approved by: sausagee

--- docker/validator-dynamic/build.sh
+++ docker/validator-dynamic/build.sh
@@ -10,4 +10,8 @@ if [ "$https_proxy" ]; then
 	    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
 fi
 
+# Build validator base image first
+docker build -f $DIR/../validator/Dockerfile $DIR/../.. --tag libra_e2e --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
+
+# Build validator_dynamic
 docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_validator_dynamic --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
