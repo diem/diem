eb544c4ca94958ed2be4e629462d00b5276951dc
Rain <rain1@calibra.com>
2020-04-09 13:30:21 -0700

    [x] split lint engine into its own crate
    This provides a clear separation between the statebetween the stateless lint
    engine and the stateful x.
    
    Closes: #3351
    Approved by: rexhoffman

--- devtools/x/src/lint/whitespace.rs
+++ devtools/x/src/lint/whitespace.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::lint::engine::prelude::*;
+use x_lint::prelude::*;
 
 #[derive(Clone, Copy, Debug)]
 pub(super) struct EofNewline;
