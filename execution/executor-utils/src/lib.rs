0bc0eb991a94ab117aafc32a7512e640b2acd9ad
Aaron Gao <gzh@fb.com>
2020-04-10 17:14:28 -0700

    [execution] return Arc<dyn DbReader> when start storage_server and executor
    Closes: #3435
    Approved by: msmouse

--- execution/executor-utils/src/lib.rs
+++ execution/executor-utils/src/lib.rs
@@ -7,16 +7,20 @@ pub mod test_helpers;
 use executor::{db_bootstrapper::bootstrap_db_if_empty, Executor};
 use libra_config::{config::NodeConfig, utils::get_genesis_txn};
 use libra_vm::LibraVM;
+use std::sync::Arc;
 use storage_client::SyncStorageClient;
+use storage_interface::DbReader;
 use storage_service::{init_libra_db, start_storage_service_with_db};
 use tokio::runtime::Runtime;
 
-pub fn create_storage_service_and_executor(config: &NodeConfig) -> (Runtime, Executor<LibraVM>) {
+pub fn create_storage_service_and_executor(
+    config: &NodeConfig,
+) -> (Arc<dyn DbReader>, Runtime, Executor<LibraVM>) {
     let (db, db_rw) = init_libra_db(config);
     bootstrap_db_if_empty::<LibraVM>(&db_rw, get_genesis_txn(config).unwrap()).unwrap();
 
-    let rt = start_storage_service_with_db(config, db);
+    let rt = start_storage_service_with_db(config, db.clone());
     let executor = Executor::new(SyncStorageClient::new(&config.storage.address).into());
 
-    (rt, executor)
+    (db, rt, executor)
 }
