840811f5f45dc5bb90e33f4f9707a6fbafcea412
Christina Lee <ccelee@fb.com>
2020-04-13 18:55:19 -0700

    [mempool] create shared_mempool folder in crate
    Closes: #3380
    Approved by: phoenix-antigravity

---
+++ mempool/src/shared_mempool/mod.rs
@@ -0,0 +1,11 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod network;
+mod runtime;
+pub(crate) mod types;
+pub use runtime::bootstrap;
+#[cfg(feature = "fuzzing")]
+pub(crate) use runtime::start_shared_mempool;
+mod coordinator;
+mod tasks;
