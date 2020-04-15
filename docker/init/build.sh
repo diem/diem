7ea0db0984e97c34c9ff3b55bc5b746923d9897c
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-04-14 22:08:05 -0700

    Add retry logic for docker build scripts
    Closes: #3429
    Approved by: sausagee

--- docker/init/build.sh
+++ docker/init/build.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
-set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
@@ -10,4 +9,13 @@ if [ "$https_proxy" ]; then
     PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
 fi
 
-docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_init --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
+for ((i = 0; i < 30; i++)); do
+  docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_init --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
+  if [[ $? -eq 0 ]]; then
+    echo "docker build succeeded"
+    exit 0
+  fi
+  echo "docker build failed. retrying in 10 secs."
+  sleep 10
+done
+exit 1
