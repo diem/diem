4991a8a4f844174f6cc6547c98093d1a93d58817
Zekun Li <zekun@calibra.com>
2020-04-15 14:40:31 -0700

    [consensus] remove ChainedBftSMR
    The only responsibility of ChainedBftSMR today is to spawn the event loop and
    hold the runtime, so we can just return the runtime as other services.
    
    There's two types of tests there
    1. test expected behavior upon certain event which belongs to unit test in event_processor
    2. test overall system works (with fault tolerance) which belongs to e2e tests in smoke tests

--- consensus/src/chained_bft/mod.rs
+++ consensus/src/chained_bft/mod.rs
@@ -4,15 +4,12 @@
 mod consensusdb;
 
 mod block_storage;
-pub mod chained_bft_smr;
-mod network;
+pub mod network;
 pub mod network_interface;
 
 pub mod epoch_manager;
 pub mod persistent_liveness_storage;
 
-#[cfg(test)]
-mod chained_bft_smr_test;
 #[cfg(test)]
 mod network_tests;
 
--- consensus/src/consensus_provider.rs
+++ consensus/src/consensus_provider.rs
@@ -3,41 +3,31 @@
 
 use crate::{
     chained_bft::{
-        chained_bft_smr::{ChainedBftSMR, ChainedBftSMRInput},
+        epoch_manager::EpochManager,
+        network::NetworkTask,
         network_interface::{ConsensusNetworkEvents, ConsensusNetworkSender},
         persistent_liveness_storage::StorageWriteProxy,
     },
+    counters,
     state_computer::ExecutionProxy,
     txn_manager::MempoolProxy,
+    util::time_service::ClockTimeService,
 };
-use anyhow::Result;
 use channel::libra_channel;
 use executor::Executor;
 use futures::channel::mpsc;
 use libra_config::config::NodeConfig;
+use libra_logger::prelude::*;
 use libra_mempool::ConsensusRequest;
 use libra_types::{on_chain_config::OnChainConfigPayload, transaction::SignedTransaction};
 use libra_vm::LibraVM;
-use safety_rules::SafetyRulesManager;
 use state_synchronizer::StateSyncClient;
 use std::sync::{Arc, Mutex};
 use storage_interface::DbReader;
+use tokio::runtime::{self, Runtime};
 
-/// Public interface to a consensus protocol.
-pub trait ConsensusProvider {
-    /// Spawns new threads, starts the consensus operations (retrieve txns, consensus protocol,
-    /// execute txns, commit txns, update txn status in the mempool, etc).
-    /// The function returns after consensus has recovered its initial state,
-    /// and has established the required connections (e.g., to mempool and
-    /// executor).
-    fn start(&mut self) -> Result<()>;
-
-    /// Stop the consensus operations. The function returns after graceful shutdown.
-    fn stop(&mut self);
-}
-
-/// Helper function to create a ConsensusProvider based on configuration
-pub fn make_consensus_provider(
+/// Helper function to start consensus based on configuration and return the runtime
+pub fn start_consensus(
     node_config: &mut NodeConfig,
     network_sender: ConsensusNetworkSender<Vec<SignedTransaction>>,
     network_events: ConsensusNetworkEvents<Vec<SignedTransaction>>,
@@ -46,23 +36,38 @@ pub fn make_consensus_provider(
     consensus_to_mempool_sender: mpsc::Sender<ConsensusRequest>,
     libra_db: Arc<dyn DbReader>,
     reconfig_events: libra_channel::Receiver<(), OnChainConfigPayload>,
-) -> Box<dyn ConsensusProvider> {
+) -> Runtime {
+    let runtime = runtime::Builder::new()
+        .thread_name("consensus-")
+        .threaded_scheduler()
+        .enable_all()
+        .build()
+        .expect("Failed to create Tokio runtime!");
     let storage = Arc::new(StorageWriteProxy::new(node_config, libra_db));
     let txn_manager = Box::new(MempoolProxy::new(consensus_to_mempool_sender));
     let state_computer = Arc::new(ExecutionProxy::new(executor, state_sync_client));
-    let input = ChainedBftSMRInput {
-        network_sender,
-        network_events,
-        safety_rules_manager: SafetyRulesManager::new(node_config),
-        state_computer,
-        txn_manager,
-        storage,
-        config: node_config.consensus.clone(),
-        reconfig_events,
-    };
+    let time_service = Arc::new(ClockTimeService::new(runtime.handle().clone()));
 
-    Box::new(ChainedBftSMR::new(
-        node_config.validator_network.as_ref().unwrap().peer_id,
-        input,
-    ))
+    let (timeout_sender, timeout_receiver) =
+        channel::new(1_024, &counters::PENDING_PACEMAKER_TIMEOUTS);
+    let (self_sender, self_receiver) = channel::new(1_024, &counters::PENDING_SELF_MESSAGES);
+
+    let epoch_mgr = EpochManager::new(
+        node_config,
+        time_service,
+        self_sender,
+        network_sender,
+        timeout_sender,
+        txn_manager,
+        state_computer,
+        storage,
+    );
+
+    let (network_task, network_receiver) = NetworkTask::new(network_events, self_receiver);
+
+    runtime.spawn(network_task.start());
+    runtime.spawn(epoch_mgr.start(timeout_receiver, network_receiver, reconfig_events));
+
+    debug!("Consensus started.");
+    runtime
 }
