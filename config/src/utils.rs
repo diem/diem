f9ba92cec6cea90c7003674a0ab3417dfff04800
Philip Hayes <philiphayes@calibra.com>
2020-04-23 17:08:30 -0700

    [network-address] convert config, config-builder
    Closes: #3573
    Approved by: metajack

--- config/src/utils.rs
+++ config/src/utils.rs
@@ -3,13 +3,13 @@
 
 use crate::config::NodeConfig;
 use get_if_addrs::get_if_addrs;
+use libra_network_address::{NetworkAddress, Protocol};
 use libra_types::{transaction::Transaction, PeerId};
-use parity_multiaddr::{Multiaddr, Protocol};
 use serde::{Serialize, Serializer};
 use std::{
     collections::{BTreeMap, HashMap},
     hash::BuildHasher,
-    net::{IpAddr, TcpListener, TcpStream},
+    net::{TcpListener, TcpStream},
 };
 
 /// Return an ephemeral, available port. On unix systems, the port returned will be in the
@@ -42,33 +42,22 @@ fn get_ephemeral_port() -> ::std::io::Result<u16> {
 }
 
 /// Extracts one local non-loopback IP address, if one exists. Otherwise returns None.
-pub fn get_local_ip() -> Option<Multiaddr> {
+pub fn get_local_ip() -> Option<NetworkAddress> {
     get_if_addrs().ok().and_then(|if_addrs| {
         if_addrs
             .into_iter()
             .find(|if_addr| !if_addr.is_loopback())
-            .and_then(|if_addr| {
-                let mut addr = Multiaddr::empty();
-                match if_addr.ip() {
-                    IpAddr::V4(a) => {
-                        addr.push(Protocol::Ip4(a));
-                    }
-                    IpAddr::V6(a) => {
-                        addr.push(Protocol::Ip6(a));
-                    }
-                }
-                Some(addr)
-            })
+            .map(|if_addr| NetworkAddress::from(Protocol::from(if_addr.ip())))
     })
 }
 
-pub fn get_available_port_in_multiaddr(is_ipv4: bool) -> Multiaddr {
-    let mut addr = Multiaddr::empty();
-    if is_ipv4 {
-        addr.push(Protocol::Ip4("0.0.0.0".parse().unwrap()));
+pub fn get_available_port_in_multiaddr(is_ipv4: bool) -> NetworkAddress {
+    let ip_proto = if is_ipv4 {
+        Protocol::Ip4("0.0.0.0".parse().unwrap())
     } else {
-        addr.push(Protocol::Ip6("::1".parse().unwrap()));
-    }
+        Protocol::Ip6("::1".parse().unwrap())
+    };
+    let mut addr = NetworkAddress::from(ip_proto);
     addr.push(Protocol::Tcp(get_available_port()));
     addr
 }
