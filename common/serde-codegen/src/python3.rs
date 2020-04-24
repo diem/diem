e1c8043cee4e25e7083a6b8b0a299a0206b2d44d
Mathieu Baudet <mathieubaudet@calibra.com>
2020-04-24 11:33:20 -0700

    [serde codegen] backport @yucong's changes from calibra

--- common/serde-codegen/src/python3.rs
+++ common/serde-codegen/src/python3.rs
@@ -13,10 +13,10 @@ pub fn output(registry: &RegistryOwned) {
 
 fn output_preambule() {
     println!(
-        r#"
+        r#"# pyre-ignore-all-errors
 from dataclasses import dataclass
 import numpy as np
-from typing import *
+import typing
 "#
     );
 }
@@ -24,31 +24,31 @@ from typing import *
 fn quote_type(format: &Format) -> String {
     use Format::*;
     match format {
-        TypeName(x) => format!("'{}'", x), // Need quotes because of circular dependencies.
+        TypeName(x) => format!("\"{}\"", x), // Need quotes because of circular dependencies.
         Unit => "None".into(),
         Bool => "np.bool".into(),
         I8 => "np.int8".into(),
         I16 => "np.int16".into(),
         I32 => "np.int32".into(),
         I64 => "np.int64".into(),
-        I128 => "Tuple(np.int64, np.int64)".into(),
+        I128 => "typing.Tuple[np.int64, np.int64]".into(),
         U8 => "np.uint8".into(),
         U16 => "np.uint16".into(),
         U32 => "np.uint32".into(),
         U64 => "np.uint64".into(),
-        U128 => "Tuple(np.uint64, np.uint64)".into(),
+        U128 => "typing.Tuple[np.uint64, np.uint64]".into(),
         F32 => "np.float32".into(),
         F64 => "np.float64".into(),
         Char => "char".into(),
         Str => "str".into(),
         Bytes => "bytes".into(),
 
-        Option(format) => format!("Optional[{}]", quote_type(format)),
-        Seq(format) => format!("Sequence[{}]", quote_type(format)),
-        Map { key, value } => format!("Dict[{}, {}]", quote_type(key), quote_type(value)),
-        Tuple(formats) => format!("Tuple[{}]", quote_types(formats)),
+        Option(format) => format!("typing.Optional[{}]", quote_type(format)),
+        Seq(format) => format!("typing.Sequence[{}]", quote_type(format)),
+        Map { key, value } => format!("typing.Dict[{}, {}]", quote_type(key), quote_type(value)),
+        Tuple(formats) => format!("typing.Tuple[{}]", quote_types(formats)),
         TupleArray { content, size } => format!(
-            "Tuple[{}]",
+            "typing.Tuple[{}]",
             quote_types(&vec![content.as_ref().clone(); *size])
         ), // Sadly, there are no fixed-size arrays in python.
 
@@ -75,11 +75,11 @@ fn output_variant(base: &str, name: &str, index: u32, variant: &VariantFormat) {
     use VariantFormat::*;
     match variant {
         Unit => println!(
-            "@dataclass\nclass _{}_{}({}):\n    INDEX={}\n",
+            "@dataclass\nclass _{}_{}({}):\n    INDEX = {}\n",
             base, name, base, index,
         ),
         NewType(format) => println!(
-            "@dataclass\nclass _{}_{}({}):\n    INDEX={}\n    value: {}\n",
+            "@dataclass\nclass _{}_{}({}):\n    INDEX = {}\n    value: {}\n",
             base,
             name,
             base,
@@ -87,7 +87,7 @@ fn output_variant(base: &str, name: &str, index: u32, variant: &VariantFormat) {
             quote_type(format)
         ),
         Tuple(formats) => println!(
-            "@dataclass\nclass _{}_{}({}):\n    INDEX={}\n    value: Tuple[{}]\n",
+            "@dataclass\nclass _{}_{}({}):\n    INDEX = {}\n    value: typing.Tuple[{}]\n",
             base,
             name,
             base,
@@ -96,7 +96,7 @@ fn output_variant(base: &str, name: &str, index: u32, variant: &VariantFormat) {
         ),
         Struct(fields) => {
             println!(
-                "@dataclass\nclass _{}_{}({}):\n    INDEX={}",
+                "@dataclass\nclass _{}_{}({}):\n    INDEX = {}",
                 base, name, base, index
             );
             output_fields(4, fields);
@@ -128,7 +128,7 @@ fn output_container(name: &str, format: &ContainerFormat) {
             quote_type(format)
         ),
         TupleStruct(formats) => println!(
-            "@dataclass\nclass {}:\n    value: Tuple[{}]\n",
+            "@dataclass\nclass {}:\n    value: typing.Tuple[{}]\n",
             name,
             quote_types(formats)
         ),
@@ -142,13 +142,13 @@ fn output_container(name: &str, format: &ContainerFormat) {
             output_variants(name, variants);
             output_variant_aliases(name, variants);
             println!(
-                "{}.VARIANTS = [{}]\n",
+                "{}.VARIANTS = [\n{}]\n",
                 name,
                 variants
                     .iter()
-                    .map(|(_, v)| format!("{}.{}", name, v.name))
+                    .map(|(_, v)| format!("    {}.{},\n", name, v.name))
                     .collect::<Vec<_>>()
-                    .join(", ")
+                    .join("")
             );
         }
     }
