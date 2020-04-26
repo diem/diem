22e9451bc25153fbcca8146516ab838df80688f7
David Wolinsky <davidiw@fb.com>
2020-04-26 01:12:45 -0700

    [language] eliminate a special use-case of config generator
    Now it accesses a proper ValidatorSet in its own memory.
    Note: This requires us to have a more proper Ed25519 key due to checks
    during deserialization...
    
    Closes: #3608
    Approved by: zekun000

--- language/e2e-tests/src/tests/validator_set_management.rs
+++ language/e2e-tests/src/tests/validator_set_management.rs
@@ -24,11 +24,11 @@ fn validator_add() {
 
     let txn = register_validator_txn(
         new_validator.account(),
+        vec![255; 32],
+        vec![254; 32],
+        vec![253; 32],
         vec![],
-        vec![],
-        vec![],
-        vec![],
-        vec![],
+        vec![252; 32],
         vec![],
         0,
     );
@@ -51,18 +51,18 @@ fn validator_rotate_key() {
     let mut executor = FakeExecutor::from_genesis_file();
     let genesis_account = Account::new_association();
     let new_validator = AccountData::new(1_000_000, 0);
-    executor.new_block();
 
     // create a FakeExecutor with a genesis from file
     executor.add_account_data(&new_validator);
+    executor.new_block();
 
     let txn = register_validator_txn(
         new_validator.account(),
+        vec![255; 32],
+        vec![254; 32],
+        vec![253; 32],
         vec![],
-        vec![],
-        vec![],
-        vec![],
-        vec![],
+        vec![252; 32],
         vec![],
         0,
     );
@@ -82,7 +82,7 @@ fn validator_rotate_key() {
     executor.apply_write_set(output.write_set());
     executor.new_block();
 
-    let txn = rotate_consensus_pubkey_txn(new_validator.account(), vec![1], 1);
+    let txn = rotate_consensus_pubkey_txn(new_validator.account(), vec![251; 32], 1);
     let output = executor.execute_transaction(txn);
     assert_eq!(
         output.status(),
