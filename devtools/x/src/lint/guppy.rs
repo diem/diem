d2a0b77da0c75df6c766ce888daff2bc32baada5
Rain <rain1@calibra.com>
2020-05-07 11:54:09 -0700

    [x] move direct-dep-dups linter to x
    This currently lives in cargo-guppy + circleci, but is better expressed as a guppy lint.
    
    The error looks like:
    
    ```
    [ERROR] [direct-dep-dups] [project]: duplicate direct dependency 'ureq':
      * 0.12.1 (libra-secure-json-rpc, cli, libra-secure-push-metrics)
      * 1.0.0 (libra-vault-client)
    ```
    
    Closes: #3787
    Approved by: bmwill

--- devtools/x/src/lint/guppy.rs
+++ devtools/x/src/lint/guppy.rs
@@ -4,7 +4,11 @@
 //! Project and package linters that run queries on guppy.
 
 use crate::config::EnforcedAttributesConfig;
-use std::{collections::HashMap, ffi::OsStr};
+use guppy::Version;
+use std::{
+    collections::{BTreeMap, HashMap},
+    ffi::OsStr,
+};
 use x_lint::prelude::*;
 
 /// Ban certain crates from being used as direct dependencies.
@@ -232,6 +236,57 @@ impl PackageLinter for IrrelevantBuildDeps {
 
         if !metadata.has_build_script() && has_build_dep {
             out.write(LintLevel::Error, "build dependencies but no build script");
+        }
+
+        Ok(RunStatus::Executed)
+    }
+}
+
+/// Ensure that packages within the workspace only depend on one version of a third-party crate.
+#[derive(Debug)]
+pub struct DirectDepDups;
+
+impl Linter for DirectDepDups {
+    fn name(&self) -> &'static str {
+        "direct-dep-dups"
+    }
+}
+
+impl ProjectLinter for DirectDepDups {
+    fn run<'l>(
+        &self,
+        ctx: &ProjectContext<'l>,
+        out: &mut LintFormatter<'l, '_>,
+    ) -> Result<RunStatus<'l>> {
+        let package_graph = ctx.package_graph()?;
+
+        // This is a map of direct deps by name -> version -> packages that depend on it.
+        let mut direct_deps: BTreeMap<&str, BTreeMap<&Version, Vec<&str>>> = BTreeMap::new();
+        package_graph.query_workspace().resolve_with_fn(|_, link| {
+            // Collect direct dependencies of workspace packages.
+            let (from, to) = link.endpoints();
+            if from.in_workspace() && !to.in_workspace() {
+                direct_deps
+                    .entry(to.name())
+                    .or_default()
+                    .entry(to.version())
+                    .or_default()
+                    .push(from.name());
+            }
+            // query_workspace + preventing further traversals will mean that only direct
+            // dependencies are considered.
+            false
+        });
+        for (direct_dep, versions) in direct_deps {
+            if versions.len() > 1 {
+                let mut msg = format!("duplicate direct dependency '{}':\n", direct_dep);
+                for (version, packages) in versions {
+                    msg.push_str(&format!("  * {} (", version));
+                    msg.push_str(&packages.join(", "));
+                    msg.push_str(")\n");
+                }
+                out.write(LintLevel::Error, msg);
+            }
         }
 
         Ok(RunStatus::Executed)
