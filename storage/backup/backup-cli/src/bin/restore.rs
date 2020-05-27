2283601e0d1d0820fbd573ee2289a5ebd68c0b40
aldenhu <aldenhu@calibra.com>
2020-05-27 16:47:33 +0000

    [backup] StateSnapshotRestoreController
    Closes: #4083
    Approved by: wqfish

--- storage/backup/backup-cli/src/bin/restore.rs
+++ storage/backup/backup-cli/src/bin/restore.rs
@@ -1,49 +1,43 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use backup_cli::{restore::restore_account_state, storage::local_fs::LocalFs};
-use std::path::PathBuf;
-use structopt::StructOpt;
-use tokio::{
-    io::{AsyncBufReadExt, BufReader},
-    stream::StreamExt,
+use backup_cli::{
+    restore::{GlobalRestoreOpt, StateSnapshotRestoreController, StateSnapshotRestoreOpt},
+    storage::local_fs::{LocalFs, LocalFsOpt},
 };
+use libradb::LibraDB;
+use std::sync::Arc;
+use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(StructOpt)]
 struct Opt {
-    #[structopt(long, parse(from_os_str))]
-    db_dir: PathBuf,
+    #[structopt(flatten)]
+    global: GlobalRestoreOpt,
+
+    #[structopt(flatten)]
+    state_snapshot: StateSnapshotRestoreOpt,
+
+    #[structopt(flatten)]
+    storage: LocalFsOpt,
 }
 
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
 
-    let mut lines = BufReader::new(tokio::io::stdin()).lines();
-
-    println!("Input version:");
-    let version = lines
-        .next()
+    let db = Arc::new(
+        LibraDB::open(
+            opt.global.db_dir,
+            false, /* read_only */
+            None,  /* pruner */
+        )
+        .expect("Failed opening DB."),
+    );
+    let storage = Arc::new(LocalFs::new_with_opt(opt.storage));
+    StateSnapshotRestoreController::new(opt.state_snapshot, storage, db)
+        .run()
         .await
-        .expect("Must provide version.")
-        .expect("Failed to read from stdin.")
-        .parse::<u64>()
-        .expect("Version must be valid u64.");
-    println!("Version: {}", version);
-
-    println!("Input manifest:");
-    let manifest = lines
-        .next()
-        .await
-        .expect("Must provide manifest.")
-        .expect("Failed to read from stdin.");
-    println!("Manifest: {}", &manifest);
-
-    // LocalFs uses absolute paths, so when reading we can construct it in any dir.
-    let storage = LocalFs::new(PathBuf::from("."));
-    restore_account_state(&storage, &manifest, version, &opt.db_dir)
-        .await
-        .expect("Failed restoring state.");
+        .expect("Failed restoring state_snapshot.");
 
     println!("Finished restoring account state.");
 }
--- storage/backup/backup-cli/src/restore.rs
+++ storage/backup/backup-cli/src/restore.rs
@@ -3,70 +3,101 @@
 
 use crate::{
     manifest::state_snapshot::StateSnapshotBackup,
-    storage::{BackupStorage, FileHandle, FileHandleRef},
+    storage::{BackupStorage, FileHandle},
     ReadRecordBytes,
 };
 use anyhow::Result;
 use libra_crypto::HashValue;
-use libra_types::{account_state_blob::AccountStateBlob, proof::SparseMerkleRangeProof};
+use libra_types::{
+    account_state_blob::AccountStateBlob, proof::SparseMerkleRangeProof, transaction::Version,
+};
 use libradb::LibraDB;
-use std::path::Path;
+use std::{path::PathBuf, sync::Arc};
+use structopt::StructOpt;
 use tokio::io::AsyncReadExt;
 
-pub async fn restore_account_state<P>(
-    storage: &dyn BackupStorage,
-    manifest_handle: &FileHandleRef,
-    version: u64,
-    db_dir: P,
-) -> Result<()>
-where
-    P: AsRef<Path> + Clone,
-{
-    let mut manifest_bytes = Vec::new();
-    storage
-        .open_for_read(manifest_handle)
-        .await?
-        .read_to_end(&mut manifest_bytes)
-        .await?;
-    let manifest: StateSnapshotBackup = serde_json::from_slice(&manifest_bytes)?;
-
-    let libradb = LibraDB::open(db_dir, false /* read_only */, None /* pruner */)?;
-    let mut receiver = libradb.get_state_restore_receiver(version, manifest.root_hash)?;
-
-    for chunk in manifest.chunks {
-        let blobs = read_account_state_chunk(storage, chunk.blobs).await?;
-        let proof = read_proof(storage, chunk.proof).await?;
-
-        receiver.add_chunk(blobs, proof)?;
-    }
-
-    receiver.finish()?;
-    Ok(())
+#[derive(StructOpt)]
+pub struct GlobalRestoreOpt {
+    #[structopt(long = "target-db-dir", parse(from_os_str))]
+    pub db_dir: PathBuf,
 }
 
-async fn read_account_state_chunk(
-    storage: &dyn BackupStorage,
-    file_handle: FileHandle,
-) -> Result<Vec<(HashValue, AccountStateBlob)>> {
-    let mut file = storage.open_for_read(&file_handle).await?;
-
-    let mut chunk = vec![];
-
-    while let Some(record_bytes) = file.read_record_bytes().await? {
-        chunk.push(lcs::from_bytes(&record_bytes)?);
-    }
-
-    Ok(chunk)
+#[derive(StructOpt)]
+pub struct StateSnapshotRestoreOpt {
+    #[structopt(long = "state-manifest")]
+    pub manifest_handle: FileHandle,
+    #[structopt(long = "state-into-version")]
+    pub version: Version,
 }
 
-async fn read_proof(
-    storage: &dyn BackupStorage,
-    file_handle: FileHandle,
-) -> Result<SparseMerkleRangeProof> {
-    let mut file = storage.open_for_read(&file_handle).await?;
-    let mut buf = Vec::new();
-    file.read_to_end(&mut buf).await?;
+pub struct StateSnapshotRestoreController {
+    storage: Arc<dyn BackupStorage>,
+    db: Arc<LibraDB>,
+    version: Version,
+    manifest_handle: FileHandle,
+}
 
-    let proof = lcs::from_bytes(&buf)?;
-    Ok(proof)
+impl StateSnapshotRestoreController {
+    pub fn new(
+        opt: StateSnapshotRestoreOpt,
+        storage: Arc<dyn BackupStorage>,
+        db: Arc<LibraDB>,
+    ) -> Self {
+        Self {
+            storage,
+            db,
+            version: opt.version,
+            manifest_handle: opt.manifest_handle,
+        }
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let mut manifest_bytes = Vec::new();
+        self.storage
+            .open_for_read(&self.manifest_handle)
+            .await?
+            .read_to_end(&mut manifest_bytes)
+            .await?;
+        let manifest: StateSnapshotBackup = serde_json::from_slice(&manifest_bytes)?;
+
+        let mut receiver = self
+            .db
+            .get_state_restore_receiver(self.version, manifest.root_hash)?;
+
+        for chunk in manifest.chunks {
+            let blobs = self.read_account_state_chunk(chunk.blobs).await?;
+            let proof = self.read_proof(chunk.proof).await?;
+
+            receiver.add_chunk(blobs, proof)?;
+        }
+
+        receiver.finish()?;
+        Ok(())
+    }
+}
+
+impl StateSnapshotRestoreController {
+    async fn read_account_state_chunk(
+        &self,
+        file_handle: FileHandle,
+    ) -> Result<Vec<(HashValue, AccountStateBlob)>> {
+        let mut file = self.storage.open_for_read(&file_handle).await?;
+
+        let mut chunk = vec![];
+
+        while let Some(record_bytes) = file.read_record_bytes().await? {
+            chunk.push(lcs::from_bytes(&record_bytes)?);
+        }
+
+        Ok(chunk)
+    }
+
+    async fn read_proof(&self, file_handle: FileHandle) -> Result<SparseMerkleRangeProof> {
+        let mut file = self.storage.open_for_read(&file_handle).await?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).await?;
+
+        let proof = lcs::from_bytes(&buf)?;
+        Ok(proof)
+    }
 }
