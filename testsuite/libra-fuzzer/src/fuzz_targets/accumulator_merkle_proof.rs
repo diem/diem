41b7338c636bf26ce94aa11ffef8a44d50241315
Brandon Williams <bmwill@calibra.com>
2020-06-01 15:15:40 -0700

    fuzzing: remove grpc-types dep
    Closes: #4175
    Approved by: davidiw

--- testsuite/libra-fuzzer/src/fuzz_targets/accumulator_merkle_proof.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The Libra Core Contributors
-// SPDX-License-Identifier: Apache-2.0
-
-use grpc_types::proto::types::AccumulatorProof as ProtoAccumulatorProof;
-use libra_types::proof::TransactionAccumulatorProof;
-
-proto_fuzz_target!(AccumulatorProofTarget => TransactionAccumulatorProof, ProtoAccumulatorProof);
