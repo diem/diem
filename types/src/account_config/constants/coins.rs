7b2899a3afc0fd8227272184b43f84bdbed61cbc
François Garillot <francois@garillot.net>
2020-06-01 07:54:01 -0700

    Removing coin2_tag from types/src/account_config/constants/coins.rs
    Closes: #4176
    Approved by: thefallentree

--- types/src/account_config/constants/coins.rs
+++ types/src/account_config/constants/coins.rs
@@ -17,12 +17,3 @@ pub fn coin1_tag() -> TypeTag {
         type_params: vec![],
     })
 }
-
-pub fn coin2_tag() -> TypeTag {
-    TypeTag::Struct(StructTag {
-        address: CORE_CODE_ADDRESS,
-        module: from_currency_code_string(COIN2_NAME).unwrap(),
-        name: coin_struct_name().to_owned(),
-        type_params: vec![],
-    })
-}
