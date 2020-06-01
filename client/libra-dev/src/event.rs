19afd1870e9806a1aee884a9f861145073e41efd
Brandon Williams <bmwill@calibra.com>
2020-06-01 15:06:58 -0700

    libra-dev: remove grpc-types dep

--- client/libra-dev/src/event.rs
+++ client/libra-dev/src/event.rs
@@ -213,17 +213,19 @@ fn test_libra_LibraEvent_from() {
 
     let event = ContractEvent::new(*event_key, sequence_number, type_tag, event_data_bytes);
 
-    let proto_txn = grpc_types::proto::types::Event::from(event);
+    let key = event.key().to_vec();
+    let type_tag = lcs::to_bytes(&event.type_tag()).expect("Failed to serialize.");
+    let event_data = event.event_data().to_vec();
 
     let mut libra_event: *mut LibraEvent = std::ptr::null_mut();
     let result = unsafe {
         libra_LibraEvent_from(
-            proto_txn.key.as_ptr(),
-            proto_txn.key.len(),
-            proto_txn.event_data.as_ptr(),
-            proto_txn.event_data.len(),
-            proto_txn.type_tag.as_ptr(),
-            proto_txn.type_tag.len(),
+            key.as_ptr(),
+            key.len(),
+            event_data.as_ptr(),
+            event_data.len(),
+            type_tag.as_ptr(),
+            type_tag.len(),
             &mut libra_event,
         )
     };
