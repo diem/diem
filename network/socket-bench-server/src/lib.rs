mimoo/libra

[network] Noise IX -> Noise IK
This pull request integrates our Noise IK implementation with network,
replacing the current Noise IX code.

Notable changes:

1. The network/noise crate has been renamed to noise-wrapper and has been moved to a module inside the network crate (network/src/noise).
2. The NoiseSocket type has been renamed to NoiseSession type, as NoiseSocket is the name of a protocol framework (https://noisesocket.org/spec/noisesocket/).
3. Noise handshake messages are not prefixed with a length field anymore (handshake messages have a known length).
4. The first handshake message now prepends a u64 timestamp (and includes it into the prologue part of noise) in the first handshake message. When receiving a handshake initialization message, a server verifies that the timestamp not older than 60 seconds in the past, and not drifting in the future for more than 10 seconds.
5. The noise code now implements two additional validation checks after parsing the first client message:
  - if a trusted peer hashmap is passed, it will check that the client's public key is in there
  - it will detect replay attacks by checking if the prologue's u64 timestamp has been seen before for that same public key

network/socket-bench-server/src/lib.rs
@@ -9,7 +9,7 @@ use futures::{
     sink::SinkExt,
     stream::{Stream, StreamExt},
 };
-use libra_crypto::test_utils::TEST_SEED;
+use libra_crypto::{test_utils::TEST_SEED, x25519, Uniform as _};
 use libra_logger::prelude::*;
 use libra_network_address::NetworkAddress;
 use memsocket::MemorySocket;
@@ -21,9 +21,13 @@ use netcore::{
         Transport, TransportExt,
     },
 };
-use noise::{NoiseConfig, NoiseSocket};
+use network::noise_wrapper::{session::NoiseSession, AntiReplayTimestamp, NoiseWrapper};
 use rand::prelude::*;
-use std::{env, ffi::OsString, sync::Arc};
+use std::{
+    env,
+    ffi::OsString,
+    sync::{Arc, RwLock},
+};
 use tokio::runtime::Handle;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
@@ -76,21 +80,31 @@ impl Args {
 }
 
 /// Build a MemorySocket + Noise transport
-pub fn build_memsocket_noise_transport() -> impl Transport<Output = NoiseSocket<MemorySocket>> {
-    MemoryTransport::default().and_then(move |socket, _addr, origin| async move {
+pub fn build_memsocket_noise_transport() -> impl Transport<Output = NoiseSession<MemorySocket>> {
+    MemoryTransport::default().and_then(move |socket, addr, origin| async move {
         let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
-        let noise_config = Arc::new(NoiseConfig::new_random(&mut rng));
-        let (_remote_static_key, socket) = noise_config.upgrade_connection(socket, origin).await?;
+        let private = x25519::PrivateKey::generate(&mut rng);
+        let noise_config = Arc::new(NoiseWrapper::new(private));
+        let noise_timestamps = Arc::new(RwLock::new(AntiReplayTimestamp::new()));
+        let remote_public_key = addr.find_noise_proto();
+        let (_remote_static_key, socket) = noise_config
+            .upgrade_connection(socket, origin, noise_timestamps, remote_public_key, None)
+            .await?;
         Ok(socket)
     })
 }
 
 /// Build a Tcp + Noise transport
-pub fn build_tcp_noise_transport() -> impl Transport<Output = NoiseSocket<TcpSocket>> {
-    TcpTransport::default().and_then(move |socket, _addr, origin| async move {
+pub fn build_tcp_noise_transport() -> impl Transport<Output = NoiseSession<TcpSocket>> {
+    TcpTransport::default().and_then(move |socket, addr, origin| async move {
         let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
-        let noise_config = Arc::new(NoiseConfig::new_random(&mut rng));
-        let (_remote_static_key, socket) = noise_config.upgrade_connection(socket, origin).await?;
+        let private = x25519::PrivateKey::generate(&mut rng);
+        let noise_config = Arc::new(NoiseWrapper::new(private));
+        let noise_timestamps = Arc::new(RwLock::new(AntiReplayTimestamp::new()));
+        let remote_public_key = addr.find_noise_proto();
+        let (_remote_static_key, socket) = noise_config
+            .upgrade_connection(socket, origin, noise_timestamps, remote_public_key, None)
+            .await?;
         Ok(socket)
     })
 }

network/src/noise_wrapper/fuzzing.rs
@@ -5,11 +5,10 @@
 // Noise Fuzzing
 // =============
 //
-// This fuzzes the wrappers we have around the Noise library `snow`.
+// This fuzzes the wrappers we have around our Noise library.
 //
 
-use super::Handshake;
-use crate::{NoiseParams, NoiseSocket, NOISE_PARAMETER};
+use crate::noise_wrapper::{AntiReplayTimestamp, NoiseWrapper};
 use futures::{
     executor::block_on,
     future::join,
@@ -17,73 +16,25 @@ use futures::{
     ready,
     task::{Context, Poll},
 };
+use libra_crypto::{test_utils::TEST_SEED, x25519, Uniform as _};
 use memsocket::MemorySocket;
 use once_cell::sync::Lazy;
-use rand_core::{impls, CryptoRng, RngCore};
-use snow::{
-    params::*,
-    resolvers::{CryptoResolver, DefaultResolver},
-    types::*,
-    Builder,
+use rand_core::SeedableRng;
+use std::{
+    io,
+    pin::Pin,
+    sync::{Arc, RwLock},
 };
-use std::{io, pin::Pin};
 
 //
 // Corpus generation
 // =================
 //
-// - CountingRng: needed to deterministically generate a keypair with snow.
-// - TestResolver: needed to use the deterministic RNG.
 // - ExposingSocket: a wrapper around MemorySocket to retrieve handshake messages being sent.
-// - NOISE_KEYPAIR: generates the same snow Keypair all the time.
+// - KEYPAIR: a unique keypair for fuzzing
 // - generate_first_two_messages: it will generate the first or second message in the handshake.
 // - generate_corpus: the function called by our fuzzer to retrieve the corpus.
 //
-
-// CountingRng is a deterministic RNG used for deterministic keypair generation
-#[derive(Default)]
-struct CountingRng(u64);
-impl RngCore for CountingRng {
-    fn next_u32(&mut self) -> u32 {
-        self.next_u64() as u32
-    }
-
-    fn next_u64(&mut self) -> u64 {
-        self.0 += 1;
-        self.0
-    }
-
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        impls::fill_bytes_via_next(self, dest)
-    }
-
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
-        Ok(self.fill_bytes(dest))
-    }
-}
-impl CryptoRng for CountingRng {}
-impl Random for CountingRng {}
-
-// TestResolver is needed to have a deterministic RNG in keypair generation
-struct TestResolver {}
-impl CryptoResolver for TestResolver {
-    fn resolve_rng(&self) -> Option<Box<dyn Random>> {
-        let rng = CountingRng(0);
-        Some(Box::new(rng))
-    }
-
-    fn resolve_dh(&self, choice: &DHChoice) -> Option<Box<dyn Dh>> {
-        DefaultResolver.resolve_dh(choice)
-    }
-
-    fn resolve_hash(&self, choice: &HashChoice) -> Option<Box<dyn Hash>> {
-        DefaultResolver.resolve_hash(choice)
-    }
-
-    fn resolve_cipher(&self, choice: &CipherChoice) -> Option<Box<dyn Cipher>> {
-        DefaultResolver.resolve_cipher(choice)
-    }
-}
 
 // ExposingSocket is needed to retrieve the noise messages peers will write on the socket
 struct ExposingSocket {
@@ -142,54 +93,37 @@ impl AsyncRead for ExposingSocket {
     }
 }
 
-// take_socket is implemented on parent structure to be able to retrieve our ExposingSocket
-impl NoiseSocket<ExposingSocket> {
-    fn take_socket(self) -> ExposingSocket {
-        self.socket
-    }
-}
-
 //
 // Actual code to generate corpus
 //
 
 // let's cache the deterministic keypair
-pub static NOISE_KEYPAIR: Lazy<snow::Keypair> = Lazy::new(|| {
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    Builder::with_resolver(parameters, Box::new(TestResolver {}))
-        .generate_keypair()
-        .unwrap()
+pub static KEYPAIR: Lazy<(x25519::PrivateKey, x25519::PublicKey)> = Lazy::new(|| {
+    let mut rng = ::rand::rngs::StdRng::from_seed(TEST_SEED);
+    let private_key = x25519::PrivateKey::generate(&mut rng);
+    let public_key = private_key.public_key();
+    (private_key, public_key)
 });
 
 fn generate_first_two_messages() -> (Vec<u8>, Vec<u8>) {
     // build
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let initiator = snow::Builder::new(parameters.clone())
-        // why not use the same as the responder :D
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .remote_public_key(&NOISE_KEYPAIR.public)
-        .build_initiator()
-        .unwrap();
-    let responder = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .build_responder()
-        .unwrap();
+    let (private_key, public_key) = KEYPAIR.clone();
+    let initiator = NoiseWrapper::new(private_key.clone());
+    let responder = NoiseWrapper::new(private_key);
 
     // create exposing socket
     let (dialer_socket, listener_socket) = ExposingSocket::new_pair();
-    let (dialer, listener) = (
-        NoiseSocket::new(dialer_socket, initiator),
-        NoiseSocket::new(listener_socket, responder),
-    );
 
-    let (dialer, listener) = (Handshake(dialer), Handshake(listener));
-
-    let (dialer_result, listener_result) =
-        block_on(join(dialer.handshake_1rt(), listener.handshake_1rt()));
+    // perform the handshake
+    let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamp::new()));
+    let (client_session, server_session) = block_on(join(
+        initiator.dial(dialer_socket, public_key),
+        responder.accept(listener_socket, anti_replay_timestamps, None),
+    ));
 
     // take result
-    let init_msg = dialer_result.unwrap().take_socket().written;
-    let resp_msg = listener_result.unwrap().take_socket().written;
+    let init_msg = client_session.unwrap().take_socket().written;
+    let resp_msg = server_session.unwrap().take_socket().written;
 
     (init_msg, resp_msg)
 }
@@ -257,31 +191,27 @@ impl<'a> AsyncRead for FakeSocket<'a> {
 
 pub fn fuzz_initiator(data: &[u8]) {
     // setup initiator
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let initiator = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .remote_public_key(&NOISE_KEYPAIR.public)
-        .build_initiator()
-        .unwrap();
-    // setup NoiseSocket
+    let (private_key, public_key) = KEYPAIR.clone();
+    let initiator = NoiseWrapper::new(private_key);
+
+    // setup NoiseSession
     let fake_socket = FakeSocket { content: data };
-    let handshake = Handshake::new(fake_socket, initiator);
+
     // send a message, then read fuzz data
-    let _ = block_on(handshake.handshake_1rt());
+    let _ = block_on(initiator.dial(fake_socket, public_key));
 }
 
 pub fn fuzz_responder(data: &[u8]) {
     // setup responder
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let responder = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .build_responder()
-        .unwrap();
-    // setup NoiseSocket
+    let (private_key, _public_key) = KEYPAIR.clone();
+    let responder = NoiseWrapper::new(private_key);
+
+    // setup NoiseSession
     let fake_socket = FakeSocket { content: data };
-    let handshake = Handshake::new(fake_socket, responder);
+    let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamp::new()));
+
     // read fuzz data
-    let _ = block_on(handshake.handshake_1rt());
+    let _ = block_on(responder.accept(fake_socket, anti_replay_timestamps, None));
 }
 
 //

network/src/noise_wrapper/mod.rs
@@ -1,0 +1,91 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//! This crate implements wrappers around our [Noise][noise] implementation.
+//! Noise is a protocol framework to encrypt and authentication connections.
+//! We use Noise to secure connections between peers in Libra.
+//! Specifically, we use the [Noise IK][ik] handshake which is a one round-trip protocol
+//! (the client sends one message, then the server responds).
+//! For more information about Noise and our implementation, refer to the [crypto] crate.
+//!
+//! Usage example:
+//!
+//! ```
+//! use network::{noise_wrapper::{NoiseWrapper, AntiReplayTimestamp}, NetworkPublicKeys};
+//! use futures::{executor, future, io::{AsyncReadExt, AsyncWriteExt}};
+//! use memsocket::MemorySocket;
+//! use libra_crypto::{x25519, ed25519, Uniform, PrivateKey, test_utils::TEST_SEED};
+//! use rand::{rngs::StdRng, SeedableRng};
+//! use libra_types::PeerId;
+//! use libra_config::config::NetworkPeerInfo;
+//! use std::{collections::HashMap, sync::{Arc, RwLock}};
+//!
+//! fn example() -> std::io::Result<()> {
+//! // create client and server NoiseWrapper
+//! let mut rng = StdRng::from_seed(TEST_SEED);
+//! let client_private = x25519::PrivateKey::generate(&mut rng);
+//! let client_public = client_private.public_key();
+//! let server_private = x25519::PrivateKey::generate(&mut rng);
+//! let server_public = server_private.public_key();
+//! let client = NoiseWrapper::new(client_private);
+//! let server = NoiseWrapper::new(server_private);
+//!
+//! // create list of trusted peers
+//! let mut trusted_peers = Arc::new(RwLock::new(HashMap::new()));
+//! {
+//!     let dummy_signing_key = ed25519::Ed25519PrivateKey::generate(&mut rng);
+//!     trusted_peers.write().unwrap().insert(PeerId::random(), NetworkPublicKeys {
+//!        signing_public_key: dummy_signing_key.public_key(),
+//!        identity_public_key: client_public,
+//!     });
+//! }
+//!
+//! // use an in-memory socket as example
+//! let (dialer_socket, listener_socket) = MemorySocket::new_pair();
+//!
+//! // anti-replay timestamps
+//! let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamp::new()));
+//!
+//! // perform the handshake
+//! let (client_session, server_session) = executor::block_on(future::join(
+//!    client.dial(dialer_socket, server_public),
+//!    server.accept(listener_socket, anti_replay_timestamps, Some(&trusted_peers)),
+//! ));
+//!
+//! let mut client_session = client_session?;
+//! let mut server_session = server_session?;
+//!
+//!
+//! // client -> server
+//! executor::block_on(client_session.write_all(b"client hello"))?;
+//! executor::block_on(client_session.flush())?;
+//!
+//! let mut buf = [0; 12];
+//! executor::block_on(server_session.read_exact(&mut buf))?;
+//! assert_eq!(&buf, b"client hello");
+//!
+//! // client <- server
+//! executor::block_on(server_session.write_all(b"server hello"))?;
+//! executor::block_on(server_session.flush())?;
+//!
+//! let mut buf = [0; 12];
+//! executor::block_on(client_session.read_exact(&mut buf))?;
+//! assert_eq!(&buf, b"server hello");
+//!
+//! Ok(())
+//! }
+//!
+//!
+//! ```
+//!
+//! [noise]: http://noiseprotocol.org/
+//! [ik]: https://noiseexplorer.com/patterns/IK
+//! [crypto]: ../libra_crypto/noise/index.html
+
+pub mod handshake;
+pub mod session;
+
+#[cfg(any(test, feature = "fuzzing"))]
+pub mod fuzzing;
+
+pub use handshake::{AntiReplayTimestamp, NoiseWrapper};

diem/diem

network/socket-bench-server/src/lib.rs
@@ -9,7 +9,7 @@ use futures::{
     sink::SinkExt,
     stream::{Stream, StreamExt},
 };
-use libra_crypto::test_utils::TEST_SEED;
+use libra_crypto::{test_utils::TEST_SEED, x25519, Uniform as _};
 use libra_logger::prelude::*;
 use libra_network_address::NetworkAddress;
 use memsocket::MemorySocket;
@@ -21,9 +21,13 @@ use netcore::{
         Transport, TransportExt,
     },
 };
-use noise::{NoiseConfig, NoiseSocket};
+use network::noise_wrapper::{session::NoiseSession, AntiReplayTimestamps, NoiseWrapper};
 use rand::prelude::*;
-use std::{env, ffi::OsString, sync::Arc};
+use std::{
+    env,
+    ffi::OsString,
+    sync::{Arc, RwLock},
+};
 use tokio::runtime::Handle;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
@@ -76,21 +80,31 @@ impl Args {
 }
 
 /// Build a MemorySocket + Noise transport
-pub fn build_memsocket_noise_transport() -> impl Transport<Output = NoiseSocket<MemorySocket>> {
-    MemoryTransport::default().and_then(move |socket, _addr, origin| async move {
+pub fn build_memsocket_noise_transport() -> impl Transport<Output = NoiseSession<MemorySocket>> {
+    MemoryTransport::default().and_then(move |socket, addr, origin| async move {
         let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
-        let noise_config = Arc::new(NoiseConfig::new_random(&mut rng));
-        let (_remote_static_key, socket) = noise_config.upgrade_connection(socket, origin).await?;
+        let private = x25519::PrivateKey::generate(&mut rng);
+        let noise_config = Arc::new(NoiseWrapper::new(private));
+        let noise_timestamps = Arc::new(RwLock::new(AntiReplayTimestamps::new()));
+        let remote_public_key = addr.find_noise_proto();
+        let (_remote_static_key, socket) = noise_config
+            .upgrade_connection(socket, origin, noise_timestamps, remote_public_key, None)
+            .await?;
         Ok(socket)
     })
 }
 
 /// Build a Tcp + Noise transport
-pub fn build_tcp_noise_transport() -> impl Transport<Output = NoiseSocket<TcpSocket>> {
-    TcpTransport::default().and_then(move |socket, _addr, origin| async move {
+pub fn build_tcp_noise_transport() -> impl Transport<Output = NoiseSession<TcpSocket>> {
+    TcpTransport::default().and_then(move |socket, addr, origin| async move {
         let mut rng: StdRng = SeedableRng::from_seed(TEST_SEED);
-        let noise_config = Arc::new(NoiseConfig::new_random(&mut rng));
-        let (_remote_static_key, socket) = noise_config.upgrade_connection(socket, origin).await?;
+        let private = x25519::PrivateKey::generate(&mut rng);
+        let noise_config = Arc::new(NoiseWrapper::new(private));
+        let noise_timestamps = Arc::new(RwLock::new(AntiReplayTimestamps::new()));
+        let remote_public_key = addr.find_noise_proto();
+        let (_remote_static_key, socket) = noise_config
+            .upgrade_connection(socket, origin, noise_timestamps, remote_public_key, None)
+            .await?;
         Ok(socket)
     })
 }

network/src/noise_wrapper/fuzzing.rs
@@ -5,11 +5,10 @@
 // Noise Fuzzing
 // =============
 //
-// This fuzzes the wrappers we have around the Noise library `snow`.
+// This fuzzes the wrappers we have around our Noise library.
 //
 
-use super::Handshake;
-use crate::{NoiseParams, NoiseSocket, NOISE_PARAMETER};
+use crate::noise_wrapper::{AntiReplayTimestamps, NoiseWrapper};
 use futures::{
     executor::block_on,
     future::join,
@@ -17,73 +16,25 @@ use futures::{
     ready,
     task::{Context, Poll},
 };
+use libra_crypto::{test_utils::TEST_SEED, x25519, Uniform as _};
 use memsocket::MemorySocket;
 use once_cell::sync::Lazy;
-use rand_core::{impls, CryptoRng, RngCore};
-use snow::{
-    params::*,
-    resolvers::{CryptoResolver, DefaultResolver},
-    types::*,
-    Builder,
+use rand_core::SeedableRng;
+use std::{
+    io,
+    pin::Pin,
+    sync::{Arc, RwLock},
 };
-use std::{io, pin::Pin};
 
 //
 // Corpus generation
 // =================
 //
-// - CountingRng: needed to deterministically generate a keypair with snow.
-// - TestResolver: needed to use the deterministic RNG.
 // - ExposingSocket: a wrapper around MemorySocket to retrieve handshake messages being sent.
-// - NOISE_KEYPAIR: generates the same snow Keypair all the time.
+// - KEYPAIR: a unique keypair for fuzzing
 // - generate_first_two_messages: it will generate the first or second message in the handshake.
 // - generate_corpus: the function called by our fuzzer to retrieve the corpus.
 //
-
-// CountingRng is a deterministic RNG used for deterministic keypair generation
-#[derive(Default)]
-struct CountingRng(u64);
-impl RngCore for CountingRng {
-    fn next_u32(&mut self) -> u32 {
-        self.next_u64() as u32
-    }
-
-    fn next_u64(&mut self) -> u64 {
-        self.0 += 1;
-        self.0
-    }
-
-    fn fill_bytes(&mut self, dest: &mut [u8]) {
-        impls::fill_bytes_via_next(self, dest)
-    }
-
-    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
-        Ok(self.fill_bytes(dest))
-    }
-}
-impl CryptoRng for CountingRng {}
-impl Random for CountingRng {}
-
-// TestResolver is needed to have a deterministic RNG in keypair generation
-struct TestResolver {}
-impl CryptoResolver for TestResolver {
-    fn resolve_rng(&self) -> Option<Box<dyn Random>> {
-        let rng = CountingRng(0);
-        Some(Box::new(rng))
-    }
-
-    fn resolve_dh(&self, choice: &DHChoice) -> Option<Box<dyn Dh>> {
-        DefaultResolver.resolve_dh(choice)
-    }
-
-    fn resolve_hash(&self, choice: &HashChoice) -> Option<Box<dyn Hash>> {
-        DefaultResolver.resolve_hash(choice)
-    }
-
-    fn resolve_cipher(&self, choice: &CipherChoice) -> Option<Box<dyn Cipher>> {
-        DefaultResolver.resolve_cipher(choice)
-    }
-}
 
 // ExposingSocket is needed to retrieve the noise messages peers will write on the socket
 struct ExposingSocket {
@@ -142,54 +93,37 @@ impl AsyncRead for ExposingSocket {
     }
 }
 
-// take_socket is implemented on parent structure to be able to retrieve our ExposingSocket
-impl NoiseSocket<ExposingSocket> {
-    fn take_socket(self) -> ExposingSocket {
-        self.socket
-    }
-}
-
 //
 // Actual code to generate corpus
 //
 
 // let's cache the deterministic keypair
-pub static NOISE_KEYPAIR: Lazy<snow::Keypair> = Lazy::new(|| {
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    Builder::with_resolver(parameters, Box::new(TestResolver {}))
-        .generate_keypair()
-        .unwrap()
+pub static KEYPAIR: Lazy<(x25519::PrivateKey, x25519::PublicKey)> = Lazy::new(|| {
+    let mut rng = ::rand::rngs::StdRng::from_seed(TEST_SEED);
+    let private_key = x25519::PrivateKey::generate(&mut rng);
+    let public_key = private_key.public_key();
+    (private_key, public_key)
 });
 
 fn generate_first_two_messages() -> (Vec<u8>, Vec<u8>) {
     // build
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let initiator = snow::Builder::new(parameters.clone())
-        // why not use the same as the responder :D
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .remote_public_key(&NOISE_KEYPAIR.public)
-        .build_initiator()
-        .unwrap();
-    let responder = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .build_responder()
-        .unwrap();
+    let (private_key, public_key) = KEYPAIR.clone();
+    let initiator = NoiseWrapper::new(private_key.clone());
+    let responder = NoiseWrapper::new(private_key);
 
     // create exposing socket
     let (dialer_socket, listener_socket) = ExposingSocket::new_pair();
-    let (dialer, listener) = (
-        NoiseSocket::new(dialer_socket, initiator),
-        NoiseSocket::new(listener_socket, responder),
-    );
 
-    let (dialer, listener) = (Handshake(dialer), Handshake(listener));
-
-    let (dialer_result, listener_result) =
-        block_on(join(dialer.handshake_1rt(), listener.handshake_1rt()));
+    // perform the handshake
+    let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamps::new()));
+    let (client_session, server_session) = block_on(join(
+        initiator.dial(dialer_socket, public_key),
+        responder.accept(listener_socket, anti_replay_timestamps, None),
+    ));
 
     // take result
-    let init_msg = dialer_result.unwrap().take_socket().written;
-    let resp_msg = listener_result.unwrap().take_socket().written;
+    let init_msg = client_session.unwrap().take_socket().written;
+    let resp_msg = server_session.unwrap().take_socket().written;
 
     (init_msg, resp_msg)
 }
@@ -257,31 +191,27 @@ impl<'a> AsyncRead for FakeSocket<'a> {
 
 pub fn fuzz_initiator(data: &[u8]) {
     // setup initiator
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let initiator = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .remote_public_key(&NOISE_KEYPAIR.public)
-        .build_initiator()
-        .unwrap();
-    // setup NoiseSocket
+    let (private_key, public_key) = KEYPAIR.clone();
+    let initiator = NoiseWrapper::new(private_key);
+
+    // setup NoiseSession
     let fake_socket = FakeSocket { content: data };
-    let handshake = Handshake::new(fake_socket, initiator);
+
     // send a message, then read fuzz data
-    let _ = block_on(handshake.handshake_1rt());
+    let _ = block_on(initiator.dial(fake_socket, public_key));
 }
 
 pub fn fuzz_responder(data: &[u8]) {
     // setup responder
-    let parameters: NoiseParams = NOISE_PARAMETER.parse().unwrap();
-    let responder = snow::Builder::new(parameters)
-        .local_private_key(&NOISE_KEYPAIR.private)
-        .build_responder()
-        .unwrap();
-    // setup NoiseSocket
+    let (private_key, _public_key) = KEYPAIR.clone();
+    let responder = NoiseWrapper::new(private_key);
+
+    // setup NoiseSession
     let fake_socket = FakeSocket { content: data };
-    let handshake = Handshake::new(fake_socket, responder);
+    let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamps::new()));
+
     // read fuzz data
-    let _ = block_on(handshake.handshake_1rt());
+    let _ = block_on(responder.accept(fake_socket, anti_replay_timestamps, None));
 }
 
 //

network/src/noise_wrapper/mod.rs
@@ -1,0 +1,92 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+//! This crate implements wrappers around our [Noise][noise] implementation.
+//! Noise is a protocol framework to encrypt and authentication connections.
+//! We use Noise to secure connections between peers in Libra.
+//! Specifically, we use the [Noise IK][ik] handshake which is a one round-trip protocol
+//! (the client sends one message, then the server responds).
+//! For more information about Noise and our implementation, refer to the [crypto] crate.
+//!
+//! Usage example:
+//!
+//! ```
+//! use network::{noise_wrapper::{NoiseWrapper, AntiReplayTimestamps}, NetworkPublicKeys};
+//! use futures::{executor, future, io::{AsyncReadExt, AsyncWriteExt}};
+//! use memsocket::MemorySocket;
+//! use libra_crypto::{x25519, ed25519, Uniform, PrivateKey, test_utils::TEST_SEED};
+//! use rand::{rngs::StdRng, SeedableRng};
+//! use libra_types::PeerId;
+//! use libra_config::config::NetworkPeerInfo;
+//! use std::{collections::HashMap, sync::{Arc, RwLock}};
+//!
+//! fn example() -> std::io::Result<()> {
+//! // create client and server NoiseWrapper
+//! let mut rng = StdRng::from_seed(TEST_SEED);
+//! let client_private = x25519::PrivateKey::generate(&mut rng);
+//! let client_public = client_private.public_key();
+//! let client = NoiseWrapper::new(client_private);
+//!
+//! let server_private = x25519::PrivateKey::generate(&mut rng);
+//! let server_public = server_private.public_key();
+//! let server = NoiseWrapper::new(server_private);
+//!
+//! // create list of trusted peers
+//! let mut trusted_peers = Arc::new(RwLock::new(HashMap::new()));
+//! {
+//!     let dummy_signing_key = ed25519::Ed25519PrivateKey::generate(&mut rng);
+//!     trusted_peers.write().unwrap().insert(PeerId::random(), NetworkPublicKeys {
+//!        signing_public_key: dummy_signing_key.public_key(),
+//!        identity_public_key: client_public,
+//!     });
+//! }
+//!
+//! // use an in-memory socket as example
+//! let (dialer_socket, listener_socket) = MemorySocket::new_pair();
+//!
+//! // anti-replay timestamps
+//! let anti_replay_timestamps = Arc::new(RwLock::new(AntiReplayTimestamps::new()));
+//!
+//! // perform the handshake
+//! let (client_session, server_session) = executor::block_on(future::join(
+//!    client.dial(dialer_socket, server_public),
+//!    server.accept(listener_socket, anti_replay_timestamps, Some(&trusted_peers)),
+//! ));
+//!
+//! let mut client_session = client_session?;
+//! let mut server_session = server_session?;
+//!
+//!
+//! // client -> server
+//! executor::block_on(client_session.write_all(b"client hello"))?;
+//! executor::block_on(client_session.flush())?;
+//!
+//! let mut buf = [0; 12];
+//! executor::block_on(server_session.read_exact(&mut buf))?;
+//! assert_eq!(&buf, b"client hello");
+//!
+//! // client <- server
+//! executor::block_on(server_session.write_all(b"server hello"))?;
+//! executor::block_on(server_session.flush())?;
+//!
+//! let mut buf = [0; 12];
+//! executor::block_on(client_session.read_exact(&mut buf))?;
+//! assert_eq!(&buf, b"server hello");
+//!
+//! Ok(())
+//! }
+//!
+//!
+//! ```
+//!
+//! [noise]: http://noiseprotocol.org/
+//! [ik]: https://noiseexplorer.com/patterns/IK
+//! [crypto]: ../libra_crypto/noise/index.html
+
+pub mod handshake;
+pub mod session;
+
+#[cfg(any(test, feature = "fuzzing"))]
+pub mod fuzzing;
+
+pub use handshake::{AntiReplayTimestamps, NoiseWrapper};