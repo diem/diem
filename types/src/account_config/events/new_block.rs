27be962aea3e4bb87330a81d11253d9b5311c155
François Garillot <francois@garillot.net>
2020-06-04 05:19:58 -0700

    Removing event_key from types/src/account_config/events/new_block.rs

--- types/src/account_config/events/new_block.rs
+++ types/src/account_config/events/new_block.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{account_address::AccountAddress, event::EventKey};
+use crate::account_address::AccountAddress;
 use anyhow::Result;
 use move_core_types::move_resource::MoveResource;
 use serde::{Deserialize, Serialize};
@@ -30,10 +30,6 @@ impl NewBlockEvent {
 
     pub fn try_from_bytes(bytes: &[u8]) -> Result<Self> {
         lcs::from_bytes(bytes).map_err(Into::into)
-    }
-
-    pub fn event_key() -> EventKey {
-        crate::block_metadata::new_block_event_key()
     }
 }
 
