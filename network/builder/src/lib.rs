17b3418f8f5d35c016155ee61b33fe11b782f893
Allen Clement <aclement@gmail.com>
2020-06-09 18:33:17 +0200

    [network] move network_builder into its own crate and setup_network out of main_node

--- network/builder/src/lib.rs
+++ network/builder/src/lib.rs
@@ -3,3 +3,11 @@
 
 pub use network::protocols::rpc::error::RpcError;
 pub mod builder;
+
+// TODO:  This module should be test-only, e.g., #[cfg(any(feature = "testing", test))]
+// At present it cannot be because network_builder must be a separate crate and the current
+// factoring of DummyNetwork requires use of network_builder.  A holistic review of the
+// network directory is needed to break internal circular dependencies.
+pub mod dummy;
+#[cfg(test)]
+mod test;
