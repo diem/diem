38a41b00f96751d5f6eb6e0d860f0aca8017ddba
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-09 21:28:38 -0700

    [Move] transaction builder in python

---
+++ language/transaction-builder-generator/src/lib.rs
@@ -0,0 +1,15 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use move_core_types::language_storage::TypeTag;
+
+/// Support for code-generation in Python 3
+pub mod python3;
+
+/// Useful error message.
+fn type_not_allowed(type_tag: &TypeTag) -> ! {
+    panic!(
+        "Transaction scripts cannot take arguments of type {}.",
+        type_tag
+    );
+}
