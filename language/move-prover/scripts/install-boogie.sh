616c395c645440daddf2960152b3822ad4fc5f19
Junkil Park <jkpark@fb.com>
2020-06-11 12:56:54 -0700

    Upgrade Z3 and Boogie versions
    - Upgrade Z3 from 4.8.7 to 4.8.8
    - Upgrade Boogie from 2.5.7 to 2.6.17
    - Check that no baseline files need to update

--- language/move-prover/scripts/install-boogie.sh
+++ language/move-prover/scripts/install-boogie.sh
@@ -3,4 +3,4 @@
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-dotnet tool update --global Boogie --version 2.5.7
+dotnet tool update --global Boogie --version 2.6.17
