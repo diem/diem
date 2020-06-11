a4f247660e01902c309aaaed5f7d4d2015d3a740
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-10 21:38:36 -0700

    [Move] fix formatting for doc strings in transaction builders

--- language/transaction-builder/src/lib.rs
+++ language/transaction-builder/src/lib.rs
@@ -145,10 +145,10 @@ register_all_txn_scripts! {
     builder: encode_add_recovery_rotation_capability,
     type_args: [],
     args: [recovery_address: Address],
-    doc: "Add the `KeyRotationCapability` for `to_recover_account` to the `RecoveryAddress`\
-          resource under `recovery_address`. Aborts if `to_recovery_account` and\
-          `to_recovery_address belong to different VASPs, if `recovery_address` does not have a\
-          `RecoveryAddress` resource, or if `to_recover_account` has already extracted its\
+    doc: "Add the `KeyRotationCapability` for `to_recover_account` to the `RecoveryAddress` \
+          resource under `recovery_address`. Aborts if `to_recovery_account` and \
+          `to_recovery_address belong to different VASPs, if `recovery_address` does not have a \
+          `RecoveryAddress` resource, or if `to_recover_account` has already extracted its \
           `KeyRotationCapability`."
 }
 
@@ -157,8 +157,8 @@ register_all_txn_scripts! {
     builder: encode_add_validator_script,
     type_args: [],
     args: [new_validator: Address],
-    doc: "Encode a program adding `new_validator` to the pending validator set. Fails if the\
-          `new_validator` address is already in the validator set, already in the pending validator set,\
+    doc: "Encode a program adding `new_validator` to the pending validator set. Fails if the \
+          `new_validator` address is already in the validator set, already in the pending validator set, \
           or does not have a `ValidatorConfig` resource stored at the address"
 }
 
@@ -167,8 +167,8 @@ register_all_txn_scripts! {
     builder: encode_burn_script,
     type_args: [type_],
     args: [nonce: U64, preburn_address: Address],
-    doc: "Permanently destroy the coins stored in the oldest burn request under the `Preburn`\
-          resource stored at `preburn_address`. This will only succeed if the sender has a\
+    doc: "Permanently destroy the coins stored in the oldest burn request under the `Preburn` \
+          resource stored at `preburn_address`. This will only succeed if the sender has a \
           `MintCapability` stored under their account and `preburn_address` has a pending burn request"
 }
 
@@ -177,7 +177,7 @@ register_all_txn_scripts! {
     builder: encode_burn_txn_fees_script,
     type_args: [currency],
     args: [],
-    doc: "Burn transaction fees that have been collected in the given `currency`,\
+    doc: "Burn transaction fees that have been collected in the given `currency`, \
           and relinquish to the association. The currency must be non-synthetic."
 }
 
@@ -186,7 +186,7 @@ register_all_txn_scripts! {
     builder: encode_cancel_burn_script,
     type_args: [type_],
     args: [preburn_address: Address],
-    doc: "Cancel the oldest burn request from `preburn_address` and return the funds to\
+    doc: "Cancel the oldest burn request from `preburn_address` and return the funds to \
           `preburn_address`.  Fails if the sender does not have a published `MintCapability`."
 }
 
@@ -195,10 +195,10 @@ register_all_txn_scripts! {
     builder: encode_create_recovery_address,
     type_args: [],
     args: [],
-    doc: "Extract the `KeyRotationCapability` for `recovery_account` and publish it in a\
-          `RecoveryAddress` resource under  `recovery_account`. Aborts if `recovery_account` has\
-           delegated its `KeyRotationCapability`, already has a`RecoveryAddress` resource, or is\
-           not a VASP. Cancel the oldest burn request from `preburn_address` and return the funds\
+    doc: "Extract the `KeyRotationCapability` for `recovery_account` and publish it in a \
+          `RecoveryAddress` resource under  `recovery_account`. Aborts if `recovery_account` has \
+           delegated its `KeyRotationCapability`, already has a`RecoveryAddress` resource, or is \
+           not a VASP. Cancel the oldest burn request from `preburn_address` and return the funds \
            to `preburn_address`.  Fails if the sender does not have a published `MintCapability`."
 }
 
@@ -207,11 +207,11 @@ register_all_txn_scripts! {
     builder: encode_transfer_with_metadata_script,
     type_args: [coin_type],
     args: [recipient_address: Address, amount: U64, metadata: Bytes, metadata_signature: Bytes],
-    doc: "Encode a program transferring `amount` coins to `recipient_address` with (optional)\
-          associated metadata `metadata` and (optional) `signature` on the metadata, amount, and\
-          sender address. The `metadata` and `signature` parameters are only required if\
-          `amount` >= 1000 LBR and the sender and recipient of the funds are two distinct VASPs.\
-          Fails if there is no account at the recipient address or if the sender's balance is lower\
+    doc: "Encode a program transferring `amount` coins to `recipient_address` with (optional) \
+          associated metadata `metadata` and (optional) `signature` on the metadata, amount, and \
+          sender address. The `metadata` and `signature` parameters are only required if \
+          `amount` >= 1000 LBR and the sender and recipient of the funds are two distinct VASPs. \
+          Fails if there is no account at the recipient address or if the sender's balance is lower \
           than `amount`"
 }
 
@@ -220,7 +220,7 @@ register_all_txn_scripts! {
     builder: encode_preburn_script,
     type_args: [type_],
     args: [amount: U64],
-    doc: "Preburn `amount` coins from the sender's account. This will only succeed if the sender\
+    doc: "Preburn `amount` coins from the sender's account. This will only succeed if the sender \
           already has a published `Preburn` resource."
 }
 
@@ -229,10 +229,10 @@ register_all_txn_scripts! {
     builder: encode_publish_shared_ed25519_public_key_script,
     type_args: [],
     args: [public_key: Bytes],
-    doc: "(1) Rotate the authentication key of the sender to `public_key`\
-          (2) Publish a resource containing a 32-byte ed25519 public key and the rotation capability\
-          of the sender under the sender's address.\
-          Aborts if the sender already has a `SharedEd25519PublicKey` resource.\
+    doc: "(1) Rotate the authentication key of the sender to `public_key` \
+          (2) Publish a resource containing a 32-byte ed25519 public key and the rotation capability \
+          of the sender under the sender's address. \
+          Aborts if the sender already has a `SharedEd25519PublicKey` resource. \
           Aborts if the length of `new_public_key` is not 32."
 }
 
@@ -241,7 +241,7 @@ register_all_txn_scripts! {
     builder: encode_add_currency_to_account_script,
     type_args: [currency],
     args: [],
-    doc: "Add the currency identified by the type `currency` to the sending accounts.\
+    doc: "Add the currency identified by the type `currency` to the sending accounts. \
           Aborts if the account already holds a balance fo `currency` type."
 }
 
@@ -250,7 +250,7 @@ register_all_txn_scripts! {
     builder: encode_register_preburner_script,
     type_args: [type_],
     args: [],
-    doc: "Publish a newly created `Preburn` resource under the sender's account.\
+    doc: "Publish a newly created `Preburn` resource under the sender's account. \
           This will fail if the sender already has a published `Preburn` resource."
 }
 
@@ -265,8 +265,8 @@ register_all_txn_scripts! {
         fullnodes_network_identity_pubkey: Bytes,
         fullnodes_network_address: Bytes
     ],
-    doc: "Encode a program registering the sender as a candidate validator with the given key information.\
-         `network_identity_pubkey` should be a X25519 public key\
+    doc: "Encode a program registering the sender as a candidate validator with the given key information. \
+         `network_identity_pubkey` should be a X25519 public key \
          `consensus_pubkey` should be a Ed25519 public c=key."
 }
 
@@ -275,7 +275,7 @@ register_all_txn_scripts! {
     builder: encode_remove_validator_script,
     type_args: [],
     args: [to_remove: Address],
-    doc: "Encode a program adding `to_remove` to the set of pending validator removals. Fails if\
+    doc: "Encode a program adding `to_remove` to the set of pending validator removals. Fails if \
           the `to_remove` address is already in the validator set or already in the pending removals."
 }
 
@@ -308,7 +308,7 @@ register_all_txn_scripts! {
     builder: encode_rotate_authentication_key_script,
     type_args: [],
     args: [new_hashed_key: Bytes],
-    doc: "Encode a program that rotates the sender's authentication key to `new_key`. `new_key`\
+    doc: "Encode a program that rotates the sender's authentication key to `new_key`. `new_key` \
           should be a 256 bit sha3 hash of an ed25519 public key."
 }
 
@@ -317,8 +317,8 @@ register_all_txn_scripts! {
     builder: encode_rotate_authentication_key_with_recovery_address_script,
     type_args: [],
     args: [recovery_address: Address, to_recover: Address, new_key: Bytes],
-    doc: "Rotate the authentication key of `to_recover` to `new_key`. Can be invoked by either\
-          `recovery_address` or `to_recover`. Aborts if `recovery_address` does not have the\
+    doc: "Rotate the authentication key of `to_recover` to `new_key`. Can be invoked by either \
+          `recovery_address` or `to_recover`. Aborts if `recovery_address` does not have the \
           `KeyRotationCapability` for `to_recover`."
 }
 
@@ -327,11 +327,11 @@ register_all_txn_scripts! {
     builder: encode_rotate_shared_ed25519_public_key_script,
     type_args: [],
     args: [new_public_key: Bytes],
-    doc: "(1) rotate the public key stored in the sender's `SharedEd25519PublicKey` resource to\
-          `new_public_key`\
-          (2) rotate the authentication key using the capability stored in the sender's\
-          `SharedEd25519PublicKey` to a new value derived from `new_public_key`\
-          Aborts if the sender does not have a `SharedEd25519PublicKey` resource.\
+    doc: "(1) rotate the public key stored in the sender's `SharedEd25519PublicKey` resource to \
+          `new_public_key` \
+          (2) rotate the authentication key using the capability stored in the sender's \
+          `SharedEd25519PublicKey` to a new value derived from `new_public_key` \
+          Aborts if the sender does not have a `SharedEd25519PublicKey` resource. \
           Aborts if the length of `new_public_key` is not 32."
 }
 
@@ -376,7 +376,7 @@ register_all_txn_scripts! {
     builder: encode_mint_lbr,
     type_args: [],
     args: [amount_lbr: U64],
-    doc: "Mints `amount_lbr` LBR from the sending account's constituent coins and deposits the\
+    doc: "Mints `amount_lbr` LBR from the sending account's constituent coins and deposits the \
           resulting LBR into the sending account."
 }
 
@@ -385,14 +385,13 @@ register_all_txn_scripts! {
     builder: encode_unmint_lbr,
     type_args: [],
     args: [amount_lbr: U64],
-    doc: "Unmints `amount_lbr` LBR from the sending account into the constituent coins and deposits\
+    doc: "Unmints `amount_lbr` LBR from the sending account into the constituent coins and deposits \
           the resulting coins into the sending account."
 }
 
 //...........................................................................
 //  Association-related scripts
 //...........................................................................
-
 
 {
     script: UpdateMintingAbility,
@@ -411,9 +410,9 @@ register_all_txn_scripts! {
     builder: encode_create_parent_vasp_account,
     type_args: [currency],
     args: [address: Address, auth_key_prefix: Bytes, human_name: Bytes, base_url: Bytes, compliance_public_key: Bytes, add_all_currencies: Bool],
-    doc: "Create an account with the ParentVASP role at `address` with authentication key\
-          `auth_key_prefix` | `new_account_address` and a 0 balance of type `currency`. If\
-          `add_all_currencies` is true, 0 balances for all available currencies in the system will\
+    doc: "Create an account with the ParentVASP role at `address` with authentication key \
+          `auth_key_prefix` | `new_account_address` and a 0 balance of type `currency`. If \
+          `add_all_currencies` is true, 0 balances for all available currencies in the system will \
           also be added. This can only be invoked by an Association account."
 }
 
@@ -422,10 +421,10 @@ register_all_txn_scripts! {
     builder: encode_create_child_vasp_account,
     type_args: [currency],
     args: [address: Address, auth_key_prefix: Bytes, add_all_currencies: Bool, initial_balance: U64],
-    doc: "Create an account with the ChildVASP role at `address` with authentication key\
-          `auth_key_prefix` | `new_account_address` and `initial_balance` of type `currency`\
-          transferred from the sender. If `add_all_currencies` is true, 0 balances for all\
-          available currencies in the system will also be added to the account. This account will\
+    doc: "Create an account with the ChildVASP role at `address` with authentication key \
+          `auth_key_prefix` | `new_account_address` and `initial_balance` of type `currency` \
+          transferred from the sender. If `add_all_currencies` is true, 0 balances for all \
+          available currencies in the system will also be added to the account. This account will \
           be a child of the transaction sender, which must be a ParentVASP."
 }
 
@@ -438,8 +437,8 @@ register_all_txn_scripts! {
     builder: encode_tiered_mint,
     type_args: [coin_type],
     args: [nonce: U64, designated_dealer_address: Address, mint_amount: U64, tier_index: U64],
-    doc: "Mints 'mint_amount' to 'designated_dealer_address' for 'tier_index' tier.\
-          Max valid tier index is 3 since there are max 4 tiers per DD.
+    doc: "Mints 'mint_amount' to 'designated_dealer_address' for 'tier_index' tier. \
+          Max valid tier index is 3 since there are max 4 tiers per DD. \
           Sender should be treasury compliance account and receiver authorized DD"
 }
 
@@ -472,7 +471,7 @@ register_all_txn_scripts! {
     builder: encode_rotate_authentication_key_script_with_nonce,
     type_args: [],
     args: [nonce: U64, new_hashed_key: Bytes],
-    doc: "Encode a program that rotates the sender's authentication key to `new_key`. `new_key`\
+    doc: "Encode a program that rotates the sender's authentication key to `new_key`. `new_key` \
           should be a 256 bit sha3 hash of an ed25519 public key. This script also takes nonce"
 }
 
