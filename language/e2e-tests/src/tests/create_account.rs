d90d880cb5293e5d86b2f74cfd9dff540f3030f8
Runtian Zhou <runtianz@fb.com>
2020-06-16 15:48:22 -0700

    [language_e2e_test] Provide function execution functionality in FakeExecutor
    Closes: #4550
    Approved by: sblackshear

--- language/e2e-tests/src/tests/create_account.rs
+++ language/e2e-tests/src/tests/create_account.rs
@@ -7,9 +7,11 @@ use crate::{
     executor::FakeExecutor,
 };
 use libra_types::{
+    account_config::lbr_type_tag,
     transaction::TransactionStatus,
     vm_error::{StatusCode, VMStatus},
 };
+use move_vm_types::values::Value;
 
 #[test]
 fn create_account() {
@@ -43,6 +45,31 @@ fn create_account() {
     assert_eq!(initial_amount, updated_receiver_balance.coin(),);
     assert_eq!(sender_balance, updated_sender_balance.coin(),);
     assert_eq!(11, updated_sender.sequence_number());
+}
+
+#[test]
+fn create_account_with_exec() {
+    let mut executor = FakeExecutor::from_genesis_file();
+    let new_account = Account::new();
+
+    // create an hosted account via function invokation.
+    executor.exec(
+        "LibraAccount",
+        "create_unhosted_account",
+        vec![lbr_type_tag()],
+        vec![
+            Value::address(*new_account.address()),
+            Value::vector_u8(new_account.auth_key_prefix()),
+            Value::bool(false),
+        ],
+        *new_account.address(),
+    );
+
+    // check that numbers in stored DB are correct
+    let updated_receiver_balance = executor
+        .read_balance_resource(&new_account, account::lbr_currency_code())
+        .expect("receiver balance must exist");
+    assert_eq!(0, updated_receiver_balance.coin());
 }
 
 #[test]
