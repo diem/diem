ea91dfcae1af0f09337b84c2ea4fc7236214a401
Shaz Qadeer <shaz@calibra.com>
2020-06-16 18:14:14 -0700

    add functionality for computing loops in a graph

--- language/move-prover/stackless-bytecode-generator/src/lib.rs
+++ language/move-prover/stackless-bytecode-generator/src/lib.rs
@@ -10,6 +10,7 @@ pub mod eliminate_imm_refs;
 pub mod eliminate_mut_refs;
 pub mod function_target;
 pub mod function_target_pipeline;
+pub mod graph;
 pub mod lifetime_analysis;
 pub mod livevar_analysis;
 pub mod packref_analysis;
@@ -18,3 +19,6 @@ pub mod stackless_bytecode;
 pub mod stackless_bytecode_generator;
 pub mod stackless_control_flow_graph;
 pub mod writeback_analysis;
+
+#[cfg(test)]
+pub mod unit_tests;
