7e2fb6d2cf1a105a31c128fc92d346397b64c524
David Wolinsky <davidiw@fb.com>
2020-06-19 17:36:14 -0700

    [config] remove ordered hash serializer
    This used to matter when the order of the config determined how genesis
    was constructed

--- config/src/utils.rs
+++ config/src/utils.rs
@@ -4,13 +4,8 @@
 use crate::config::NodeConfig;
 use get_if_addrs::get_if_addrs;
 use libra_network_address::{NetworkAddress, Protocol};
-use libra_types::{transaction::Transaction, PeerId};
-use serde::{Serialize, Serializer};
-use std::{
-    collections::{BTreeMap, HashMap},
-    hash::BuildHasher,
-    net::{TcpListener, TcpStream},
-};
+use libra_types::transaction::Transaction;
+use std::net::{TcpListener, TcpStream};
 
 /// Return an ephemeral, available port. On unix systems, the port returned will be in the
 /// TIME_WAIT state ensuring that the OS won't hand out this port for some grace period.
@@ -58,20 +53,6 @@ pub fn get_available_port_in_multiaddr(is_ipv4: bool) -> NetworkAddress {
         Protocol::Ip6("::1".parse().unwrap())
     };
     NetworkAddress::from(ip_proto).push(Protocol::Tcp(get_available_port()))
-}
-
-/// Serialize HashMaps as BTreeMaps for consistent ordering
-pub fn serialize_ordered_map<S, V, H>(
-    value: &HashMap<PeerId, V, H>,
-    serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-    H: BuildHasher,
-    V: Serialize,
-{
-    let ordered: BTreeMap<_, _> = value.iter().collect();
-    ordered.serialize(serializer)
 }
 
 pub fn get_genesis_txn(config: &NodeConfig) -> anyhow::Result<&Transaction> {
