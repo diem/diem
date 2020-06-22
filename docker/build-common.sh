c584ac9b45a4cf3a3e3e713550a9d9ea3680e20d
Andrey Chursin <achursin@calibra.com>
2020-06-22 16:35:51 -0700

    [build] Refactor common build cmd line into signle shell script
    We had same long build string and RUSTFLAGS scattered across many Dockerfiles.
    
    This makes update of docker files comlicated and increases chance of an error(for example, skipping some rust flag or not building some binary)
    
    It is also important that this script is same across all dockerfiles, as otherwise layer caching is not working effectively
    
    Closes: #4664
    Approved by: sausagee

---
+++ docker/build-common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+# This is a common compilation scripts across different docker file
+# It unifies RUSFLAGS, compilation flags (like --release) and set of binary crates to compile in common docker layer
+
+export RUSTFLAGS="-Ctarget-cpu=skylake -Ctarget-feature=+aes,+sse2,+sse4.1,+ssse3"
+cargo build --release -p libra-management -p libra-node -p cli -p config-builder -p libra-key-manager -p safety-rules -p db-bootstrapper -p backup-cli
+
+rm -r target/release/{build,deps,incremental}
+
+strip /libra/target/release/config-builder
+strip /libra/target/release/cli
+strip /libra/target/release/db-backup
+strip /libra/target/release/db-bootstrapper
+strip /libra/target/release/db-restore
+strip /libra/target/release/libra-management
