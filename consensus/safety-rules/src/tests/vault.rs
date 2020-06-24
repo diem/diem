1849faa18a62b977dcb8131d07590fc5e4161bda
David Wolinsky <davidiw@fb.com>
2020-06-24 16:09:20 -0700

    [safety-rules] Simplify test interface
    Passing all the parameters around quickly becomes janky especially if we
    add more, this allows us to isolate different variants in a single spot.

--- consensus/safety-rules/src/tests/vault.rs
+++ consensus/safety-rules/src/tests/vault.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{tests::suite, PersistentSafetyStorage, SafetyRulesManager, TSafetyRules};
+use crate::{tests::suite, PersistentSafetyStorage, SafetyRulesManager};
 use libra_crypto::{ed25519::Ed25519PrivateKey, Uniform};
 use libra_secure_storage::{KVStorage, Storage, VaultStorage};
 use libra_types::validator_signer::ValidatorSigner;
@@ -12,41 +12,37 @@ use libra_types::validator_signer::ValidatorSigner;
 #[ignore]
 #[test]
 fn test() {
-    suite::run_test_suite(safety_rules, false);
-    suite::run_test_suite(safety_rules, true);
+    suite::run_test_suite(&safety_rules(false));
+    suite::run_test_suite(&safety_rules(true));
 }
 
-fn safety_rules(
-    verify_vote_proposal_signature: bool,
-) -> (
-    Box<dyn TSafetyRules>,
-    ValidatorSigner,
-    Option<Ed25519PrivateKey>,
-) {
-    let signer = ValidatorSigner::from_int(0);
-    let host = "http://localhost:8200".to_string();
-    let token = "root_token".to_string();
-    let mut storage = Storage::from(VaultStorage::new(host, token, None, None));
-    storage.reset_and_clear().unwrap();
+fn safety_rules(verify_vote_proposal_signature: bool) -> suite::Callback {
+    Box::new(move || {
+        let signer = ValidatorSigner::from_int(0);
+        let host = "http://localhost:8200".to_string();
+        let token = "root_token".to_string();
+        let mut storage = Storage::from(VaultStorage::new(host, token, None, None));
+        storage.reset_and_clear().unwrap();
 
-    let waypoint = crate::test_utils::validator_signers_to_waypoint(&[&signer]);
-    let storage = PersistentSafetyStorage::initialize(
-        storage,
-        signer.author(),
-        signer.private_key().clone(),
-        Ed25519PrivateKey::generate_for_testing(),
-        waypoint,
-    );
-    let safety_rules_manager =
-        SafetyRulesManager::new_local(storage, verify_vote_proposal_signature);
-    let safety_rules = safety_rules_manager.client();
-    (
-        safety_rules,
-        signer,
-        if verify_vote_proposal_signature {
-            Some(Ed25519PrivateKey::generate_for_testing())
-        } else {
-            None
-        },
-    )
+        let waypoint = crate::test_utils::validator_signers_to_waypoint(&[&signer]);
+        let storage = PersistentSafetyStorage::initialize(
+            storage,
+            signer.author(),
+            signer.private_key().clone(),
+            Ed25519PrivateKey::generate_for_testing(),
+            waypoint,
+        );
+        let safety_rules_manager =
+            SafetyRulesManager::new_local(storage, verify_vote_proposal_signature);
+        let safety_rules = safety_rules_manager.client();
+        (
+            safety_rules,
+            signer,
+            if verify_vote_proposal_signature {
+                Some(Ed25519PrivateKey::generate_for_testing())
+            } else {
+                None
+            },
+        )
+    })
 }
