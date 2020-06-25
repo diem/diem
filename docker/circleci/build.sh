4c56851c1a4c5bbe8d251730cffe4104b00bbd7d
Andrey Chursin <achursin@calibra.com>
2020-06-25 15:47:44 -0700

    [build] Delete circleci docker image

--- docker/circleci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright (c) The Libra Core Contributors
-# SPDX-License-Identifier: Apache-2.0
-set -e
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-$DIR/../libra-build.sh $DIR/Dockerfile github_circleci
