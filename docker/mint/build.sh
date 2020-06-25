57d87df8ac435a359a6224743ac2f7162eeb74c8
Andrey Chursin <achursin@calibra.com>
2020-06-24 20:18:43 -0700

    [build] Refactor build.sh to same pattern
    - Adds retry to every build.sh file - previously some of them had it, and some did not
    - Each build.sh now looks the same - it basically calls into libra-build.sh with appropriate docker file name
    - Remove build-in-container code from cluster-test - this was not used
    
    Closes: #4737
    Approved by: sausagee

--- docker/mint/build.sh
+++ docker/mint/build.sh
@@ -5,9 +5,4 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-PROXY=""
-if [ "$https_proxy" ]; then
-    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
-fi
-
-docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_mint  --build-arg GIT_REV=$(git rev-parse HEAD) --build-arg GIT_UPSTREAM=$(git merge-base --fork-point origin/master) --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY
+$DIR/../libra-build.sh $DIR/Dockerfile libra_mint
--- docker/validator/build.sh
+++ docker/validator/build.sh
@@ -5,9 +5,4 @@ set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-PROXY=""
-if [ "$https_proxy" ]; then
-    PROXY=" --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy"
-fi
-
-docker build -f $DIR/Dockerfile $DIR/../.. --tag libra_e2e --build-arg GIT_REV="$(git rev-parse HEAD)" --build-arg GIT_UPSTREAM="$(git merge-base HEAD origin/master)" --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" $PROXY "$@"
+$DIR/../libra-build.sh $DIR/../validator/Dockerfile libra_e2e
