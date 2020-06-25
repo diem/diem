fee095c1b6aa8b466ba5ff7d0669f1e438a25af2
David Wolinsky <davidiw@fb.com>
2020-06-24 18:55:51 -0700

    [safety-rules] make benchmark work again and run quickly
    * Sample size is down a lot and so is time to measure, adjust these if
    you need higher fidelity, but the numbers are sequentially larger which
    is what we expect.
    * Removed process since it adds a lot of time and proivdes little value
    and additional crate complexities -- will revisit later.
    
    Closes: #4735
    Approved by: aching

--- consensus/safety-rules/benches/safety_rules.rs
+++ consensus/safety-rules/benches/safety_rules.rs
@@ -1,14 +1,13 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use consensus_types::block::{block_test_utils, block_test_utils::random_payload, Block};
+use consensus_types::block::block_test_utils;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use libra_config::config::{OnDiskStorageConfig, SecureBackend};
 use libra_crypto::{ed25519::Ed25519PrivateKey, Uniform};
 use libra_secure_storage::{InMemoryStorage, OnDiskStorage, Storage};
 use libra_types::validator_signer::ValidatorSigner;
 use safety_rules::{
-    process_client_wrapper::ProcessClientWrapper, test_utils, PersistentSafetyStorage,
+    test_utils, PersistentSafetyStorage,
     SafetyRulesManager, TSafetyRules,
 };
 use tempfile::NamedTempFile;
@@ -16,11 +15,12 @@ use tempfile::NamedTempFile;
 /// Execute an in order series of blocks (0 <- 1 <- 2 <- 3 and commit 0 and continue to rotate
 /// left, appending new blocks on the right, committing the left most block
 fn lsr(mut safety_rules: Box<dyn TSafetyRules>, signer: ValidatorSigner, n: u64) {
-    let data = random_payload(2048);
+    let data = block_test_utils::random_payload(1);
 
-    let genesis_block = Block::make_genesis_block();
-    let genesis_qc = block_test_utils::certificate_for_genesis();
-    let mut round = genesis_block.round();
+    let (proof, genesis_qc) = test_utils::make_genesis(&signer);
+    safety_rules.initialize(&proof).unwrap();
+
+    let mut round = genesis_qc.certified_block().round();
 
     round += 1;
     let mut b0 = test_utils::make_proposal_with_qc(round, genesis_qc, &signer, None);
@@ -114,25 +114,14 @@ fn thread(n: u64) {
     lsr(safety_rules_manager.client(), signer, n);
 }
 
-fn process(n: u64) {
-    let file_path = NamedTempFile::new().unwrap().into_temp_path().to_path_buf();
-    let mut config = OnDiskStorageConfig::default();
-    config.path = file_path;
-    let backend = SecureBackend::OnDiskStorage(config);
-    let client_wrapper = ProcessClientWrapper::new(backend, false);
-    let signer = client_wrapper.signer();
-
-    lsr(Box::new(client_wrapper), signer, n);
-}
-
 pub fn benchmark(c: &mut Criterion) {
     let count = 100;
     let mut group = c.benchmark_group("SafetyRules");
+    group.measurement_time(std::time::Duration::from_secs(5)).sample_size(10);
     group.bench_function("InMemory", |b| b.iter(|| in_memory(black_box(count))));
     group.bench_function("OnDisk", |b| b.iter(|| on_disk(black_box(count))));
     group.bench_function("Serializer", |b| b.iter(|| serializer(black_box(count))));
     group.bench_function("Thread", |b| b.iter(|| thread(black_box(count))));
-    group.bench_function("Process", |b| b.iter(|| process(black_box(count))));
 }
 
 criterion_group!(benches, benchmark);
