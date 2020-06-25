2e4ca689e350f8252fe2bd8f970acba0f937b2b7
Andrey Chursin <achursin@calibra.com>
2020-06-25 16:16:10 -0700

    [build] set -e for build-common.sh

--- docker/build-common.sh
+++ docker/build-common.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Copyright (c) The Libra Core Contributors
 # SPDX-License-Identifier: Apache-2.0
+set -e
 
 # This is a common compilation scripts across different docker file
 # It unifies RUSFLAGS, compilation flags (like --release) and set of binary crates to compile in common docker layer
 
 export RUSTFLAGS="-Ctarget-cpu=skylake -Ctarget-feature=+aes,+sse2,+sse4.1,+ssse3"
-cargo build --release -p libra-management -p libra-node -p cli -p config-builder -p libra-key-manager -p safety-rules -p db-bootstrapper -p backup-cli
+cargo build --release -p libra-management -p libra-node -p cli -p config-builder -p libra-key-manager -p safety-rules -p db-bootstrapper -p backup-cli "$@"
 
 rm -r target/release/{build,deps,incremental}
 
