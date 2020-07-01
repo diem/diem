6c0f14128ece9b1afa9e816c5df7323a2d018012
jolestar <jolestar@gmail.com>
2020-07-01 18:37:51 +0800

    [types] Fix event_key json serialize and deserialize bug.
    Closes: #4865
    Approved by: matbd

--- types/src/unit_tests/contract_event_test.rs
+++ types/src/unit_tests/contract_event_test.rs
@@ -1,8 +1,12 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::contract_event::{ContractEvent, EventWithProof};
+use crate::{
+    contract_event::{ContractEvent, EventWithProof},
+    event::EventKey,
+};
 use lcs::test_helpers::assert_canonical_encode_decode;
+use move_core_types::language_storage::TypeTag;
 use proptest::prelude::*;
 
 proptest! {
@@ -15,4 +19,14 @@ proptest! {
     fn event_with_proof_lcs_roundtrip(event_with_proof in any::<EventWithProof>()) {
         assert_canonical_encode_decode(event_with_proof);
     }
+}
+
+#[test]
+fn test_event_json_serialize() {
+    let event_key = EventKey::random();
+    let contract_event = ContractEvent::new(event_key, 0, TypeTag::Address, vec![0u8]);
+    let contract_json =
+        serde_json::to_string(&contract_event).expect("event serialize to json should succeed.");
+    let contract_event2: ContractEvent = serde_json::from_str(contract_json.as_str()).unwrap();
+    assert_eq!(contract_event, contract_event2)
 }
