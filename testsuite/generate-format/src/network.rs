154ffdc00e4c68a8a5476de124ee52dc53ec3ccf
Philip Hayes <philiphayes@calibra.com>
2020-07-07 18:55:28 -0700

    [network] add encrypted addrs to generate-format
    Closes: #4950
    Approved by: gregnazario

--- testsuite/generate-format/src/network.rs
+++ testsuite/generate-format/src/network.rs
@@ -44,6 +44,8 @@ pub fn get_registry() -> Result<Registry> {
     tracer.trace_type::<handshake::v1::HandshakeMsg>(&samples)?;
     tracer.trace_type::<address::NetworkAddress>(&samples)?;
     tracer.trace_type::<address::RawNetworkAddress>(&samples)?;
+    tracer.trace_type::<address::encrypted::EncNetworkAddress>(&samples)?;
+    tracer.trace_type::<address::encrypted::RawEncNetworkAddress>(&samples)?;
 
     tracer.trace_type::<messaging::v1::ErrorCode>(&samples)?;
     tracer.trace_type::<handshake::v1::ProtocolId>(&samples)?;
