c6d60977a9e36838f7ecf0f7c7265180383489a8
David Wong <davidwg@calibra.com>
2020-07-08 20:17:54 -0700

    [consensus] removing MIRAI assumption that is false
    This assumption is false (as was found by fuzzing).

--- consensus/consensus-types/src/block_data.rs
+++ consensus/consensus-types/src/block_data.rs
@@ -98,11 +98,6 @@ impl BlockData {
     }
 
     pub fn round(&self) -> Round {
-        // Round numbers:
-        // - are reset to 0 periodically.
-        // - do not exceed std::u64::MAX - 2 per the 3 chain safety rule
-        // (ConsensusState::commit_rule_for_certified_block)
-        assumed_postcondition!(self.round < std::u64::MAX - 1);
         self.round
     }
 
