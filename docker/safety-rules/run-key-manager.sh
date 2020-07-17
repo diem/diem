d5f70262ff925740e7f0a8c927b03ac82a4d8404
Joshua Lind <joshlind@fb.com>
2020-07-17 15:45:37 -0700

    [Docker] Update Key Manager Docker containers to support chain ids and naming.

--- docker/safety-rules/run-key-manager.sh
+++ docker/safety-rules/run-key-manager.sh
@@ -4,7 +4,8 @@
 set -e
 
 # Example Usage:
-# ./run-key-manager.sh <IMAGE> <JSON_RPC_ENDPOINT> <ROTATION_PERIOD_SECS> <STRUCT_LOGGER> <STRUCT_LOGGER_LOCATION> <VAULT_HOST> <VAULT_TOKEN>
+# ./run-key-manager.sh <IMAGE> <CFG_CHAIN_ID> <CFG_JSON_RPC_ENDPOINT> <CFG_ROTATION_PERIOD_SECS> \
+#   <CFG_VAULT_HOST> <CFG_VAULT_TOKEN> <STRUCT_LOGGER> <STRUCT_LOGGER_LOCATION>
 #
 # Note:
 # (i) if arguments are not specified they will be assigned defaults.
@@ -12,22 +13,24 @@ set -e
 #      STRUCT_LOGGER_LOCATION set according to the chosen logger.
 
 IMAGE="${1:-libra_safety_rules:latest}"
-JSON_RPC_ENDPOINT="${2:-http://127.0.0.1:8080}"
-ROTATION_PERIOD_SECS="${3:-3600}"
-STRUCT_LOGGER="${4:-STRUCT_LOG_UDP_ADDR}"
-STRUCT_LOGGER_LOCATION="${5:-127.0.0.1:5044}"
-VAULT_HOST="${6:-http://127.0.0.1:8200}"
-VAULT_TOKEN="${7:-root_token}"
+CFG_CHAIN_ID="${2:-TESTING}"
+CFG_JSON_RPC_ENDPOINT="${3:-http://127.0.0.1:8080}"
+CFG_ROTATION_PERIOD_SECS="${4:-3600}"
+CFG_VAULT_HOST="${5:-http://127.0.0.1:8200}"
+CFG_VAULT_TOKEN="${6:-root_token}"
+STRUCT_LOGGER="${7:-STRUCT_LOG_UDP_ADDR}"
+STRUCT_LOGGER_LOCATION="${8:-127.0.0.1:5044}"
 
 docker network create --subnet 172.18.0.0/24 testnet || true
 
 docker run \
-    -e JSON_RPC_ENDPOINT="$JSON_RPC_ENDPOINT" \
-    -e ROTATION_PERIOD_SECS="$ROTATION_PERIOD_SECS" \
+    -e CFG_CHAIN_ID="$CFG_CHAIN_ID" \
+    -e CFG_JSON_RPC_ENDPOINT="$CFG_JSON_RPC_ENDPOINT" \
+    -e CFG_ROTATION_PERIOD_SECS="$CFG_ROTATION_PERIOD_SECS" \
+    -e CFG_VAULT_HOST="$CFG_VAULT_HOST" \
+    -e CFG_VAULT_TOKEN="$CFG_VAULT_TOKEN" \
     -e STRUCT_LOGGER="$STRUCT_LOGGER" \
     -e STRUCT_LOGGER_LOCATION="$STRUCT_LOGGER_LOCATION" \
-    -e VAULT_HOST="$VAULT_HOST" \
-    -e VAULT_TOKEN="$VAULT_TOKEN" \
     --ip 172.18.0.3 \
     --network testnet \
     "$IMAGE" \
