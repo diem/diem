9b6ca974be24d83fc47e05f57014fa6242f2cbe4
Young Yang Liauw <youngyl@fb.com>
2020-07-17 20:53:49 -0700

    [docker] update libra genesis tool
    Update libra-genesis-tool after split from libra-management.

--- docker/build-common.sh
+++ docker/build-common.sh
@@ -7,7 +7,7 @@ set -e
 # It unifies RUSFLAGS, compilation flags (like --release) and set of binary crates to compile in common docker layer
 
 export RUSTFLAGS="-Ctarget-cpu=skylake -Ctarget-feature=+aes,+sse2,+sse4.1,+ssse3"
-cargo build --release -p libra-management -p libra-node -p cli -p config-builder -p libra-key-manager -p safety-rules -p db-bootstrapper -p backup-cli -p cluster-test "$@"
+cargo build --release -p libra-genesis-tool -p libra-node -p cli -p config-builder -p libra-key-manager -p safety-rules -p db-bootstrapper -p backup-cli -p cluster-test "$@"
 
 rm -rf target/release/{build,deps,incremental}
 
