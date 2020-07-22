cf7be7b5898f43c10e702fcf10eca1052ad11b84
Mathieu Baudet <mathieubaudet@calibra.com>
2020-07-22 14:24:12 -0700

    [transaction builder generator] Support decoding of `Script` data into a structured representation
    Closes: #5347
    Approved by: sblackshear

--- language/transaction-builder/generator/examples/rust/stdlib_demo.rs
+++ language/transaction-builder/generator/examples/rust/stdlib_demo.rs
@@ -2,8 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use libra_canonical_serialization as lcs;
-use libra_stdlib::encode_peer_to_peer_with_metadata_script;
+use libra_stdlib::{encode_peer_to_peer_with_metadata_script, ScriptCall};
 use libra_types::{AccountAddress, Identifier, TypeTag, StructTag};
+use serde_bytes::ByteBuf as Bytes;
 
 fn main() {
     let token = TypeTag::Struct(StructTag {
@@ -16,7 +17,16 @@ fn main() {
                                 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22]);
     let amount = 1234567;
     let script =
-        encode_peer_to_peer_with_metadata_script(token, payee, amount, Vec::new(), Vec::new());
+        encode_peer_to_peer_with_metadata_script(token, payee.clone(), amount, Bytes::from(Vec::new()), Bytes::from(Vec::new()));
+
+    let call = ScriptCall::decode(&script);
+    match call {
+        Some(ScriptCall::PeerToPeerWithMetadata { amount: a, payee: p, .. }) => {
+            assert_eq!(a, amount);
+            assert_eq!(p, payee);
+        }
+        _ => panic!("unexpected type of script"),
+    }
 
     let output = lcs::to_bytes(&script).unwrap();
     for o in output {
--- language/transaction-builder/generator/src/common.rs
+++ language/transaction-builder/generator/src/common.rs
@@ -1,7 +1,11 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+use heck::CamelCase;
+use libra_types::transaction::{ArgumentABI, ScriptABI, TypeArgumentABI};
 use move_core_types::language_storage::TypeTag;
+use serde_reflection::{ContainerFormat, Format, Named, VariantFormat};
+use std::collections::BTreeMap;
 
 /// Useful error message.
 pub(crate) fn type_not_allowed(type_tag: &TypeTag) -> ! {
@@ -13,12 +17,58 @@ pub(crate) fn type_not_allowed(type_tag: &TypeTag) -> ! {
 
 /// Clean up doc comments extracter by the Move prover.
 pub(crate) fn prepare_doc_string(doc: &str) -> String {
-    let mut doc = doc.replace("\n", " ").trim().to_string();
-    loop {
-        let doc2 = doc.replace("  ", " ");
-        if doc == doc2 {
-            return doc;
-        }
-        doc = doc2;
+    doc.replace("\n ", "\n").trim().to_string()
+}
+
+fn quote_type_as_format(type_tag: &TypeTag) -> Format {
+    use TypeTag::*;
+    match type_tag {
+        Bool => Format::Bool,
+        U8 => Format::U8,
+        U64 => Format::U64,
+        U128 => Format::U128,
+        Address => Format::TypeName("AccountAddress".into()),
+        Vector(type_tag) => match type_tag.as_ref() {
+            U8 => Format::Bytes,
+            _ => type_not_allowed(type_tag),
+        },
+
+        Struct(_) | Signer => type_not_allowed(type_tag),
     }
+}
+
+fn quote_type_parameter_as_field(ty_arg: &TypeArgumentABI) -> Named<Format> {
+    Named {
+        name: ty_arg.name().to_string(),
+        value: Format::TypeName("TypeTag".into()),
+    }
+}
+
+fn quote_parameter_as_field(arg: &ArgumentABI) -> Named<Format> {
+    Named {
+        name: arg.name().to_string(),
+        value: quote_type_as_format(arg.type_tag()),
+    }
+}
+
+pub(crate) fn make_abi_enum_container(abis: &[ScriptABI]) -> ContainerFormat {
+    let mut variants = BTreeMap::new();
+    for (index, abi) in abis.iter().enumerate() {
+        let mut fields = Vec::new();
+        for ty_arg in abi.ty_args() {
+            fields.push(quote_type_parameter_as_field(ty_arg));
+        }
+        for arg in abi.args() {
+            fields.push(quote_parameter_as_field(arg));
+        }
+        let format = VariantFormat::Struct(fields);
+        variants.insert(
+            index as u32,
+            Named {
+                name: abi.name().to_camel_case(),
+                value: format,
+            },
+        );
+    }
+    ContainerFormat::Enum(variants)
 }
