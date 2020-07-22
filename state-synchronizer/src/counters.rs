ae0d883673dee92c7710fdd40f30dd0dc2cb25fe
Christina Lee <ccelee@fb.com>
2020-07-22 14:13:17 -0700

    [state sync] update counters/dashboards
    Closes: #5253
    Approved by: zekun000

--- state-synchronizer/src/counters.rs
+++ state-synchronizer/src/counters.rs
@@ -2,10 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use libra_metrics::{
-    register_histogram, register_int_counter, register_int_counter_vec, register_int_gauge,
-    DurationHistogram, IntCounter, IntCounterVec, IntGauge,
+    register_histogram, register_histogram_vec, register_int_counter, register_int_counter_vec,
+    register_int_gauge, register_int_gauge_vec, DurationHistogram, Histogram, HistogramVec,
+    IntCounter, IntCounterVec, IntGauge, IntGaugeVec,
 };
 use once_cell::sync::Lazy;
+
+// network send result labels
+pub const SEND_SUCCESS_LABEL: &str = "success";
+pub const SEND_FAIL_LABEL: &str = "fail";
+
+// msg type labels
+pub const SYNC_MSG_LABEL: &str = "sync";
+pub const COMMIT_MSG_LABEL: &str = "commit";
+pub const CHUNK_REQUEST_MSG_LABEL: &str = "chunk_request";
+pub const CHUNK_RESPONSE_MSG_LABEL: &str = "chunk_response";
+
+// msg sender label
+pub const CONSENSUS_SENDER_LABEL: &str = "consensus";
+
+// version type labels
+pub const COMMITTED_VERSION_LABEL: &str = "committed"; // Version of latest ledger info committed.
+pub const SYNCED_VERSION_LABEL: &str = "synced"; // Version of most recent txn that was synced (even if it is not backed by an LI)
+pub const TARGET_VERSION_LABEL: &str = "target"; // Version a node is trying to catch up to
 
 /// Counter of pending network events to State Synchronizer
 pub static PENDING_STATE_SYNCHRONIZER_NETWORK_EVENTS: Lazy<IntCounterVec> = Lazy::new(|| {
@@ -24,17 +43,16 @@ pub static REQUESTS_SENT: Lazy<IntCounterVec> = Lazy::new(|| {
         // metric description
         "Number of sync requests sent from a node",
         // metric labels
-        &["requested_peer_id"]
+        &["requested_peer_id", "result"]
     )
     .unwrap()
 });
 
-/// Number of sync responses a node received
-pub static RESPONSES_RECEIVED: Lazy<IntCounterVec> = Lazy::new(|| {
+pub static RESPONSES_SENT: Lazy<IntCounterVec> = Lazy::new(|| {
     register_int_counter_vec!(
-        "libra_state_sync_responses_received_total",
-        "Number of sync responses a node received",
-        &["response_sender_id"]
+        "libra_state_sync_responses_sent_total",
+        "Number of sync responses sent from a node",
+        &["recipient", "result"]
     )
     .unwrap()
 });
@@ -59,12 +77,12 @@ pub static APPLY_CHUNK_FAILURE: Lazy<IntCounterVec> = Lazy::new(|| {
     .unwrap()
 });
 
-/// Count the overall number of transactions state synchronizer has retrieved since last restart.
-/// Large values mean that a node has been significantly behind and had to replay a lot of txns.
-pub static STATE_SYNC_TXN_REPLAYED: Lazy<IntCounter> = Lazy::new(|| {
-    register_int_counter!(
-        "libra_state_sync_txns_replayed_total",
-        "Number of transactions the state synchronizer has retrieved since last restart"
+/// Average number of transactions in a received chunk response
+pub static STATE_SYNC_CHUNK_SIZE: Lazy<HistogramVec> = Lazy::new(|| {
+    register_histogram_vec!(
+        "libra_state_sync_chunk_size",
+        "Number of transactions in a state sync chunk response",
+        &["sender"]
     )
     .unwrap()
 });
@@ -79,11 +97,11 @@ pub static ACTIVE_UPSTREAM_PEERS: Lazy<IntGauge> = Lazy::new(|| {
     .unwrap()
 });
 
-/// Most recent version that has been committed
-pub static COMMITTED_VERSION: Lazy<IntGauge> = Lazy::new(|| {
-    register_int_gauge!(
-        "libra_state_sync_committed_version",
-        "Most recent version that has been committed"
+pub static VERSION: Lazy<IntGaugeVec> = Lazy::new(|| {
+    register_int_gauge_vec!(
+        "libra_state_sync_version",
+        "Version involved in state sync progress",
+        &["type"] // see version type labels above
     )
     .unwrap()
 });
@@ -98,15 +116,6 @@ pub static SYNC_PROGRESS_DURATION: Lazy<DurationHistogram> = Lazy::new(|| {
         )
         .unwrap()
     )
-});
-
-/// Version a node is trying to catch up to
-pub static TARGET_VERSION: Lazy<IntGauge> = Lazy::new(|| {
-    register_int_gauge!(
-        "libra_state_sync_target_version",
-        "Version a node is trying to catch up to"
-    )
-    .unwrap()
 });
 
 /// Number of timeouts that occur during sync
@@ -126,4 +135,32 @@ pub static COMMIT_TIMEOUT: Lazy<IntCounterVec> = Lazy::new(|| {
         &["component"] // component with which state sync timed out with: consensus, mempool
     )
         .unwrap()
+});
+
+/// Time it takes for state sync to fully execute a chunk (via executor proxy)
+pub static EXECUTE_CHUNK_DURATION: Lazy<Histogram> = Lazy::new(|| {
+    register_histogram!(
+        "libra_state_sync_execute_chunk_duration_s",
+        "Histogram of time it takes for state sync's executor proxy to fully execute a chunk"
+    )
+    .unwrap()
+});
+
+/// Number of times a long-poll subscription is delivered
+pub static SUBSCRIPTION_DELIVERY_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_state_sync_subscription_delivery_count",
+        "Number of times a node delivers a subscription for FN long-poll"
+    )
+    .unwrap()
+});
+
+/// Time it takes to process a state sync message
+pub static PROCESS_MSG_LATENCY: Lazy<HistogramVec> = Lazy::new(|| {
+    register_histogram_vec!(
+        "libra_state_sync_process_msg_latency",
+        "Time it takes to process a message in state sync",
+        &["type", "sender"]
+    )
+    .unwrap()
 });
