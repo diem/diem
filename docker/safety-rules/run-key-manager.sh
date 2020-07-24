5bc407be43b898b8ca6e064dc6586025935ad760
Greg Nazario <gnazario@fb.com>
2020-07-24 16:32:26 -0700

    [logging] moved structured logging to use TCP instead of UDP
    In order to decrease the likelihood of dropped messages, we're movin  to
    TCP instead of UDP to send messages.  This will ensure that the
    successfully sent messages actually make it, but we will still be
    dropping messages in the event of overloads or errors.

--- docker/safety-rules/run-key-manager.sh
+++ docker/safety-rules/run-key-manager.sh
@@ -9,7 +9,7 @@ set -e
 #
 # Note:
 # (i) if arguments are not specified they will be assigned defaults.
-# (ii) structured logger (STRUCT_LOGGER) must be one of STRUCT_LOG_FILE or STRUCT_LOG_UDP_ADDR, with
+# (ii) structured logger (STRUCT_LOGGER) must be one of STRUCT_LOG_FILE or STRUCT_LOG_TCP_ADDR, with
 #      STRUCT_LOGGER_LOCATION set according to the chosen logger.
 
 IMAGE="${1:-libra_safety_rules:latest}"
@@ -18,7 +18,7 @@ CFG_JSON_RPC_ENDPOINT="${3:-http://127.0.0.1:8080}"
 CFG_ROTATION_PERIOD_SECS="${4:-3600}"
 CFG_VAULT_HOST="${5:-http://127.0.0.1:8200}"
 CFG_VAULT_TOKEN="${6:-root_token}"
-STRUCT_LOGGER="${7:-STRUCT_LOG_UDP_ADDR}"
+STRUCT_LOGGER="${7:-STRUCT_LOG_TCP_ADDR}"
 STRUCT_LOGGER_LOCATION="${8:-127.0.0.1:5044}"
 
 docker network create --subnet 172.18.0.0/24 testnet || true
