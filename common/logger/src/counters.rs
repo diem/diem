9c09a6394c608457c2d294a7b9ba8d7df46b6b6b
Greg Nazario <gnazario@fb.com>
2020-07-28 11:08:38 -0700

    [logging] Added metrics around submission of struct logs
    In order to have higher confidence around logging, this adds a slew of
    counters to keep track of errors, and sent messages.  Because if you
    can't log it, then you'll have to use metrics to know what's going on.
    
    Closes: #5309
    Approved by: andll

--- common/logger/src/counters.rs
+++ common/logger/src/counters.rs
@@ -4,7 +4,77 @@
 use once_cell::sync::Lazy;
 use prometheus::{register_int_counter, IntCounter};
 
-/// Count of the struct logs.
+/// Count of the struct logs submitted by macro
 pub static STRUCT_LOG_COUNT: Lazy<IntCounter> = Lazy::new(|| {
     register_int_counter!("libra_struct_log_count", "Count of the struct logs.").unwrap()
+});
+
+/// Count of struct logs processed, but not necessarily sent
+pub static PROCESSED_STRUCT_LOG_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_processed_count",
+        "Count of the struct logs received by the sender."
+    )
+    .unwrap()
+});
+
+/// Count of struct logs submitted through TCP
+pub static SENT_STRUCT_LOG_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_tcp_submit_count",
+        "Count of the struct logs submitted by TCP."
+    )
+    .unwrap()
+});
+
+/// Count of logs failed to be submitted (but not by the queue)
+pub static DROPPED_STRUCT_LOG_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_intentionally_dropped_count",
+        "Count of the intentionally dropped struct logs."
+    )
+    .unwrap()
+});
+
+/// Metric for when we connect the outbound TCP
+pub static STRUCT_LOG_TCP_CONNECT_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_tcp_connect_count",
+        "Count of the tcp connections made for struct logs."
+    )
+    .unwrap()
+});
+
+/// Metric for when we fail to log during sending to the queue
+pub static STRUCT_LOG_QUEUE_ERROR_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_queue_error_count",
+        "Count of all errors during queuing struct logs."
+    )
+    .unwrap()
+});
+
+/// Metric for when we fail to log during sending
+pub static STRUCT_LOG_SEND_ERROR_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_send_error_count",
+        "Count of all errors during sending struct logs."
+    )
+    .unwrap()
+});
+
+pub static STRUCT_LOG_CONNECT_ERROR_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_connect_error_count",
+        "Count of all errors during connecting for struct logs."
+    )
+    .unwrap()
+});
+
+pub static STRUCT_LOG_PARSE_ERROR_COUNT: Lazy<IntCounter> = Lazy::new(|| {
+    register_int_counter!(
+        "libra_struct_log_parse_erro_count",
+        "Count of all parse errors during struct logs."
+    )
+    .unwrap()
 });
