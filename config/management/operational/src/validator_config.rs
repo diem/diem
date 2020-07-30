7b9e90a808034cc31c991aa99264c515a0aace19
Greg Nazario <gnazario@fb.com>
2020-07-30 14:35:07 -0700

    [management] simplify output of operational commands
    This now outputs all commands as a JSON blob no matter what the output
    type was.  ValidatorInfo drops voting power, ValidatorConfig named
    simpler, and output keys are dropped from rotation outputs.

--- config/management/operational/src/validator_config.rs
+++ config/management/operational/src/validator_config.rs
@@ -227,9 +227,9 @@ impl ValidatorConfig {
 #[derive(Serialize)]
 pub struct DecryptedValidatorConfig {
     pub consensus_public_key: Ed25519PublicKey,
-    pub validator_network_identity_public_key: x25519::PublicKey,
+    pub validator_network_key: x25519::PublicKey,
     pub validator_network_address: NetworkAddress,
-    pub full_node_network_identity_public_key: x25519::PublicKey,
+    pub full_node_network_key: x25519::PublicKey,
     pub full_node_network_address: NetworkAddress,
 }
 
@@ -252,11 +252,9 @@ impl DecryptedValidatorConfig {
 
         Ok(DecryptedValidatorConfig {
             consensus_public_key: validator_config.consensus_public_key.clone(),
-            validator_network_identity_public_key: validator_config
-                .validator_network_identity_public_key,
+            validator_network_key: validator_config.validator_network_identity_public_key,
             validator_network_address,
-            full_node_network_identity_public_key: validator_config
-                .full_node_network_identity_public_key,
+            full_node_network_key: validator_config.full_node_network_identity_public_key,
             full_node_network_address,
         })
     }
