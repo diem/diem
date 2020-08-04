c684bd7a619b881cc6a5ad61201b6e6ac50f6092
danielxiangzl <danielxiang@fb.com>
2020-08-04 16:26:13 -0500

    commit-strong-commit tradeoff

--- types/src/validator_verifier.rs
+++ types/src/validator_verifier.rs
@@ -226,7 +226,7 @@ impl ValidatorVerifier {
             }
         }
 
-        if aggregated_voting_power < self.quorum_voting_power {
+        if aggregated_voting_power < self.quorum_voting_power + (self.total_voting_power-self.quorum_voting_power) * 10 / 10 {
             return Err(VerifyError::TooLittleVotingPower {
                 voting_power: aggregated_voting_power,
                 quorum_voting_power: self.quorum_voting_power,
