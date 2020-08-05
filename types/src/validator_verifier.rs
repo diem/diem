5dafa4d24b6b80c9bc983165eb125cc896dd0456
danielxiangzl <danielxiang@fb.com>
2020-08-05 01:17:25 -0500

    test

--- types/src/validator_verifier.rs
+++ types/src/validator_verifier.rs
@@ -226,7 +226,7 @@ impl ValidatorVerifier {
             }
         }
 
-        if aggregated_voting_power < self.quorum_voting_power + (self.total_voting_power-self.quorum_voting_power) * 1 / 10 {
+        if aggregated_voting_power < self.quorum_voting_power + (self.total_voting_power-self.quorum_voting_power) * 0 / 10 {
             return Err(VerifyError::TooLittleVotingPower {
                 voting_power: aggregated_voting_power,
                 quorum_voting_power: self.quorum_voting_power,
