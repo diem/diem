e0e4abc1f67f461c8fc67611f0a85aaa91536f1e
danielxiangzl <danielxiang@fb.com>
2020-08-04 23:17:07 -0500

    test

--- types/src/validator_verifier.rs
+++ types/src/validator_verifier.rs
@@ -226,7 +226,7 @@ impl ValidatorVerifier {
             }
         }
 
-        if aggregated_voting_power < self.quorum_voting_power + (self.total_voting_power-self.quorum_voting_power) * 8 / 10 {
+        if aggregated_voting_power < self.quorum_voting_power + (self.total_voting_power-self.quorum_voting_power) * 7 / 10 {
             return Err(VerifyError::TooLittleVotingPower {
                 voting_power: aggregated_voting_power,
                 quorum_voting_power: self.quorum_voting_power,
