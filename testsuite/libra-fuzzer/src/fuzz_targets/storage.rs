5be521f89cf47ee262c5fb73cfb8863480f61725
David Wong <davidwg@calibra.com>
2020-08-17 10:52:04 -0700

    [fuzzing] adding helpers for structure fuzzing

--- testsuite/libra-fuzzer/src/fuzz_targets/storage.rs
+++ testsuite/libra-fuzzer/src/fuzz_targets/storage.rs
@@ -1,17 +1,12 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::FuzzTargetImpl;
+use crate::{corpus_from_strategy, fuzz_data_to_value, FuzzTargetImpl};
 use libra_proptest_helpers::ValueGenerator;
 use libradb::{
     schema::fuzzing::fuzz_decode, test_helper::arb_blocks_to_commit, test_save_blocks_impl,
 };
-use proptest::{
-    collection::vec,
-    prelude::*,
-    strategy::{Strategy, ValueTree},
-    test_runner::{self, TestRunner},
-};
+use proptest::{collection::vec, prelude::*};
 
 #[derive(Clone, Debug, Default)]
 pub struct StorageSaveBlocks;
@@ -21,21 +16,13 @@ impl FuzzTargetImpl for StorageSaveBlocks {
         "Storage save blocks"
     }
 
+    fn generate(&self, _idx: usize, _gen: &mut ValueGenerator) -> Option<Vec<u8>> {
+        Some(corpus_from_strategy(arb_blocks_to_commit()))
+    }
+
     fn fuzz(&self, data: &[u8]) {
-        let passthrough_rng =
-            test_runner::TestRng::from_seed(test_runner::RngAlgorithm::PassThrough, &data);
-
-        let config = test_runner::Config::default();
-        let mut runner = TestRunner::new_with_rng(config, passthrough_rng);
-
-        let strategy = arb_blocks_to_commit();
-        let strategy_tree = match strategy.new_tree(&mut runner) {
-            Ok(x) => x,
-            Err(_) => return,
-        };
-        let data = strategy_tree.current();
-
-        test_save_blocks_impl(data);
+        let input = fuzz_data_to_value(data, arb_blocks_to_commit());
+        test_save_blocks_impl(input);
     }
 }
 
--- testsuite/libra-fuzzer/src/lib.rs
+++ testsuite/libra-fuzzer/src/lib.rs
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use libra_proptest_helpers::ValueGenerator;
+use proptest::{
+    strategy::{Strategy, ValueTree},
+    test_runner::{self, RngAlgorithm, TestRunner},
+};
 use rand::RngCore;
 use std::{fmt, ops::Deref, str::FromStr};
 
@@ -9,7 +13,6 @@ pub mod commands;
 #[cfg(test)]
 mod coverage;
 pub mod fuzz_targets;
-
 /// Implementation for a particular target of a fuzz operation.
 pub trait FuzzTargetImpl: Sync + Send + fmt::Debug {
     /// The name of the fuzz target.
@@ -57,4 +60,40 @@ impl FromStr for FuzzTarget {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         FuzzTarget::by_name(s).ok_or_else(|| format!("Fuzz target '{}' not found (run `list`)", s))
     }
+}
+
+/// Helper to generate random bytes that can be used with proptest
+/// to generate a value following the passed strategy.
+fn corpus_from_strategy(strategy: impl Strategy) -> Vec<u8> {
+    // randomly-seeded recording RNG
+    let mut seed = [0u8; 32];
+    let mut rng = rand::thread_rng();
+    rng.fill_bytes(&mut seed);
+    let recorder_rng = test_runner::TestRng::from_seed(RngAlgorithm::Recorder, &seed);
+    let mut runner = TestRunner::new_with_rng(test_runner::Config::default(), recorder_rng);
+
+    // generate the value
+    strategy
+        .new_tree(&mut runner)
+        .expect("creating a new value should succeed")
+        .current();
+
+    // dump the bytes
+    runner.bytes_used()
+}
+
+/// Helper to convert a bytearray to a value implenting the Arbitrary trait.
+pub fn fuzz_data_to_value<T: std::fmt::Debug>(
+    data: &[u8],
+    strategy: impl Strategy<Value = T>,
+) -> T {
+    // setup proptest with passthrough RNG
+    let passthrough_rng =
+        test_runner::TestRng::from_seed(test_runner::RngAlgorithm::PassThrough, &data);
+    let config = test_runner::Config::default();
+    let mut runner = TestRunner::new_with_rng(config, passthrough_rng);
+
+    // create a value based on the arbitrary implementation of T
+    let strategy_tree = strategy.new_tree(&mut runner).expect("should not happen");
+    strategy_tree.current()
 }
