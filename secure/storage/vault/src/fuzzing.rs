b52f556c1c68b775acab9c3d58e4dd89af2d14cf
Joshua Lind <joshlind@fb.com>
2020-08-18 21:36:37 -0700

    [Fuzzing] Add fuzz target for process_policy_list_response() in vault client

--- secure/storage/vault/src/fuzzing.rs
+++ secure/storage/vault/src/fuzzing.rs
@@ -1,3 +1,4 @@
+use crate::ListPoliciesResponse;
 use libra_types::proptest_types::arb_json_value;
 use proptest::prelude::*;
 use serde_json::Value;
@@ -17,11 +18,30 @@ prop_compose! {
     }
 }
 
+// This generates an arbitrary policy list response returned by vault.
+prop_compose! {
+    pub fn arb_policy_list_response(
+    )(
+        status in any::<u16>(),
+        status_text in any::<String>(),
+        policies in prop::collection::vec(any::<String>(), 1..100)
+    ) -> Response {
+        let policy_list = ListPoliciesResponse {
+            policies,
+        };
+        let policy_list =
+            serde_json::to_string::<ListPoliciesResponse>(&policy_list).unwrap();
+        Response::new(status, &status_text, &policy_list)
+    }
+}
+
 // Note: these tests ensure that the various fuzzers are maintained (i.e., not broken
 // at some time in the future and only discovered when a fuzz test fails).
 #[cfg(test)]
 mod tests {
-    use crate::{fuzzing::arb_generic_response, process_generic_response};
+    use crate::{
+        fuzzing::arb_generic_response, process_generic_response, process_policy_list_response,
+    };
     use proptest::prelude::*;
 
     proptest! {
@@ -30,6 +50,11 @@ mod tests {
         #[test]
         fn process_generic_response_proptest(input in arb_generic_response()) {
             let _ = process_generic_response(input);
+        }
+
+        #[test]
+        fn process_policy_list_response_proptest(input in arb_policy_list_response()) {
+            let _ = process_policy_list_response(input);
         }
     }
 }
--- testsuite/libra-fuzzer/src/fuzz_targets/secure_storage_vault.rs
+++ testsuite/libra-fuzzer/src/fuzz_targets/secure_storage_vault.rs
@@ -3,12 +3,15 @@
 
 use crate::{corpus_from_strategy, fuzz_data_to_value, FuzzTargetImpl};
 use libra_proptest_helpers::ValueGenerator;
-use libra_vault_client::{fuzzing::arb_generic_response, process_generic_response};
+use libra_vault_client::{
+    fuzzing::{arb_generic_response, arb_policy_list_response},
+    process_generic_response, process_policy_list_response,
+};
 
 #[derive(Clone, Debug, Default)]
 pub struct VaultGenericResponse;
 
-/// This implementation will fuzz the process_generic_response(): the method used by the vault
+/// This implementation will fuzz process_generic_response(): the method used by the vault
 /// client to process generic responses from the vault backend.
 impl FuzzTargetImpl for VaultGenericResponse {
     fn description(&self) -> &'static str {
@@ -22,5 +25,25 @@ impl FuzzTargetImpl for VaultGenericResponse {
     fn fuzz(&self, data: &[u8]) {
         let input = fuzz_data_to_value(data, arb_generic_response());
         let _ = process_generic_response(input);
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct VaultPolicyListResponse;
+
+/// This implementation will fuzz process_policy_list_response(): the method used by the vault
+/// client to process policy lists from the vault backend.
+impl FuzzTargetImpl for VaultPolicyListResponse {
+    fn description(&self) -> &'static str {
+        "Secure storage vault: process_policy_list_response()"
+    }
+
+    fn generate(&self, _idx: usize, _gen: &mut ValueGenerator) -> Option<Vec<u8>> {
+        Some(corpus_from_strategy(arb_policy_list_response()))
+    }
+
+    fn fuzz(&self, data: &[u8]) {
+        let input = fuzz_data_to_value(data, arb_policy_list_response());
+        let _ = process_policy_list_response(input);
     }
 }
