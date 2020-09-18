d718857f1214522d71e76f9b4299f2dc9b080215
Xiao Li <ilx@fb.com>
2020-09-17 18:09:43 -0700

    graceful shutdown network runtimes in to avoid panic error log thread network- panicked at SelectNextSome polled after terminated

--- libra-node/src/main_node.rs
+++ libra-node/src/main_node.rs
@@ -36,10 +36,18 @@ pub struct LibraHandle {
     _rpc: Runtime,
     _mempool: Runtime,
     _state_synchronizer: StateSynchronizer,
-    _network_runtimes: Vec<Runtime>,
+    network_runtimes: Vec<Runtime>,
     _consensus_runtime: Option<Runtime>,
     _debug: NodeDebugService,
     _backup: Runtime,
+}
+
+impl Drop for LibraHandle {
+    fn drop(&mut self) {
+        while self.network_runtimes.len() > 0 {
+            self.network_runtimes.remove(0).shutdown_background();
+        }
+    }
 }
 
 // Fetch chain ID from on-chain resource
@@ -295,7 +303,7 @@ pub fn setup_environment(node_config: &NodeConfig, logger: Option<Arc<Logger>>) 
     }
 
     LibraHandle {
-        _network_runtimes: network_runtimes,
+        network_runtimes,
         _rpc: rpc_runtime,
         _mempool: mempool,
         _state_synchronizer: state_synchronizer,
