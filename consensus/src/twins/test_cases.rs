e01bd7487f63365551130992e7a5206beea68b37
Zekun Li <zekun@calibra.com>
2020-09-22 13:09:29 -0700

    [twins] calculate timeout from the test case

--- consensus/src/twins/test_cases.rs
+++ consensus/src/twins/test_cases.rs
@@ -1,10 +1,12 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use consensus_types::common::Round;
+use consensus_types::common::{Author, Round};
+use libra_config::config::RoundProposerConfig;
+use libra_types::account_address::AccountAddress;
 use serde::Deserialize;
 use serde::Serialize;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 type Idx = usize;
 
@@ -22,7 +24,7 @@ struct TestCases {
 }
 
 #[test]
-fn test_format() {
+fn test_case_format() {
     let sample = r#"
     {"num_of_nodes": 4,
      "num_of_twins": 1,
@@ -40,4 +42,60 @@ fn test_format() {
     assert_eq!(test_cases.num_of_nodes, 4);
     assert_eq!(test_cases.num_of_twins, 1);
     assert_eq!(test_cases.scenarios.len(), 2);
+}
+
+impl TestCase {
+    fn to_round_proposer_config(&self, authors: &Vec<Author>) -> Vec<RoundProposerConfig> {
+        let mut round_proposers = HashMap::new();
+        let mut timeouts: Vec<_> = authors.iter().map(|_| HashSet::new()).collect();
+        for (round, leader_idx) in &self.round_leaders {
+            let leader = authors[*leader_idx];
+            round_proposers.insert(*round, leader);
+            for partition in self.round_partitions.get(&round).unwrap() {
+                let with_leader = partition.iter().find(|idx| **idx == *leader_idx).is_some();
+                if !with_leader {
+                    for node_idx in partition {
+                        timeouts[*node_idx].insert(*round);
+                    }
+                }
+            }
+        }
+        timeouts
+            .into_iter()
+            .map(|timeout_rounds| RoundProposerConfig {
+                round_proposers: round_proposers.clone(),
+                timeout_rounds,
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn test_case_conversion() {
+    let round_leaders: HashMap<_, _> = vec![(1, 0), (2, 1), (3, 2)].into_iter().collect();
+    let round_partitions = vec![
+        (1, vec![vec![0, 1], vec![2, 3]]),
+        (2, vec![vec![0], vec![1, 2, 3]]),
+        (3, vec![vec![0, 3], vec![2], vec![1]]),
+    ]
+    .into_iter()
+    .collect();
+    let authors: Vec<_> = (0..4).map(|_| AccountAddress::random()).collect();
+    let expected_leaders: HashMap<Round, Author> = round_leaders
+        .iter()
+        .map(|(r, idx)| (*r, authors[*idx]))
+        .collect();
+    let test_case = TestCase {
+        round_leaders,
+        round_partitions,
+    };
+    let configs = test_case.to_round_proposer_config(&authors);
+    for config in &configs {
+        assert_eq!(config.round_proposers, expected_leaders);
+    }
+
+    assert_eq!(configs[0].timeout_rounds, vec![2, 3].into_iter().collect());
+    assert_eq!(configs[1].timeout_rounds, vec![3].into_iter().collect());
+    assert_eq!(configs[2].timeout_rounds, vec![1].into_iter().collect());
+    assert_eq!(configs[3].timeout_rounds, vec![1, 3].into_iter().collect());
 }
