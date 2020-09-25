19acdde2b89422a9f5a356b04dd8f56efc7add1b
Clark Barrett <cbarrett@fb.com>
2020-09-25 13:20:11 -0700

    Added install script for cvc4
    Closes: #6228

---
+++ language/move-prover/scripts/install-cvc4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+curl -L0 https://cvc4.cs.stanford.edu/downloads/builds/macos/cvc4-25-09-20.zip -o cvc4-25-09-20.zip
+unzip cvc4-25-09-20.zip
+sudo cp 25-09-20/cvc4 /usr/local/bin/
+rm -rf 25-09-20
+rm -rf cvc4-25-09-20.zip
