1ce8327b1ac7cca4c71c46d0a29fd8e42171f446
Joshua Lind <joshlind@fb.com>
2020-09-28 23:12:41 -0700

    [Smoke Tests] Support a threading model for the key manager smoke test.

--- testsuite/smoke-test/src/key_manager.rs
+++ testsuite/smoke-test/src/key_manager.rs
@@ -1,62 +1,63 @@
 // Copyright (c) The Libra Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{smoke_test_environment::SmokeTestEnvironment, workspace_builder};
-use libra_config::config::{Identity, KeyManagerConfig, NodeConfig};
+use crate::{smoke_test_environment::SmokeTestEnvironment, test_utils::load_backend_storage};
+use libc::pthread_cancel;
+use libra_config::config::NodeConfig;
 use libra_global_constants::CONSENSUS_KEY;
-use libra_key_manager::libra_interface::{JsonRpcLibraInterface, LibraInterface};
+use libra_key_manager::{
+    libra_interface::{JsonRpcLibraInterface, LibraInterface},
+    KeyManager,
+};
 use libra_secure_storage::{CryptoStorage, Storage};
-use std::{convert::TryInto, process::Command, thread::sleep, time::Duration};
-
-const KEY_MANAGER_BIN: &str = "libra-key-manager";
+use libra_secure_time::RealTimeService;
+use libra_types::chain_id::ChainId;
+use std::{
+    convert::TryInto, os::unix::thread::JoinHandleExt, thread, thread::sleep, time::Duration,
+};
 
 #[test]
 #[ignore]
 fn test_key_manager_consensus_rotation() {
-    // Create and launch a local validator swarm of 2 nodes.
-    let mut env = SmokeTestEnvironment::new(2);
+    // Start a key manager time service
+    let key_manager_time = RealTimeService::new();
+
+    // Create and launch a local validator swarm of 1 node.
+    let mut env = SmokeTestEnvironment::new(1);
     env.validator_swarm.launch();
 
-    // Create a node config for the key manager by extracting the first node config in the swarm.
+    // Fetch the first node config in the swarm
     let node_config_path = env.validator_swarm.config.config_files.get(0).unwrap();
     let node_config = NodeConfig::load(&node_config_path).unwrap();
-    let json_rpc_endpoint = format!("http://127.0.0.1:{}", node_config.rpc.address.port());
 
-    let mut key_manager_config = KeyManagerConfig::default();
-    key_manager_config.json_rpc_endpoint = json_rpc_endpoint.clone();
-    key_manager_config.rotation_period_secs = 10;
-    key_manager_config.sleep_period_secs = 1000; // Large sleep period to force a single rotation
-
-    // Load validator's on disk storage and update key manager secure backend in config
-    let storage: Storage = if let Identity::FromStorage(storage_identity) =
-        &node_config.validator_network.as_ref().unwrap().identity
-    {
-        let storage_backend = storage_identity.backend.clone();
-        key_manager_config.secure_backend = storage_backend.clone();
-        (&storage_backend).try_into().unwrap()
-    } else {
-        panic!("Couldn't load identity from storage");
-    };
-
-    // Save the key manager config to disk
-    let key_manager_config_path = node_config_path.with_file_name("key_manager.yaml");
-    key_manager_config.save(&key_manager_config_path).unwrap();
+    // Load validator's on disk storage
+    let secure_backend = load_backend_storage(&&node_config);
+    let storage: Storage = (&secure_backend).try_into().unwrap();
 
     // Create a json-rpc connection to the blockchain and verify storage matches the on-chain state.
-    let libra_interface = JsonRpcLibraInterface::new(json_rpc_endpoint);
+    let json_rpc_endpoint = format!("http://127.0.0.1:{}", node_config.rpc.address.port());
+    let libra_interface = JsonRpcLibraInterface::new(json_rpc_endpoint.clone());
     let account = node_config.validator_network.unwrap().peer_id();
     let current_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
     let validator_info = libra_interface.retrieve_validator_info(account).unwrap();
     assert_eq!(&current_consensus, validator_info.consensus_public_key());
 
-    // Spawn the key manager and sleep until a rotation occurs.
-    let mut command = Command::new(workspace_builder::get_bin(KEY_MANAGER_BIN));
-    command
-        .current_dir(workspace_builder::workspace_root())
-        .arg(key_manager_config_path);
+    // Create the key manager
+    let key_manager_storage: Storage = (&secure_backend).try_into().unwrap();
+    let mut key_manager = KeyManager::new(
+        JsonRpcLibraInterface::new(json_rpc_endpoint),
+        key_manager_storage,
+        key_manager_time,
+        1,
+        1000, // Large sleep period to force a single rotation
+        1000,
+        ChainId::test(),
+    );
 
-    let mut key_manager = command.spawn().unwrap();
-    sleep(Duration::from_secs(20));
+    // Spawn the key manager and execute a rotation
+    sleep(Duration::from_secs(10)); // Wait a few seconds for the key to become stale
+    let key_manager_thread = thread::spawn(move || key_manager.execute());
+    sleep(Duration::from_secs(10)); // Wait a few seconds for the blockchain to update
 
     // Verify the consensus key has been rotated in secure storage and on-chain.
     let rotated_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
@@ -64,6 +65,10 @@ fn test_key_manager_consensus_rotation() {
     assert_eq!(&rotated_consensus, validator_info.consensus_public_key());
     assert_ne!(current_consensus, rotated_consensus);
 
-    // Kill the key manager process
-    key_manager.kill().unwrap();
+    // Kill the key manager thread
+    unsafe {
+        let raw_handle = key_manager_thread.as_pthread_t();
+        let result = pthread_cancel(raw_handle);
+        assert_eq!(0, result);
+    }
 }
