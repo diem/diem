1e4b4bfdf579030661f7950880ab8964ac1d878f
Ville Sundell <development@solarius.fi>
2020-10-01 20:05:37 +0300

    [scripts] Added convenience script start_cli_swarm.sh
    Recently swarm was changed so, that --libra-node and --cli-path are
    madatory, raising two issues:
        - there were no easy way to lauch swarm with CLI, because the
          documentation was out-of-date, and
        - the new way of lauching would not fit well to the documentation
          because the user might have "debug" or "release" version.
    
    This script solves both problems by making lauching the swarm nice and
    easy. It also forwards aguments after "--" straight to the swarm, so
    user can configure their swarm if so desired.
    
    Default is to build and use "debug" build, but "release" build can be
    specified by issuing "-r" or "--release".

---
+++ scripts/cli/start_cli_swarm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Copyright (c) The Libra Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+print_help()
+{
+	echo "Build spawn a local swarm and open the CLI."
+	echo "\`$0 -r|--release\` to use release build or"
+	echo "\`$0\` to use debug build."
+	echo "Use -- to pass arguments directly to libra-swarm."
+}
+
+source "$HOME/.cargo/env"
+
+SCRIPT_PATH="$(dirname $0)"
+
+RELEASE="debug"
+FLAGS=""
+
+while [[ ! -z "$1" ]]; do
+	case "$1" in
+		-h | --help)
+			print_help;exit 0;;
+		-r | --release)
+			RELEASE="release";
+			FLAGS="--release";;
+		--)
+			shift
+			break
+			;;
+		*) echo "Invalid option"; print_help; exit 0;
+	esac
+	shift
+done
+
+echo "Building and running swarm in $RELEASE mode with the following flags: $FLAGS"
+
+cargo build -p libra-node $FLAGS
+cargo build -p cli $FLAGS
+cargo run -p libra-swarm -- -s --libra-node $SCRIPT_PATH/../../target/$RELEASE/libra-node --cli-path $SCRIPT_PATH/../../target/$RELEASE/cli "$@"
