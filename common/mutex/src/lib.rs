760137e6d80f4995c12d87edbf08f2067d9142a8
David Wong <davidwg@calibra.com>
2020-10-07 10:37:25 -0700

    [libra-mutex] adding a mutex crate to avoid having unwrap everywhere in the codebase

---
+++ common/mutex/src/lib.rs
@@ -0,0 +1,49 @@
+use std::sync::{Mutex as StdMutex, MutexGuard};
+
+/// A simple wrapper around the lock() function of a std::sync::Mutex
+/// The only difference is that you don't need to call unwrap() on it.
+#[derive(Debug)]
+pub struct Mutex<T>(StdMutex<T>);
+
+impl<T> Mutex<T> {
+    /// creates mutex
+    pub fn new(t: T) -> Self {
+        Self(StdMutex::new(t))
+    }
+
+    /// lock the mutex
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        self.0
+            .lock()
+            .expect("libra cannot currently handle a poisoned lock")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use std::{sync::Arc, thread};
+
+    #[test]
+    fn test_libra_mutex() {
+        let a = 7u8;
+        let mutex = Arc::new(Mutex::new(a));
+        let mutex2 = mutex.clone();
+        let mutex3 = mutex.clone();
+
+        let thread1 = thread::spawn(move || {
+            let mut b = mutex2.lock();
+            *b = 8;
+        });
+        let thread2 = thread::spawn(move || {
+            let mut b = mutex3.lock();
+            *b = 9;
+        });
+
+        let _ = thread1.join();
+        let _ = thread2.join();
+
+        println!("{:?}", *mutex.lock());
+    }
+}
