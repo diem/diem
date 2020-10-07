92a7c2c9751bb8cf0149b0fa21dd37e10232d432
Greg Nazario <gnazario@fb.com>
2020-10-07 14:50:33 -0700

    [network] Remove unused NetworkErrorKind
    Closes: #6440

--- network/src/error.rs
+++ network/src/error.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::peer_manager::PeerManagerError;
-use futures::channel::{mpsc, oneshot};
-use libra_types::validator_verifier::VerifyError;
 use std::io;
 use thiserror::Error;
 
@@ -20,32 +18,8 @@ pub enum NetworkErrorKind {
     #[error("Lcs error")]
     LcsError,
 
-    #[error("Error parsing protobuf message")]
-    ProtobufParseError,
-
-    #[error("Invalid signature error")]
-    SignatureError,
-
-    #[error("Error sending on mpsc channel")]
-    MpscSendError,
-
-    #[error("Oneshot channel unexpectedly dropped")]
-    OneshotCanceled,
-
-    #[error("Error setting timeout")]
-    TimerError,
-
-    #[error("Operation timed out")]
-    TimedOut,
-
-    #[error("Unknown tokio::time Error variant")]
-    UnknownTimerError,
-
     #[error("PeerManager error")]
     PeerManagerError,
-
-    #[error("Parsing error")]
-    ParsingError,
 
     #[error("Peer not connected")]
     NotConnected,
@@ -79,30 +53,6 @@ impl From<lcs::Error> for NetworkError {
     }
 }
 
-impl From<VerifyError> for NetworkError {
-    fn from(err: VerifyError) -> NetworkError {
-        anyhow::Error::new(err)
-            .context(NetworkErrorKind::SignatureError)
-            .into()
-    }
-}
-
-impl From<mpsc::SendError> for NetworkError {
-    fn from(err: mpsc::SendError) -> NetworkError {
-        anyhow::Error::new(err)
-            .context(NetworkErrorKind::MpscSendError)
-            .into()
-    }
-}
-
-impl From<oneshot::Canceled> for NetworkError {
-    fn from(err: oneshot::Canceled) -> NetworkError {
-        anyhow::Error::new(err)
-            .context(NetworkErrorKind::OneshotCanceled)
-            .into()
-    }
-}
-
 impl From<PeerManagerError> for NetworkError {
     fn from(err: PeerManagerError) -> NetworkError {
         match err {
@@ -116,11 +66,5 @@ impl From<PeerManagerError> for NetworkError {
                 .context(NetworkErrorKind::PeerManagerError)
                 .into(),
         }
-    }
-}
-
-impl From<tokio::time::Elapsed> for NetworkError {
-    fn from(_err: tokio::time::Elapsed) -> NetworkError {
-        NetworkErrorKind::TimedOut.into()
     }
 }
