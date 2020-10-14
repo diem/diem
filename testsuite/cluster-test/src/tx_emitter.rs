b7c3dbc8c9ca43796de4274136d4e45dd77d117d
snakech <zihao.chen31@gmail.com>
2020-10-14 15:03:22 -0700

    [cluster-test] Add support for CT to loading up to 16 VASP accounts as seed
    Closes: #6521

--- testsuite/cluster-test/src/tx_emitter.rs
+++ testsuite/cluster-test/src/tx_emitter.rs
@@ -51,8 +51,8 @@ use tokio::{task::JoinHandle, time};
 const MAX_TXN_BATCH_SIZE: usize = 100; // Max transactions per account in mempool
                                        // Please make 'MAX_CHILD_VASP_NUM' consistency with 'MAX_CHILD_ACCOUNTS' constant under VASP.move
 const MAX_CHILD_VASP_NUM: usize = 256;
+const MAX_VASP_ACCOUNT_NUM: usize = 16;
 const DD_KEY: &str = "dd.key";
-const VASP_KEY: &str = "vasp.key";
 
 pub struct TxEmitter {
     accounts: Vec<AccountData>,
@@ -238,8 +238,9 @@ impl TxEmitter {
         let num_accounts = req.accounts_per_client * num_clients;
         if self.premainnet {
             assert!(
-                num_accounts < MAX_CHILD_VASP_NUM,
-                "VASP only supports to create max 256 child accounts, but try to create {} accounts",
+                num_accounts <= MAX_VASP_ACCOUNT_NUM * MAX_CHILD_VASP_NUM,
+                "VASP only supports to create max {} child accounts, but try to create {} accounts",
+                MAX_VASP_ACCOUNT_NUM * MAX_CHILD_VASP_NUM,
                 num_accounts
             );
         }
@@ -345,8 +346,13 @@ impl TxEmitter {
         })
     }
 
-    pub async fn load_vasp_account(&self, client: &JsonRpcAsyncClient) -> Result<AccountData> {
-        let mint_key: Ed25519PrivateKey = generate_key::load_key(VASP_KEY);
+    pub async fn load_vasp_account(
+        &self,
+        client: &JsonRpcAsyncClient,
+        index: usize,
+    ) -> Result<AccountData> {
+        let file = "vasp".to_owned() + index.to_string().as_str() + ".key";
+        let mint_key: Ed25519PrivateKey = generate_key::load_key(file);
         let mint_key_pair: KeyPair<Ed25519PrivateKey, Ed25519PublicKey> = KeyPair::from(mint_key);
         let address = libra_types::account_address::from_public_key(&mint_key_pair.public_key);
         let sequence_number = query_sequence_numbers(&client, &[address])
@@ -423,8 +429,12 @@ impl TxEmitter {
         } else {
             let mut seed_accounts = vec![];
             info!("Loading VASP account as seed accounts");
-            let account = self.load_vasp_account(&client).await?;
-            seed_accounts.push(account);
+            let load_account_num = min(seed_account_num, MAX_VASP_ACCOUNT_NUM);
+            for i in 0..load_account_num {
+                let account = self.load_vasp_account(&client, i).await?;
+                seed_accounts.push(account);
+            }
+            info!("Loaded {} VASP accounts", seed_accounts.len());
             seed_accounts
         };
         Ok(seed_accounts)
@@ -1063,22 +1073,15 @@ async fn create_new_accounts(
     let mut i = 0;
     let mut accounts = vec![];
     while i < num_new_accounts {
+        let batch_size = min(
+            max_num_accounts_per_batch as usize,
+            min(MAX_TXN_BATCH_SIZE, num_new_accounts - i),
+        );
         let mut batch = if reuse_account {
-            info!("loading {} accounts if they exist", num_new_accounts);
-            gen_reusable_accounts(
-                &client,
-                min(
-                    max_num_accounts_per_batch as usize,
-                    min(MAX_TXN_BATCH_SIZE, num_new_accounts - i),
-                ),
-                &mut rng,
-            )
-            .await?
+            info!("loading {} accounts if they exist", batch_size);
+            gen_reusable_accounts(&client, batch_size, &mut rng).await?
         } else {
-            gen_random_accounts(min(
-                max_num_accounts_per_batch as usize,
-                min(MAX_TXN_BATCH_SIZE, num_new_accounts - i),
-            ))
+            gen_random_accounts(batch_size)
         };
         let requests = gen_create_child_txn_requests(
             &mut source_account,
