befd6656152872e4c2618706b33eb5109245245c
aldenhu <aldenhu@calibra.com>
2020-10-19 15:16:47 -0700

    [backup] download metadata files in parallel

---
+++ storage/backup/backup-cli/src/metrics/metadata.rs
@@ -0,0 +1,29 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use libra_secure_push_metrics::{register_int_gauge, IntGauge};
+use once_cell::sync::Lazy;
+
+pub static NUM_META_FILES: Lazy<IntGauge> = Lazy::new(|| {
+    register_int_gauge!(
+        "libra_backup_metadata_num_files",
+        "Number of metadata files in total."
+    )
+    .unwrap()
+});
+
+pub static NUM_META_MISS: Lazy<IntGauge> = Lazy::new(|| {
+    register_int_gauge!(
+        "libra_backup_metadata_num_file_cache_misses",
+        "Number of metadata files to download due to non-existence in local cache."
+    )
+    .unwrap()
+});
+
+pub static NUM_META_DOWNLOAD: Lazy<IntGauge> = Lazy::new(|| {
+    register_int_gauge!(
+        "libra_backup_metadata_num_file_downloads",
+        "Number of metadata files to download due to non-existence in local cache."
+    )
+    .unwrap()
+});
