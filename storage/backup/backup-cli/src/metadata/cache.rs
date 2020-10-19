0d81b445b43c7448986150f676f5a74615c9d0c5
aldenhu <aldenhu@calibra.com>
2020-10-19 15:16:47 -0700

    [backup] download metadata files in parallel

--- storage/backup/backup-cli/src/metadata/cache.rs
+++ storage/backup/backup-cli/src/metadata/cache.rs
@@ -3,6 +3,7 @@
 
 use crate::{
     metadata::{view::MetadataView, Metadata},
+    metrics::metadata::{NUM_META_DOWNLOAD, NUM_META_FILES, NUM_META_MISS},
     storage::{BackupStorage, FileHandle},
 };
 use anyhow::{anyhow, Result};
@@ -12,6 +13,7 @@ use std::{
     collections::{HashMap, HashSet},
     path::PathBuf,
     sync::Arc,
+    time::Instant,
 };
 use structopt::StructOpt;
 use tokio::{
@@ -52,6 +54,7 @@ pub async fn sync_and_load(
     opt: &MetadataCacheOpt,
     storage: Arc<dyn BackupStorage>,
 ) -> Result<MetadataView> {
+    let timer = Instant::now();
     let cache_dir = opt.cache_dir();
     create_dir_all(&cache_dir).await?; // create if not present already
 
@@ -77,31 +80,46 @@ pub async fn sync_and_load(
         .collect();
     let remote_hashes: HashSet<_> = remote_file_handle_by_hash.keys().cloned().collect();
     info!("Metadata files listed.");
+    NUM_META_FILES.set(remote_hashes.len() as i64);
 
     // Sync local cache with remote metadata files.
     let stale_local_hashes = local_hashes.difference(&remote_hashes);
-    let new_remote_hashes = remote_hashes.difference(&local_hashes);
+    let new_remote_hashes = remote_hashes.difference(&local_hashes).collect::<Vec<_>>();
     let up_to_date_local_hashes = local_hashes.intersection(&remote_hashes);
 
     for h in stale_local_hashes {
         remove_file(&cache_dir.join(&*h)).await?;
     }
-    for h in new_remote_hashes.clone() {
-        let file_handle = remote_file_handle_by_hash.get(&*h).unwrap();
-        tokio::io::copy(
-            &mut storage.open_for_read(file_handle).await?,
-            &mut OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open(&cache_dir.join(&*h))
-                .await?,
-        )
+
+    NUM_META_MISS.set(new_remote_hashes.len() as i64);
+    NUM_META_DOWNLOAD.set(0);
+    let futs = new_remote_hashes.iter().map(|h| {
+        let fh_by_h_ref = &remote_file_handle_by_hash;
+        let storage_ref = &storage;
+        let cache_dir_ref = &cache_dir;
+
+        async move {
+            let file_handle = fh_by_h_ref.get(*h).expect("In map.");
+            tokio::io::copy(
+                &mut storage_ref.open_for_read(file_handle).await?,
+                &mut OpenOptions::new()
+                    .write(true)
+                    .create_new(true)
+                    .open(&cache_dir_ref.join(*h))
+                    .await?,
+            )
+            .await?;
+            NUM_META_DOWNLOAD.inc();
+            Ok(())
+        }
+    });
+    futures::StreamExt::buffered(futures::stream::iter(futs), num_cpus::get())
+        .collect::<Result<Vec<_>>>()
         .await?;
-    }
 
     // Load metadata from synced cache files.
     let mut metadata_vec = Vec::new();
-    for h in new_remote_hashes.chain(up_to_date_local_hashes) {
+    for h in new_remote_hashes.into_iter().chain(up_to_date_local_hashes) {
         let cached_file = cache_dir.join(&*h);
         metadata_vec.extend(
             OpenOptions::new()
@@ -113,7 +131,10 @@ pub async fn sync_and_load(
                 .into_iter(),
         )
     }
-    info!("Metadata cache loaded.");
+    info!(
+        "Metadata cache loaded in {:.2} seconds.",
+        timer.elapsed().as_secs_f64()
+    );
     Ok(metadata_vec.into())
 }
 
