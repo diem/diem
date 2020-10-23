776b43ba2221c496df4900a11de54ed238781710
David Wong <davidwg@calibra.com>
2020-10-23 08:42:30 -0700

    [fuzzing] split fuzzer scripts in multiple scripts for usability, also remove dead-link flag
    Closes: #6584

--- testsuite/libra-fuzzer/README.md
+++ testsuite/libra-fuzzer/README.md
@@ -99,24 +99,18 @@ To test coverage of our fuzzers you can run the following command with [grcov](h
 RUSTFLAGS='--cfg feature="fuzzing"' CORPUS_PATH=fuzz/corpus cargo xtest --html-cov-dir <some path for html output> -p libra-fuzzer -- coverage
 ```
 
-### Google OSS-Fuzz Integration
+### Building a single fuzzer
 
 To integrate our fuzzers with [Google OSS-Fuzz](https://github.com/google/oss-fuzz) project,
 we need to have one binary per fuzzer.
+This can also be handy when you want to analyze a fuzzer with tools like Instruments.
 For this, build.rs can create a fuzzer binary based on an environement variable.
 Use it as such:
 
 ```sh
-SINGLE_FUZZ_TARGET="consensus_proposal" cargo build --manifest-path fuzz/Cargo.toml --bin fuzzer_builder
-```
-
-Note that you might want to add more flags [[1]](https://github.com/rust-fuzz/cargo-fuzz/blob/2243de096b15b79b719ce7489f014d7d8ce197ee/src/project.rs#L153)[[2]](https://github.com/rust-fuzz/cargo-fuzz/blob/2243de096b15b79b719ce7489f014d7d8ce197ee/src/project.rs#L174).
-
-For example for MacOS:
-
-```sh
-cd fuzz
-SINGLE_FUZZ_TARGET="consensus_proposal" RUSTC_BOOTSTRAP=1 RUSTFLAGS="--cfg fuzzing -Cpasses=sancov -Cllvm-args=-sanitizer-coverage-level=4 -Cllvm-args=-sanitizer-coverage-trace-compares -Cllvm-args=-sanitizer-coverage-inline-8bit-counters -Cllvm-args=-sanitizer-coverage-trace-geps -Cllvm-args=-sanitizer-coverage-prune-blocks=0 -Cllvm-args=-sanitizer-coverage-pc-table -Clink-dead-code -Zsanitizer=address -Cdebug-assertions" FUZZ_TARGET="vm_value" cargo build --verbose --target x86_64-apple-darwin --bin fuzzer_builder
+cd libra/testsuite/libra-fuzzer
+fuzz/google-oss-fuzz/build_fuzzer.sh ConsensusProposal .
+./ConsensusProposal
 ```
 
 ### Troubleshooting
--- testsuite/libra-fuzzer/fuzz/google-oss-fuzz/build.sh
+++ testsuite/libra-fuzzer/fuzz/google-oss-fuzz/build.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # This build script is used with Google OSS-Fuzz to build fuzzer with their docker container.
+# It must be called from libra/testsuite/libra-fuzzer
+# usage: OUT=out_dir ./build.sh
 
 # recipe:
 # -------
@@ -13,47 +15,18 @@
 # fetch all dependencies (needed for patching rocksdb)
 cargo fetch
 
-# for rocksdb we need to link libc++ at the end
-export RUSTFLAGS="-C link-arg=-L/usr/local/lib -C link-arg=-lc++"
-
 # 1. list fuzzers
 cargo run --bin libra-fuzzer list --no-desc > fuzzer_list
 
 # 2. build corpus and move to $OUT
 cat fuzzer_list | while read -r line
 do
-    cargo run --bin libra-fuzzer generate -n 128 $line
-    zip -r $OUT/"$line"_seed_corpus.zip fuzz/corpus/$line
-    rm -r fuzz/corpus/$line
+    fuzz/google-oss-fuzz/build_corpus.sh $line $OUT
 done
-
-# RUSTC_BOOTSTRAP: to get some nightly features like ASAN
-export RUSTC_BOOTSTRAP=1
-
-# export fuzzing flags
-RUSTFLAGS="$RUSTFLAGS --cfg fuzzing"          # used to change code logic
-RUSTFLAGS="$RUSTFLAGS -Cdebug-assertions"     # to get debug_assert in rust
-RUSTFLAGS="$RUSTFLAGS -Zsanitizer=address"    # address sanitizer (ASAN)
-
-RUSTFLAGS="$RUSTFLAGS -Cpasses=sancov"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-level=4"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-trace-compares"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-inline-8bit-counters"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-trace-geps"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-prune-blocks=0"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-pc-table"
-RUSTFLAGS="$RUSTFLAGS -Clink-dead-code"
-RUSTFLAGS="$RUSTFLAGS -Cllvm-args=-sanitizer-coverage-stack-depth"
-RUSTFLAGS="$RUSTFLAGS -Ccodegen-units=1"
-
-export RUSTFLAGS
 
 # 3. build all the fuzzers!
 cat fuzzer_list | while read -r line
 do
-    # build
-    export SINGLE_FUZZ_TARGET="$line"
-    cargo build --manifest-path fuzz/Cargo.toml --bin fuzzer_builder --target x86_64-unknown-linux-gnu
-    # move fuzzer to $OUT
-    mv $SRC/libra/target/x86_64-unknown-linux-gnu/debug/fuzzer_builder $OUT/$SINGLE_FUZZ_TARGET
+    # build & move fuzzer to $OUT
+    fuzz/google-oss-fuzz/build_fuzzer.sh $line $OUT
 done
