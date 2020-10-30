8719477be51b09477ca94a7c183f8e314022b5ac
Aaron Gao <gzh@fb.com>
2020-10-30 16:55:50 -0700

    [librarian] add json support
    Closes: #6635

--- storage/librarian/src/main.rs
+++ storage/librarian/src/main.rs
@@ -3,8 +3,11 @@
 #![allow(dead_code)]
 
 use anyhow::Result;
+
 use libra_types::account_address::AccountAddress;
 use libradb::librarian::Librarian;
+use serde::Serialize;
+use serde_json::to_string_pretty;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -14,8 +17,13 @@ use structopt::StructOpt;
     about = "A command line tool that offers multiple data access commands for LibraDB"
 )]
 struct Opt {
+    /// The parent dir of libradb
     #[structopt(long = "db", parse(from_os_str))]
     db_dir: PathBuf,
+
+    /// Whether output in json format
+    #[structopt(long)]
+    json: bool,
 
     #[structopt(subcommand)]
     cmd: Command,
@@ -75,33 +83,46 @@ fn main() {
 fn run_cmd() -> Result<()> {
     let opt = Opt::from_args();
     let librarian = Librarian::new(opt.db_dir)?;
+    let is_json = opt.json;
     match opt.cmd {
         Command::Status => {
-            println!("{:?}", librarian.get_startup_info()?);
+            print(librarian.get_startup_info()?, is_json)?;
         }
         Command::Txn(txn_op) => match txn_op {
             TxnOp::Get { version } => {
-                println!("{:?}", librarian.get_txn_by_version(version)?);
+                print(&librarian.get_txn_by_version(version)?, is_json)?;
             }
             TxnOp::Scan { from, to } => {
                 let to = match to {
                     Some(to) => to,
                     None => librarian.get_committed_version()?,
                 };
-                println!("{:?}", librarian.scan_txn_by_version(from, to)?);
+                print(librarian.scan_txn_by_version(from, to)?, is_json)?;
             }
         },
         Command::Account(account_cmd) => {
             let address = account_cmd.address;
             match account_cmd.account_op {
                 AccountOp::Get { version } => {
-                    println!(
-                        "{:?}",
-                        librarian.get_account_state_by_version(address, version)?
-                    );
+                    print(
+                        librarian.get_account_state_by_version(address, version)?,
+                        is_json,
+                    )?;
                 }
             }
         }
+    }
+    Ok(())
+}
+
+fn print<T>(data: T, is_json: bool) -> Result<()>
+where
+    T: Serialize + std::fmt::Debug,
+{
+    if is_json {
+        println!("{}", to_string_pretty(&data)?);
+    } else {
+        println!("{:?}", data)
     }
     Ok(())
 }
