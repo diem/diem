e1f9b614617146e6d2a245cca1d18debaf46c520
Joshua Lind <joshlind@fb.com>
2020-11-13 21:45:51 -0800

    [Smoke Tests] Add smoke test attribute and update key manager test.
    Closes: #6707

---
+++ testsuite/smoke-test/attribute/src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+/// This macro allows tests to be defined using the "smoke_test" attribute.
+/// What this does is declare each test as a "rusty_fork_test" (so that it's
+/// spawned in a separate process) and sets a test timeout of 2 minutes (so that
+/// if the test takes longer than 2 minutes to run, it will automatically be
+/// killed and marked as a test failure).
+#[proc_macro_attribute]
+pub fn smoke_test(_: TokenStream, input: TokenStream) -> proc_macro::TokenStream {
+    let input = proc_macro2::TokenStream::from(input);
+    let output = quote! {
+        rusty_fork_test! {
+            #![rusty_fork(timeout_ms = 120000)] // 2 minute timeout
+            #[test]
+            #input
+        }
+    };
+    TokenStream::from(output)
+}
--- testsuite/smoke-test/src/key_manager.rs
+++ testsuite/smoke-test/src/key_manager.rs
@@ -3,68 +3,73 @@
 
 use crate::{
     smoke_test_environment::SmokeTestEnvironment,
-    test_utils::libra_swarm_utils::{get_json_rpc_libra_interface, load_node_config},
-    workspace_builder,
+    test_utils::libra_swarm_utils::load_backend_storage,
 };
-use libra_config::config::{Identity, KeyManagerConfig};
+use libra_config::config::NodeConfig;
 use libra_global_constants::CONSENSUS_KEY;
-use libra_key_manager::libra_interface::LibraInterface;
+use libra_key_manager::{
+    libra_interface::{JsonRpcLibraInterface, LibraInterface},
+    KeyManager,
+};
 use libra_secure_storage::{CryptoStorage, Storage};
-use std::{convert::TryInto, process::Command, thread::sleep, time::Duration};
+use libra_secure_time::RealTimeService;
+use libra_smoke_test_attribute::smoke_test;
+use libra_types::chain_id::ChainId;
+use rusty_fork::rusty_fork_test;
+use std::{convert::TryInto, thread, thread::sleep, time::Duration};
 
-const KEY_MANAGER_BIN: &str = "libra-key-manager";
-
-#[test]
-#[ignore]
+#[smoke_test]
 fn test_key_manager_consensus_rotation() {
+    // Create and launch a local validator swarm
     let mut env = SmokeTestEnvironment::new(1);
     env.validator_swarm.launch();
 
-    // Create a node config for the key manager by extracting the first node config in the swarm.
-    let (node_config, config_path) = load_node_config(&env.validator_swarm, 0);
-    let mut key_manager_config = KeyManagerConfig::default();
-    key_manager_config.json_rpc_endpoint =
-        format!("http://127.0.0.1:{}", node_config.json_rpc.address.port());
-    key_manager_config.rotation_period_secs = 10;
-    key_manager_config.sleep_period_secs = 1000; // Large sleep period to force a single rotation
+    // Fetch the first node config in the swarm
+    let node_config_path = env.validator_swarm.config.config_files.get(0).unwrap();
+    let node_config = NodeConfig::load(&node_config_path).unwrap();
 
-    // Load validator's on disk storage and update key manager secure backend in config
-    let storage: Storage = if let Identity::FromStorage(storage_identity) =
-        &node_config.validator_network.as_ref().unwrap().identity
-    {
-        let storage_backend = storage_identity.backend.clone();
-        key_manager_config.secure_backend = storage_backend.clone();
-        (&storage_backend).try_into().unwrap()
-    } else {
-        panic!("Couldn't load identity from storage");
-    };
-
-    // Save the key manager config to disk
-    let key_manager_config_path = config_path.with_file_name("key_manager.yaml");
-    key_manager_config.save(&key_manager_config_path).unwrap();
+    // Load validator's on disk storage
+    let secure_backend = load_backend_storage(&env.validator_swarm, 0);
+    let storage: Storage = (&secure_backend).try_into().unwrap();
 
     // Create a json-rpc connection to the blockchain and verify storage matches the on-chain state.
-    let libra_interface = get_json_rpc_libra_interface(&env.validator_swarm, 0);
+    let json_rpc_endpoint = format!(
+        "http://127.0.0.1:{}",
+        env.validator_swarm.get_client_port(0)
+    );
+    let libra_interface = JsonRpcLibraInterface::new(json_rpc_endpoint.clone());
     let account = node_config.validator_network.unwrap().peer_id();
     let current_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
     let validator_info = libra_interface.retrieve_validator_info(account).unwrap();
     assert_eq!(&current_consensus, validator_info.consensus_public_key());
 
-    // Spawn the key manager and sleep until a rotation occurs.
-    let mut command = Command::new(workspace_builder::get_bin(KEY_MANAGER_BIN));
-    command
-        .current_dir(workspace_builder::workspace_root())
-        .arg(key_manager_config_path);
+    // Create the key manager
+    let key_manager_storage: Storage = (&secure_backend).try_into().unwrap();
+    let mut key_manager = KeyManager::new(
+        JsonRpcLibraInterface::new(json_rpc_endpoint),
+        key_manager_storage,
+        RealTimeService::new(),
+        1,
+        1000, // Large sleep period to force a single rotation
+        1000,
+        ChainId::test(),
+    );
 
-    let mut key_manager = command.spawn().unwrap();
-    sleep(Duration::from_secs(20));
+    // Spawn the key manager and execute a rotation
+    let _key_manager_thread = thread::spawn(move || key_manager.execute());
 
     // Verify the consensus key has been rotated in secure storage and on-chain.
-    let rotated_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
-    let validator_info = libra_interface.retrieve_validator_info(account).unwrap();
-    assert_eq!(&rotated_consensus, validator_info.consensus_public_key());
-    assert_ne!(current_consensus, rotated_consensus);
+    for _ in 0..10 {
+        sleep(Duration::from_secs(6));
 
-    // Kill the key manager process
-    key_manager.kill().unwrap();
+        let rotated_consensus = storage.get_public_key(CONSENSUS_KEY).unwrap().public_key;
+        let validator_info = libra_interface.retrieve_validator_info(account).unwrap();
+        if current_consensus != rotated_consensus
+            && validator_info.consensus_public_key() == &rotated_consensus
+        {
+            return; // The consensus key was successfully rotated
+        }
+    }
+
+    panic!("The key manager failed to rotate the consensus key!");
 }
