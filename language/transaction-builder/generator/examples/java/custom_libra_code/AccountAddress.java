f870d54877f87ce551fe2aa785d33d672d179224
Mathieu Baudet <mathieubaudet@fb.com>
2020-11-18 14:21:21 -0800

    [serde-generate] upgrade to 0.16.0
    Closes: #6721

--- language/transaction-builder/generator/examples/java/custom_libra_code/AccountAddress.java
+++ language/transaction-builder/generator/examples/java/custom_libra_code/AccountAddress.java
@@ -7,17 +7,18 @@ public static AccountAddress valueOf(byte[] values) {
     if (values.length != LENGTH) {
         throw new java.lang.IllegalArgumentException("Invalid length for AccountAddress");
     }
-    Byte[] address = new Byte[LENGTH];
+    java.util.List<Byte> address = new java.util.ArrayList<Byte>(LENGTH);
     for (int i = 0; i < LENGTH; i++) {
-        address[i] = Byte.valueOf(values[i]);
+        address.add(Byte.valueOf(values[i]));
     }
     return new AccountAddress(address);
 }
 
 public byte[] toBytes() {
     byte[] bytes = new byte[LENGTH];
-    for (int i = 0; i < LENGTH; i++) {
-        bytes[i] = value[i].byteValue();
+    int i = 0;
+    for (Byte item : value) {
+        bytes[i++] = item.byteValue();
     }
     return bytes;
 }
