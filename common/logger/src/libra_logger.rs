0e67edd3bdfdf6c2f2397d817d50e10c4c98bcc9
Brandon Williams <bmwill@fb.com>
2020-12-02 14:16:32 -0800

    logger: change default remote log level to info
    Closes: #6791

--- common/logger/src/libra_logger.rs
+++ common/logger/src/libra_logger.rs
@@ -111,7 +111,7 @@ impl LibraLoggerBuilder {
         Self {
             channel_size: CHANNEL_SIZE,
             level: Level::Info,
-            remote_level: Level::Debug,
+            remote_level: Level::Info,
             address: None,
             printer: Some(Box::new(StderrWriter)),
             is_async: false,
