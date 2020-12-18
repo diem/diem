d084f4702cb9fb631886620780070dcd40e03f94
Greg Nazario <gnazario@fb.com>
2020-12-18 13:59:01 -0800

    [rate-limited] Support AsyncRead and AsyncWrite
    In order to support both inbound and outbound rate limiting on the
    networking stack, we should support both AsyncRead and AsyncWrite.  With
    the combine usage here, we should be able to extend the polling byte
    application to other traits in the future.

--- common/rate-limiter/src/async_lib.rs
+++ common/rate-limiter/src/async_lib.rs
@@ -5,71 +5,129 @@ use crate::rate_limit::Bucket;
 use diem_infallible::Mutex;
 use futures::{
     io::AsyncRead,
+    ready,
     task::{Context, Poll},
+    AsyncWrite, Future,
 };
 use pin_project::pin_project;
-use std::{pin::Pin, sync::Arc};
-use tokio::time::delay_until;
+use std::{io, pin::Pin, sync::Arc};
+use tokio::time::{delay_until, Delay};
 
-/// A rate limiter for `AsyncRead` interfaces to rate limit read bytes
-///
-/// This will pause and wait to send any future bytes until it's permitted to in the future
-#[pin_project]
-pub struct AsyncReadRateLimiter<T: AsyncRead + Unpin> {
-    #[pin]
-    reader: T,
-    rate_limiter: Arc<Mutex<Bucket>>,
+/// An inner struct for keeping track of the delay, and bucket of rate limiting
+struct PollRateLimiter {
+    bucket: Arc<Mutex<Bucket>>,
+    delay: Option<Delay>,
 }
 
-impl<T: AsyncRead + Unpin> AsyncReadRateLimiter<T> {
-    pub fn new(reader: T, rate_limiter: Option<Arc<Mutex<Bucket>>>) -> Self {
-        let rate_limiter = rate_limiter.unwrap_or_else(|| Arc::new(Mutex::new(Bucket::open())));
-
-        Self {
-            reader,
-            rate_limiter,
+impl PollRateLimiter {
+    fn new(bucket: Option<Arc<Mutex<Bucket>>>) -> Self {
+        let bucket = bucket.unwrap_or_else(|| Arc::new(Mutex::new(Bucket::open())));
+        PollRateLimiter {
+            bucket,
+            delay: None,
         }
     }
-}
 
-impl<T: AsyncRead + Unpin> AsyncRead for AsyncReadRateLimiter<T> {
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut [u8],
-    ) -> Poll<std::io::Result<usize>> {
-        let requested = buf.len();
-
-        let allowed: usize;
-        match self.rate_limiter.lock().acquire_tokens(requested) {
-            Ok(num) => allowed = num,
-            Err(wait_time) => {
-                // Wait until we can actually get a value
-                let waker = cx.waker().clone();
-                tokio::task::spawn(async move {
-                    delay_until(tokio::time::Instant::from_std(wait_time)).await;
-                    waker.wake_by_ref();
-                });
-                return Poll::Pending;
+    /// Poll and attempt to acquire the `requested` amount of tokens.
+    /// Keep trying until some amount of tokens are acquired.  Note: This doesn't provide
+    /// fairness so if two pollers hold the same bucket, one could continually lose.
+    fn poll_acquire(&mut self, cx: &mut Context<'_>, requested: usize) -> Poll<usize> {
+        loop {
+            // Wait until the delay is finished.
+            if let Some(ref mut delay) = self.delay {
+                ready!(Pin::new(delay).poll(cx));
+                self.delay = None;
+            }
+            // Try to acquire some tokens. If we're rate limited, we have to wait
+            // before trying again.
+            match self.bucket.lock().acquire_tokens(requested) {
+                Ok(allowed) => return Poll::Ready(allowed),
+                Err(wait_time) => {
+                    self.delay = Some(delay_until(tokio::time::Instant::from_std(wait_time)));
+                }
             }
         }
+    }
 
-        // Otherwise, let's get as much as we're allowing
-        let buf: &mut [u8] = &mut buf[..allowed];
+    /// Poll an inner reader or writer, rate limited by the `bucket`.  This will provide an amount
+    /// of bytes allowed ot be read including partial reads.  It will return unused tokens back to
+    /// the bucket on partial reads / writes.
+    pub fn poll_limited<
+        T,
+        Action: FnOnce(Pin<&mut T>, &mut Context<'_>, usize) -> Poll<io::Result<usize>>,
+    >(
+        &mut self,
+        resource: Pin<&mut T>,
+        cx: &mut Context<'_>,
+        requested: usize,
+        poll_resource: Action,
+    ) -> Poll<io::Result<usize>> {
+        let allowed = ready!(self.poll_acquire(cx, requested));
+        let result = poll_resource(resource, cx, allowed);
 
         // In order to have an accurate throttle rate, we remove tokens, and then add ones we don't use
-        // The other option is to just throttle on how much we request anyways, which is less accurate, but
-        // faster.
-        let result = Pin::new(&mut self.reader).poll_read(cx, buf);
         let tokens_to_return = match &result {
             Poll::Ready(Ok(actual)) => allowed.saturating_sub(*actual),
             _ => allowed,
         };
-
-        // Return unused tokens
-        self.rate_limiter.lock().add_tokens(tokens_to_return);
+        self.bucket.lock().add_tokens(tokens_to_return);
 
         result
+    }
+}
+
+/// A rate limiter for `AsyncRead` or `AsyncWrite` interfaces to rate limit read/write bytes
+///
+/// This will pause and wait to send any future bytes until it's permitted to in the future
+#[pin_project]
+pub struct AsyncRateLimiter<T> {
+    #[pin]
+    inner: T,
+    rate_limiter: PollRateLimiter,
+}
+
+impl<T> AsyncRateLimiter<T> {
+    pub fn new(inner: T, bucket: Option<Arc<Mutex<Bucket>>>) -> Self {
+        Self {
+            inner,
+            rate_limiter: PollRateLimiter::new(bucket),
+        }
+    }
+}
+
+impl<T: AsyncRead> AsyncRead for AsyncRateLimiter<T> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<io::Result<usize>> {
+        let this = self.project();
+        this.rate_limiter
+            .poll_limited(this.inner, cx, buf.len(), |resource, cx, allowed| {
+                resource.poll_read(cx, &mut buf[..allowed])
+            })
+    }
+}
+
+impl<T: AsyncWrite> AsyncWrite for AsyncRateLimiter<T> {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        let this = self.project();
+        this.rate_limiter
+            .poll_limited(this.inner, cx, buf.len(), |resource, cx, allowed| {
+                resource.poll_write(cx, &buf[..allowed])
+            })
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().inner.poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.project().inner.poll_close(cx)
     }
 }
 
@@ -89,7 +147,7 @@ mod tests {
     async fn test_async_read() {
         let source: &[u8] = b"12345678901234567890123456";
         let rate_limiter = Arc::new(Mutex::new(Bucket::new(NonZeroUsize!(15), NonZeroUsize!(5))));
-        let mut reader = AsyncReadRateLimiter::new(source, Some(rate_limiter));
+        let mut reader = AsyncRateLimiter::new(source, Some(rate_limiter));
 
         let mut buf: [u8; 30] = [0; 30];
         assert_eq!(15, reader.read(&mut buf).await.expect("Successful read"));
