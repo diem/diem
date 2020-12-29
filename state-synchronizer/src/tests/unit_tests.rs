14b3ddb295db14a52fd744ce08c15c941a25c657
Joshua Lind <joshlind@fb.com>
2020-12-29 14:11:48 -0800

    [State Sync] Clean up unit tests and reduce duplication.

--- state-synchronizer/src/tests/unit_tests.rs
+++ state-synchronizer/src/tests/unit_tests.rs
@@ -6,63 +6,37 @@ use diem_config::config::{PeerNetworkId, UpstreamConfig};
 use netcore::transport::ConnectionOrigin;
 use std::{collections::HashMap, time::Duration};
 
+const NUM_CHUNKS_TO_PROCESS: u64 = 50;
+const NUM_PICKS_TO_MAKE: u64 = 1000;
+
 #[test]
-fn test_request_manager() {
-    let peers = vec![
-        PeerNetworkId::random_validator(),
-        PeerNetworkId::random_validator(),
-        PeerNetworkId::random_validator(),
-        PeerNetworkId::random_validator(),
-    ];
-    let mut request_manager = RequestManager::new(
-        UpstreamConfig::default(),
-        Duration::from_secs(10),
-        Duration::from_secs(30),
-        HashMap::new(),
-    );
-    for peer_id in peers.clone() {
-        request_manager.enable_peer(peer_id, ConnectionOrigin::Outbound);
+fn test_request_manager_invalid_chunk() {
+    let (mut request_manager, validators) = generate_request_manager_and_test_validators(10, 4);
+
+    // Process multiple invalid chunk responses from validator 0
+    for _ in 0..NUM_CHUNKS_TO_PROCESS {
+        request_manager.process_invalid_chunk(&validators[0]);
     }
 
-    for _ in 0..50 {
-        request_manager.process_invalid_chunk(&peers[0]);
-    }
+    // Calculate pick counts for the validators
+    let pick_counts = calculate_pick_counts_for_validators(&mut request_manager, NUM_PICKS_TO_MAKE);
 
-    let mut pick_counts = HashMap::new();
-    for _ in 0..1000 {
-        let picked_peer_id = request_manager.pick_peers()[0].clone();
-        let counter = pick_counts.entry(picked_peer_id).or_insert(0);
-        *counter += 1;
-    }
-
-    // unwrap_or needed because the peer with bad score may never be picked, and may be
-    // missing from pick_counts
-    assert!(pick_counts.get(&peers[0]).unwrap_or(&0) < pick_counts.get(&peers[1]).unwrap());
-    assert!(pick_counts.get(&peers[0]).unwrap_or(&0) < pick_counts.get(&peers[2]).unwrap());
-    assert!(pick_counts.get(&peers[0]).unwrap_or(&0) < pick_counts.get(&peers[3]).unwrap());
+    // Verify validator 0 is chosen less than the other validators
+    let validator_0_count = pick_counts.get(&validators[0]).unwrap_or(&0);
+    assert!(validator_0_count < pick_counts.get(&validators[1]).unwrap());
+    assert!(validator_0_count < pick_counts.get(&validators[2]).unwrap());
+    assert!(validator_0_count < pick_counts.get(&validators[3]).unwrap());
 }
 
 #[test]
 fn test_remove_requests() {
-    let peers = vec![
-        PeerNetworkId::random_validator(),
-        PeerNetworkId::random_validator(),
-    ];
-    let mut request_manager = RequestManager::new(
-        UpstreamConfig::default(),
-        Duration::from_secs(0),
-        Duration::from_secs(30),
-        HashMap::new(),
-    );
-    for peer in peers.iter() {
-        request_manager.enable_peer(peer.clone(), ConnectionOrigin::Outbound);
-    }
+    let (mut request_manager, validators) = generate_request_manager_and_test_validators(0, 2);
 
-    request_manager.add_request(1, vec![peers[0].clone()]);
-    request_manager.add_request(3, vec![peers[1].clone()]);
-    request_manager.add_request(5, vec![peers[0].clone()]);
-    request_manager.add_request(10, vec![peers[0].clone()]);
-    request_manager.add_request(12, vec![peers[1].clone()]);
+    request_manager.add_request(1, vec![validators[0].clone()]);
+    request_manager.add_request(3, vec![validators[1].clone()]);
+    request_manager.add_request(5, vec![validators[0].clone()]);
+    request_manager.add_request(10, vec![validators[0].clone()]);
+    request_manager.add_request(12, vec![validators[1].clone()]);
 
     request_manager.remove_requests(5);
 
@@ -75,28 +49,59 @@ fn test_remove_requests() {
 
 #[test]
 fn test_request_manager_request_metadata() {
-    let peers = vec![
-        PeerNetworkId::random_validator(),
-        PeerNetworkId::random_validator(),
-    ];
-    let mut request_manager = RequestManager::new(
-        UpstreamConfig::default(),
-        Duration::from_secs(0),
-        Duration::from_secs(30),
-        HashMap::new(),
-    );
-    for peer in peers.iter() {
-        request_manager.enable_peer(peer.clone(), ConnectionOrigin::Outbound);
-    }
+    let (mut request_manager, validators) = generate_request_manager_and_test_validators(0, 2);
+
     assert!(request_manager.get_first_request_time(1).is_none());
 
-    request_manager.add_request(1, vec![peers[0].clone()]);
+    request_manager.add_request(1, vec![validators[0].clone()]);
     request_manager.check_timeout(1).unwrap();
-    request_manager.add_request(1, vec![peers[1].clone()]);
-    assert!(request_manager.peer_score(&peers[0]).unwrap() < 99.0);
-    assert!(request_manager.peer_score(&peers[1]).unwrap() > 99.0);
+    request_manager.add_request(1, vec![validators[1].clone()]);
+    assert!(request_manager.peer_score(&validators[0]).unwrap() < 99.0);
+    assert!(request_manager.peer_score(&validators[1]).unwrap() > 99.0);
     assert!(
         request_manager.get_first_request_time(1).unwrap()
             <= request_manager.get_last_request_time(1).unwrap()
     );
+}
+
+/// Picks a peer to send a chunk request to (multiple times) and constructs a pick count
+/// for each of the chosen peers.
+fn calculate_pick_counts_for_validators(
+    request_manager: &mut RequestManager,
+    number_of_picks_to_execute: u64,
+) -> HashMap<PeerNetworkId, u64> {
+    let mut pick_counts = HashMap::new();
+
+    for _ in 0..number_of_picks_to_execute {
+        let picked_peers = request_manager.pick_peers();
+        assert_eq!(1, picked_peers.len()); // Ensure only one validator per multicast level
+
+        let picked_peer = picked_peers[0].clone();
+        let counter = pick_counts.entry(picked_peer).or_insert(0);
+        *counter += 1;
+    }
+
+    pick_counts
+}
+
+/// Generates a new request manager with a specified number of validator peers enabled.
+fn generate_request_manager_and_test_validators(
+    request_timeout: u64,
+    num_validators: u64,
+) -> (RequestManager, Vec<PeerNetworkId>) {
+    let mut request_manager = RequestManager::new(
+        UpstreamConfig::default(),
+        Duration::from_secs(request_timeout),
+        Duration::from_secs(30),
+        HashMap::new(),
+    );
+
+    let mut validators = Vec::new();
+    for _ in 0..num_validators {
+        let validator = PeerNetworkId::random_validator();
+        request_manager.enable_peer(validator.clone(), ConnectionOrigin::Outbound);
+        validators.push(validator);
+    }
+
+    (request_manager, validators)
 }
