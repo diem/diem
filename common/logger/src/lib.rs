54508a74d46243e1edee65474ff8a56cb625a9b1
Joel Marcey <joelm@fb.com>
2021-01-04 12:41:40 -0800

    Do not manually copy READMEs to the developer site any longer
    Closes: #7103

--- common/logger/src/lib.rs
+++ common/logger/src/lib.rs
@@ -8,7 +8,7 @@
 //! A set of logging macros (`info!`, `error!`, `warn!`, `debug!`, and `trace!`) is provided for
 //! emitting logs at different levels. All of these macros support the addition of providing
 //! structured data along with a formatted text message.  For guidelines on which level to use,
-//! see the [coding guidelines](https://developers.diem.com/docs/community/coding-guidelines#logging).
+//! see the [coding guidelines](https://developers.diem.com/docs/core/coding-guidelines#logging).
 //!
 //! The below examples do no type checking for structured log fields, and instead just serialize
 //! whatever is given.
--- developers.diem.com/scripts/build_docs.sh
+++ developers.diem.com/scripts/build_docs.sh
@@ -81,38 +81,6 @@ done
 # Install Rust (Netlify will need this for website previews, for example)
 install_rustup
 
-# create needed output directories, ignore error if they exist already
-mkdir -p docs/crates
-mkdir -p docs/community
-
-# manually copy crate README files from fixed directory
-###
-echo "-----------------------------------"
-echo "Manually Copying READMEs to docs/crates"
-echo "-----------------------------------"
-sed -i.old '/^# /d' ../language/bytecode-verifier/README.md; cp ../language/bytecode-verifier/README.md docs/crates/bytecode-verifier.md
-sed -i.old '/^# /d' ../consensus/README.md; cp ../consensus/README.md docs/crates/consensus.md
-sed -i.old '/^# /d' ../crypto/crypto/README.md; cp ../crypto/crypto/README.md docs/crates/crypto.md
-sed -i.old '/^# /d' ../execution/README.md; cp ../execution/README.md docs/crates/execution.md
-sed -i.old '/^# /d' ../language/README.md; cp ../language/README.md docs/crates/move-language.md
-sed -i.old '/^# /d' ../language/compiler/README.md; cp ../language/compiler/README.md docs/crates/ir-to-bytecode.md
-sed -i.old '/^# /d' ../mempool/README.md; cp ../mempool/README.md docs/crates/mempool.md
-sed -i.old '/^# /d' ../network/README.md; cp ../network/README.md docs/crates/network.md
-sed -i.old '/^# /d' ../storage/README.md; cp ../storage/README.md docs/crates/storage.md
-sed -i.old '/^# /d' ../language/vm/README.md; cp ../language/vm/README.md docs/crates/vm.md
-
-echo "-----------------------------------"
-echo "Manually Copy Coding Guidelines"
-echo "-----------------------------------"
-sed -i.old '/^# Diem Core Coding Guidelines/d' ../documentation/coding_guidelines.md
-cp ../documentation/coding_guidelines.md docs/community/coding-guidelines.md
-
-echo "-----------------------------------"
-echo "Manually Copy Contributing Guidelines"
-echo "-----------------------------------"
-sed -i.old '/^# Diem Core Contributing Guidelines/d' ../CONTRIBUTING.md
-cp ../CONTRIBUTING.md docs/community/contributing.md
-
 if [[ $BUILD_RUSTDOCS == true ]]; then
   echo "-----------------------------------"
   echo "Generating API reference via Rustdoc"
