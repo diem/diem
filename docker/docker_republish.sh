6466b1c8d982ecaf884b8925e968b5ef49b8303f
Rex Hoffman <rexhoffman@fb.com>
2021-01-26 00:30:14 -0800

    [docker] prefer diem, length time before cleanup, cleanup libra org.
    Closes: #7361

--- docker/docker_republish.sh
+++ docker/docker_republish.sh
@@ -15,6 +15,7 @@ function usage {
   echo "-t the tag that exists in hub.docker.com."
   echo "-o override tag that should be pushed to target repo."
   echo "-r target repo of image push"
+  echo "-g target org, defaults to 'diem'"
   echo "-d disable signing for target repository (ecr requires this)"
   echo "-h this message"
 }
@@ -22,10 +23,11 @@ function usage {
 DOCKERHUB_TAG=;
 OUTPUT_TAG=;
 TARGET_REPO=docker.io
+TARGET_ORG=diem
 DISABLE_TRUST=false
 
 #parse args
-while getopts "t:o:r:dh" arg; do
+while getopts "t:o:r:g:dh" arg; do
   case $arg in
     t)
       DOCKERHUB_TAG=$OPTARG
@@ -35,6 +37,9 @@ while getopts "t:o:r:dh" arg; do
       ;;
     r)
       TARGET_REPO=$OPTARG
+      ;;
+    g)
+      TARGET_ORG=$OPTARG
       ;;
     d)
       DISABLE_TRUST=true
@@ -55,33 +60,33 @@ fi
 set -x
 
 #Pull the latest docker hub images so we can push them to ECR
-docker pull --disable-content-trust=false docker.io/libra/init:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/faucet:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/tools:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/validator:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/validator_tcb:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/cluster_test:"$DOCKERHUB_TAG"
-docker pull --disable-content-trust=false docker.io/libra/client:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/init:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/faucet:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/tools:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/validator:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/validator_tcb:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/cluster_test:"$DOCKERHUB_TAG"
+docker pull --disable-content-trust=false docker.io/diem/client:"$DOCKERHUB_TAG"
 
 if [[ $DISABLE_TRUST == "true" ]]; then
   export DOCKER_CONTENT_TRUST=0
 fi
 
 #Push the proper locations to novi ecr.
-docker tag libra/init:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/init:"$OUTPUT_TAG"
-docker tag libra/faucet:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/faucet:"$OUTPUT_TAG"
-docker tag libra/tools:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/tools:"$OUTPUT_TAG"
-docker tag libra/validator:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/validator:"$OUTPUT_TAG"
-docker tag libra/validator_tcb:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/validator_tcb:"$OUTPUT_TAG"
-docker tag libra/cluster_test:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/cluster_test:"$OUTPUT_TAG"
-docker tag libra/client:"$DOCKERHUB_TAG" "$TARGET_REPO"/diem/client:"$OUTPUT_TAG"
+docker tag diem/init:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/init:"$OUTPUT_TAG"
+docker tag diem/faucet:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/faucet:"$OUTPUT_TAG"
+docker tag diem/tools:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/tools:"$OUTPUT_TAG"
+docker tag diem/validator:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/validator:"$OUTPUT_TAG"
+docker tag diem/validator_tcb:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/validator_tcb:"$OUTPUT_TAG"
+docker tag diem/cluster_test:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/cluster_test:"$OUTPUT_TAG"
+docker tag diem/client:"$DOCKERHUB_TAG" "$TARGET_REPO"/"$TARGET_ORG"/client:"$OUTPUT_TAG"
 
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/init:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/faucet:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/tools:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/validator:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/validator_tcb:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/cluster_test:"$OUTPUT_TAG"
-docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/diem/client:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/init:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/faucet:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/tools:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/validator:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/validator_tcb:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/cluster_test:"$OUTPUT_TAG"
+docker push --disable-content-trust="$DISABLE_TRUST" "$TARGET_REPO"/"$TARGET_ORG"/client:"$OUTPUT_TAG"
 
 set +x
