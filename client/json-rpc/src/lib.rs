9d02f381f734670d07aa7c57c6d9dd701e0a277e
Brandon Williams <bmwill@fb.com>
2021-01-29 15:49:19 -0800

    client: remove old blocking client

--- client/json-rpc/src/lib.rs
+++ client/json-rpc/src/lib.rs
@@ -1,11 +1,9 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-mod blocking;
 mod client;
 mod response;
 
-pub use blocking::JsonRpcClient;
 pub use client::{
     get_response_from_batch, process_batch_response, JsonRpcAsyncClient, JsonRpcAsyncClientError,
     JsonRpcBatch,
