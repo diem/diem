06cef6223859c5fc3b340abfb11aa215a3e2375d
Brandon Williams <bmwill@fb.com>
2021-01-31 14:53:57 -0800

    client: remove old async client

--- client/json-rpc/src/lib.rs
+++ client/json-rpc/src/lib.rs
@@ -1,16 +1,8 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-mod client;
-mod response;
-
-pub use client::{
-    get_response_from_batch, process_batch_response, JsonRpcAsyncClient, JsonRpcAsyncClientError,
-    JsonRpcBatch,
-};
 pub use diem_json_rpc_types::{errors, views};
 pub use diem_types::{account_address::AccountAddress, transaction::SignedTransaction};
-pub use response::{JsonRpcResponse, ResponseAsView};
 
 // new implementation module
 
