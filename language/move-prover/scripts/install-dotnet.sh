aeb9d133e18b988b420ab735f16e0c3d093c2993
Junkil Park <jkpark@fb.com>
2021-02-08 10:36:29 -0800

    Removed the scripts in the move-prover directory
    - The scripts in `diem/language/move-prover/scripts` are deprecated.
    - How to install the tools that Prover requires is described in
      `diem/language/move-prover/doc/user/install.md` using
    `diem/scripts/dev_setup.sh`
    
    Closes: #7549

--- language/move-prover/scripts/install-dotnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# Copyright (c) The Diem Core Contributors
-# SPDX-License-Identifier: Apache-2.0
-
-brew tap isen-ng/dotnet-sdk-versions
-brew cask install dotnet-sdk-3.1.100
-brew cask install dotnet
