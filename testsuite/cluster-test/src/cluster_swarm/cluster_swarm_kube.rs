723f6cd263ac1672240a2c00d6812c9fe48bedbb
Rex Hoffman <rexhoffman@fb.com>
2021-02-10 14:17:13 -0800

    [kube] update kube and tests

--- testsuite/cluster-test/src/cluster_swarm/cluster_swarm_kube.rs
+++ testsuite/cluster-test/src/cluster_swarm/cluster_swarm_kube.rs
@@ -138,7 +138,7 @@ impl ClusterSwarmKube {
         let config = Config::new(
             reqwest::Url::parse(HEALTH_CHECK_URL).expect("Failed to parse kubernetes endpoint url"),
         );
-        let client = Client::new(config);
+        let client = Client::try_from(config)?;
         let credentials_provider = WebIdentityProvider::from_k8s_env();
         let dispatcher =
             rusoto_core::HttpClient::new().expect("failed to create request dispatcher");
