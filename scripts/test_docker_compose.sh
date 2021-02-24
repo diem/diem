821a8d9f646710fc92f790bb830d268a06452d54
Max Kaplan <confidential@fb.com>
2021-02-24 11:12:04 -0800

    [docker-compose] Add github action for testing

--- scripts/test_docker_compose.sh
+++ scripts/test_docker_compose.sh
@@ -4,29 +4,30 @@
 
 set timeout 5
 
-cd [file dirname $argv0]
-
+set basedir [file normalize [file dirname $argv0]]
+cd $basedir
 
 ### Test the validator/faucet
-spawn $env(SHELL)
-cd [file dirname $argv0]
+spawn /bin/bash
+cd $basedir
 expect_before {
     timeout { puts "\rERROR: Timeout!\r"; exit 1 }
     eof { puts "\rERROR: eof!\r"; exit 1 }
 }
 
-send "../docker/compose/validator-testnet\r"
+send "cd ../docker/compose/validator-testnet\r"
 send "docker-compose up --remove-orphans\r"
 # Order is non-deterministic, so we test both ways
 expect {
-  "*validator_1*Diem is running*" { expect  "*faucet_1*running*" }
-  "*faucet_1*running*" { expect "*validator_1*Diem is running*" }
+  "validator_1*Diem is running" { expect  "faucet_1*running" }
+  "faucet_1*running*" { expect "validator_1*Diem is running" }
 }
 
+sleep 3
 
 ### Test the CLI client
-spawn $env(SHELL)
-cd [file dirname $argv0]
+spawn /bin/bash
+cd $basedir
 expect_before {
     timeout { puts "\rERROR: Timeout!\r"; exit 1 }
     eof { puts "\rERROR: eof!\r"; exit 1 }
