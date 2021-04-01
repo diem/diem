dcab6b655002931e097362c6edc88ba7d159f4af
Rain <rain@sunshowers.io>
2021-03-31 20:32:05 -0700

    [x] update nextest
    Adds a couple new features: JUnit reporting, and printing out failing
    test results at the end.

--- devtools/x/src/nextest.rs
+++ devtools/x/src/nextest.rs
@@ -131,7 +131,7 @@ pub fn run(args: Args, xctx: XContext) -> Result<()> {
         Coloring::Always => Color::Always,
         Coloring::Never => Color::Never,
     };
-    let reporter = TestReporter::new(&test_list, color, args.reporter_opts);
+    let mut reporter = TestReporter::new(&test_list, color, args.reporter_opts);
 
     let run_stats = runner.try_execute(|event| reporter.report_event(event))?;
     if !run_stats.is_success() {
