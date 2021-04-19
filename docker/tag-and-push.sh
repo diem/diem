b64b55c5499a8cbf025ee09ae95483bb933b5703
Young Yang Liauw <youngyl@fb.com>
2021-04-19 13:13:21 -0700

    [dkr] restore script needed in buildspec
    This partially reverts 9ed22830
    
    Closes: #8232

---
+++ docker/tag-and-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) The Diem Core Contributors
+# SPDX-License-Identifier: Apache-2.0
+# tag-and-push.sh is used tag an image with multiple tags and push them to the target repo. Use ":" as the separator
+# between multiple tags
+# Example:
+# SOURCE=diem_validator:latest TARGET_REPO=1234567890.dkr.ecr.us-west-2.amazonaws.com/diem_cluster_test TARGET_TAGS=master:master_39cnja0 tag-and-push.sh
+
+set -e
+
+TARGET_TAGS_ARR=(${TARGET_TAGS//:/ })
+for TAG in "${TARGET_TAGS_ARR[@]}"
+do
+  TARGET=${TARGET_REPO}:${TAG}
+  echo "Tagging ${SOURCE} to ${TARGET}"
+  docker tag ${SOURCE} ${TARGET}
+  echo "Pushing ${SOURCE} to ${TARGET}"
+  docker push ${TARGET}
+done
