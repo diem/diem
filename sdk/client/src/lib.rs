437246d8127dba4a0a84bba676b6c9a6016e65a0
Philip Hayes <philiphayes@fb.com>
2021-04-21 16:06:29 -0700

    [sdk] #[doc(hidden)] the VerifyingClient
    We don't want users relying on unstable/experimental code for which we can't
    guarantee API stability.

--- sdk/client/src/lib.rs
+++ sdk/client/src/lib.rs
@@ -17,6 +17,10 @@ cfg_async! {
     pub use client::Client;
 
     mod verifying_client;
+    // WARNING: the VerifyingClient is currently experimental; it's not recommended
+    // to use it until it stabilizes further
+    // TODO(philiphayes): make this pub once verifying_client is stable.
+    #[doc(hidden)]
     pub use verifying_client::{Storage, InMemoryStorage, VerifyingClient};
 }
 
