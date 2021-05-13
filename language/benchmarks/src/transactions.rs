e7cff7f10ff0d68b22d528113895cacc04341a26
Runtian Zhou <runtianz@fb.com>
2021-05-13 13:03:59 -0700

    [benchmarks] Change the measurement

--- language/benchmarks/src/transactions.rs
+++ language/benchmarks/src/transactions.rs
@@ -28,10 +28,10 @@ where
     S::Value: AUTransactionGen,
 {
     /// The number of accounts created by default.
-    pub const DEFAULT_NUM_ACCOUNTS: usize = 100;
+    pub const DEFAULT_NUM_ACCOUNTS: usize = 1000;
 
     /// The number of transactions created by default.
-    pub const DEFAULT_NUM_TRANSACTIONS: usize = 500;
+    pub const DEFAULT_NUM_TRANSACTIONS: usize = 10000;
 
     /// Creates a new transaction bencher with default settings.
     pub fn new(strategy: S) -> Self {
