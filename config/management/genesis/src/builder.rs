709805331c36f40d4990cab5bfb852009594e071
Brandon Williams <bmwill@fb.com>
2021-05-28 15:43:58 -0700

    wip

--- config/management/genesis/src/builder.rs
+++ config/management/genesis/src/builder.rs
@@ -10,13 +10,23 @@ use diem_global_constants::{
 };
 use diem_management::{
     config::ConfigPath,
-    constants::{self, VALIDATOR_OPERATOR},
+    constants::{self, VALIDATOR_CONFIG, VALIDATOR_OPERATOR},
     error::Error,
     secure_backend::SharedBackend,
 };
 use diem_secure_storage::{CryptoStorage, KVStorage, Namespaced, Storage};
-use diem_types::chain_id::ChainId;
+use diem_transaction_builder::stdlib as transaction_builder;
+use diem_types::{
+    account_address,
+    account_address::AccountAddress,
+    chain_id::ChainId,
+    transaction::{
+        RawTransaction, ScriptFunction, SignedTransaction, Transaction, TransactionPayload,
+    },
+    waypoint::Waypoint,
+};
 use serde::{de::DeserializeOwned, Serialize};
+use vm_genesis::{OperatorAssignment, OperatorRegistration};
 
 pub struct GenesisBuilder<S> {
     storage: S,
@@ -58,13 +68,29 @@ impl<S: KVStorage> GenesisBuilder<S> {
             .map_err(Into::into)
     }
 
+    pub fn root_key(&self) -> Result<Ed25519PublicKey> {
+        let layout = self.layout()?;
+        self.with_namespace(&layout.diem_root)
+            .get(DIEM_ROOT_KEY)
+            .map(|r| r.value)
+            .map_err(Into::into)
+    }
+
     pub fn set_treasury_compliance_key(
         &mut self,
         treasury_compliance_key: Ed25519PublicKey,
     ) -> Result<()> {
         let layout = self.layout()?;
-        self.with_namespace_mut(&layout.diem_root)
+        self.with_namespace_mut(&layout.treasury_compliance)
             .set(TREASURY_COMPLIANCE_KEY, treasury_compliance_key)
+            .map_err(Into::into)
+    }
+
+    pub fn treasury_compliance_key(&self) -> Result<Ed25519PublicKey> {
+        let layout = self.layout()?;
+        self.with_namespace(&layout.treasury_compliance)
+            .get(TREASURY_COMPLIANCE_KEY)
+            .map(|r| r.value)
             .map_err(Into::into)
     }
 
@@ -78,10 +104,51 @@ impl<S: KVStorage> GenesisBuilder<S> {
             .map_err(Into::into)
     }
 
+    pub fn operator_key(&self, operator: &str) -> Result<Ed25519PublicKey> {
+        self.with_namespace(operator)
+            .get(OPERATOR_KEY)
+            .map(|r| r.value)
+            .map_err(Into::into)
+    }
+
     pub fn set_operator(&mut self, validator: &str, operator: &str) -> Result<()> {
         self.with_namespace_mut(validator)
             .set(VALIDATOR_OPERATOR, operator)
             .map_err(Into::into)
+    }
+
+    pub fn operator(&self, validator: &str) -> Result<String> {
+        self.with_namespace(validator)
+            .get(VALIDATOR_OPERATOR)
+            .map(|r| r.value)
+            .map_err(Into::into)
+    }
+
+    /// Produces a set of OperatorAssignments from the remote storage.
+    pub fn operator_assignments(&self) -> Result<Vec<OperatorAssignment>> {
+        let layout = self.layout()?;
+
+        let mut operator_assignments = Vec::new();
+
+        for owner in &layout.owners {
+            let owner_key = self.owner_key(owner).ok();
+
+            let operator_name = self.operator(owner)?;
+            let operator_key = self.operator_key(&operator_name)?;
+            let operator_account = account_address::from_public_key(&operator_key);
+
+            let set_operator_script =
+                transaction_builder::encode_set_validator_operator_script_function(
+                    operator_name.as_bytes().to_vec(),
+                    operator_account,
+                )
+                .into_script_function();
+
+            let owner_name_vec = owner.as_bytes().to_vec();
+            operator_assignments.push((owner_key, owner_name_vec, set_operator_script));
+        }
+
+        Ok(operator_assignments)
     }
 
     pub fn set_owner_key(
@@ -94,6 +161,13 @@ impl<S: KVStorage> GenesisBuilder<S> {
             .map_err(Into::into)
     }
 
+    pub fn owner_key(&self, owner: &str) -> Result<Ed25519PublicKey> {
+        self.with_namespace(owner)
+            .get(OWNER_KEY)
+            .map(|r| r.value)
+            .map_err(Into::into)
+    }
+
     pub fn set_validator_config(
         &mut self,
         validator_namespace: &str,
@@ -103,5 +177,72 @@ impl<S: KVStorage> GenesisBuilder<S> {
         chain_id: ChainId,
     ) -> Result<()> {
         todo!()
+    }
+
+    pub fn validator_config(&self, operator: &str) -> Result<ScriptFunction> {
+        if let Ok(txn) = self
+            .with_namespace(operator)
+            .get::<ScriptFunction>(VALIDATOR_CONFIG)
+            .map(|r| r.value)
+        {
+            Ok(txn)
+        } else {
+            let txn = self
+                .with_namespace(operator)
+                .get::<Transaction>(VALIDATOR_CONFIG)
+                .map(|r| r.value)?;
+            let txn = if let Transaction::UserTransaction(txn) = txn {
+                txn
+            } else {
+                return Err(anyhow::anyhow!("Invalid Validator Config"));
+            };
+            if let TransactionPayload::ScriptFunction(txn) =
+                txn.into_raw_transaction().into_payload()
+            {
+                Ok(txn)
+            } else {
+                Err(anyhow::anyhow!("Invalid Validator Config"))
+            }
+        }
+    }
+
+    /// Produces a set of OperatorRegistrations from the remote storage.
+    pub fn operator_registrations(&self) -> Result<Vec<OperatorRegistration>> {
+        let layout = self.layout()?;
+        let mut registrations = Vec::new();
+
+        for operator in &layout.operators {
+            let operator_key = self.operator_key(operator)?;
+            let validator_config_tx = self.validator_config(operator)?;
+
+            registrations.push((
+                operator_key,
+                operator.as_bytes().to_vec(),
+                validator_config_tx,
+            ));
+        }
+
+        Ok(registrations)
+    }
+
+    pub fn build(&self, chain_id: ChainId) -> Result<Transaction> {
+        let diem_root_key = self.root_key()?;
+        let treasury_compliance_key = self.treasury_compliance_key()?;
+        let operator_assignments = self.operator_assignments()?;
+        let operator_registrations = self.operator_registrations()?;
+
+        // Only have an allowlist of stdlib scripts
+        let script_policy = None;
+
+        let genesis = vm_genesis::encode_genesis_transaction(
+            diem_root_key,
+            treasury_compliance_key,
+            &operator_assignments,
+            &operator_registrations,
+            script_policy,
+            chain_id,
+        );
+
+        Ok(genesis)
     }
 }
