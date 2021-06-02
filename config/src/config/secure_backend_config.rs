c54a0bf376ce315e2e358990b65f517a7a76670d
Brandon Williams <bmwill@fb.com>
2021-06-02 15:10:48 -0700

    config: add helper for querying if a backend config has a namespace

--- config/src/config/secure_backend_config.rs
+++ config/src/config/secure_backend_config.rs
@@ -21,6 +21,19 @@ pub enum SecureBackend {
     OnDiskStorage(OnDiskStorageConfig),
 }
 
+impl SecureBackend {
+    pub fn namespace(&self) -> Option<&str> {
+        match self {
+            SecureBackend::GitHub(GitHubConfig { namespace, .. })
+            | SecureBackend::Vault(VaultConfig { namespace, .. })
+            | SecureBackend::OnDiskStorage(OnDiskStorageConfig { namespace, .. }) => {
+                namespace.as_deref()
+            }
+            SecureBackend::InMemoryStorage => None,
+        }
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct GitHubConfig {
