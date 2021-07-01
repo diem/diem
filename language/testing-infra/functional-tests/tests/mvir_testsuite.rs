5aaf5cceb66827d08711fbe1138e9e478e84ba0b
Victor Gao <vgao@calibra.com>
2021-07-01 04:41:59 -0700

    [language][functional tests] move `test-infra` tests into the `functional-tests` crate itself
    These are meta tests to check that the functional test framework itself
    is working as intended. It is confusing to have them in the same place
    as other normal tests. This gets them moved into the framework library.

---
+++ language/testing-infra/functional-tests/tests/mvir_testsuite.rs
@@ -0,0 +1,14 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Meta tests to check that the functional test framework itself is working as expected.
+
+// Note: the path of the entry point function is included as part of the test name.
+// Importing it and giving it an alias will make it more readable.
+use functional_tests::harness::run_mvir_functional_test as functional_tests_meta_tests_mvir;
+
+datatest_stable::harness!(
+    functional_tests_meta_tests_mvir,
+    "tests/mvir-testsuite",
+    r".*\.mvir"
+);
