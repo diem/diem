183b48197dac16533c861544bbaf53e2d7c8122d
Brandon Williams <bmwill@fb.com>
2021-07-06 11:20:30 -0700

    jsonrpc: convert block-metadata test to forge

--- json-rpc/tests/jsonrpc-integration-tests/main.rs
+++ json-rpc/tests/jsonrpc-integration-tests/main.rs
@@ -1,11 +1,13 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
+use diem_types::ledger_info::LedgerInfoWithSignatures;
 use forge::{
     forge_main, ForgeConfig, LocalFactory, Options, PublicUsageContext, PublicUsageTest, Result,
     Test,
 };
 use serde_json::json;
+use std::ops::Deref;
 
 #[allow(dead_code)]
 mod helper;
@@ -13,7 +15,7 @@ use helper::JsonRpcTestHelper;
 
 fn main() -> Result<()> {
     let tests = ForgeConfig {
-        public_usage_tests: &[&CurrencyInfo],
+        public_usage_tests: &[&CurrencyInfo, &BlockMetadata],
         admin_tests: &[],
         network_tests: &[],
     };
@@ -60,6 +62,79 @@ impl PublicUsageTest for CurrencyInfo {
                     "to_xdx_exchange_rate": 1.0
                 }
             ])
+        );
+
+        Ok(())
+    }
+}
+
+struct BlockMetadata;
+
+impl Test for BlockMetadata {
+    fn name(&self) -> &'static str {
+        "jsonrpc::block-metadata"
+    }
+}
+
+impl PublicUsageTest for BlockMetadata {
+    fn run<'t>(&self, ctx: &mut PublicUsageContext<'t>) -> Result<()> {
+        let env = JsonRpcTestHelper::new(ctx.url().to_owned());
+
+        // Fund and account to ensure there are some txns on the chain
+        let account = ctx.random_account();
+        ctx.create_parent_vasp_account(account.authentication_key())?;
+        ctx.fund(account.address(), 10)?;
+
+        // batch request
+        let resp = env.send_request(json!([
+            {"jsonrpc": "2.0", "method": "get_metadata", "params": [], "id": 1},
+            {"jsonrpc": "2.0", "method": "get_state_proof", "params": [0], "id": 2}
+        ]));
+
+        // extract both responses
+        let resps: Vec<serde_json::Value> =
+            serde_json::from_value(resp).expect("should be valid serde_json::Value");
+        let metadata = &resps.iter().find(|g| g["id"] == 1).unwrap()["result"];
+        let state_proof = &resps.iter().find(|g| g["id"] == 2).unwrap()["result"];
+
+        // extract header and ensure they match in both responses
+        let diem_chain_id = &resps[0]["diem_chain_id"];
+        let diem_ledger_timestampusec = &resps[0]["diem_ledger_timestampusec"];
+        let diem_ledger_version = &resps[0]["diem_ledger_version"];
+
+        assert_eq!(diem_chain_id, &resps[1]["diem_chain_id"]);
+        assert_eq!(
+            diem_ledger_timestampusec,
+            &resps[1]["diem_ledger_timestampusec"]
+        );
+        assert_eq!(diem_ledger_version, &resps[1]["diem_ledger_version"]);
+
+        // parse metadata
+        assert_eq!(&metadata["chain_id"], diem_chain_id);
+        assert_eq!(&metadata["timestamp"], diem_ledger_timestampusec);
+        assert_eq!(&metadata["version"], diem_ledger_version);
+        assert_eq!(metadata["chain_id"], ctx.chain_id().id());
+        // All genesis's start with closed publishing so this should be populated with a
+        // list of allowed scripts and publishing off
+        assert_ne!(metadata["script_hash_allow_list"], json!([]));
+        assert_eq!(metadata["module_publishing_allowed"], false);
+        assert_eq!(metadata["diem_version"], 3);
+        assert_eq!(metadata["dual_attestation_limit"], 1000000000);
+        assert_ne!(diem_ledger_timestampusec, 0);
+        assert_ne!(diem_ledger_version, 0);
+
+        // prove the accumulator_root_hash
+        let info_hex = state_proof["ledger_info_with_signatures"].as_str().unwrap();
+        let info: LedgerInfoWithSignatures =
+            bcs::from_bytes(&hex::decode(&info_hex).unwrap()).unwrap();
+        let expected_hash = info
+            .deref()
+            .ledger_info()
+            .transaction_accumulator_hash()
+            .to_hex();
+        assert_eq!(
+            expected_hash,
+            metadata["accumulator_root_hash"].as_str().unwrap()
         );
 
         Ok(())
