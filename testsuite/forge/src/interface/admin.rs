ceaa6824ec75dfddb51e2e6a31b311cd3a9ce2f9
Brandon Williams <bmwill@fb.com>
2021-07-06 10:34:13 -0700

    forge: add some helper methods to AdminContext

--- testsuite/forge/src/interface/admin.rs
+++ testsuite/forge/src/interface/admin.rs
@@ -3,7 +3,7 @@
 
 use super::{ChainInfo, CoreContext, Test};
 use crate::Result;
-use diem_sdk::client::BlockingClient;
+use diem_sdk::{client::BlockingClient, types::LocalAccount};
 
 /// The testing interface which defines a test written from the perspective of the Admin of the
 /// network. This means that the test will have access to the Root account but do not control any
@@ -29,7 +29,19 @@ impl<'t> AdminContext<'t> {
         &self.core
     }
 
+    pub fn rng(&mut self) -> &mut ::rand::rngs::StdRng {
+        self.core.rng()
+    }
+
     pub fn client(&self) -> BlockingClient {
         BlockingClient::new(&self.chain_info.json_rpc_url)
+    }
+
+    pub fn chain_info(&mut self) -> &mut ChainInfo<'t> {
+        &mut self.chain_info
+    }
+
+    pub fn random_account(&mut self) -> LocalAccount {
+        LocalAccount::generate(self.core.rng())
     }
 }
